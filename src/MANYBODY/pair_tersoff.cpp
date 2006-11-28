7894c03dec7f1ff32c94cbde04cf5d31df7e51f3
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2006-11-28 17:00:39 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@171 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/MANYBODY/pair_tersoff.cpp
+++ src/MANYBODY/pair_tersoff.cpp
@@ -451,8 +451,7 @@ void PairTersoff::read_file(char *file)
     params[nparams].biga = atof(words[14]);
 
     if (params[nparams].lam3 < 0.0 || params[nparams].c <= 0.0 || 
-	params[nparams].d <= 0.0 || params[nparams].h > 1.0 ||
-	params[nparams].h < -1.0 || params[nparams].powern <= 0.0 || 
+	params[nparams].d <= 0.0 || params[nparams].powern <= 0.0 || 
 	params[nparams].beta < 0.0 || params[nparams].lam2 < 0.0 || 
 	params[nparams].bigb < 0.0 || params[nparams].bigr < 0.0 ||
 	params[nparams].bigd < 0.0 ||

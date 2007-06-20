d71efd0c137f641f03f5cac5759b8074f37f1690
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2007-06-20 13:40:02 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@625 f3b2605a-c512-4ea7-a41b-209d697bcdaa

---
+++ src/MEAM/pair_colloid.h
@@ -0,0 +1,48 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   www.cs.sandia.gov/~sjplimp/lammps.html
+   Steve Plimpton, sjplimp@sandia.gov, Sandia National Laboratories
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under 
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifndef PAIR_COLLOID_H
+#define PAIR_COLLOID_H
+
+#include "pair.h"
+
+namespace LAMMPS_NS {
+
+class PairColloid : public Pair {
+ public:
+  PairColloid(class LAMMPS *);
+  ~PairColloid();
+  void compute(int, int);
+  void settings(int, char **);
+  void coeff(int, char **);
+  double init_one(int, int);
+  void write_restart(FILE *);
+  void read_restart(FILE *);
+  void write_restart_settings(FILE *);
+  void read_restart_settings(FILE *);
+  void single(int, int, int, int, double, double, double, int, One &);
+
+ private:
+  double cut_global;
+  double **cut;
+  double **a12,**d1,**d2,**a1,**a2,**offset;
+  double **sigma,**sigma3,**sigma6;
+  double **lj1,**lj2,**lj3,**lj4;
+  int **form;
+
+  void allocate();
+};
+
+}
+
+#endif

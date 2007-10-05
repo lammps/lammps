61c17b4b29c6f87a5082e4d670c2dc50156e2f94
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2007-10-05 19:00:38 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@977 f3b2605a-c512-4ea7-a41b-209d697bcdaa

---
+++ src/style_dpd.h
@@ -0,0 +1,28 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under 
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef AtomInclude
+#include "atom_vec_dpd.h"
+#endif
+
+#ifdef AtomClass
+AtomStyle(dpd,AtomVecDPD)
+#endif
+
+#ifdef PairInclude
+#include "pair_dpd.h"
+#endif
+
+#ifdef PairClass
+PairStyle(dpd,PairDPD)
+#endif

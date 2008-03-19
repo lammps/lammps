972f8a081a31b20345193f3c53df0fb7cb47aebb
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2008-03-19 15:39:24 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@1639 f3b2605a-c512-4ea7-a41b-209d697bcdaa

---
+++ src/compute_ke.h
@@ -0,0 +1,33 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under 
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifndef COMPUTE_KE_H
+#define COMPUTE_KE_H
+
+#include "compute.h"
+
+namespace LAMMPS_NS {
+
+class ComputeKE : public Compute {
+ public:
+  ComputeKE(class LAMMPS *, int, char **);
+  void init();
+  double compute_scalar();
+
+ private:
+  double pfactor;
+};
+
+}
+
+#endif
--- src/style.h
+++ src/style.h
@@ -80,6 +80,7 @@ CommandStyle(write_restart,WriteRestart)
 #include "compute_coord_atom.h"
 #include "compute_displace_atom.h"
 #include "compute_group_group.h"
+#include "compute_ke.h"
 #include "compute_ke_atom.h"
 #include "compute_pe.h"
 #include "compute_pe_atom.h"
@@ -101,6 +102,7 @@ ComputeStyle(centro/atom,ComputeCentroAtom)
 ComputeStyle(coord/atom,ComputeCoordAtom)
 ComputeStyle(displace/atom,ComputeDisplaceAtom)
 ComputeStyle(group/group,ComputeGroupGroup)
+ComputeStyle(ke,ComputeKE)
 ComputeStyle(ke/atom,ComputeKEAtom)
 ComputeStyle(pe,ComputePE)
 ComputeStyle(pe/atom,ComputePEAtom)

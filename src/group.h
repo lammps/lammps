75d43933d43188065e743bca49e54ea3c7317f11
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2008-04-04 23:17:17 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@1717 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/group.h
+++ src/group.h
@@ -31,7 +31,7 @@ class Group : protected Pointers {
   ~Group();
   void assign(int, char **);         // assign atoms to a group
   void create(char *, int *);        // add flagged atoms to a group
-  int find(const char *);                  // lookup name in list of groups
+  int find(const char *);            // lookup name in list of groups
   void write_restart(FILE *);
   void read_restart(FILE *);
 

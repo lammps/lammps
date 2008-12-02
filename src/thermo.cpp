97bcc0c23dc6bc5205aae0fa7e7c8204c210ead8
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2008-12-02 20:18:44 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@2268 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/thermo.cpp
+++ src/thermo.cpp
@@ -224,7 +224,8 @@ void Thermo::init()
   int ivariable;
   for (i = 0; i < nvariable; i++) {
     ivariable = input->variable->find(id_variable[i]);
-    if (ivariable < 0) error->all("Could not find thermo custom variable name");
+    if (ivariable < 0) 
+      error->all("Could not find thermo custom variable name");
     variables[i] = ivariable;
   }
 

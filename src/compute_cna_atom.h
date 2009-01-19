7e414a8dc8c0892ee49e2aa2e7a98e925fa8c8f0
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2009-01-19 17:25:38 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@2498 f3b2605a-c512-4ea7-a41b-209d697bcdaa

---
+++ src/compute_cna_atom.h
@@ -0,0 +1,41 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifndef COMPUTE_CNA_ATOM_H
+#define COMPUTE_CNA_ATOM_H
+
+#include "compute.h"
+
+namespace LAMMPS_NS {
+
+class ComputeCNAAtom : public Compute {
+ public:
+  ComputeCNAAtom(class LAMMPS *, int, char **);
+  ~ComputeCNAAtom();
+  void init();
+  void init_list(int, class NeighList *);
+  void compute_peratom();
+  double memory_usage();
+
+ private:
+  int nmax;
+  double cutsq;
+  class NeighList *list;
+  int **nearest;
+  int *nnearest;
+  double *pattern;
+};
+
+}
+
+#endif

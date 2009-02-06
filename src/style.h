08a9ab78fe0458ac24002ff204e113f09c09cc4d
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2009-02-06 17:05:46 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@2558 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/style.h
+++ src/style.h
@@ -364,6 +364,7 @@ RegionStyle(union,RegUnion)
 #include "style_opt.h"
 #include "style_peri.h"
 #include "style_poems.h"
+#include "style_reax.h"
 #include "style_xtc.h"
 
 // user add-ons: individual classes and packages

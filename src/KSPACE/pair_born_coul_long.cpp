d089e246b94ca8c3a16443519244a1696e97bc09
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2009-06-04 16:53:17 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@2853 f3b2605a-c512-4ea7-a41b-209d697bcdaa

---
+++ src/KSPACE/pair_born_coul_long.cpp
@@ -0,0 +1,472 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under 
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+/* ----------------------------------------------------------------------
+   Contributing author: Ahmed Ismail (SNL)
+------------------------------------------------------------------------- */
+
+#include "math.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "pair_born_coul_long.h"
+#include "atom.h"
+#include "comm.h"
+#include "force.h"
+#include "kspace.h"
+#include "neighbor.h"
+#include "neigh_list.h"
+#include "memory.h"
+#include "error.h"
+
+using namespace LAMMPS_NS;
+
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+
+#define EWALD_F   1.12837917
+#define EWALD_P   0.3275911
+#define A1        0.254829592
+#define A2       -0.284496736
+#define A3        1.421413741
+#define A4       -1.453152027
+#define A5        1.061405429
+
+/* ---------------------------------------------------------------------- */
+
+PairBornCoulLong::PairBornCoulLong(LAMMPS *lmp) : Pair(lmp) {}
+
+/* ---------------------------------------------------------------------- */
+
+PairBornCoulLong::~PairBornCoulLong()
+{
+  if (allocated) {
+    memory->destroy_2d_int_array(setflag);
+    memory->destroy_2d_double_array(cutsq);
+
+    memory->destroy_2d_double_array(cut_lj);
+    memory->destroy_2d_double_array(cut_ljsq);
+    memory->destroy_2d_double_array(a);
+    memory->destroy_2d_double_array(rho);
+    memory->destroy_2d_double_array(sigma);
+    memory->destroy_2d_double_array(c);
+    memory->destroy_2d_double_array(d);
+    memory->destroy_2d_double_array(rhoinv);
+    memory->destroy_2d_double_array(born1);
+    memory->destroy_2d_double_array(born2);
+    memory->destroy_2d_double_array(born3);
+    memory->destroy_2d_double_array(offset);
+  }
+}
+
+/* ---------------------------------------------------------------------- */
+
+void PairBornCoulLong::compute(int eflag, int vflag)
+{
+  int i,j,ii,jj,inum,jnum,itype,jtype;
+  double qtmp,xtmp,ytmp,ztmp,delx,dely,delz,evdwl,ecoul,fpair;
+  double rsq,r2inv,r6inv,forcecoul,forceborn,factor_coul,factor_lj;
+  double grij,expm2,prefactor,t,erfc;
+  double r,rexp;
+  int *ilist,*jlist,*numneigh,**firstneigh;
+
+  evdwl = ecoul = 0.0;
+  if (eflag || vflag) ev_setup(eflag,vflag);
+  else evflag = vflag_fdotr = 0;
+
+  double **x = atom->x;
+  double **f = atom->f;
+  double *q = atom->q;
+  int *type = atom->type;
+  int nlocal = atom->nlocal;
+  int nall = nlocal + atom->nghost;
+  double *special_coul = force->special_coul;
+  double *special_lj = force->special_lj;
+  int newton_pair = force->newton_pair;
+  double qqrd2e = force->qqrd2e;
+
+  inum = list->inum;
+  ilist = list->ilist;
+  numneigh = list->numneigh;
+  firstneigh = list->firstneigh;
+  
+  // loop over neighbors of my atoms
+
+  for (ii = 0; ii < inum; ii++) {
+    i = ilist[ii];
+    qtmp = q[i];
+    xtmp = x[i][0];
+    ytmp = x[i][1];
+    ztmp = x[i][2];
+    itype = type[i];
+    jlist = firstneigh[i];
+    jnum = numneigh[i];
+
+    for (jj = 0; jj < jnum; jj++) {
+      j = jlist[jj];
+
+      if (j < nall) factor_coul = factor_lj = 1.0;
+      else {
+	factor_coul = special_coul[j/nall];
+	factor_lj = special_lj[j/nall];
+	j %= nall;
+      }
+
+      delx = xtmp - x[j][0];
+      dely = ytmp - x[j][1];
+      delz = ztmp - x[j][2];
+      rsq = delx*delx + dely*dely + delz*delz;
+      jtype = type[j];
+
+      if (rsq < cutsq[itype][jtype]) {
+	r2inv = 1.0/rsq;
+
+	if (rsq < cut_coulsq) {
+	  r = sqrt(rsq);
+	  grij = g_ewald * r;
+	  expm2 = exp(-grij*grij);
+	  t = 1.0 / (1.0 + EWALD_P*grij);
+	  erfc = t * (A1+t*(A2+t*(A3+t*(A4+t*A5)))) * expm2;
+	  prefactor = qqrd2e * qtmp*q[j]/r;
+	  forcecoul = prefactor * (erfc + EWALD_F*grij*expm2);
+	  if (factor_coul < 1.0) forcecoul -= (1.0-factor_coul)*prefactor;
+	} else forcecoul = 0.0;
+
+	if (rsq < cut_ljsq[itype][jtype]) {
+	  r6inv = r2inv*r2inv*r2inv;
+          r = sqrt(rsq);
+	  rexp = exp((sigma[itype][jtype]-r)*rhoinv[itype][jtype]);
+	  forceborn = born1[itype][jtype]*r*rexp - born2[itype][jtype]*r6inv
+	    + born3[itype][jtype]*r2inv*r6inv;
+	} else forceborn = 0.0;
+
+	fpair = (forcecoul + factor_lj*forceborn) * r2inv;
+
+	f[i][0] += delx*fpair;
+	f[i][1] += dely*fpair;
+	f[i][2] += delz*fpair;
+	if (newton_pair || j < nlocal) {
+	  f[j][0] -= delx*fpair;
+	  f[j][1] -= dely*fpair;
+	  f[j][2] -= delz*fpair;
+	}
+
+	if (eflag) {
+	  if (rsq < cut_coulsq) {
+	    ecoul = prefactor*erfc;
+	    if (factor_coul < 1.0) ecoul -= (1.0-factor_coul)*prefactor;
+	  } else ecoul = 0.0;
+	  if (rsq < cut_ljsq[itype][jtype]) {
+	    evdwl = a[itype][jtype]*rexp - c[itype][jtype]*r6inv 
+	      + d[itype][jtype]*r6inv*r2inv - offset[itype][jtype];
+	    evdwl *= factor_lj;
+	  } else evdwl = 0.0;
+	}
+
+	if (evflag) ev_tally(i,j,nlocal,newton_pair,
+			     evdwl,ecoul,fpair,delx,dely,delz);
+      }
+    }
+  }
+
+  if (vflag_fdotr) virial_compute();
+}
+
+/* ----------------------------------------------------------------------
+   allocate all arrays
+------------------------------------------------------------------------- */
+
+void PairBornCoulLong::allocate()
+{
+  allocated = 1;
+  int n = atom->ntypes;
+
+  setflag = memory->create_2d_int_array(n+1,n+1,"pair:setflag");
+  for (int i = 1; i <= n; i++)
+    for (int j = i; j <= n; j++)
+      setflag[i][j] = 0;
+
+  cutsq = memory->create_2d_double_array(n+1,n+1,"pair:cutsq");
+
+  cut_lj = memory->create_2d_double_array(n+1,n+1,"pair:cut_lj");
+  cut_ljsq = memory->create_2d_double_array(n+1,n+1,"pair:cut_ljsq");
+  a = memory->create_2d_double_array(n+1,n+1,"pair:a");
+  rho = memory->create_2d_double_array(n+1,n+1,"pair:rho");
+  sigma = memory->create_2d_double_array(n+1,n+1,"pair:sigma");
+  c = memory->create_2d_double_array(n+1,n+1,"pair:c");
+  d = memory->create_2d_double_array(n+1,n+1,"pair:d");
+  rhoinv = memory->create_2d_double_array(n+1,n+1,"pair:rhoinv");
+  born1 = memory->create_2d_double_array(n+1,n+1,"pair:born1");
+  born2 = memory->create_2d_double_array(n+1,n+1,"pair:born2");
+  born3 = memory->create_2d_double_array(n+1,n+1,"pair:born3");
+  offset = memory->create_2d_double_array(n+1,n+1,"pair:offset");
+}
+
+/* ----------------------------------------------------------------------
+   global settings
+------------------------------------------------------------------------- */
+
+void PairBornCoulLong::settings(int narg, char **arg)
+{
+  if (narg < 1 || narg > 2) error->all("Illegal pair_style command");
+
+  cut_lj_global = atof(arg[0]);
+  if (narg == 1) cut_coul = cut_lj_global;
+  else cut_coul = atof(arg[1]);
+
+  // reset cutoffs that have been explicitly set
+
+  if (allocated) {
+    int i,j;
+    for (i = 1; i <= atom->ntypes; i++)
+      for (j = i+1; j <= atom->ntypes; j++)
+	if (setflag[i][j]) cut_lj[i][j] = cut_lj_global;
+  }
+}
+
+/* ----------------------------------------------------------------------
+   set coeffs for one or more type pairs
+------------------------------------------------------------------------- */
+
+void PairBornCoulLong::coeff(int narg, char **arg)
+{
+  if (narg < 7 || narg > 8) error->all("Incorrect args for pair coefficients");
+  if (!allocated) allocate();
+
+  int ilo,ihi,jlo,jhi;
+  force->bounds(arg[0],atom->ntypes,ilo,ihi);
+  force->bounds(arg[1],atom->ntypes,jlo,jhi);
+
+  double a_one = atof(arg[2]);
+  double rho_one = atof(arg[3]);
+  double sigma_one = atof(arg[4]);
+  if (rho_one <= 0) error->all("Incorrect args for pair coefficients");
+  double c_one = atof(arg[5]);
+  double d_one = atof(arg[6]);
+
+  double cut_lj_one = cut_lj_global;
+  if (narg == 8) cut_lj_one = atof(arg[7]);
+
+  int count = 0;
+  for (int i = ilo; i <= ihi; i++) {
+    for (int j = MAX(jlo,i); j <= jhi; j++) {
+      a[i][j] = a_one;
+      rho[i][j] = rho_one;
+      sigma[i][j] = sigma_one;
+      c[i][j] = c_one;
+      d[i][j] = d_one;
+      cut_lj[i][j] = cut_lj_one;
+      setflag[i][j] = 1;
+      count++;
+    }
+  }
+
+  if (count == 0) error->all("Incorrect args for pair coefficients");
+}
+
+/* ----------------------------------------------------------------------
+   init for one type pair i,j and corresponding j,i
+------------------------------------------------------------------------- */
+
+double PairBornCoulLong::init_one(int i, int j)
+{
+  if (setflag[i][j] == 0) error->all("All pair coeffs are not set");
+
+  double cut = MAX(cut_lj[i][j],cut_coul);
+  cut_ljsq[i][j] = cut_lj[i][j] * cut_lj[i][j];
+
+  rhoinv[i][j] = 1.0/rho[i][j];
+  born1[i][j] = a[i][j]/rho[i][j];
+  born2[i][j] = 6.0*c[i][j];
+  born3[i][j] = 8.0*d[i][j];
+     
+  if (offset_flag) {
+    double rexp = exp(-cut_lj[i][j]/rho[i][j]);
+    offset[i][j] = a[i][j]*rexp - c[i][j]/pow(cut_lj[i][j],6.0)
+      + d[i][j]/pow(cut_lj[i][j],8.0);
+  } else offset[i][j] = 0.0;
+
+  cut_ljsq[j][i] = cut_ljsq[i][j];
+  a[j][i] = a[i][j];
+  c[j][i] = c[i][j];
+  d[j][i] = d[i][j];
+  rhoinv[j][i] = rhoinv[i][j];
+  sigma[j][i] = sigma[i][j];
+  born1[j][i] = born1[i][j];
+  born2[j][i] = born2[i][j];
+  born3[j][i] = born3[i][j];
+  offset[j][i] = offset[i][j];
+
+  return cut;
+}
+
+/* ----------------------------------------------------------------------
+   init specific to this pair style
+------------------------------------------------------------------------- */
+
+void PairBornCoulLong::init_style()
+{
+  if (!atom->q_flag)
+    error->all("Pair style born/coul/long requires atom attribute q");
+
+  cut_coulsq = cut_coul * cut_coul;
+
+  // insure use of KSpace long-range solver, set g_ewald
+
+  if (force->kspace == NULL)
+    error->all("Pair style is incompatible with KSpace style");
+  g_ewald = force->kspace->g_ewald;
+
+  int irequest = neighbor->request(this);
+}
+
+/* ----------------------------------------------------------------------
+  proc 0 writes to restart file
+------------------------------------------------------------------------- */
+
+void PairBornCoulLong::write_restart(FILE *fp)
+{
+  write_restart_settings(fp);
+
+  int i,j;
+  for (i = 1; i <= atom->ntypes; i++)
+    for (j = i; j <= atom->ntypes; j++) {
+      fwrite(&setflag[i][j],sizeof(int),1,fp);
+      if (setflag[i][j]) {
+	fwrite(&a[i][j],sizeof(double),1,fp);
+	fwrite(&rho[i][j],sizeof(double),1,fp);
+	fwrite(&sigma[i][j],sizeof(double),1,fp);
+	fwrite(&c[i][j],sizeof(double),1,fp);
+	fwrite(&d[i][j],sizeof(double),1,fp);
+	fwrite(&cut_lj[i][j],sizeof(double),1,fp);
+      }
+    }
+}
+
+/* ----------------------------------------------------------------------
+  proc 0 reads from restart file, bcasts
+------------------------------------------------------------------------- */
+
+void PairBornCoulLong::read_restart(FILE *fp)
+{
+  read_restart_settings(fp);
+
+  allocate();
+
+  int i,j;
+  int me = comm->me;
+  for (i = 1; i <= atom->ntypes; i++)
+    for (j = i; j <= atom->ntypes; j++) {
+      if (me == 0) fread(&setflag[i][j],sizeof(int),1,fp);
+      MPI_Bcast(&setflag[i][j],1,MPI_INT,0,world);
+      if (setflag[i][j]) {
+	if (me == 0) {
+	  fread(&a[i][j],sizeof(double),1,fp);
+	  fread(&rho[i][j],sizeof(double),1,fp);
+	  fread(&sigma[i][j],sizeof(double),1,fp);
+	  fread(&c[i][j],sizeof(double),1,fp);
+	  fread(&d[i][j],sizeof(double),1,fp);
+	  fread(&cut_lj[i][j],sizeof(double),1,fp);
+	}
+	MPI_Bcast(&a[i][j],1,MPI_DOUBLE,0,world);
+	MPI_Bcast(&rho[i][j],1,MPI_DOUBLE,0,world);
+	MPI_Bcast(&sigma[i][j],1,MPI_DOUBLE,0,world);
+	MPI_Bcast(&c[i][j],1,MPI_DOUBLE,0,world);
+	MPI_Bcast(&d[i][j],1,MPI_DOUBLE,0,world);
+	MPI_Bcast(&cut_lj[i][j],1,MPI_DOUBLE,0,world);
+      }
+    }
+}
+
+/* ----------------------------------------------------------------------
+  proc 0 writes to restart file
+------------------------------------------------------------------------- */
+
+void PairBornCoulLong::write_restart_settings(FILE *fp)
+{
+  fwrite(&cut_lj_global,sizeof(double),1,fp);
+  fwrite(&cut_coul,sizeof(double),1,fp);
+  fwrite(&offset_flag,sizeof(int),1,fp);
+  fwrite(&mix_flag,sizeof(int),1,fp);
+}
+
+/* ----------------------------------------------------------------------
+  proc 0 reads from restart file, bcasts
+------------------------------------------------------------------------- */
+
+void PairBornCoulLong::read_restart_settings(FILE *fp)
+{
+  if (comm->me == 0) {
+    fread(&cut_lj_global,sizeof(double),1,fp);
+    fread(&cut_coul,sizeof(double),1,fp);
+    fread(&offset_flag,sizeof(int),1,fp);
+    fread(&mix_flag,sizeof(int),1,fp);
+  }
+  MPI_Bcast(&cut_lj_global,1,MPI_DOUBLE,0,world);
+  MPI_Bcast(&cut_coul,1,MPI_DOUBLE,0,world);
+  MPI_Bcast(&offset_flag,1,MPI_INT,0,world);
+  MPI_Bcast(&mix_flag,1,MPI_INT,0,world);
+}
+
+/* ---------------------------------------------------------------------- */
+
+double PairBornCoulLong::single(int i, int j, int itype, int jtype,
+				double rsq, 
+				double factor_coul, double factor_lj,
+				double &fforce)
+{
+  double r2inv,r6inv,r,rexp,grij,expm2,t,erfc,prefactor;
+  double forcecoul,forceborn,phicoul,phiborn;
+
+  r2inv = 1.0/rsq;
+  if (rsq < cut_coulsq) {
+    r = sqrt(rsq);
+    grij = g_ewald * r;
+    expm2 = exp(-grij*grij);
+    t = 1.0 / (1.0 + EWALD_P*grij);
+    erfc = t * (A1+t*(A2+t*(A3+t*(A4+t*A5)))) * expm2;
+    prefactor = force->qqrd2e * atom->q[i]*atom->q[j]/r;
+    forcecoul = prefactor * (erfc + EWALD_F*grij*expm2);
+    if (factor_coul < 1.0) forcecoul -= (1.0-factor_coul)*prefactor;
+  } else forcecoul = 0.0;
+  if (rsq < cut_ljsq[itype][jtype]) {
+    r6inv = r2inv*r2inv*r2inv;
+    r = sqrt(rsq);
+    rexp = exp(-r*rhoinv[itype][jtype]);
+    forceborn = born1[itype][jtype]*r*rexp - born2[itype][jtype]*r6inv
+      + born3[itype][jtype]*r2inv*r6inv;
+  } else forceborn = 0.0;
+  fforce = (forcecoul + factor_lj*forceborn) * r2inv;
+  
+  double eng = 0.0;
+  if (rsq < cut_coulsq) {
+    phicoul = prefactor*erfc;
+    if (factor_coul < 1.0) phicoul -= (1.0-factor_coul)*prefactor;
+    eng += phicoul;
+  }
+  if (rsq < cut_ljsq[itype][jtype]) {
+    phiborn = a[itype][jtype]*rexp - c[itype][jtype]*r6inv 
+      + d[itype][jtype]*r2inv*r6inv - offset[itype][jtype];
+    eng += factor_lj*phiborn;
+  }
+  return eng;
+}
+
+/* ---------------------------------------------------------------------- */
+
+void *PairBornCoulLong::extract(char *str)
+{
+  if (strcmp(str,"cut_coul") == 0) return (void *) &cut_coul;
+  return NULL;
+}
+
---
+++ src/KSPACE/pair_born_coul_long.h
@@ -0,0 +1,50 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under 
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifndef PAIR_BORN_COUL_LONG_H
+#define PAIR_BORN_COUL_LONG_H
+
+#include "pair.h"
+
+namespace LAMMPS_NS {
+
+class PairBornCoulLong : public Pair {
+ public:
+  PairBornCoulLong(class LAMMPS *);
+  ~PairBornCoulLong();
+  void compute(int, int);
+  void settings(int, char **);
+  void coeff(int, char **);
+  void init_style();
+  double init_one(int, int);
+  void write_restart(FILE *);
+  void read_restart(FILE *);
+  void write_restart_settings(FILE *);
+  void read_restart_settings(FILE *);
+  double single(int, int, int, int, double, double, double, double &);
+  void *extract(char *);
+
+ private:
+  double cut_lj_global;
+  double **cut_lj,**cut_ljsq;
+  double cut_coul,cut_coulsq;
+  double **a,**rho,**sigma,**c,**d;
+  double **rhoinv,**born1,**born2,**born3,**offset;
+  double g_ewald;
+
+  void allocate();
+};
+
+}
+
+#endif

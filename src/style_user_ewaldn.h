193b29c22659328fa27ec0faa54b91c24045227c
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2009-07-02 22:32:55 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@2977 f3b2605a-c512-4ea7-a41b-209d697bcdaa

---
+++ src/style_user_ewaldn.h
@@ -0,0 +1,30 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under 
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef KSpaceInclude
+#include "ewald_n.h"
+#endif
+
+#ifdef KSpaceClass
+KSpaceStyle(ewald/n,EwaldN)
+#endif
+
+#ifdef PairInclude
+#include "pair_buck_coul.h"
+#include "pair_lj_coul.h"
+#endif
+
+#ifdef PairClass
+PairStyle(buck/coul,PairBuckCoul)
+PairStyle(lj/coul,PairLJCoul)
+#endif

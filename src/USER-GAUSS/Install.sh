5496003661343930a0e7d025085a5f28edcfa916
Axel Kohlmeyer <akohlmey@gmail.com>
2010-05-13 16:07:51 -0400

    added coulomb correction term due to short-range distance dependent dielectric permittivity.

--- src/USER-GAUSS/Install.sh
+++ src/USER-GAUSS/Install.sh
@@ -3,8 +3,10 @@
 if (test $1 = 1) then
 
   cp -p pair_gauss_cut.cpp ..
+  cp -p pair_coul_diel.cpp ..
 
   cp -p pair_gauss_cut.h ..
+  cp -p pair_coul_diel.h ..
 
 #  if (test -e ../pair_lj_charmm_coul_long.cpp) then
 #      cp -p pair_lj_charmm_coul_long_omp.cpp ..
@@ -14,8 +16,10 @@ if (test $1 = 1) then
 elif (test $1 = 0) then
 
   rm ../pair_gauss_cut.cpp
+  rm ../pair_coul_diel.cpp
 
   rm ../pair_gauss_cut.h
+  rm ../pair_coul_diel.h
 
 #  rm -f ../pair_lj_charmm_coul_long_omp.cpp
 #  rm -f ../pair_lj_charmm_coul_long_omp.h

82b8f7aa1653bae9bd2cd909d37cbccb13805590
Peter Shannon <pietro.shannon@gmail.com>
2010-05-27 16:43:18 -0400

    Addition of pair_yukawa_omp(.cpp/.h)

---
+++ src/USER-OPENMP/pair_yukawa_omp.cpp
@@ -0,0 +1,363 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#include "math.h"
+#include "stdlib.h"
+#include "pair_yukawa_omp.h"
+#include "atom.h"
+#include "force.h"
+#include "comm.h"
+#include "neigh_list.h"
+#include "memory.h"
+#include "error.h"
+
+using namespace LAMMPS_NS;
+
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+
+/* ---------------------------------------------------------------------- */
+
+PairYukawaOMP::PairYukawaOMP(LAMMPS *lmp) : PairOMP(lmp) {}
+
+/* ---------------------------------------------------------------------- */
+
+PairYukawaOMP::~PairYukawaOMP()
+{
+  if (allocated) {
+    memory->destroy_2d_int_array(setflag);
+    memory->destroy_2d_double_array(cutsq);
+
+    memory->destroy_2d_double_array(cut);
+    memory->destroy_2d_double_array(a);
+    memory->destroy_2d_double_array(offset);
+  }
+}
+
+/* ---------------------------------------------------------------------- */
+
+void PairYukawaOMP::compute(int eflag, int vflag)
+{
+  if (eflag || vflag) {
+    ev_setup(eflag,vflag);
+    ev_setup_thr(eflag,vflag);
+  } else evflag = vflag_fdotr = 0;
+
+  if (evflag) {
+    if (eflag) {
+      if (force->newton_pair) return eval<1,1,1>();
+      else return eval<1,1,0>();
+    } else {
+      if (force->newton_pair) return eval<1,0,1>();
+      else return eval<1,0,0>();
+    }
+  } else {
+    if (force->newton_pair) return eval<0,0,1>();
+    else return eval<0,0,0>();
+  }
+}
+
+template <int EVFLAG, int EFLAG, int NEWTON_PAIR>
+void PairYukawaOMP::eval()
+{
+
+#if defined(_OPENMP)
+#pragma omp parallel default(shared)
+#endif
+  {
+    int i,j,ii,jj,inum,jnum,itype,jtype,tid;
+    double xtmp,ytmp,ztmp,delx,dely,delz,ecoul,fpair;
+    double rsq,r2inv,r,rinv,screening,forceyukawa,factor_coul;
+    int *ilist,*jlist,*numneigh,**firstneigh;
+
+    ecoul = 0.0;
+
+    const int nlocal = atom->nlocal;
+    const int nall = nlocal + atom->nghost;
+    const int nthreads = comm->nthreads;
+
+    double **x = atom->x;
+    double **f = atom->f;
+    int *type = atom->type;
+    double *special_coul = force->special_lj;
+
+    inum = list->inum;
+    ilist = list->ilist;
+    numneigh = list->numneigh;
+    firstneigh = list->firstneigh;
+
+    // loop over neighbors of my atoms
+
+    int iifrom, iito;
+    f = loop_setup_thr(f, iifrom, iito, tid, inum, nall, nthreads);
+    for (ii = iifrom; ii < iito; ++ii) {
+      i = ilist[ii];
+      xtmp = x[i][0];
+      ytmp = x[i][1];
+      ztmp = x[i][2];
+      itype = type[i];
+      jlist = firstneigh[i];
+      jnum = numneigh[i];
+
+      for (jj = 0; jj < jnum; jj++) {
+	j = jlist[jj];
+
+	if (j < nall) factor_coul = 1.0;
+	else {
+	  factor_coul = special_coul[j/nall];
+	  j %= nall;
+	}
+
+	delx = xtmp - x[j][0];
+	dely = ytmp - x[j][1];
+	delz = ztmp - x[j][2];
+	rsq = delx*delx + dely*dely + delz*delz;
+	jtype = type[j];
+
+	if (rsq < cutsq[itype][jtype]) {
+          r2inv = 1.0/rsq;
+          r = sqrt(rsq);
+          rinv = 1.0/r;
+	  screening = exp(-kappa*r);
+	  forceyukawa = a[itype][jtype] * screening * (kappa + rinv);
+
+	  f[i][0] += delx*fpair;
+	  f[i][1] += dely*fpair;
+	  f[i][2] += delz*fpair;
+	  if (NEWTON_PAIR || j < nlocal) {
+	    f[j][0] -= delx*fpair;
+	    f[j][1] -= dely*fpair;
+	    f[j][2] -= delz*fpair;
+	  }
+
+	  if (EFLAG) {
+	    ecoul = a[itype][jtype] * screening * rinv - offset[itype][jtype];
+	    ecoul *= factor_coul;
+	  }
+
+	  if (EVFLAG) ev_tally(i,j,nlocal,NEWTON_PAIR,
+			     0.0,ecoul,fpair,delx,dely,delz);
+	}
+      }
+    }
+
+    // reduce per thread forces into global force array.
+    force_reduce_thr(atom->f, nall, nthreads, tid);
+  }
+  ev_reduce_thr();
+  if (vflag_fdotr) virial_compute();
+}
+
+/* ----------------------------------------------------------------------
+   allocate all arrays
+------------------------------------------------------------------------- */
+
+void PairYukawaOMP::allocate()
+{
+  allocated = 1;
+  int n = atom->ntypes;
+
+  setflag = memory->create_2d_int_array(n+1,n+1,"pair:setflag");
+  for (int i = 1; i <= n; i++)
+    for (int j = i; j <= n; j++)
+      setflag[i][j] = 0;
+
+  cutsq = memory->create_2d_double_array(n+1,n+1,"pair:cutsq");
+
+  cut = memory->create_2d_double_array(n+1,n+1,"pair:cut");
+  a = memory->create_2d_double_array(n+1,n+1,"pair:a");
+  offset = memory->create_2d_double_array(n+1,n+1,"pair:offset");
+}
+
+/* ----------------------------------------------------------------------
+   global settings
+------------------------------------------------------------------------- */
+
+void PairYukawaOMP::settings(int narg, char **arg)
+{
+  if (narg != 2) error->all("Illegal pair_style command");
+
+  kappa = force->numeric(arg[0]);
+  cut_global = force->numeric(arg[1]);
+
+  // reset cutoffs that have been explicitly set
+
+  if (allocated) {
+    int i,j;
+    for (i = 1; i <= atom->ntypes; i++)
+      for (j = i+1; j <= atom->ntypes; j++)
+	if (setflag[i][j]) cut[i][j] = cut_global;
+  }
+}
+
+/* ----------------------------------------------------------------------
+   set coeffs for one or more type pairs
+------------------------------------------------------------------------- */
+
+void PairYukawaOMP::coeff(int narg, char **arg)
+{
+  if (narg < 3 || narg > 4) error->all("Incorrect args for pair coefficients");
+  if (!allocated) allocate();
+
+  int ilo,ihi,jlo,jhi;
+  force->bounds(arg[0],atom->ntypes,ilo,ihi);
+  force->bounds(arg[1],atom->ntypes,jlo,jhi);
+
+  double a_one = force->numeric(arg[2]);
+
+  double cut_one = cut_global;
+  if (narg == 4) cut_one = force->numeric(arg[3]);
+
+  int count = 0;
+  for (int i = ilo; i <= ihi; i++) {
+    for (int j = MAX(jlo,i); j <= jhi; j++) {
+      a[i][j] = a_one;
+      cut[i][j] = cut_one;
+      setflag[i][j] = 1;
+      count++;
+    }
+  }
+
+  if (count == 0) error->all("Incorrect args for pair coefficients");
+}
+
+/* ----------------------------------------------------------------------
+   init for one type pair i,j and corresponding j,i
+------------------------------------------------------------------------- */
+
+double PairYukawaOMP::init_one(int i, int j)
+{
+  if (setflag[i][j] == 0) {
+    a[i][j] = mix_energy(a[i][i],a[j][j],1.0,1.0);
+    cut[i][j] = mix_distance(cut[i][i],cut[j][j]);
+  }
+
+  if (offset_flag) {
+    double screening = exp(-kappa * cut[i][j]);
+    offset[i][j] = a[i][j] * screening / cut[i][j];
+  } else offset[i][j] = 0.0;
+
+  a[j][i] = a[i][j];
+  offset[j][i] = offset[i][j];
+
+  return cut[i][j];
+}
+
+/* ----------------------------------------------------------------------
+   proc 0 writes to restart file
+------------------------------------------------------------------------- */
+
+void PairYukawaOMP::write_restart(FILE *fp)
+{
+  write_restart_settings(fp);
+
+  int i,j;
+  for (i = 1; i <= atom->ntypes; i++)
+    for (j = i; j <= atom->ntypes; j++) {
+      fwrite(&setflag[i][j],sizeof(int),1,fp);
+      if (setflag[i][j]) {
+	fwrite(&a[i][j],sizeof(double),1,fp);
+	fwrite(&cut[i][j],sizeof(double),1,fp);
+      }
+    }
+}
+
+/* ----------------------------------------------------------------------
+   proc 0 reads from restart file, bcasts
+------------------------------------------------------------------------- */
+
+void PairYukawaOMP::read_restart(FILE *fp)
+{
+  read_restart_settings(fp);
+
+  allocate();
+
+  int i,j;
+  int me = comm->me;
+  for (i = 1; i <= atom->ntypes; i++)
+    for (j = i; j <= atom->ntypes; j++) {
+      if (me == 0) fread(&setflag[i][j],sizeof(int),1,fp);
+      MPI_Bcast(&setflag[i][j],1,MPI_INT,0,world);
+      if (setflag[i][j]) {
+	if (me == 0) {
+	  fread(&a[i][j],sizeof(double),1,fp);
+	  fread(&cut[i][j],sizeof(double),1,fp);
+	}
+	MPI_Bcast(&a[i][j],1,MPI_DOUBLE,0,world);
+	MPI_Bcast(&cut[i][j],1,MPI_DOUBLE,0,world);
+      }
+    }
+}
+
+/* ----------------------------------------------------------------------
+   proc 0 writes to restart file
+------------------------------------------------------------------------- */
+
+void PairYukawaOMP::write_restart_settings(FILE *fp)
+{
+  fwrite(&kappa,sizeof(double),1,fp);
+  fwrite(&cut_global,sizeof(double),1,fp);
+  fwrite(&offset_flag,sizeof(int),1,fp);
+  fwrite(&mix_flag,sizeof(int),1,fp);
+}
+
+/* ----------------------------------------------------------------------
+   proc 0 reads from restart file, bcasts
+------------------------------------------------------------------------- */
+
+void PairYukawaOMP::read_restart_settings(FILE *fp)
+{
+  if (comm->me == 0) {
+    fread(&kappa,sizeof(double),1,fp);
+    fread(&cut_global,sizeof(double),1,fp);
+    fread(&offset_flag,sizeof(int),1,fp);
+    fread(&mix_flag,sizeof(int),1,fp);
+  }
+  MPI_Bcast(&kappa,1,MPI_DOUBLE,0,world);
+  MPI_Bcast(&cut_global,1,MPI_DOUBLE,0,world);
+  MPI_Bcast(&offset_flag,1,MPI_INT,0,world);
+  MPI_Bcast(&mix_flag,1,MPI_INT,0,world);
+}
+
+/* ---------------------------------------------------------------------- */
+
+double PairYukawaOMP::single(int i, int j, int itype, int jtype, double rsq,
+			  double factor_coul, double factor_lj,
+			  double &fforce)
+{
+  double r2inv,r,rinv,screening,forceyukawa,phi;
+
+  r2inv = 1.0/rsq;
+  r = sqrt(rsq);
+  rinv = 1.0/r;
+  screening = exp(-kappa*r);
+  forceyukawa = a[itype][jtype] * screening * (kappa + rinv);
+  fforce = factor_coul*forceyukawa * r2inv;
+
+  phi = a[itype][jtype] * screening * rinv - offset[itype][jtype];
+  return factor_coul*phi;
+}
+
+/* ---------------------------------------------------------------------- */
+
+double PairYukawaOMP::memory_usage()
+{
+  const int n=atom->ntypes;
+
+  double bytes = PairOMP::memory_usage();
+
+  bytes += 9*((n+1)*(n+1) * sizeof(double) + (n+1)*sizeof(double *));
+  bytes += 1*((n+1)*(n+1) * sizeof(int) + (n+1)*sizeof(int *));
+
+  return bytes;
+}
---
+++ src/USER-OPENMP/pair_yukawa_omp.h
@@ -0,0 +1,56 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef PAIR_CLASS
+
+PairStyle(yukawa/omp,PairYukawaOMP)
+
+#else
+
+#ifndef LMP_PAIR_YUKAWA_OMP_H
+#define LMP_PAIR_YUKAWA_OMP_H
+
+#include "pair_omp.h"
+
+namespace LAMMPS_NS {
+
+class PairYukawaOMP : public PairOMP {
+ public:
+  PairYukawaOMP(class LAMMPS *);
+  virtual ~PairYukawaOMP();
+  virtual void compute(int, int);
+  virtual void settings(int, char **);
+  virtual void coeff(int, char **);
+  virtual double init_one(int, int);
+  virtual void write_restart(FILE *);
+  virtual void read_restart(FILE *);
+  virtual void write_restart_settings(FILE *);
+  virtual void read_restart_settings(FILE *);
+  virtual double single(int, int, int, int, double, double, double, double &);
+  virtual double memory_usage();
+
+ protected:
+  template <int EVFLAG, int EFLAG, int NEWTON_PAIR> void eval();
+
+ protected:
+  double cut_global;
+  double kappa;
+  double **cut,**a,**offset;
+
+  void allocate();
+};
+
+}
+
+#endif
+#endif

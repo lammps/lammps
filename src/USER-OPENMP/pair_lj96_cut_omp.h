a04413faeda99a14f0d168067fb673855ff4ec48
Peter Shannon <pietro.shannon@gmail.com>
2010-05-27 16:30:58 -0400

    Addition of pair_lj96_cut_omp(.cpp/.h)

---
+++ src/USER-OPENMP/pair_lj96_cut_omp.h
@@ -0,0 +1,75 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef PAIR_CLASS
+
+PairStyle(lj96/cut/omp,PairLJ96CutOMP)
+
+#else
+
+#ifndef LMP_PAIR_LJ96_CUT_OMP_H
+#define LMP_PAIR_LJ96_CUT_OMP_H
+
+#include "pair_omp.h"
+
+namespace LAMMPS_NS {
+
+class PairLJ96CutOMP : public PairOMP {
+ public:
+
+  PairLJ96CutOMP(class LAMMPS *);
+  ~PairLJ96CutOMP();
+
+  virtual void compute(int, int);
+  virtual void compute_inner();
+  virtual void compute_middle();
+  virtual void compute_outer(int, int);
+
+  virtual double single(int, int, int, int,
+			double, double, double, double &);
+
+  virtual void settings(int, char **);
+  virtual void coeff(int, char **);
+
+  virtual void init_style();
+  virtual void init_list(int, class NeighList *);
+  virtual double init_one(int, int);
+
+  virtual void write_restart(FILE *);
+  virtual void read_restart(FILE *);
+  virtual void write_restart_settings(FILE *);
+  virtual void read_restart_settings(FILE *);
+
+  virtual double memory_usage();
+
+ protected:
+  template <int EVFLAG, int EFLAG, int NEWTON_PAIR> void eval();
+  template <int NEWTON_PAIR> void eval_inner();
+  template <int NEWTON_PAIR> void eval_middle();
+  template <int EVFLAG, int EFLAG, int VFLAG, int NEWTON_PAIR>
+  void eval_outer();
+
+ protected:
+  double cut_global;
+  double **cut;
+  double **epsilon,**sigma;
+  double **lj1,**lj2,**lj3,**lj4,**offset;
+  double *cut_respa;
+
+  void allocate();
+};
+
+}
+
+#endif
+#endif

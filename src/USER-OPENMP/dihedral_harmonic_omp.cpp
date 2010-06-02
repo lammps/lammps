7a98d901d06488515f421fe1660f4b59026c7330
Ryan Houlihan <ryan.houlihan90@gmail.com>
2010-06-02 17:39:07 -0400

    Dihedral Harmonic with OMP

---
+++ src/USER-OPENMP/dihedral_harmonic_omp.cpp
@@ -0,0 +1,403 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+/* ----------------------------------------------------------------------
+   Contributing author: Paul Crozier (SNL)
+------------------------------------------------------------------------- */
+
+#include "mpi.h"
+#include "math.h"
+#include "stdlib.h"
+#include "dihedral_harmonic_omp.h"
+#include "atom.h"
+#include "comm.h"
+#include "neighbor.h"
+#include "domain.h"
+#include "force.h"
+#include "update.h"
+#include "memory.h"
+#include "error.h"
+
+#if defined(_OPENMP)
+#include <omp.h>
+#endif
+
+using namespace LAMMPS_NS;
+
+#define TOLERANCE 0.05
+#define SMALL     0.001
+
+/* ---------------------------------------------------------------------- */
+
+DihedralHarmonicOMP::DihedralHarmonicOMP(LAMMPS *lmp) : DihedralOMP(lmp) {}
+
+/* ---------------------------------------------------------------------- */
+
+DihedralHarmonicOMP::~DihedralHarmonicOMP()
+{
+  if (allocated) {
+    memory->sfree(setflag);
+    memory->sfree(k);
+    memory->sfree(sign);
+    memory->sfree(multiplicity);
+    memory->sfree(cos_shift);
+    memory->sfree(sin_shift);
+  }
+}
+
+/* ---------------------------------------------------------------------- */
+
+void DihedralHarmonicOMP::compute(int eflag, int vflag)
+ {
+  if (eflag || vflag) {
+    ev_setup(eflag,vflag);
+    ev_setup_thr(eflag,vflag);
+  } else evflag = 0;
+
+  if (evflag) {
+    if (eflag) {
+      if (force->newton_bond) return eval<1,1,1>();
+      else return eval<1,1,0>();
+    } else {
+      if (force->newton_bond) return eval<1,0,1>();
+      else return eval<1,0,0>();
+    }
+  } else {
+    if (force->newton_bond) return eval<0,0,1>();
+    else return eval<0,0,0>();
+  }
+}
+template <int EVFLAG, int EFLAG, int NEWTON_BOND>
+void DihedralHarmonicOMP::eval()
+{
+
+#if defined(_OPENMP)
+#pragma omp parallel default(shared)
+#endif
+  {
+    int i1,i2,i3,i4,i,m,n,type,tid;
+    double vb1x,vb1y,vb1z,vb2x,vb2y,vb2z,vb3x,vb3y,vb3z,vb2xm,vb2ym,vb2zm;
+    double edihedral,f1[3],f2[3],f3[3],f4[3];
+    double ax,ay,az,bx,by,bz,rasq,rbsq,rgsq,rg,rginv,ra2inv,rb2inv,rabinv;
+    double df,df1,ddf1,fg,hg,fga,hgb,gaa,gbb;
+    double dtfx,dtfy,dtfz,dtgx,dtgy,dtgz,dthx,dthy,dthz;
+    double c,s,p,sx2,sy2,sz2;
+
+    edihedral = 0.0;
+
+    const int nlocal = atom->nlocal;
+    const int nall = nlocal + atom->nghost;
+    const int nthreads = comm->nthreads;
+
+    double **x = atom->x;
+    double **f = atom->f;
+    int **dihedrallist = neighbor->dihedrallist;
+    int ndihedrallist = neighbor->ndihedrallist;
+
+    // loop over neighbors of my atoms
+
+    int nfrom, nto;
+    f = loop_setup_thr(f,nfrom, nto, tid, ndihedrallist, nall, nthreads);
+    for (n = nfrom; n < nto; ++n) {
+        i1 = dihedrallist[n][0];
+        i2 = dihedrallist[n][1];
+        i3 = dihedrallist[n][2];
+        i4 = dihedrallist[n][3];
+        type = dihedrallist[n][4];
+
+        // 1st bond
+
+        vb1x = x[i1][0] - x[i2][0];
+        vb1y = x[i1][1] - x[i2][1];
+        vb1z = x[i1][2] - x[i2][2];
+        domain->minimum_image(vb1x,vb1y,vb1z);
+
+        // 2nd bond
+
+        vb2x = x[i3][0] - x[i2][0];
+        vb2y = x[i3][1] - x[i2][1];
+        vb2z = x[i3][2] - x[i2][2];
+        domain->minimum_image(vb2x,vb2y,vb2z);
+
+        vb2xm = -vb2x;
+        vb2ym = -vb2y;
+        vb2zm = -vb2z;
+        domain->minimum_image(vb2xm,vb2ym,vb2zm);
+
+        // 3rd bond
+
+        vb3x = x[i4][0] - x[i3][0];
+        vb3y = x[i4][1] - x[i3][1];
+        vb3z = x[i4][2] - x[i3][2];
+        domain->minimum_image(vb3x,vb3y,vb3z);
+
+        // c,s calculation
+
+        ax = vb1y*vb2zm - vb1z*vb2ym;
+        ay = vb1z*vb2xm - vb1x*vb2zm;
+        az = vb1x*vb2ym - vb1y*vb2xm;
+        bx = vb3y*vb2zm - vb3z*vb2ym;
+        by = vb3z*vb2xm - vb3x*vb2zm;
+        bz = vb3x*vb2ym - vb3y*vb2xm;
+
+        rasq = ax*ax + ay*ay + az*az;
+        rbsq = bx*bx + by*by + bz*bz;
+        rgsq = vb2xm*vb2xm + vb2ym*vb2ym + vb2zm*vb2zm;
+        rg = sqrt(rgsq);
+
+        rginv = ra2inv = rb2inv = 0.0;
+        if (rg > 0) rginv = 1.0/rg;
+        if (rasq > 0) ra2inv = 1.0/rasq;
+        if (rbsq > 0) rb2inv = 1.0/rbsq;
+        rabinv = sqrt(ra2inv*rb2inv);
+
+        c = (ax*bx + ay*by + az*bz)*rabinv;
+        s = rg*rabinv*(ax*vb3x + ay*vb3y + az*vb3z);
+
+        // error check
+
+        if (c > 1.0 + TOLERANCE || c < (-1.0 - TOLERANCE)) {
+          int me;
+          MPI_Comm_rank(world,&me);
+          if (screen) {
+            char str[128];
+            sprintf(str,"Dihedral problem: %d %d %d %d %d %d",
+                    me,update->ntimestep,
+                    atom->tag[i1],atom->tag[i2],atom->tag[i3],atom->tag[i4]);
+            error->warning(str);
+            fprintf(screen,"  1st atom: %d %g %g %g\n",
+                    me,x[i1][0],x[i1][1],x[i1][2]);
+            fprintf(screen,"  2nd atom: %d %g %g %g\n",
+                    me,x[i2][0],x[i2][1],x[i2][2]);
+            fprintf(screen,"  3rd atom: %d %g %g %g\n",
+                    me,x[i3][0],x[i3][1],x[i3][2]);
+            fprintf(screen,"  4th atom: %d %g %g %g\n",
+                    me,x[i4][0],x[i4][1],x[i4][2]);
+          }
+        }
+
+        if (c > 1.0) c = 1.0;
+        if (c < -1.0) c = -1.0;
+
+        m = multiplicity[type];
+        p = 1.0;
+        df1 = 0.0;
+
+        for (i = 0; i < m; i++) {
+          ddf1 = p*c - df1*s;
+          df1 = p*s + df1*c;
+          p = ddf1;
+        }
+
+        p = p*cos_shift[type] + df1*sin_shift[type];
+        df1 = df1*cos_shift[type] - ddf1*sin_shift[type];
+        df1 *= -m;
+        p += 1.0;
+
+        if (m == 0) {
+          p = 1.0 + cos_shift[type];
+          df1 = 0.0;
+        }
+
+	  if (EFLAG) edihedral = k[type] * p;
+	    fg = vb1x*vb2xm + vb1y*vb2ym + vb1z*vb2zm;
+            hg = vb3x*vb2xm + vb3y*vb2ym + vb3z*vb2zm;
+            fga = fg*ra2inv*rginv;
+            hgb = hg*rb2inv*rginv;
+            gaa = -ra2inv*rg;
+            gbb = rb2inv*rg;
+
+            dtfx = gaa*ax;
+            dtfy = gaa*ay;
+            dtfz = gaa*az;
+            dtgx = fga*ax - hgb*bx;
+            dtgy = fga*ay - hgb*by;
+            dtgz = fga*az - hgb*bz;
+            dthx = gbb*bx;
+            dthy = gbb*by;
+            dthz = gbb*bz;
+
+            df = -k[type] * df1;
+
+            sx2 = df*dtgx;
+            sy2 = df*dtgy;
+            sz2 = df*dtgz;
+
+            f1[0] = df*dtfx;
+            f1[1] = df*dtfy;
+            f1[2] = df*dtfz;
+
+            f2[0] = sx2 - f1[0];
+            f2[1] = sy2 - f1[1];
+            f2[2] = sz2 - f1[2];
+
+            f4[0] = df*dthx;
+            f4[1] = df*dthy;
+            f4[2] = df*dthz;
+
+            f3[0] = -sx2 - f4[0];
+            f3[1] = -sy2 - f4[1];
+            f3[2] = -sz2 - f4[2];
+
+            // apply force to each of 4 atoms
+
+            if (NEWTON_BOND || i1 < nlocal) {
+              f[i1][0] += f1[0];
+              f[i1][1] += f1[1];
+              f[i1][2] += f1[2];
+            }
+
+            if (NEWTON_BOND || i2 < nlocal) {
+              f[i2][0] += f2[0];
+              f[i2][1] += f2[1];
+              f[i2][2] += f2[2];
+            }
+
+            if (NEWTON_BOND || i3 < nlocal) {
+              f[i3][0] += f3[0];
+              f[i3][1] += f3[1];
+              f[i3][2] += f3[2];
+            }
+
+            if (NEWTON_BOND || i4 < nlocal) {
+              f[i4][0] += f4[0];
+              f[i4][1] += f4[1];
+              f[i4][2] += f4[2];
+            }
+
+	  if (EVFLAG) ev_tally_thr(i1,i2,i3,i4,nlocal,NEWTON_BOND,edihedral,f1,f3,f4,
+	       vb1x,vb1y,vb1z,vb2x,vb2y,vb2z,vb3x,vb3y,vb3z,tid);
+	}
+    // reduce per thread forces into global force array.
+    force_reduce_thr(atom->f, nall, nthreads, tid);
+  }
+}
+
+/* ---------------------------------------------------------------------- */
+
+void DihedralHarmonicOMP::allocate()
+{
+  allocated = 1;
+  int n = atom->ndihedraltypes;
+
+  k = (double *) memory->smalloc((n+1)*sizeof(double),"dihedral:k");
+  sign = (int *) memory->smalloc((n+1)*sizeof(double),"dihedral:sign");
+  multiplicity = (int *)
+    memory->smalloc((n+1)*sizeof(double),"dihedral:multiplicity");
+  cos_shift = (double *)
+    memory->smalloc((n+1)*sizeof(double),"dihedral:cos_shift");
+  sin_shift = (double *)
+    memory->smalloc((n+1)*sizeof(double),"dihedral:sin_shift");
+
+  setflag = (int *) memory->smalloc((n+1)*sizeof(int),"dihedral:setflag");
+  for (int i = 1; i <= n; i++) setflag[i] = 0;
+}
+
+/* ----------------------------------------------------------------------
+   set coeffs for one type
+------------------------------------------------------------------------- */
+
+void DihedralHarmonicOMP::coeff(int which, int narg, char **arg)
+{
+  if (which > 0) return;
+  if (narg != 4) error->all("Incorrect args for dihedral coefficients");
+  if (!allocated) allocate();
+
+  int ilo,ihi;
+  force->bounds(arg[0],atom->ndihedraltypes,ilo,ihi);
+
+  double k_one = force->numeric(arg[1]);
+  int sign_one = force->inumeric(arg[2]);
+  int multiplicity_one = force->inumeric(arg[3]);
+
+  // require sign = +/- 1 for backwards compatibility
+  // arbitrary phase angle shift could be allowed, but would break
+  //   backwards compatibility and is probably not needed
+
+  if (sign_one != -1 && sign_one != 1)
+    error->all("Incorrect sign arg for dihedral coefficients");
+  if (multiplicity_one < 0)
+    error->all("Incorrect multiplicity arg for dihedral coefficients");
+
+  int count = 0;
+  for (int i = ilo; i <= ihi; i++) {
+    k[i] = k_one;
+    sign[i] = sign_one;
+    if (sign[i] == 1) {
+      cos_shift[i] = 1;
+      sin_shift[i] = 0;
+    } else {
+      cos_shift[i] = -1;
+      sin_shift[i] = 0;
+    }
+    multiplicity[i] = multiplicity_one;
+    setflag[i] = 1;
+    count++;
+  }
+
+  if (count == 0) error->all("Incorrect args for dihedral coefficients");
+}
+
+/* ----------------------------------------------------------------------
+   proc 0 writes out coeffs to restart file
+------------------------------------------------------------------------- */
+
+void DihedralHarmonicOMP::write_restart(FILE *fp)
+{
+  fwrite(&k[1],sizeof(double),atom->ndihedraltypes,fp);
+  fwrite(&sign[1],sizeof(int),atom->ndihedraltypes,fp);
+  fwrite(&multiplicity[1],sizeof(int),atom->ndihedraltypes,fp);
+}
+
+/* ----------------------------------------------------------------------
+   proc 0 reads coeffs from restart file, bcasts them
+------------------------------------------------------------------------- */
+
+void DihedralHarmonicOMP::read_restart(FILE *fp)
+{
+  allocate();
+
+  if (comm->me == 0) {
+    fread(&k[1],sizeof(double),atom->ndihedraltypes,fp);
+    fread(&sign[1],sizeof(int),atom->ndihedraltypes,fp);
+    fread(&multiplicity[1],sizeof(int),atom->ndihedraltypes,fp);
+  }
+  MPI_Bcast(&k[1],atom->ndihedraltypes,MPI_DOUBLE,0,world);
+  MPI_Bcast(&sign[1],atom->ndihedraltypes,MPI_INT,0,world);
+  MPI_Bcast(&multiplicity[1],atom->ndihedraltypes,MPI_INT,0,world);
+
+  for (int i = 1; i <= atom->ndihedraltypes; i++) {
+    setflag[i] = 1;
+    if (sign[i] == 1) {
+      cos_shift[i] = 1;
+      sin_shift[i] = 0;
+    } else {
+      cos_shift[i] = -1;
+      sin_shift[i] = 0;
+    }
+  }
+}
+
+/* ---------------------------------------------------------------------- */
+
+double DihedralHarmonicOMP::memory_usage()
+{
+  const int n=atom->ntypes;
+
+  double bytes = DihedralOMP::memory_usage();
+
+  bytes += 9*((n+1)*(n+1) * sizeof(double) + (n+1)*sizeof(double *));
+  bytes += 1*((n+1)*(n+1) * sizeof(int) + (n+1)*sizeof(int *));
+
+  return bytes;
+}
---
+++ src/USER-OPENMP/dihedral_harmonic_omp.h
@@ -0,0 +1,52 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef DIHEDRAL_CLASS
+
+DihedralStyle(harmonic/omp,DihedralHarmonicOMP)
+
+#else
+
+#ifndef LMP_DIHEDRAL_HARMONIC_OMP_H
+#define LMP_DIHEDRAL_HARMONIC_OMP_H
+
+#include "stdio.h"
+#include "dihedral_omp.h"
+
+namespace LAMMPS_NS {
+
+class DihedralHarmonicOMP : public DihedralOMP {
+ public:
+  DihedralHarmonicOMP(class LAMMPS *);
+  ~DihedralHarmonicOMP();
+  virtual void compute(int, int);
+  virtual void coeff(int, int, char **);
+  virtual void write_restart(FILE *);
+  virtual void read_restart(FILE *);
+
+  virtual double memory_usage();
+
+ protected:
+  template <int EVFLAG, int EFLAG, int NEWTON_BOND> void eval();
+
+private:
+  double *k,*cos_shift,*sin_shift;
+  int *sign,*multiplicity;
+
+  void allocate();
+};
+
+}
+
+#endif
+#endif

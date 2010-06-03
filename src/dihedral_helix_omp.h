2bd2bf1d43e1f695286256a10e7db0e7a62de051
Ryan Houlihan <ryan.houlihan90@gmail.com>
2010-06-03 10:11:00 -0400

    Dihedral Helix with OMP implemented

---
+++ src/dihedral_helix_omp.h
@@ -0,0 +1,51 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef DIHEDRAL_CLASS
+
+DihedralStyle(helix/omp,DihedralHelixOMP)
+
+#else
+
+#ifndef LMP_DIHEDRAL_HELIX_OMP_H
+#define LMP_DIHEDRAL_HELIX_OMP_H
+
+#include "stdio.h"
+#include "dihedral_omp.h"
+
+namespace LAMMPS_NS {
+
+class DihedralHelixOMP : public DihedralOMP {
+ public:
+  DihedralHelixOMP(class LAMMPS *);
+  ~DihedralHelixOMP();
+  virtual void compute(int, int);
+  virtual void coeff(int, int, char **);
+  virtual void write_restart(FILE *);
+  virtual void read_restart(FILE *);
+
+  virtual double memory_usage();
+
+ protected:
+  template <int EVFLAG, int EFLAG, int NEWTON_BOND> void eval();
+
+private:
+  double *aphi,*bphi,*cphi;
+
+  void allocate();
+};
+
+}
+
+#endif
+#endif

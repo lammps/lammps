1fca8b88d40a3881fb416a98bae0c0b103e3af42
Axel Kohlmeyer <akohlmey@gmail.com>
2010-07-22 16:24:59 -0400

    add new pair style with OpenMP support to install script.

--- src/USER-OPENMP/Install.sh
+++ src/USER-OPENMP/Install.sh
@@ -132,6 +132,8 @@ if (test $1 = 1) then
     cp pair_lj_charmm_coul_long_omp.cpp ..
     cp pair_lj_cut_coul_long_omp.h ..
     cp pair_lj_cut_coul_long_omp.cpp ..
+    cp pair_lj_cut_coul_long_tip4p_omp.h ..
+    cp pair_lj_cut_coul_long_tip4p_omp.cpp ..
     cp ewald_omp.h ..
     cp ewald_omp.cpp ..
   fi
@@ -261,6 +263,8 @@ elif (test $1 = 0) then
   rm -f ../pair_lj_charmm_coul_long_omp.cpp
   rm -f ../pair_lj_cut_coul_long_omp.h
   rm -f ../pair_lj_cut_coul_long_omp.cpp
+  rm -f ../pair_lj_cut_coul_long_tip4p_omp.h
+  rm -f ../pair_lj_cut_coul_long_tip4p_omp.cpp
   rm -f ../ewald_omp.h
   rm -f ../ewald_omp.cpp
 

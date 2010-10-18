ef4931ac75b076ef2320f6fe03d3d486cf1115b8
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2010-10-18 15:47:52 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@5055 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/SRD/Install.sh
+++ src/SRD/Install.sh
@@ -3,21 +3,13 @@
 if (test $1 == 1) then
 
   cp fix_srd.cpp ..
-  cp fix_srd2.cpp ..
-  cp fix_wall_srd.cpp ..
 
   cp fix_srd.h ..
-  cp fix_srd2.h ..
-  cp fix_wall_srd.h ..
 
 elif (test $1 == 0) then
 
   rm ../fix_srd.cpp
-  rm ../fix_srd2.cpp
-  rm ../fix_wall_srd.cpp
 
   rm ../fix_srd.h
-  rm ../fix_srd2.h
-  rm ../fix_wall_srd.h
 
 fi

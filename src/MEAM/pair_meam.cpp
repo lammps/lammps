c41643fd03b552f601976560a70273b7ee60042f
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2010-10-21 17:32:09 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@5085 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/MEAM/pair_meam.cpp
+++ src/MEAM/pair_meam.cpp
@@ -764,7 +764,8 @@ int PairMEAM::pack_comm(int n, int *list, double *buf, int pbc_flag, int *pbc)
     buf[m++] = tsq_ave[j][1];
     buf[m++] = tsq_ave[j][2];
   }
-  return 38;
+
+  return comm_forward;
 }
 
 /* ---------------------------------------------------------------------- */
@@ -840,7 +841,7 @@ int PairMEAM::pack_reverse_comm(int n, int first, double *buf)
     buf[m++] = tsq_ave[i][2];
   }
 
-  return 30;
+  return comm_reverse;
 }
 
 /* ---------------------------------------------------------------------- */

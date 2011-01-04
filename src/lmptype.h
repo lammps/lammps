027806517749edc316a506d714647b831545d40b
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2011-01-04 19:14:14 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@5459 f3b2605a-c512-4ea7-a41b-209d697bcdaa

---
+++ src/lmptype.h
@@ -0,0 +1,26 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under 
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifndef LMP_LMPTYPE_H
+#define LMP_LMPTYPE_H
+
+#include "stdint.h"
+
+namespace LAMMPS_NS {
+
+typedef uint64_t bigint;
+#define MAXINT32 0x7FFFFFFF
+
+}
+
+#endif

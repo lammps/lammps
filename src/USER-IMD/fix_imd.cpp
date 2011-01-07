bb93fa20251d6408af2bcded2113e233e3c8828a
Axel Kohlmeyer <akohlmey@gmail.com>
2011-01-07 11:36:24 -0500

    make the code compile again.

--- src/USER-IMD/fix_imd.cpp
+++ src/USER-IMD/fix_imd.cpp
@@ -874,7 +874,7 @@ FixIMD::FixIMD(LAMMPS *lmp, int narg, char **arg) :
   bigint n;
   if (igroup == group->find("all"))  n = atom->natoms;
   else n = group->count(igroup);
-  if (n > MAXINT32) error->all("Too many atoms for fix imd");
+  if (n > MAXSMALLINT) error->all("Too many atoms for fix imd");
   num_coords = static_cast<int> (n);
 
   MPI_Comm_rank(world,&me);

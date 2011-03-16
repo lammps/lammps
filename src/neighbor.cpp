9ac3d51b88ed55323a10350b2fd4fea62344ad4e
Axel Kohlmeyer <akohlmey@gmail.com>
2011-03-16 18:07:02 -0400

    Revert "avoid segfault in distcheck with nsq neighbor list"
    This reverts commit 7f533ae84d9b6c43e6b0e6d9ae163d92467d658b.

--- src/neighbor.cpp
+++ src/neighbor.cpp
@@ -1093,8 +1093,6 @@ void Neighbor::build()
     }
     if (boxcheck) {
       if (triclinic == 0) {
-	bboxlo = domain->boxlo;
-	bboxhi = domain->boxhi;
 	boxlo_hold[0] = bboxlo[0];
 	boxlo_hold[1] = bboxlo[1];
 	boxlo_hold[2] = bboxlo[2];

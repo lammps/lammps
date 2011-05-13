99859cb405a098173b88d7c59c02e0f52f97009e
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2011-05-13 20:46:15 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@6093 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/modify.cpp
+++ src/modify.cpp
@@ -274,7 +274,7 @@ void Modify::setup_pre_force(int vflag)
     for (int i = 0; i < n_pre_force; i++)
       fix[list_pre_force[i]]->setup_pre_force(vflag);
   else if (update->whichflag == 2)
-    for (int i = 0; i < n_pre_force; i++)
+    for (int i = 0; i < n_min_pre_force; i++)
       fix[list_min_pre_force[i]]->min_setup_pre_force(vflag);
 }
 

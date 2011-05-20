40af1682eeb574b85f5939fbe0200ea6ab2a8b22
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2011-05-20 15:24:03 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@6149 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/USER-ATC/Install.sh
+++ src/USER-ATC/Install.sh
@@ -9,8 +9,9 @@ if (test $1 = 1) then
     sed -i -e 's|^PKG_INC =[ \t]*|&-I../../lib/atc |' ../Makefile.package
     sed -i -e 's|^PKG_PATH =[ \t]*|&-L../../lib/atc |' ../Makefile.package
     sed -i -e 's|^PKG_LIB =[ \t]*|&-latc |' ../Makefile.package
-    sed -i -e 's|^PKG_SYSPATH =[ \t]*|&$(user-atc_SYSPATH) |' ../Makefile.package
+    sed -i -e 's|^PKG_SYSINC =[ \t]*|&$(user-atc_SYSINC) |' ../Makefile.package
     sed -i -e 's|^PKG_SYSLIB =[ \t]*|&$(user-atc_SYSLIB) |' ../Makefile.package
+    sed -i -e 's|^PKG_SYSPATH =[ \t]*|&$(user-atc_SYSPATH) |' ../Makefile.package
   fi
 
   cp fix_atc.h ..

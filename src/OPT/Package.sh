4578ea9bc699344e767671bb2155c97ab35895df
Axel Kohlmeyer <akohlmey@gmail.com>
2011-08-26 14:35:46 -0400

    update opt package management scripts

--- src/OPT/Package.sh
+++ src/OPT/Package.sh
@@ -33,6 +33,12 @@ for file in *.cpp *.h; do
   if (test $file = pair_lj_cut_coul_long_opt.h -a ! -e ../pair_lj_cut_coul_long.cpp) then
     continue
   fi
+  if (test $file = pair_lj_cut_coul_long_tip4p_opt.cpp -a ! -e ../pair_lj_cut_coul_long_tip4p.cpp) then
+    continue
+  fi
+  if (test $file = pair_lj_cut_coul_long_tip4p_opt.h -a ! -e ../pair_lj_cut_coul_long_tip4p.cpp) then
+    continue
+  fi
 
   if (test ! -e ../$file) then
     echo "  creating src/$file"

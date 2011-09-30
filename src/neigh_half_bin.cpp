9b3f9e1b44e45bc8b1c3881012dbe11c7f1128f8
Axel Kohlmeyer <akohlmey@gmail.com>
2011-09-30 18:43:38 -0400

    simplified & expanded neighborlist OpenMP support

--- src/neigh_half_bin.cpp
+++ src/neigh_half_bin.cpp
@@ -14,7 +14,10 @@
 #include "neighbor.h"
 #include "neigh_list.h"
 #include "atom.h"
+#include "comm.h"
 #include "error.h"
+
+#include <omp.h>
 
 using namespace LAMMPS_NS;
 
@@ -27,13 +30,22 @@ using namespace LAMMPS_NS;
 
 void Neighbor::half_bin_no_newton(NeighList *list)
 {
-  int i,j,k,n,itype,jtype,ibin,which;
-  double xtmp,ytmp,ztmp,delx,dely,delz,rsq;
-  int *neighptr;
-
   // bin local & ghost atoms
 
   bin_atoms();
+
+  const int nthreads = comm->nthreads;
+  const int nlocal = (includegroup) ? atom->nfirst : atom->nlocal;
+
+  NEIGH_OMP_INIT;
+#if defined(_OPENMP)
+#pragma omp parallel default(none) shared(list)
+#endif
+  NEIGH_OMP_SETUP(nlocal);
+
+  int i,j,k,n,itype,jtype,ibin,which;
+  double xtmp,ytmp,ztmp,delx,dely,delz,rsq;
+  int *neighptr;
 
   // loop over each atom, storing neighbors
 
@@ -45,30 +57,32 @@ void Neighbor::half_bin_no_newton(NeighList *list)
   int *type = atom->type;
   int *mask = atom->mask;
   int *molecule = atom->molecule;
-  int nlocal = atom->nlocal;
-  if (includegroup) nlocal = atom->nfirst;
   int molecular = atom->molecular;
 
   int *ilist = list->ilist;
   int *numneigh = list->numneigh;
   int **firstneigh = list->firstneigh;
-  int **pages = list->pages;
   int nstencil = list->nstencil;
   int *stencil = list->stencil;
 
-  int inum = 0;
-  int npage = 0;
+  // each thread works on its own page
+  int npage = tid;
   int npnt = 0;
 
-  for (i = 0; i < nlocal; i++) {
+  for (i = ifrom; i < ito; i++) {
 
     if (pgsize - npnt < oneatom) {
       npnt = 0;
-      npage++;
-      if (npage == list->maxpage) pages = list->add_pages();
+      npage += nthreads;
+      // only one thread at a time may check whether we 
+      // need new neighbor list pages and then add to them.
+#if defined(_OPENMP)
+#pragma omp critical
+#endif
+      if (npage >= list->maxpage) list->add_pages(nthreads);
     }
 
-    neighptr = &pages[npage][npnt];
+    neighptr = &(list->pages[npage][npnt]);
     n = 0;
 
     itype = type[i];
@@ -104,15 +118,15 @@ void Neighbor::half_bin_no_newton(NeighList *list)
       }
     }
 
-    ilist[inum++] = i;
+    ilist[i] = i;
     firstneigh[i] = neighptr;
     numneigh[i] = n;
     npnt += n;
     if (n > oneatom || npnt >= pgsize)
       error->one(FLERR,"Neighbor list overflow, boost neigh_modify one or page");
   }
-
-  list->inum = inum;
+  NEIGH_OMP_CLOSE;
+  list->inum = nlocal;
 }
 
 /* ----------------------------------------------------------------------
@@ -122,7 +136,7 @@ void Neighbor::half_bin_no_newton(NeighList *list)
 ------------------------------------------------------------------------- */
 
 void Neighbor::half_bin_newton(NeighList *list)
-
+{
   // bin local & ghost atoms
 
   bin_atoms();
@@ -130,93 +144,93 @@ void Neighbor::half_bin_newton(NeighList *list)
   const int nthreads = comm->nthreads;
   const int nlocal = (includegroup) ? atom->nfirst : atom->nlocal;
 
-  // make sure we have at least one page for each thread
-  if (nthreads > list->maxpage) {
-    list->add_pages(nthreads - list->maxpage);
-  }
-  int inum = 0;
-
+  NEIGH_OMP_INIT;
 #if defined(_OPENMP)
-#pragma omp parallel default(none) shared(list,inum)
+#pragma omp parallel default(none) shared(list)
 #endif
-  {
+  NEIGH_OMP_SETUP(nlocal);
 
-    // get thread id and then assign each thread a fixed chunk of atoms
-#if defined(_OPENMP)
-    const int tid = omp_get_thread_num();
-#else
-    const int tid = 0;
-#endif
-    const int idelta = 1 + nlocal/nthreads;
-    const int ifrom = tid*idelta;
-    int ito   = ifrom + idelta;
-    if (ito > nlocal)
-      ito = nlocal;
+  int i,j,k,n,itype,jtype,ibin,which;
+  double xtmp,ytmp,ztmp,delx,dely,delz,rsq;
+  int *neighptr;
 
-    int i,j,k,n,itype,jtype,ibin,which;
-    double xtmp,ytmp,ztmp,delx,dely,delz,rsq;
-    int *neighptr;
+  // loop over each atom, storing neighbors
 
-    // loop over each atom, storing neighbors
+  int **special = atom->special;
+  int **nspecial = atom->nspecial;
+  int *tag = atom->tag;
 
-    int **special = atom->special;
-    int **nspecial = atom->nspecial;
-    int *tag = atom->tag;
+  double **x = atom->x;
+  int *type = atom->type;
+  int *mask = atom->mask;
+  int *molecule = atom->molecule;
+  int molecular = atom->molecular;
 
-    double **x = atom->x;
-    int *type = atom->type;
-    int *mask = atom->mask;
-    int *molecule = atom->molecule;
-    int molecular = atom->molecular;
+  int *ilist = list->ilist;
+  int *numneigh = list->numneigh;
+  int **firstneigh = list->firstneigh;
+  int nstencil = list->nstencil;
+  int *stencil = list->stencil;
 
-    int *ilist = list->ilist;
-    int *numneigh = list->numneigh;
-    int **firstneigh = list->firstneigh;
-    int nstencil = list->nstencil;
-    int *stencil = list->stencil;
-  
-    // each thread works on its own page
-    int npage = tid;
-    int npnt = 0;
+  // each thread works on its own page
+  int npage = tid;
+  int npnt = 0;
 
-    for (i = ifrom; i < ito; i++) {
+  for (i = ifrom; i < ito; i++) {
 
-      if (pgsize - npnt < oneatom) {
-	npnt = 0;
-	npage += nthreads;
-
-	// only one thread at a time may check
-	// whether we need new neighbor list pages
-	// and then add to them.
+    if (pgsize - npnt < oneatom) {
+      npnt = 0;
+      npage += nthreads;
+      // only one thread at a time may check  whether we
+      // need new neighbor list pages and then add to them.
 #if defined(_OPENMP)
 #pragma omp critical
 #endif
-	if (npage >= list->maxpage) {
-	  list->add_pages(nthreads);
+      if (npage >= list->maxpage) list->add_pages(nthreads);
+    }
+
+    neighptr = &(list->pages[npage][npnt]);
+    n = 0;
+
+    itype = type[i];
+    xtmp = x[i][0];
+    ytmp = x[i][1];
+    ztmp = x[i][2];
+
+    // loop over rest of atoms in i's bin, ghosts are at end of linked list
+    // if j is owned atom, store it, since j is beyond i in linked list
+    // if j is ghost, only store if j coords are "above and to the right" of i
+
+    for (j = bins[i]; j >= 0; j = bins[j]) {
+      if (j >= nlocal) {
+	if (x[j][2] < ztmp) continue;
+	if (x[j][2] == ztmp) {
+	  if (x[j][1] < ytmp) continue;
+	  if (x[j][1] == ytmp && x[j][0] < xtmp) continue;
 	}
       }
 
-      neighptr = &(list->pages[npage][npnt]);
-      n = 0;
+      jtype = type[j];
+      if (exclude && exclusion(i,j,itype,jtype,mask,molecule)) continue;
 
-      itype = type[i];
-      xtmp = x[i][0];
-      ytmp = x[i][1];
-      ztmp = x[i][2];
+      delx = xtmp - x[j][0];
+      dely = ytmp - x[j][1];
+      delz = ztmp - x[j][2];
+      rsq = delx*delx + dely*dely + delz*delz;
 
-      // loop over rest of atoms in i's bin, ghosts are at end of linked list
-      // if j is owned atom, store it, since j is beyond i in linked list
-      // if j is ghost, only store if j coords are "above and to the right" of i
+      if (rsq <= cutneighsq[itype][jtype]) {
+	if (molecular) {
+	  which = find_special(special[i],nspecial[i],tag[j]);
+	  if (which >= 0) neighptr[n++] = j ^ (which << SBBITS);
+	} else neighptr[n++] = j;
+      }
+    }
 
-      for (j = bins[i]; j >= 0; j = bins[j]) {
-	if (j >= nlocal) {
-	  if (x[j][2] < ztmp) continue;
-	  if (x[j][2] == ztmp) {
-	    if (x[j][1] < ytmp) continue;
-	    if (x[j][1] == ytmp && x[j][0] < xtmp) continue;
-	  }
-	}
+    // loop over all atoms in other bins in stencil, store every pair
 
+    ibin = coord2bin(x[i]);
+    for (k = 0; k < nstencil; k++) {
+      for (j = binhead[ibin+stencil[k]]; j >= 0; j = bins[j]) {
 	jtype = type[j];
 	if (exclude && exclusion(i,j,itype,jtype,mask,molecule)) continue;
 
@@ -232,43 +246,17 @@ void Neighbor::half_bin_newton(NeighList *list)
 	  } else neighptr[n++] = j;
 	}
       }
-
-      // loop over all atoms in other bins in stencil, store every pair
-
-      ibin = coord2bin(x[i]);
-      for (k = 0; k < nstencil; k++) {
-	for (j = binhead[ibin+stencil[k]]; j >= 0; j = bins[j]) {
-	  jtype = type[j];
-	  if (exclude && exclusion(i,j,itype,jtype,mask,molecule)) continue;
-
-	  delx = xtmp - x[j][0];
-	  dely = ytmp - x[j][1];
-	  delz = ztmp - x[j][2];
-	  rsq = delx*delx + dely*dely + delz*delz;
-
-	  if (rsq <= cutneighsq[itype][jtype]) {
-	    if (molecular) {
-	      which = find_special(special[i],nspecial[i],tag[j]);
-	      if (which >= 0) neighptr[n++] = j ^ (which << SBBITS);
-	    } else neighptr[n++] = j;
-	  }
-	}
-      }
-
-#if defined(_OPENMP)
-#pragma omp critical
-#endif
-      ilist[inum++] = i;
-
-      firstneigh[i] = neighptr;
-      numneigh[i] = n;
-      npnt += n;
-      if (n > oneatom || npnt >= pgsize)
-	error->one(FLERR,"Neighbor list overflow, boost neigh_modify one or page");
     }
 
-    list->inum = inum;
+    ilist[i] = i;
+    firstneigh[i] = neighptr;
+    numneigh[i] = n;
+    npnt += n;
+    if (n > oneatom || npnt >= pgsize)
+      error->one(FLERR,"Neighbor list overflow, boost neigh_modify one or page");
   }
+  NEIGH_OMP_CLOSE;
+  list->inum = nlocal;
 }
 
 /* ----------------------------------------------------------------------
@@ -279,13 +267,22 @@ void Neighbor::half_bin_newton(NeighList *list)
 
 void Neighbor::half_bin_newton_tri(NeighList *list)
 {
-  int i,j,k,n,itype,jtype,ibin,which;
-  double xtmp,ytmp,ztmp,delx,dely,delz,rsq;
-  int *neighptr;
-
   // bin local & ghost atoms
 
   bin_atoms();
+
+  const int nthreads = comm->nthreads;
+  const int nlocal = (includegroup) ? atom->nfirst : atom->nlocal;
+
+  NEIGH_OMP_INIT;
+#if defined(_OPENMP)
+#pragma omp parallel default(none) shared(list)
+#endif
+  NEIGH_OMP_SETUP(nlocal);
+
+  int i,j,k,n,itype,jtype,ibin,which;
+  double xtmp,ytmp,ztmp,delx,dely,delz,rsq;
+  int *neighptr;
 
   // loop over each atom, storing neighbors
 
@@ -297,30 +294,32 @@ void Neighbor::half_bin_newton_tri(NeighList *list)
   int *type = atom->type;
   int *mask = atom->mask;
   int *molecule = atom->molecule;
-  int nlocal = atom->nlocal;
-  if (includegroup) nlocal = atom->nfirst;
   int molecular = atom->molecular;
 
   int *ilist = list->ilist;
   int *numneigh = list->numneigh;
   int **firstneigh = list->firstneigh;
-  int **pages = list->pages;
   int nstencil = list->nstencil;
   int *stencil = list->stencil;
 
-  int inum = 0;
-  int npage = 0;
+  // each thread works on its own page
+  int npage = tid;
   int npnt = 0;
 
-  for (i = 0; i < nlocal; i++) {
+  for (i = ifrom; i < ito; i++) {
 
     if (pgsize - npnt < oneatom) {
       npnt = 0;
-      npage++;
-      if (npage == list->maxpage) pages = list->add_pages();
+      npage += nthreads;
+      // only one thread at a time may check  whether we
+      // need new neighbor list pages and then add to them.
+#if defined(_OPENMP)
+#pragma omp critical
+#endif
+      if (npage >= list->maxpage) list->add_pages(nthreads);
     }
 
-    neighptr = &pages[npage][npnt];
+    neighptr = &(list->pages[npage][npnt]);
     n = 0;
 
     itype = type[i];
@@ -363,13 +362,13 @@ void Neighbor::half_bin_newton_tri(NeighList *list)
       }
     }
 
-    ilist[inum++] = i;
+    ilist[i] = i;
     firstneigh[i] = neighptr;
     numneigh[i] = n;
     npnt += n;
     if (n > oneatom || npnt >= pgsize)
       error->one(FLERR,"Neighbor list overflow, boost neigh_modify one or page");
   }
-
-  list->inum = inum;
+  NEIGH_OMP_CLOSE;
+  list->inum = nlocal;
 }
--- src/neighbor.h
+++ src/neighbor.h
@@ -289,6 +289,39 @@ class Neighbor : protected Pointers {
   };
 };
 
+// these macros hide some ugly and redundant OpenMP related stuff
+#if defined(_OPENMP)
+
+// make sure we have at least one page for each thread
+#define NEIGH_OMP_INIT					\
+  if (nthreads > list->maxpage)				\
+    list->add_pages(nthreads - list->maxpage)
+
+// get thread id and then assign each thread a fixed chunk of atoms
+#define NEIGH_OMP_SETUP(num)				\
+  {							\
+    const int tid = omp_get_thread_num();		\
+    const int idelta = 1 + num/nthreads;		\
+    const int ifrom = tid*idelta;			\
+    int ito   = ifrom + idelta;				\
+    if (ito > num)					\
+      ito = num
+
+#define NEIGH_OMP_CLOSE }
+
+#else /* !defined(_OPENMP) */
+
+#define NEIGH_OMP_INIT
+
+#define NEIGH_OMP_SETUP(num)				\
+    const int tid = 0;					\
+    const int ifrom = 0;				\
+    const int ito = num
+
+#define NEIGH_OMP_CLOSE
+
+#endif
+
 }
 
 #endif

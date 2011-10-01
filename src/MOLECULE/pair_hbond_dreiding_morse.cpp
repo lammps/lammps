a681c4a12f93cf36ba9069ab54eb8b78b70f022e
Axel Kohlmeyer <akohlmey@gmail.com>
2011-09-30 21:10:55 -0400

    fix bug in hbond energy accumulation for dreiding

--- src/MOLECULE/pair_hbond_dreiding_morse.cpp
+++ src/MOLECULE/pair_hbond_dreiding_morse.cpp
@@ -46,14 +46,14 @@ void PairHbondDreidingMorse::compute(int eflag, int vflag)
 {
   int i,j,k,m,ii,jj,kk,inum,jnum,knum,itype,jtype,ktype;
   double delx,dely,delz,rsq,rsq1,rsq2,r1,r2;
-  double factor_hb,force_angle,force_kernel,evdwl;
+  double factor_hb,force_angle,force_kernel,evdwl,evdwl_total;
   double c,s,a,b,ac,a11,a12,a22,vx1,vx2,vy1,vy2,vz1,vz2;
   double fi[3],fj[3],delr1[3],delr2[3];
   double r,dr,dexp,eng_morse,switch1,switch2;
   int *ilist,*jlist,*klist,*numneigh,**firstneigh;
   Param *pm;
 
-  evdwl = 0.0;
+  evdwl_total = evdwl = 0.0;
   if (eflag || vflag) ev_setup(eflag,vflag);
   else evflag = vflag_fdotr = 0;
   
@@ -190,10 +190,13 @@ void PairHbondDreidingMorse::compute(int eflag, int vflag)
 	    f[k][2] -= vz1 + vz2;
 
 	    // KIJ instead of IJK b/c delr1/delr2 are both with respect to k
-
-	    if (evflag) ev_tally3(k,i,j,evdwl,0.0,fi,fj,delr1,delr2);
-
-	    hbcount++;
+            if (evflag) {
+              ev_tally3(k,i,j,evdwl,0.0,fi,fj,delr1,delr2);
+              if (eflag_global) {
+                hbcount++;
+                evdwl_total += evdwl;
+              }
+            }
 	  }
         }
       }
@@ -202,7 +205,7 @@ void PairHbondDreidingMorse::compute(int eflag, int vflag)
 
   if (eflag_global) {
     pvector[0] = hbcount;
-    pvector[1] = evdwl;
+    pvector[1] = evdwl_total;
   }
 }
 

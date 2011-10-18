2da0d83cfef9801ca8c9281d6ce30472b580d219
Axel Kohlmeyer <akohlmey@gmail.com>
2011-10-18 19:00:23 -0400

    prepare for adding the rest of the multi-threaded improper styles

--- src/MOLECULE/improper_harmonic.h
+++ src/MOLECULE/improper_harmonic.h
@@ -28,13 +28,13 @@ namespace LAMMPS_NS {
 class ImproperHarmonic : public Improper {
  public:
   ImproperHarmonic(class LAMMPS *);
-  ~ImproperHarmonic();
-  void compute(int, int);
+  virtual ~ImproperHarmonic();
+  virtual void compute(int, int);
   void coeff(int, char **);
   void write_restart(FILE *);
   void read_restart(FILE *);
 
- private:
+ protected:
   double *k,*chi;
 
   void allocate();

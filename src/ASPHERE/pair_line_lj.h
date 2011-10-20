fd155252ea24a3d24dd35f6e674b43981e718b5b
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2011-10-20 15:09:45 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@7154 f3b2605a-c512-4ea7-a41b-209d697bcdaa

---
+++ src/ASPHERE/pair_line_lj.h
@@ -0,0 +1,61 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under 
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef PAIR_CLASS
+
+PairStyle(line,PairLine)
+
+#else
+
+#ifndef LMP_PAIR_LINE_H
+#define LMP_PAIR_LINE_H
+
+#include "pair.h"
+
+namespace LAMMPS_NS {
+
+class PairLine : public Pair {
+ public:
+  PairLine(class LAMMPS *);
+  ~PairLine();
+  void compute(int, int);
+  void settings(int, char **);
+  void coeff(int, char **);
+  double init_one(int, int);
+
+ protected:
+  double cut_global;
+  double **cut;
+  double **epsilon,**sigma;
+  double **lj1,**lj2,**lj3,**lj4;
+  class AtomVecLine *avec;
+
+  struct Discrete {
+    double dx,dy;
+    double sigma;
+  };
+  Discrete *discrete;           // list of all discretes for all lines
+  int ndiscrete;                // number of discretes in list
+  int dmax;                     // allocated size of discrete list
+  int *dnum;                    // number of discretes per line, 0 if uninit
+  int *dfirst;                  // index of first discrete per each line
+  int nmax;                     // allocated size of dnum,dfirst vectors
+
+  void allocate();
+  void discretize(int, double);
+};
+
+}
+
+#endif
+#endif

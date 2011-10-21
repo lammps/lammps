8b175d0a61b1416d0582209f7f5b432e936d0111
Axel Kohlmeyer <akohlmey@gmail.com>
2011-10-21 19:08:00 -0400

    add multi-threaded versions of tri/line pair styles

--- src/ASPHERE/pair_tri_lj.h
+++ src/ASPHERE/pair_tri_lj.h
@@ -27,8 +27,8 @@ namespace LAMMPS_NS {
 class PairTriLJ : public Pair {
  public:
   PairTriLJ(class LAMMPS *);
-  ~PairTriLJ();
-  void compute(int, int);
+  virtual ~PairTriLJ();
+  virtual void compute(int, int);
   void settings(int, char **);
   void coeff(int, char **);
   double init_one(int, int);

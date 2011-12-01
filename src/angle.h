b3cd459ea9fb523d95593643ac7b4d508995bbe9
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2011-12-01 16:05:02 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@7263 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/angle.h
+++ src/angle.h
@@ -31,6 +31,7 @@ class Angle : protected Pointers {
   Angle(class LAMMPS *);
   virtual ~Angle();
   virtual void init();
+  virtual void init_style() {}
   virtual void compute(int, int) = 0;
   virtual void settings(int, char **) {}
   virtual void coeff(int, char **) = 0;

13dd52a694b9b8ef5fb4a5f074995b3d33dd5047
Axel Kohlmeyer <akohlmey@gmail.com>
2011-12-03 10:05:06 -0500

    more comments added including usage instructions

--- tools/hack_openmp_for_pgi.sh
+++ tools/hack_openmp_for_pgi.sh
@@ -5,6 +5,10 @@
 # compilers. This modification incurs a performance
 # penalty, though, so it is not part of the regular code.
 #
+# to apply the hack. go to src/, install all packages
+# as needed and then run: sh ../tools/hack_openmp_for_pgi.sh
+# this needs to be repeated after each "make package-update"
+#
 # Axel Kohlmeyer <akohlmey@gmail.com>
 
 for f in *.h *.cpp

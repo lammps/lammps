6282d92c3b67bfc1d7bc7db2ce2d3ba041c55c35
Axel Kohlmeyer <akohlmey@gmail.com>
2011-12-12 22:05:34 -0500

    add tersoff/table support

--- regtest/restart/compare.sh
+++ regtest/restart/compare.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-for s in *.data-*-* ; do \
-  t=`echo $s | sed -e 's/.data-/.ref-/'`
-  c=`echo $s | sed -e 's/.data-/.coeff-/'`
+for s in *.restart-*-* ; do \
+  t=`echo $s | sed -e 's/.restart-/.ref-/'`
+  c=`echo $s | sed -e 's/.restart-/.coeff-/'`
+  s=`echo $s | sed -e 's/.restart-/.data-/'`
+  test -f $s || echo "no data file $s found for restart $r"
+  test -f $s || continue
   grep -v LAMMPS $s | sed -e 's/-0\.0000000000/0.0000000000 /g' > ${t}
   test -f $c && grep -v LAMMPS $c | sed -e 's/-0\.0000000000/0.0000000000 /g' >> ${t}
   if [ -f refoutput/${t} ]

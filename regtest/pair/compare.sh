2e72528df5c340a9dd1fe747bafc26762c1d4b11
Axel Kohlmeyer <akohlmey@gmail.com>
2012-01-01 20:20:18 -0500

    compare regular with overlay variants, too

--- regtest/pair/compare.sh
+++ regtest/pair/compare.sh
@@ -10,3 +10,15 @@ for s in log.* ; do \
      echo no reference output for $s
   fi
 done
+
+for s in log.overlay* ; do \
+  r=`echo $s | sed -e 's/overlay/charged/'`
+  t=`echo $r | sed -e 's/log.charged/ref.ovc-charged/'`
+  u=`echo $s | sed -e 's/log.overlay/ref.ovc-overlay/'`
+
+  grep -v ^Loop $r | egrep -v '(LAMMPS|OpenMP|MPI|serial)' | grep -v 'Memory usage' | sed -e 's/-0\.0000000000/0.0000000000 /g' > $t
+  grep -v ^Loop $s | egrep -v '(LAMMPS|OpenMP|MPI|serial)' | grep -v 'Memory usage' | sed -e 's/-0\.0000000000/0.0000000000 /g' > $u
+
+  diff -u $t $u && rm $t $u
+done
+

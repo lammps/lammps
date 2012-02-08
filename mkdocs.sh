7b2d6e6599b8f8ac32a992297f3724f374c1ed0b
Axel Kohlmeyer <akohlmey@gmail.com>
2012-02-07 20:32:19 -0500

    check for missing files when building manual pdf

--- mkdocs.sh
+++ mkdocs.sh
@@ -13,8 +13,17 @@ do \
 	fi
 done
 
+# build txt2html tool
 make -C txt2html
 
+# convert all .txt files to html
 ./txt2html/txt2html doc/*.txt
+
+# check if we have any new html files,
+# that are not yet in the book file.
+for s in `echo doc/*.txt | sed -e 's,\.txt,\.html,g'`
+do \
+  grep -q $s lammps.book || echo doc file $s missing in lammps.book
+done
 htmldoc --batch lammps.book
 

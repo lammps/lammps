a328a5a84690ea5f3062a805ee4dfc6963d4e734
Axel Kohlmeyer <akohlmey@gmail.com>
2012-02-17 12:31:09 -0500

    missing change

--- src/USER-OMP/pair_beck_omp.cpp
+++ src/USER-OMP/pair_beck_omp.cpp
@@ -28,7 +28,7 @@ using namespace LAMMPS_NS;
 PairBeckOMP::PairBeckOMP(LAMMPS *lmp) :
   PairBeck(lmp), ThrOMP(lmp, THR_PAIR)
 {
-  suffix_compat |= Suffix::OMP;
+  suffix_flag |= Suffix::OMP;
   respa_enable = 0;
 }
 

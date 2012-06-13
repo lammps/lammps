0cb327257799055d95c826b701836128b4bbf960
Axel Kohlmeyer <akohlmey@gmail.com>
2012-06-13 12:27:33 -0400

    include author credit info

--- src/reader_xyz.cpp
+++ src/reader_xyz.cpp
@@ -11,6 +11,10 @@
    See the README file in the top-level LAMMPS directory.
 ------------------------------------------------------------------------- */
 
+/* ----------------------------------------------------------------------
+   Contributing author: Axel Kohlmeyer (Temple U)
+------------------------------------------------------------------------- */
+
 #include "string.h"
 #include "stdlib.h"
 #include "reader_xyz.h"

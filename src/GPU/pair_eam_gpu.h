1624264bc847c07fc890211281bf74c35b5bd9f3
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2012-06-29 14:07:22 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@8426 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/GPU/pair_eam_gpu.h
+++ src/GPU/pair_eam_gpu.h
@@ -5,7 +5,7 @@
 
    Copyright (2003) Sandia Corporation.  Under the terms of Contract
    DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
-   certain rights in this software.  This software is distributed under
+   certain rights in this software.  This software is distributed under 
    the GNU General Public License.
 
    See the README file in the top-level LAMMPS directory.
@@ -32,6 +32,7 @@ class PairEAMGPU : public PairEAM {
   virtual ~PairEAMGPU();
   void compute(int, int);
   void init_style();
+  double single(int, int, int, int, double, double, double, double &);
   double memory_usage();
 
   int pack_comm(int, int *, double *, int, int *);
@@ -44,7 +45,7 @@ class PairEAMGPU : public PairEAM {
   double cpu_time;
   int *gpulist;
   void *fp_pinned;
-  bool fp_single;
+  bool fp_single;  
 };
 
 }

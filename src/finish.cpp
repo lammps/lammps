adc385d450ef5daa4d60d894ba001c763fad21b6
pscrozi <pscrozi@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2012-08-02 17:12:44 +0000

    Need more samples in order to get a non-zero timing for kspace FFT flops calculation. Boosting from 5 to 1000.
    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@8523 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/finish.cpp
+++ src/finish.cpp
@@ -429,7 +429,7 @@ void Finish::end(int flag)
 
     int nsteps = update->nsteps;
 
-    int nsample = 5;
+    int nsample = 1000;
     double time3d,time1d;
     force->kspace->timing(nsample,time3d,time1d);
 

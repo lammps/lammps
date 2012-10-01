a7f5b0ab8e2456431998e55b415bcaf4a540522f
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2012-10-01 15:25:00 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@8818 f3b2605a-c512-4ea7-a41b-209d697bcdaa

---
+++ src/pair_coul_dsf.h
@@ -0,0 +1,62 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under 
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef PAIR_CLASS
+
+PairStyle(coul/dsf,PairCoulDSF)
+
+#else
+
+#ifndef LMP_PAIR_COUL_DSF_H
+#define LMP_PAIR_COUL_DSF_H
+
+#include "pair.h"
+
+namespace LAMMPS_NS {
+
+class PairCoulDSF : public Pair {
+ public:
+  PairCoulDSF(class LAMMPS *);
+  ~PairCoulDSF();
+  void compute(int, int);
+  void settings(int, char **);
+  void coeff(int, char **);
+  void init_style();
+  double init_one(int, int);
+  void write_restart(FILE *);
+  void read_restart(FILE *);
+  void write_restart_settings(FILE *);
+  void read_restart_settings(FILE *);
+  double single(int, int, int, int, double, double, double, double &);
+  void *extract(char *, int &);
+
+ protected:
+  double cut_coul,cut_coulsq;
+  double alpha;
+  double f_shift,e_shift;
+  
+  void allocate();
+};
+
+}
+
+#endif
+#endif
+
+/* ERROR/WARNING messages:
+
+E: Pair style coul/dsf requires atom attribute q
+
+The atom style defined does not have this attribute.
+
+*/

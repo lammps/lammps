6984d53442fe8f758b224e15da13e42e79d07105
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2012-10-12 15:40:45 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@8950 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/finish.cpp
+++ src/finish.cpp
@@ -434,7 +434,7 @@ void Finish::end(int flag)
     int nsample = 1;
     int nfft = force->kspace->timing_3d(nsample,time3d);
     while (time3d < 1.0) {
-      nsample *= 5;
+      nsample *= 2;
       nfft = force->kspace->timing_3d(nsample,time3d);
     }
 
@@ -446,7 +446,7 @@ void Finish::end(int flag)
     nsample = 1;
     nfft = force->kspace->timing_1d(nsample,time1d);
     while (time1d < 1.0) {
-      nsample *= 5;
+      nsample *= 2;
       nfft = force->kspace->timing_1d(nsample,time1d);
     }
     

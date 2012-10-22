be1593f66e1b04fc8233c36118ff29922ccb51b1
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2012-10-22 14:31:31 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@8987 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- tools/moltemplate/examples/all_atom_examples/convert_LAMMPS_to_LT_examples/cnad-cnt/README_run_lammps.sh
+++
@@ -1,9 +0,0 @@
-
-LAMMPS_BINARY="lmp_ubuntu"  # change this if your binary has a different name
-
-# To verify that the files created by ltemplify.py and moltemplate.sh are
-# valid LAMMPS files, you can start a short simulation with them this way:
-$LAMMPS_BINARY -i run.in.nvt
-
-# NOTE: BECAUSE ALL OF THE ORIGINAL FORCE FIELD PARAMETERS WERE INTENTIONALLY
-#       REMOVED, THE SYSTEM WILL MOVE IN A VERY UNREALISTIC WAY.

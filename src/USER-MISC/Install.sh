8bd8b77489bd8f3475a6361f0cf9a424ed0de4d3
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2012-10-26 23:01:35 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@9013 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/USER-MISC/Install.sh
+++ src/USER-MISC/Install.sh
@@ -10,12 +10,19 @@ if (test $1 = 1) then
   cp angle_cosine_shift.cpp ..
   cp angle_cosine_shift_exp.cpp ..
   cp angle_dipole.cpp ..
+  cp angle_fourier.cpp ..
+  cp angle_fourier_simple.cpp ..
+  cp angle_quartic.cpp ..
   cp bond_harmonic_shift.cpp ..
   cp bond_harmonic_shift_cut.cpp ..
   cp compute_ackland_atom.cpp ..
   cp compute_temp_rotate.cpp ..
   cp dihedral_cosine_shift_exp.cpp ..
+  cp dihedral_fourier.cpp ..
+  cp dihedral_nharmonic.cpp ..
+  cp dihedral_quadratic.cpp ..
   cp dihedral_table.cpp ..
+  cp improper_fourier.cpp ..
   cp fix_addtorque.cpp ..
   cp fix_imd.cpp ..
   cp fix_smd.cpp ..
@@ -32,12 +39,19 @@ if (test $1 = 1) then
   cp angle_cosine_shift.h ..
   cp angle_cosine_shift_exp.h ..
   cp angle_dipole.h ..
+  cp angle_fourier.h ..
+  cp angle_fourier_simple.h ..
+  cp angle_quartic.h ..
   cp bond_harmonic_shift.h ..
   cp bond_harmonic_shift_cut.h ..
   cp compute_ackland_atom.h ..
   cp compute_temp_rotate.h ..
   cp dihedral_cosine_shift_exp.h ..
+  cp dihedral_fourier.h ..
+  cp dihedral_nharmonic.h ..
+  cp dihedral_quadratic.h ..
   cp dihedral_table.h ..
+  cp improper_fourier.h ..
   cp fix_addtorque.h ..
   cp fix_imd.h ..
   cp fix_smd.h ..
@@ -55,13 +69,20 @@ elif (test $1 = 0) then
 
   rm -f ../angle_cosine_shift.cpp
   rm -f ../angle_cosine_shift_exp.cpp
+  rm -f ../angle_fourier.cpp
+  rm -f ../angle_fourier_simple.cpp
   rm -f ../angle_dipole.cpp
+  rm -f ../angle_quartic.cpp
   rm -f ../bond_harmonic_shift.cpp
   rm -f ../bond_harmonic_shift_cut.cpp
   rm -f ../compute_ackland_atom.cpp
   rm -f ../compute_temp_rotate.cpp
   rm -f ../dihedral_cosine_shift_exp.cpp
+  rm -f ../dihedral_fourier.cpp
+  rm -f ../dihedral_nharmonic.cpp
+  rm -f ../dihedral_quadratic.cpp
   rm -f ../dihedral_table.cpp
+  rm -f ../improper_fourier.cpp
   rm -f ../fix_addtorque.cpp
   rm -f ../fix_imd.cpp
   rm -f ../fix_smd.cpp
@@ -79,12 +100,19 @@ elif (test $1 = 0) then
   rm -f ../angle_cosine_shift.h
   rm -f ../angle_cosine_shift_exp.h
   rm -f ../angle_dipole.h
+  rm -f ../angle_fourier.h
+  rm -f ../angle_fourier_simple.h
+  rm -f ../angle_quartic.h
   rm -f ../bond_harmonic_shift.h
   rm -f ../bond_harmonic_shift_cut.h
   rm -f ../compute_ackland_atom.h
   rm -f ../compute_temp_rotate.h
   rm -f ../dihedral_cosine_shift_exp.h
+  rm -f ../dihedral_fourier.h
+  rm -f ../dihedral_nharmonic.h
+  rm -f ../dihedral_quadratic.h
   rm -f ../dihedral_table.h
+  rm -f ../improper_fourier.h
   rm -f ../fix_addtorque.h
   rm -f ../fix_imd.h
   rm -f ../fix_smd.h
---
+++ src/USER-MISC/improper_fourier.h
@@ -0,0 +1,51 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under 
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef IMPROPER_CLASS
+
+ImproperStyle(fourier,ImproperFourier)
+
+#else
+
+#ifndef LMP_IMPROPER_FOURIER_H
+#define LMP_IMPROPER_FOURIER_H
+
+#include "stdio.h"
+#include "improper.h"
+
+namespace LAMMPS_NS {
+
+class ImproperFourier : public Improper {
+ public:
+  ImproperFourier(class LAMMPS *);
+  ~ImproperFourier();
+  void compute(int, int);
+  void coeff(int, char **);
+  void write_restart(FILE *);
+  void read_restart(FILE *);
+
+ protected:
+  double *k, *C0, *C1, *C2;
+  int *all;
+  void addone(const int &i1,const int &i2,const int &i3,const int &i4,
+              const int &type,const int &evflag,const int &eflag,
+              const double &vb1x, const double &vb1y, const double &vb1z,
+              const double &vb2x, const double &vb2y, const double &vb2z,
+              const double &vb3x, const double &vb3y, const double &vb3z);
+  void allocate();
+};
+
+}
+
+#endif
+#endif

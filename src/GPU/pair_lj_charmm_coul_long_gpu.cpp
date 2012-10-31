05a02426a98e6156cbc732f78d6b665ca7ad2b2f
pscrozi <pscrozi@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2012-10-31 15:55:27 +0000

    Updated GPU package to correspond with coulomb table changes.
    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@9035 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/GPU/pair_lj_charmm_coul_long_gpu.cpp
+++ src/GPU/pair_lj_charmm_coul_long_gpu.cpp
@@ -175,7 +175,7 @@ void PairLJCharmmCoulLongGPU::init_style()
 
   // setup force tables
 
-  if (ncoultablebits) init_tables();
+  if (ncoultablebits) init_tables(cut_coul,cut_respa);
 
   int maxspecial=0;
   if (atom->molecular)

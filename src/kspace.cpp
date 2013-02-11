dc2259535b119540880e060363d02b2669f9d3cc
Axel Kohlmeyer <akohlmey@gmail.com>
2013-02-11 22:15:15 +0100

    make fft benchmark test optional

--- src/kspace.cpp
+++ src/kspace.cpp
@@ -41,6 +41,7 @@ KSpace::KSpace(LAMMPS *lmp, int narg, char **arg) : Pointers(lmp)
   gewaldflag = 0;
   minorder = 2;
   overlap_allowed = 1;
+  fftbench = 1;
 
   order_6 = 5;
   gridflag_6 = 0;
@@ -318,6 +319,12 @@ void KSpace::modify_params(int narg, char **arg)
       if (iarg+2 > narg) error->all(FLERR,"Illegal kspace_modify command");
       if (strcmp(arg[iarg+1],"yes") == 0) compute_flag = 1;
       else if (strcmp(arg[iarg+1],"no") == 0) compute_flag = 0;
+      else error->all(FLERR,"Illegal kspace_modify command");
+      iarg += 2;
+    } else if (strcmp(arg[iarg],"fftbench") == 0) {
+      if (iarg+2 > narg) error->all(FLERR,"Illegal kspace_modify command");
+      if (strcmp(arg[iarg+1],"yes") == 0) fftbench = 1;
+      else if (strcmp(arg[iarg+1],"no") == 0) fftbench = 0;
       else error->all(FLERR,"Illegal kspace_modify command");
       iarg += 2;
     } else if (strcmp(arg[iarg],"diff") == 0) {

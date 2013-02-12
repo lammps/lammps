2495f08cb24887a17e9b441fbe23449d9b3ed10a
Axel Kohlmeyer <akohlmey@gmail.com>
2013-02-12 14:06:51 +0100

    minor cleanups

--- src/USER-OMP/pppm_omp.cpp
+++ src/USER-OMP/pppm_omp.cpp
@@ -36,10 +36,8 @@ using namespace MathSpecial;
 
 #ifdef FFT_SINGLE
 #define ZEROF 0.0f
-#define ONEF  1.0f
 #else
 #define ZEROF 0.0
-#define ONEF  1.0
 #endif
 
 #define EPS_HOC 1.0e-7
@@ -157,35 +155,35 @@ void PPPMOMP::compute_gf_ik()
       sqk = square(unitkx*kper) + square(unitky*lper) + square(unitkz*mper);
 
       if (sqk != 0.0) {
-	numerator = 12.5663706/sqk;
-	denominator = gf_denom(snx,sny,snz);
-	sum1 = 0.0;
+        numerator = 12.5663706/sqk;
+        denominator = gf_denom(snx,sny,snz);
+        sum1 = 0.0;
 
-	for (nx = -nbx; nx <= nbx; nx++) {
-	  qx = unitkx*(kper+nx_pppm*nx);
-	  sx = exp(-0.25*square(qx/g_ewald));
-	  argx = 0.5*qx*xprd/nx_pppm;
-	  wx = powsinxx(argx,twoorder);
+        for (nx = -nbx; nx <= nbx; nx++) {
+          qx = unitkx*(kper+nx_pppm*nx);
+          sx = exp(-0.25*square(qx/g_ewald));
+          argx = 0.5*qx*xprd/nx_pppm;
+          wx = powsinxx(argx,twoorder);
 
-	  for (ny = -nby; ny <= nby; ny++) {
-	    qy = unitky*(lper+ny_pppm*ny);
-	    sy = exp(-0.25*square(qy/g_ewald));
-	    argy = 0.5*qy*yprd/ny_pppm;
-	    wy = powsinxx(argy,twoorder);
+          for (ny = -nby; ny <= nby; ny++) {
+            qy = unitky*(lper+ny_pppm*ny);
+            sy = exp(-0.25*square(qy/g_ewald));
+            argy = 0.5*qy*yprd/ny_pppm;
+            wy = powsinxx(argy,twoorder);
 
-	    for (nz = -nbz; nz <= nbz; nz++) {
-	      qz = unitkz*(mper+nz_pppm*nz);
-	      sz = exp(-0.25*square(qz/g_ewald));
-	      argz = 0.5*qz*zprd_slab/nz_pppm;
-	      wz = powsinxx(argz,twoorder);
+            for (nz = -nbz; nz <= nbz; nz++) {
+              qz = unitkz*(mper+nz_pppm*nz);
+              sz = exp(-0.25*square(qz/g_ewald));
+              argz = 0.5*qz*zprd_slab/nz_pppm;
+              wz = powsinxx(argz,twoorder);
 
-	      dot1 = unitkx*kper*qx + unitky*lper*qy + unitkz*mper*qz;
-	      dot2 = qx*qx+qy*qy+qz*qz;
-	      sum1 += (dot1/dot2) * sx*sy*sz * wx*wy*wz;
-	    }
-	  }
-	}
-	greensfn[n] = numerator*sum1/denominator;
+              dot1 = unitkx*kper*qx + unitky*lper*qy + unitkz*mper*qz;
+              dot2 = qx*qx+qy*qy+qz*qz;
+              sum1 += (dot1/dot2) * sx*sy*sz * wx*wy*wz;
+            }
+          }
+        }
+        greensfn[n] = numerator*sum1/denominator;
       } else greensfn[n] = 0.0;
     }
   } // end of parallel region
@@ -258,23 +256,23 @@ void PPPMOMP::compute_gf_ad()
       sqk = qx*qx + qy*qy + qz*qz;
 
       if (sqk != 0.0) {
-	numerator = MY_4PI/sqk;
-	denominator = gf_denom(snx,sny,snz);
-	greensfn[n] = numerator*sx*sy*sz*wx*wy*wz/denominator;
-	sf0 += sf_precoeff1[n]*greensfn[n];
-	sf1 += sf_precoeff2[n]*greensfn[n];
-	sf2 += sf_precoeff3[n]*greensfn[n];
-	sf3 += sf_precoeff4[n]*greensfn[n];
-	sf4 += sf_precoeff5[n]*greensfn[n];
-	sf5 += sf_precoeff6[n]*greensfn[n];
+        numerator = MY_4PI/sqk;
+        denominator = gf_denom(snx,sny,snz);
+        greensfn[n] = numerator*sx*sy*sz*wx*wy*wz/denominator;
+        sf0 += sf_precoeff1[n]*greensfn[n];
+        sf1 += sf_precoeff2[n]*greensfn[n];
+        sf2 += sf_precoeff3[n]*greensfn[n];
+        sf3 += sf_precoeff4[n]*greensfn[n];
+        sf4 += sf_precoeff5[n]*greensfn[n];
+        sf5 += sf_precoeff6[n]*greensfn[n];
       } else {
-	greensfn[n] = 0.0;
-	sf0 += sf_precoeff1[n]*greensfn[n];
-	sf1 += sf_precoeff2[n]*greensfn[n];
-	sf2 += sf_precoeff3[n]*greensfn[n];
-	sf3 += sf_precoeff4[n]*greensfn[n];
-	sf4 += sf_precoeff5[n]*greensfn[n];
-	sf5 += sf_precoeff6[n]*greensfn[n];
+        greensfn[n] = 0.0;
+        sf0 += sf_precoeff1[n]*greensfn[n];
+        sf1 += sf_precoeff2[n]*greensfn[n];
+        sf2 += sf_precoeff3[n]*greensfn[n];
+        sf3 += sf_precoeff4[n]*greensfn[n];
+        sf4 += sf_precoeff5[n]*greensfn[n];
+        sf5 += sf_precoeff6[n]*greensfn[n];
       }
     }
   } // end of paralle region
@@ -337,7 +335,13 @@ void PPPMOMP::fieldforce_ik()
 
   const dbl3_t * _noalias const x = (dbl3_t *) atom->x[0];
   const double * _noalias const q = atom->q;
+  const int3_t * _noalias const p2g = (int3_t *) part2grid[0];
+
   const double qqrd2e = force->qqrd2e;
+  const double boxlox = boxlo[0];
+  const double boxloy = boxlo[1];
+  const double boxloz = boxlo[2];
+
   const int nthreads = comm->nthreads;
   const int nlocal = atom->nlocal;
 
@@ -345,8 +349,8 @@ void PPPMOMP::fieldforce_ik()
 #pragma omp parallel default(none)
 #endif
   {
-    FFT_SCALAR dx,dy,dz,x0,y0,z0,ekx,eky,ekz;
-    int i,ifrom,ito,tid,l,m,n,nx,ny,nz,mx,my,mz;
+    FFT_SCALAR x0,y0,z0,ekx,eky,ekz;
+    int i,ifrom,ito,tid,l,m,n,mx,my,mz;
 
     loop_setup_thr(ifrom,ito,tid,nlocal,nthreads);
 
@@ -356,30 +360,30 @@ void PPPMOMP::fieldforce_ik()
     FFT_SCALAR * const * const r1d = static_cast<FFT_SCALAR **>(thr->get_rho1d());
 
     for (i = ifrom; i < ito; ++i) {
-      nx = part2grid[i][0];
-      ny = part2grid[i][1];
-      nz = part2grid[i][2];
-      dx = nx+shiftone - (x[i].x-boxlo[0])*delxinv;
-      dy = ny+shiftone - (x[i].y-boxlo[1])*delyinv;
-      dz = nz+shiftone - (x[i].z-boxlo[2])*delzinv;
+      const int nx = p2g[i].a;
+      const int ny = p2g[i].b;
+      const int nz = p2g[i].t;
+      const FFT_SCALAR dx = nx+shiftone - (x[i].x-boxlox)*delxinv;
+      const FFT_SCALAR dy = ny+shiftone - (x[i].y-boxloy)*delyinv;
+      const FFT_SCALAR dz = nz+shiftone - (x[i].z-boxloz)*delzinv;
 
       compute_rho1d_thr(r1d,dx,dy,dz);
 
       ekx = eky = ekz = ZEROF;
       for (n = nlower; n <= nupper; n++) {
-	mz = n+nz;
-	z0 = r1d[2][n];
-	for (m = nlower; m <= nupper; m++) {
-	  my = m+ny;
-	  y0 = z0*r1d[1][m];
-	  for (l = nlower; l <= nupper; l++) {
-	    mx = l+nx;
-	    x0 = y0*r1d[0][l];
-	    ekx -= x0*vdx_brick[mz][my][mx];
-	    eky -= x0*vdy_brick[mz][my][mx];
-	    ekz -= x0*vdz_brick[mz][my][mx];
-	  }
-	}
+        mz = n+nz;
+        z0 = r1d[2][n];
+        for (m = nlower; m <= nupper; m++) {
+          my = m+ny;
+          y0 = z0*r1d[1][m];
+          for (l = nlower; l <= nupper; l++) {
+            mx = l+nx;
+            x0 = y0*r1d[0][l];
+            ekx -= x0*vdx_brick[mz][my][mx];
+            eky -= x0*vdy_brick[mz][my][mx];
+            ekz -= x0*vdz_brick[mz][my][mx];
+          }
+        }
       }
 
       // convert E-field to force
@@ -411,7 +415,12 @@ void PPPMOMP::fieldforce_ad()
 
   const dbl3_t * _noalias const x = (dbl3_t *) atom->x[0];
   const double * _noalias const q = atom->q;
+  const int3_t * _noalias const p2g = (int3_t *) part2grid[0];
   const double qqrd2e = force->qqrd2e;
+  const double boxlox = boxlo[0];
+  const double boxloy = boxlo[1];
+  const double boxloz = boxlo[2];
+
   const int nthreads = comm->nthreads;
   const int nlocal = atom->nlocal;
 
@@ -419,9 +428,9 @@ void PPPMOMP::fieldforce_ad()
 #pragma omp parallel default(none)
 #endif
   {
-    int i,ifrom,ito,tid,l,m,n,nx,ny,nz,mx,my,mz;
-    FFT_SCALAR dx,dy,dz,ekx,eky,ekz;
     double s1,s2,s3,sf;
+    FFT_SCALAR ekx,eky,ekz;
+    int i,ifrom,ito,tid,l,m,n,mx,my,mz;
 
     loop_setup_thr(ifrom,ito,tid,nlocal,nthreads);
 
@@ -432,28 +441,28 @@ void PPPMOMP::fieldforce_ad()
     FFT_SCALAR * const * const d1d = static_cast<FFT_SCALAR **>(thr->get_drho1d());
 
     for (i = ifrom; i < ito; ++i) {
-      nx = part2grid[i][0];
-      ny = part2grid[i][1];
-      nz = part2grid[i][2];
-      dx = nx+shiftone - (x[i].x-boxlo[0])*delxinv;
-      dy = ny+shiftone - (x[i].y-boxlo[1])*delyinv;
-      dz = nz+shiftone - (x[i].z-boxlo[2])*delzinv;
+      const int nx = p2g[i].a;
+      const int ny = p2g[i].b;
+      const int nz = p2g[i].t;
+      const FFT_SCALAR dx = nx+shiftone - (x[i].x-boxlox)*delxinv;
+      const FFT_SCALAR dy = ny+shiftone - (x[i].y-boxloy)*delyinv;
+      const FFT_SCALAR dz = nz+shiftone - (x[i].z-boxloz)*delzinv;
 
       compute_rho1d_thr(r1d,dx,dy,dz);
       compute_drho1d_thr(d1d,dx,dy,dz);
 
       ekx = eky = ekz = ZEROF;
       for (n = nlower; n <= nupper; n++) {
-	mz = n+nz;
-	for (m = nlower; m <= nupper; m++) {
-	  my = m+ny;
-	  for (l = nlower; l <= nupper; l++) {
-	    mx = l+nx;
-	    ekx += d1d[0][l]*r1d[1][m]*r1d[2][n]*u_brick[mz][my][mx];
-	    eky += r1d[0][l]*d1d[1][m]*r1d[2][n]*u_brick[mz][my][mx];
-	    ekz += r1d[0][l]*r1d[1][m]*d1d[2][n]*u_brick[mz][my][mx];
-	  }
-	}
+        mz = n+nz;
+        for (m = nlower; m <= nupper; m++) {
+          my = m+ny;
+          for (l = nlower; l <= nupper; l++) {
+            mx = l+nx;
+            ekx += d1d[0][l]*r1d[1][m]*r1d[2][n]*u_brick[mz][my][mx];
+            eky += r1d[0][l]*d1d[1][m]*r1d[2][n]*u_brick[mz][my][mx];
+            ekz += r1d[0][l]*r1d[1][m]*d1d[2][n]*u_brick[mz][my][mx];
+          }
+        }
       }
       ekx *= hx_inv;
       eky *= hy_inv;
@@ -473,13 +482,13 @@ void PPPMOMP::fieldforce_ad()
       s2 = x[i].y*hy_inv;
       sf = sf_coeff[2]*sin(MY_2PI*s2);
       sf += sf_coeff[3]*sin(MY_4PI*s2);
-      sf *= 2*qi;
+      sf *= 2.0*qi;
       f[i].y += qfactor*(eky - sf);
 
       s3 = x[i].z*hz_inv;
       sf = sf_coeff[4]*sin(MY_2PI*s3);
       sf += sf_coeff[5]*sin(MY_4PI*s3);
-      sf *= 2*qi;
+      sf *= 2.0*qi;
       if (slabflag != 2) f[i].z += qfactor*(ekz - sf);
     }
   } // end of parallel region
@@ -527,36 +536,36 @@ void PPPMOMP::fieldforce_peratom()
 
       u = v0 = v1 = v2 = v3 = v4 = v5 = ZEROF;
       for (n = nlower; n <= nupper; n++) {
-	mz = n+nz;
-	z0 = r1d[2][n];
-	for (m = nlower; m <= nupper; m++) {
-	  my = m+ny;
-	  y0 = z0*r1d[1][m];
-	  for (l = nlower; l <= nupper; l++) {
-	    mx = l+nx;
-	    x0 = y0*r1d[0][l];
-	    if (eflag_atom) u += x0*u_brick[mz][my][mx];
-	    if (vflag_atom) {
-	      v0 += x0*v0_brick[mz][my][mx];
-	      v1 += x0*v1_brick[mz][my][mx];
-	      v2 += x0*v2_brick[mz][my][mx];
-	      v3 += x0*v3_brick[mz][my][mx];
-	      v4 += x0*v4_brick[mz][my][mx];
-	      v5 += x0*v5_brick[mz][my][mx];
-	    }
-	  }
-	}
+        mz = n+nz;
+        z0 = r1d[2][n];
+        for (m = nlower; m <= nupper; m++) {
+          my = m+ny;
+          y0 = z0*r1d[1][m];
+          for (l = nlower; l <= nupper; l++) {
+            mx = l+nx;
+            x0 = y0*r1d[0][l];
+            if (eflag_atom) u += x0*u_brick[mz][my][mx];
+            if (vflag_atom) {
+              v0 += x0*v0_brick[mz][my][mx];
+              v1 += x0*v1_brick[mz][my][mx];
+              v2 += x0*v2_brick[mz][my][mx];
+              v3 += x0*v3_brick[mz][my][mx];
+              v4 += x0*v4_brick[mz][my][mx];
+              v5 += x0*v5_brick[mz][my][mx];
+            }
+          }
+        }
       }
 
       const double qi = q[i];
       if (eflag_atom) eatom[i] += qi*u;
       if (vflag_atom) {
-	vatom[i][0] += qi*v0;
-	vatom[i][1] += qi*v1;
-	vatom[i][2] += qi*v2;
-	vatom[i][3] += qi*v3;
-	vatom[i][4] += qi*v4;
-	vatom[i][5] += qi*v5;
+        vatom[i][0] += qi*v0;
+        vatom[i][1] += qi*v1;
+        vatom[i][2] += qi*v2;
+        vatom[i][3] += qi*v3;
+        vatom[i][4] += qi*v4;
+        vatom[i][5] += qi*v5;
       }
     }
   } // end of parallel region

00be9561e711ba6148aab2a8d9385899b92cd7ad
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2013-02-15 17:36:53 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@9512 f3b2605a-c512-4ea7-a41b-209d697bcdaa

---
+++ src/USER-OMP/fix_nphug_omp.h
@@ -0,0 +1,98 @@
+/* -*- c++ -*- ----------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef FIX_CLASS
+
+FixStyle(nphug/omp,FixNPHugOMP)
+
+#else
+
+#ifndef LMP_FIX_NPHUG_OMP_H
+#define LMP_FIX_NPHUG_OMP_H
+
+#include "fix_nh_omp.h"
+
+namespace LAMMPS_NS {
+
+class FixNPHugOMP : public FixNHOMP {
+ public:
+  FixNPHugOMP(class LAMMPS *, int, char **);
+  virtual ~FixNPHugOMP();
+
+  virtual void init();
+  virtual void setup(int);
+  virtual int modify_param(int, char **);
+  virtual int pack_restart_data(double *); // pack restart data
+  virtual void restart(char *);
+
+ private:
+  class Compute *pe;               // PE compute pointer
+
+  void compute_temp_target();
+  double compute_vector(int);
+  double compute_etotal();
+  double compute_vol();
+  double compute_hugoniot();
+  double compute_us();
+  double compute_up();
+
+  char *id_pe;
+  int peflag;
+  int v0_set,p0_set,e0_set;
+  double v0,p0,e0,rho0;
+  int idir;
+  int uniaxial;
+
+  int size_restart_global();
+};
+
+}
+
+#endif
+#endif
+
+/* ERROR/WARNING messages:
+
+E: Pstart and Pstop must have the same value
+
+Self-explanatory.
+
+E: Specified target stress must be uniaxial or hydrostatic
+
+Self-explanatory.
+
+E: For triclinic deformation, specified target stress must be hydrostatic
+
+Triclinic pressure control is allowed using the tri keyword, but
+non-hydrostatic pressure control can not be used in this case.
+
+E: Temperature control must be used with fix nphug
+
+The temp keyword must be provided.
+
+E: Pressure control must be used with fix nphug
+
+A pressure control keyword (iso, aniso, tri, x, y, or z) must be
+provided.
+
+E: Potential energy ID for fix nvt/nph/npt does not exist
+
+A compute for potential energy must be defined.
+
+E: Illegal ... command
+
+Self-explanatory.  Check the input script syntax and compare to the
+documentation for the command.  You can use -echo screen as a
+command-line option when running LAMMPS to see the offending line.
+
+*/

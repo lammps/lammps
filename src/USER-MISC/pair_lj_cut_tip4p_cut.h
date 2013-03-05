4be41208c17cb6884d04019eca209c77be911f1e
Axel Kohlmeyer <akohlmey@gmail.com>
2013-03-05 11:02:00 +0100

    add /omp version of lj/cut/tip4p/cut

--- src/USER-MISC/pair_lj_cut_tip4p_cut.h
+++ src/USER-MISC/pair_lj_cut_tip4p_cut.h
@@ -1,3 +1,16 @@
+/* -*- c++ -*- ----------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
 #ifdef PAIR_CLASS
 
 PairStyle(lj/cut/tip4p/cut,PairLJCutTIP4PCut)
@@ -15,14 +28,17 @@ class PairLJCutTIP4PCut : public Pair {
 
  public:
   PairLJCutTIP4PCut(class LAMMPS *);
- ~PairLJCutTIP4PCut();
-  void compute(int, int);
+  virtual ~PairLJCutTIP4PCut();
+
+  virtual void compute(int, int);
+
   void settings(int, char **);
   void coeff(int, char **);
   void init_style();
   double init_one(int, int);
   void write_restart(FILE *);
   void read_restart(FILE *);
+
   virtual void write_restart_settings(FILE *);
   virtual void read_restart_settings(FILE *);
 

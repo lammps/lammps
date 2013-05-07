b7da4d691ce90e1602276571268a5aecebb06245
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2013-05-07 21:14:00 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@9808 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- tools/moltemplate/examples/CG_protein_folding_examples/1beadProtSci2010/moltemplate_files/README.sh
+++
@@ -1,7 +0,0 @@
-# run moltemplate this way
-
-moltemplate.sh -overlay-dihdedrals system.lt
-
-# This will generate various files with names ending in *.in* and *.data
-# which are needed by LAMMPS.
-

3b9e00c73106a1e6f71130addb1ed49ae5c2de65
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2013-05-30 16:21:44 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@9936 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/KIM/Install.sh
+++ src/KIM/Install.sh
@@ -1,4 +1,35 @@
 # Install/unInstall package files in LAMMPS
+
+# Install/unInstall package files in LAMMPS
+# mode = 0/1/2 for uninstall/install/update
+
+mode=$1
+
+# arg1 = file, arg2 = file it depends on
+
+action () {
+  if (test $mode = 0) then
+    rm -f ../$1
+  elif (! cmp -s $1 ../$1) then
+    if (test -z "$2" || test -e ../$2) then
+      cp $1 ..
+      if (test $mode = 2) then
+        echo "  updating src/$1"
+      fi
+    fi
+  elif (test ! -z "$2") then
+    if (test ! -e ../$2) then
+      rm -f ../$1
+    fi
+  fi
+}
+
+# all package files with no dependencies
+
+for file in *.cpp *.h; do
+  action $file
+done
+
 # edit 2 Makefile.package files to include/exclude package info
 
 if (test $1 = 1) then

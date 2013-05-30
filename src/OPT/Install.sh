3f142bb498cf65d58e64f55a515ed7e4985e3910
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2013-05-30 16:27:43 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@9938 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/OPT/Install.sh
+++ src/OPT/Install.sh
@@ -1,54 +1,44 @@
 # Install/unInstall package files in LAMMPS
-# do not install child files if parent does not exist
+# mode = 0/1/2 for uninstall/install/update
 
-if (test $1 = 1) then
+mode=$1
 
-  if (test -e ../pair_eam.cpp) then
-    cp pair_eam_opt.cpp ..
-    cp pair_eam_alloy_opt.cpp ..
-    cp pair_eam_fs_opt.cpp ..
-    cp pair_eam_opt.h ..
-    cp pair_eam_alloy_opt.h ..
-    cp pair_eam_fs_opt.h ..
+# arg1 = file, arg2 = file it depends on
+
+action () {
+  if (test $mode = 0) then
+    rm -f ../$1
+  elif (! cmp -s $1 ../$1) then
+    if (test -z "$2" || test -e ../$2) then
+      cp $1 ..
+      if (test $mode = 2) then
+        echo "  updating src/$1"
+      fi
+    fi
+  elif (test ! -z "$2") then
+    if (test ! -e ../$2) then
+      rm -f ../$1
+    fi
   fi
+}
 
-  if (test -e ../pair_lj_charmm_coul_long.cpp) then
-    cp pair_lj_charmm_coul_long_opt.cpp ..
-    cp pair_lj_charmm_coul_long_opt.h ..
-    cp pair_lj_cut_coul_long_opt.cpp ..
-    cp pair_lj_cut_coul_long_opt.h ..
-    cp pair_lj_cut_tip4p_long_opt.cpp ..
-    cp pair_lj_cut_tip4p_long_opt.h ..
-    cp pair_lj_long_coul_long_opt.cpp ..
-    cp pair_lj_long_coul_long_opt.h ..
-  fi
+# list of files with optional dependcies
 
-  cp pair_lj_cut_opt.cpp ..
-  cp pair_lj_cut_opt.h ..
-
-  cp pair_morse_opt.cpp ..
-  cp pair_morse_opt.h ..
-
-elif (test $1 = 0) then
-
-  rm -f ../pair_eam_opt.cpp
-  rm -f ../pair_eam_alloy_opt.cpp
-  rm -f ../pair_eam_fs_opt.cpp
-  rm -f ../pair_lj_charmm_coul_long_opt.cpp
-  rm -f ../pair_lj_cut_coul_long_opt.cpp
-  rm -f ../pair_lj_cut_tip4p_long_opt.cpp
-  rm -f ../pair_lj_cut_opt.cpp
-  rm -f ../pair_lj_long_coul_long_opt.cpp
-  rm -f ../pair_morse_opt.cpp
-
-  rm -f ../pair_eam_opt.h
-  rm -f ../pair_eam_alloy_opt.h
-  rm -f ../pair_eam_fs_opt.h
-  rm -f ../pair_lj_charmm_coul_long_opt.h
-  rm -f ../pair_lj_cut_coul_long_opt.h
-  rm -f ../pair_lj_cut_tip4p_long_opt.h
-  rm -f ../pair_lj_cut_opt.h
-  rm -f ../pair_lj_long_coul_long_opt.h
-  rm -f ../pair_morse_opt.h
-
-fi
+action pair_eam_alloy_opt.cpp pair_eam_alloy.cpp
+action pair_eam_alloy_opt.h pair_eam_alloy.cpp
+action pair_eam_fs_opt.cpp pair_eam_fs.cpp
+action pair_eam_fs_opt.h pair_eam_fs.cpp
+action pair_eam_opt.cpp pair_eam.cpp
+action pair_eam_opt.h pair_eam.cpp
+action pair_lj_charmm_coul_long_opt.cpp pair_lj_charmm_coul_long.cpp
+action pair_lj_charmm_coul_long_opt.h pair_lj_charmm_coul_long.cpp
+action pair_lj_cut_coul_long_opt.cpp pair_lj_cut_coul_long.cpp
+action pair_lj_cut_coul_long_opt.h pair_lj_cut_coul_long.cpp
+action pair_lj_cut_opt.cpp
+action pair_lj_cut_opt.h
+action pair_lj_cut_tip4p_long_opt.cpp pair_lj_cut_tip4p_long.cpp
+action pair_lj_cut_tip4p_long_opt.h pair_lj_cut_tip3p_long.cpp 
+action pair_lj_long_coul_long_opt.cpp pair_lj_long_coul_long.cpp
+action pair_lj_long_coul_long_opt.h pair_lj_long_coul_long.cpp
+action pair_morse_opt.cpp
+action pair_morse_opt.h

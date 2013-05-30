9683c210d602bc144280a698971f57aa7b1abb72
Axel Kohlmeyer <akohlmey@gmail.com>
2013-05-30 11:42:20 +0200

    bugfix and better readability

--- src/USER-CG-CMM/Install.sh
+++ src/USER-CG-CMM/Install.sh
@@ -20,27 +20,27 @@ action () {
 #      package file               dependency (optional)
 action angle_cg_cmm.h             angle_harmonic.cpp
 action angle_cg_cmm.cpp           angle_harmonic.cpp
-
 action angle_sdk.h                angle_harmonic.cpp
 action angle_sdk.cpp              angle_harmonic.cpp
 
-action pair_lj_sdk_coul_long.cpp  pppm.cpp
+action pair_cg_cmm_coul_long.h    pppm.cpp
 action pair_cg_cmm_coul_long.cpp  pppm.cpp
 action pair_lj_sdk_coul_long.h    pppm.cpp
-action pair_cg_cmm_coul_long.h    pppm.cpp
+action pair_lj_sdk_coul_long.cpp  pppm.cpp
 
-action pair_lj_sdk_coul_msm.cpp   msm.cpp
 action pair_lj_sdk_coul_msm.h     msm.cpp
+action pair_lj_sdk_coul_msm.cpp   msm.cpp
 
 action cg_cmm_parms.h
 action cg_cmm_parms.cpp
 
 action pair_cmm_common.h
 action pair_cmm_common.cpp
-action pair_cg_cmm.cpp
 action pair_cg_cmm.h
+action pair_cg_cmm.cpp
+action pair_cg_cmm_coul_cut.h
 action pair_cg_cmm_coul_cut.cpp
 
-action pair_lj_sdk.cpp
-action pair_lj_sdk.h
 action lj_sdk_common.h
+action pair_lj_sdk.h
+action pair_lj_sdk.cpp

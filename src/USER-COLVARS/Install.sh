8623d964ade461d9aaffa2fa5f33f50ea2a86eb7
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2013-05-30 16:45:41 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@9948 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/USER-COLVARS/Install.sh
+++ src/USER-COLVARS/Install.sh
@@ -1,4 +1,33 @@
 # Install/unInstall package files in LAMMPS
+# mode = 0/1/2 for uninstall/install/update
+
+mode=$1
+
+# arg1 = file, arg2 = file it depends on
+
+action () {
+  if (test $mode = 0) then
+    rm -f ../$1
+  elif (! cmp -s $1 ../$1) then
+    if (test -z "$2" || test -e ../$2) then
+      cp $1 ..
+      if (test $mode = 2) then
+        echo "  updating src/$1"
+      fi
+    fi
+  elif (test ! -z "$2") then
+    if (test ! -e ../$2) then
+      rm -f ../$1
+    fi
+  fi
+}
+
+# all package files with no dependencies
+
+for file in *.cpp *.h; do
+  action $file
+done
+
 # edit 2 Makefile.package files to include/exclude package info
 
 if (test $1 = 1) then
@@ -22,11 +51,6 @@ include ..\/..\/lib\/colvars\/Makefile.lammps
 
   fi
 
-  cp colvarproxy_lammps.h ..
-  cp colvarproxy_lammps.cpp ..
-  cp fix_colvars.h ..
-  cp fix_colvars.cpp ..
-
 elif (test $1 = 0) then
 
   if (test -e ../Makefile.package) then
@@ -37,8 +61,4 @@ elif (test $1 = 0) then
     sed -i -e '/^include.*colvars.*$/d' ../Makefile.package.settings
   fi
 
-  rm -f ../colvarproxy_lammps.h
-  rm -f ../colvarproxy_lammps.cpp
-  rm -f ../fix_colvars.h
-  rm -f ../fix_colvars.cpp
 fi

c8283169c223c77ac5ae2b8bc4c4f9d39db0b026
Axel Kohlmeyer <akohlmey@gmail.com>
2013-06-09 18:49:16 +0200

    include python, bench and potentials in build distribution

--- extract-current.sh
+++ extract-current.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-git archive -v --format=tar --prefix=lammps-current/  HEAD  README LICENSE doc/Manual.pdf src lib tools/*.cpp tools/*.f | gzip -9c - > ../lammps-current.tar.gz
+git archive -v --format=tar --prefix=lammps-current/  HEAD  README LICENSE doc/Manual.pdf src lib python bench potentials tools/*.cpp tools/*.f | gzip -9c - > ../lammps-current.tar.gz
 

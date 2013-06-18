ab6a2a3d3083522c0f84ab4fa12b1a862ef9bfb7
Axel Kohlmeyer <akohlmey@gmail.com>
2013-06-19 00:54:06 +0200

    since we use unwrapped coordinates, we must not compute the minimum image unless specifically asked for it.

--- src/USER-COLVARS/colvarproxy_lammps.cpp
+++ src/USER-COLVARS/colvarproxy_lammps.cpp
@@ -201,7 +201,6 @@ cvm::rvector colvarproxy_lammps::position_distance(cvm::atom_pos const &pos1,
   double xtmp = pos2.x - pos1.x;
   double ytmp = pos2.y - pos1.y;
   double ztmp = pos2.z - pos1.z;
-  _lmp->domain->minimum_image(xtmp,ytmp,ztmp);
   return cvm::rvector(xtmp, ytmp, ztmp);
 }
 
@@ -211,7 +210,6 @@ cvm::real colvarproxy_lammps::position_dist2(cvm::atom_pos const &pos1,
   double xtmp = pos2.x - pos1.x;
   double ytmp = pos2.y - pos1.y;
   double ztmp = pos2.z - pos1.z;
-  _lmp->domain->minimum_image(xtmp,ytmp,ztmp);
   return cvm::real(xtmp*xtmp + ytmp*ytmp + ztmp*ztmp);
 }
 

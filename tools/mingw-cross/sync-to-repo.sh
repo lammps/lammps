1c0cde1a0e1847b4da36083470903315dd801109
Axel Kohlmeyer <akohlmey@gmail.com>
2013-06-25 18:23:39 +0200

    added script to synchronize windows installers to the webserver

---
+++ tools/mingw-cross/sync-to-repo.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# sync windows installers to repository
+
+
+if [ -n "${MINGW_REPO_USER}" ] \
+&& [ -n "${MINGW_REPO_HOST}" ] \
+&& [ -n "${MINGW_REPO_DIR}" ]
+then
+    pushd mingw-cross
+
+    rsync -arpv --delete lammps-32bit-20??????.exe \
+        ${MINGW_REPO_USER}@${MINGW_REPO_HOST}:${MINGW_REPO_DIR}/32bit
+    rsync -arpv --delete lammps-32bit-mpich2-20??????.exe \
+        ${MINGW_REPO_USER}@${MINGW_REPO_HOST}:${MINGW_REPO_DIR}/32bit-mpi
+    rsync -arpv --delete lammps-64bit-20??????.exe \
+        ${MINGW_REPO_USER}@${MINGW_REPO_HOST}:${MINGW_REPO_DIR}/64bit
+    rsync -arpv --delete lammps-64bit-mpich2-20??????.exe \
+        ${MINGW_REPO_USER}@${MINGW_REPO_HOST}:${MINGW_REPO_DIR}/64bit-mpi
+
+    ssh ${MINGW_REPO_USER}@${MINGW_REPO_HOST} "cd ${MINGW_REPO_DIR}/../../../; ./mkhtmlindex.sh *"
+    popd
+else
+    cat <<EOF
+
+Required environment variables to determine the target repository
+account, server and location are not fully configured.
+MINGW_REPO_USER=${MINGW_REPO_USER}
+MINGW_REPO_HOST=${MINGW_REPO_HOST}
+MINGW_REPO_DIR=${MINGW_REPO_DIR}
+
+EOF
+    exit 1
+fi

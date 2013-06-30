386eedca981d3be1abb15cd6714fca85ed2c3cba
Axel Kohlmeyer <akohlmey@gmail.com>
2013-06-30 15:47:29 +0200

    improve webpages and generator to include tracking and better help

--- tools/rpm/mkhtmlindex.sh
+++ tools/rpm/mkhtmlindex.sh
@@ -9,22 +9,40 @@ fi
 for d in $(find "$@" -type d -print)
 do \
     dir=$(basename $d)
+    uplink=index.html
+    [$dir == windows] && uplink=windows.html
     # index file header
     cat > "$d/index.html" <<EOF
 <html>
-  <head><title>LAMMPS-ICMS RPM Repository: $d</title></head>
+  <head><title>LAMMPS-ICMS Binaries Repository: $d</title>
+
+    <script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-12573382-3']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
+
+
+  </head>
   <body style="font-family: mono, fixed, sans; margin-bottom: 2%; margin-top: 2%; margin-left: 5%; margin-right: 5%;">
-    <h1>LAMMPS-ICMS RPM Repository: $d</h1>
+    <h1>LAMMPS-ICMS Binaries Repository: $d</h1>
     <h2>Contents of $dir</h2>
     <hr>
     <table>
     <tbody>
-      <tr><th>[DIR]</th><th></th><th align="left"> &nbsp; <a href="../index.html">(Up one level)</a></th></tr>
+      <tr><th>[DIR]</th><th></th><th align="left"> &nbsp; <a href="../${uplink}">(Up one level)</a></th></tr>
 EOF
     # first all directory entries
     for e in $(/bin/ls -1 "$d")
       do [ "$e" = "repodata" ] && continue
       [ "$e" = "drpms" ] && continue
+      [ "$e" = "cache" ] && continue
       if [ -d "$d/$e" ]
       then
         echo '<tr><td>[DIR]</td><td></td>' >> "$d/index.html"

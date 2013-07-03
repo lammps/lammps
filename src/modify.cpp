fa420b3d0ae57acd1a32e0d38225add063c22827
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2013-07-03 22:58:37 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@10221 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/modify.cpp
+++ src/modify.cpp
@@ -31,7 +31,7 @@ using namespace FixConst;
 
 #define DELTA 4
 #define BIG 1.0e20
-#define NEXCEPT 3       // change when add to exceptions in add_fix()
+#define NEXCEPT 4       // change when add to exceptions in add_fix()
 
 /* ---------------------------------------------------------------------- */
 
@@ -609,7 +609,7 @@ int Modify::min_reset_ref()
 
 void Modify::add_fix(int narg, char **arg, char *suffix)
 {
-  const char *exceptions[NEXCEPT] = {"GPU","OMP","cmap"};
+  const char *exceptions[NEXCEPT] = {"GPU","OMP","atom/property","cmap"};
 
   if (narg < 3) error->all(FLERR,"Illegal fix command");
 

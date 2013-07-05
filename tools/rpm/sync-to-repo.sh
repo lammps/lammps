cca11fcc5a182604eb4d49cc36452fa62442ff3a
Axel Kohlmeyer <akohlmey@gmail.com>
2013-07-05 14:01:57 +0200

    add option to prune repo before syncing

--- tools/rpm/sync-to-repo.sh
+++ tools/rpm/sync-to-repo.sh
@@ -1,6 +1,80 @@
 #!/bin/sh
 # sync data from build directory to repository
 
+# function to selectively purge
+purge_rpms () {
+  dir=$1
+  sub=$2
+  ref=$(date +%s)
+  old=999999999
+
+  for rpm in ${dir}/lammps${sub}-20[0-9][0-9]*.rpm
+  do \
+      [ -f ${rpm} ] || continue
+      # re-set symbolic link to latest entry
+      p=$(echo ${rpm} | sed -e 's@^\(.*/\)\(lammps-.*\)\(20[0-9][0-9]\+\)\(-[0-9].*\)\(\..*\.rpm\)$@\1@')
+      r=$(echo ${rpm} | sed -e 's@^\(.*/\)\(lammps-.*\)\(20[0-9][0-9]\+\)\(-[0-9].*\)\(\..*\.rpm\)$@\2@')
+      t=$(echo ${rpm} | sed -e 's@^\(.*/\)\(lammps-.*\)\(20[0-9][0-9]\+\)\(-[0-9].*\)\(\..*\.rpm\)$@\3@')
+      v=$(echo ${rpm} | sed -e 's@^\(.*/\)\(lammps-.*\)\(20[0-9][0-9]\+\)\(-[0-9].*\)\(\..*\.rpm\)$@\4@')
+      e=$(echo ${rpm} | sed -e 's@^\(.*/\)\(lammps-.*\)\(20[0-9][0-9]\+\)\(-[0-9].*\)\(\..*\.rpm\)$@\5@')
+
+      # compute age difference in days
+      y=$(echo ${t} | cut -c 1-4)
+      m=$(echo ${t} | cut -c 5-6)
+      d=$(echo ${t} | cut -c 7-8)
+      s=$(date +%s -d "${m}/${d}/${y}")
+      age=$(expr \( $ref - $s \) / 86400)
+
+      if [ $age -lt $old ] 
+      then
+          old=$age
+          sym="${r}${t}${v}${e}"
+          sto="${p}${r}latest${e}"
+      fi
+
+      # after one weeks we keep only one per week.
+      unset tmp
+      if [ $age -gt 7 ]
+      then
+          w=$(expr $age / 7)
+          eval tmp=\$week${w}
+          if [ -n "$tmp" ]
+          then
+              echo "rm -f ${rpm}"
+          fi
+          eval week${w}=1
+      fi
+
+      # after about three months we keep only one per month
+      unset tmp
+      if [ $age -gt 84 ]
+      then
+          m=$(expr $age / 28)
+          eval tmp=\$month${m}
+          if [ -n "$tmp" ]
+          then
+              echo "rm -f ${rpm}"
+          fi
+          eval month${m}=1
+      fi
+      unset tmp
+
+      # after about one year we keep only one per year
+      if [ $age -gt 336 ]
+      then
+          y=$(expr $age / 336)
+          eval tmp=\$year${y}
+          if [ -n "$tmp" ]
+          then
+              echo "rm -f ${rpm}"
+          fi
+          eval year${y}=1
+      fi
+  done
+  echo "rm -f ${sto}"
+  echo "ln -s ${sym} ${sto}"
+}
+
 if ! which rpmbuild > /dev/null 2>&1
 then
     echo "Cannot build rpms without 'rpmbuild' installed"
@@ -8,6 +82,7 @@ then
 fi
 
 MYRPM_BUILD_DIR=$(rpmbuild --showrc 2>&1 | sed -n -e 's/^.*: _topdir[	 ]\+\(.*\)$/\1/p')
+datestr=$(date +%Y%m%d)
 
 if [ -n "${MYRPM_REPO_USER}" ] \
 && [ -n "${MYRPM_REPO_HOST}" ] \
@@ -19,15 +94,25 @@ then
     do \
         test -f $f && mv -v $f debug/
     done
+
+    prune_rpm debug -debuginfo
+    prune_rpm .
+    prune_rpm . -common
+    prune_rpm . -doc
+    prune_rpm . -openmpi
+    prune_rpm . -mpich2
+    prune_rpm . -python
+
+    exit 0
+
     createrepo -v --deltas --num-deltas 2 --max-delta-rpm-size 30000000 .
-    rsync -arpv --delete debug repodata drpms *.rpm \
-        ${MYRPM_REPO_USER}@${MYRPM_REPO_HOST}:${MYRPM_REPO_DIR}/
+    rsync -arpv --exclude cache --delete ./ \
+        ${MYRPM_REPO_USER}@${MYRPM_REPO_HOST}:${MYRPM_REPO_DIR}
     # we use the source rpm on suse, since they do not have a "dist" tag.
     if grep -q -i suse /etc/issue && test $(uname -i) = i386
     then
-        cd ../SRPMS
-        rsync -arpv *.rpm \
-            ${MYRPM_REPO_USER}@${MYRPM_REPO_HOST}:${MYRPM_REPO_DIR}/../../../source/
+        rsync -arpv SRPMS/ \
+            ${MYRPM_REPO_USER}@${MYRPM_REPO_HOST}:${MYRPM_REPO_DIR}/../../../source
     fi
     ssh ${MYRPM_REPO_USER}@${MYRPM_REPO_HOST} "cd ${MYRPM_REPO_DIR}/../../../; ./mkhtmlindex.sh *"
     popd

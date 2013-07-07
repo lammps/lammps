d70645185aa22d577710457cae0883d89762370c
Axel Kohlmeyer <akohlmey@gmail.com>
2013-07-07 21:25:10 +0200

    correct output text as reported on lammps-users

--- src/delete_atoms.cpp
+++ src/delete_atoms.cpp
@@ -406,17 +406,17 @@ void DeleteAtoms::options(int narg, char **arg)
   int iarg = 0;
   while (iarg < narg) {
     if (strcmp(arg[iarg],"compress") == 0) {
-      if (iarg+2 > narg) error->all(FLERR,"Illegal delete_bonds command");
+      if (iarg+2 > narg) error->all(FLERR,"Illegal delete_atoms command");
       if (strcmp(arg[iarg+1],"yes") == 0) compress_flag = 1;
       else if (strcmp(arg[iarg+1],"no") == 0) compress_flag = 0;
-      else error->all(FLERR,"Illegal delete_bonds command");
+      else error->all(FLERR,"Illegal delete_atoms command");
       iarg += 2;
     } else if (strcmp(arg[iarg],"mol") == 0) {
-      if (iarg+2 > narg) error->all(FLERR,"Illegal delete_bonds command");
+      if (iarg+2 > narg) error->all(FLERR,"Illegal delete_atoms command");
       if (strcmp(arg[iarg+1],"yes") == 0) mol_flag = 1;
       else if (strcmp(arg[iarg+1],"no") == 0) mol_flag = 0;
-      else error->all(FLERR,"Illegal delete_bonds command");
+      else error->all(FLERR,"Illegal delete_atoms command");
       iarg += 2;
-    } else error->all(FLERR,"Illegal delete_bonds command");
+    } else error->all(FLERR,"Illegal delete_atoms command");
   }
 }

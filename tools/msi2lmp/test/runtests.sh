4de9ded00908be1c5cd3dbebc78486eac5bd2864
Axel Kohlmeyer <akohlmey@gmail.com>
2013-07-11 20:59:48 +0200

    next test example: one hydrogen and one water

--- tools/msi2lmp/test/runtests.sh
+++ tools/msi2lmp/test/runtests.sh
@@ -8,7 +8,7 @@ CHECKDATA=./data-compare.pl
 counter=0
 
 # Class1 tests
-for m in hydrogen water
+for m in hydrogen water h2-h2o
 do \
     before=$counter
     ${MSI2LMP} ${m}-class1 -c 1 -p 2 	\
@@ -37,11 +37,11 @@ do \
     [ $before -eq $counter ] && rm ${m}-class2.data ${m}-class2.data2 log.${m}-class2
 done
 
-# Class2 tests with cff91
-for m in water
+# Class2 tests with pcff
+for m in water h2-h2o
 do \
     before=$counter
-    ${MSI2LMP} ${m}-class2 -c 2 -p 2 -f cff91	\
+    ${MSI2LMP} ${m}-class2 -c 2 -p 2 -f pcff	\
         || counter=$(expr $counter + 1)
     ${LAMMPS} -log none -in in.${m}-class2	\
         || counter=$(expr $counter + 1)

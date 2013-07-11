894cd7edb0e748094c2267112a06bdf03cdb7c92
Axel Kohlmeyer <akohlmey@gmail.com>
2013-07-11 21:05:42 +0200

    count tests and errors and report both

--- tools/msi2lmp/test/runtests.sh
+++ tools/msi2lmp/test/runtests.sh
@@ -6,52 +6,56 @@ LAMMPS=../../../src/lmp_serial
 CHECKDATA=./data-compare.pl
 
 counter=0
+errors=0
 
 # Class1 tests
 for m in hydrogen water h2-h2o
 do \
-    before=$counter
+    before=$errors
     ${MSI2LMP} ${m}-class1 -c 1 -p 2 	\
-        || counter=$(expr $counter + 1)
+        || errors=$(expr $errors + 1)
     ${LAMMPS} -log none -in in.${m}-class1	\
-        || counter=$(expr $counter + 1)
+        || errors=$(expr $errors + 1)
     ${CHECKDATA} ${m}-class1.data reference/${m}-class1.data	\
-        || counter=$(expr $counter + 1)
+        || errors=$(expr $errors + 1)
     ${CHECKDATA} ${m}-class1.data2 reference/${m}-class1.data2	\
-        || counter=$(expr $counter + 1)
-    [ $before -eq $counter ] && rm ${m}-class1.data ${m}-class1.data2 log.${m}-class1
+        || errors=$(expr $errors + 1)
+    [ $before -eq $errors ] && rm ${m}-class1.data ${m}-class1.data2 log.${m}-class1
+    counter=$(expr $counter + 4)
 done
 
 # Class2 tests with compass
 for m in hydrogen
 do \
-    before=$counter
+    before=$errors
     ${MSI2LMP} ${m}-class2 -c 2 -p 2 -f compass_published	\
-        || counter=$(expr $counter + 1)
+        || errors=$(expr $errors + 1)
     ${LAMMPS} -log none -in in.${m}-class2	\
-        || counter=$(expr $counter + 1)
+        || errors=$(expr $errors + 1)
     ${CHECKDATA} ${m}-class2.data reference/${m}-class2.data	\
-        || counter=$(expr $counter + 1)
+        || errors=$(expr $errors + 1)
     ${CHECKDATA} ${m}-class2.data2 reference/${m}-class2.data2	\
-        || counter=$(expr $counter + 1)
-    [ $before -eq $counter ] && rm ${m}-class2.data ${m}-class2.data2 log.${m}-class2
+        || errors=$(expr $errors + 1)
+    [ $before -eq $errors ] && rm ${m}-class2.data ${m}-class2.data2 log.${m}-class2
+    counter=$(expr $counter + 4)
 done
 
 # Class2 tests with pcff
 for m in water h2-h2o
 do \
-    before=$counter
+    before=$errors
     ${MSI2LMP} ${m}-class2 -c 2 -p 2 -f pcff	\
-        || counter=$(expr $counter + 1)
+        || errors=$(expr $errors + 1)
     ${LAMMPS} -log none -in in.${m}-class2	\
-        || counter=$(expr $counter + 1)
+        || errors=$(expr $errors + 1)
     ${CHECKDATA} ${m}-class2.data reference/${m}-class2.data	\
-        || counter=$(expr $counter + 1)
+        || errors=$(expr $errors + 1)
     ${CHECKDATA} ${m}-class2.data2 reference/${m}-class2.data2	\
-        || counter=$(expr $counter + 1)
-    [ $before -eq $counter ] && rm ${m}-class2.data ${m}-class2.data2 log.${m}-class2
+        || errors=$(expr $errors + 1)
+    [ $before -eq $errors ] && rm ${m}-class2.data ${m}-class2.data2 log.${m}-class2
+    counter=$(expr $counter + 4)
 done
 
-echo "Total error count: $counter"
+echo "Total error count: $errors / $counter"
 echo
 

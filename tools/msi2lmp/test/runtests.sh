503bf92a95da1f67a27099ad84f5df6f3f49b7f4
Axel Kohlmeyer <akohlmey@gmail.com>
2013-07-11 23:56:21 +0200

    split class two tests into two variants 2a and 2b with pcff and compass

--- tools/msi2lmp/test/runtests.sh
+++ tools/msi2lmp/test/runtests.sh
@@ -9,7 +9,7 @@ counter=0
 errors=0
 
 # Class1 tests
-for m in hydrogen water h2-h2o
+for m in hydrogen water h2-h2o ethane
 do \
     before=$errors
     ${MSI2LMP} ${m}-class1 -c 1 -p 2 	\
@@ -28,31 +28,31 @@ done
 for m in hydrogen
 do \
     before=$errors
-    ${MSI2LMP} ${m}-class2 -c 2 -p 2 -f compass_published	\
+    ${MSI2LMP} ${m}-class2a -c 2 -p 2 -f compass_published	\
         || errors=$(expr $errors + 1)
-    ${LAMMPS} -log none -in in.${m}-class2	\
+    ${LAMMPS} -log none -in in.${m}-class2a	\
         || errors=$(expr $errors + 1)
-    ${CHECKDATA} ${m}-class2.data reference/${m}-class2.data	\
+    ${CHECKDATA} ${m}-class2a.data reference/${m}-class2a.data	\
         || errors=$(expr $errors + 1)
-    ${CHECKDATA} ${m}-class2.data2 reference/${m}-class2.data2	\
+    ${CHECKDATA} ${m}-class2a.data2 reference/${m}-class2a.data2	\
         || errors=$(expr $errors + 1)
-    [ $before -eq $errors ] && rm ${m}-class2.data ${m}-class2.data2 log.${m}-class2
+    [ $before -eq $errors ] && rm ${m}-class2a.data ${m}-class2a.data2 log.${m}-class2a
     counter=$(expr $counter + 4)
 done
 
 # Class2 tests with pcff
-for m in water h2-h2o
+for m in water h2-h2o ethane
 do \
     before=$errors
-    ${MSI2LMP} ${m}-class2 -c 2 -p 2 -f pcff	\
+    ${MSI2LMP} ${m}-class2b -c 2 -p 2 -f pcff	\
         || errors=$(expr $errors + 1)
-    ${LAMMPS} -log none -in in.${m}-class2	\
+    ${LAMMPS} -log none -in in.${m}-class2b	\
         || errors=$(expr $errors + 1)
-    ${CHECKDATA} ${m}-class2.data reference/${m}-class2.data	\
+    ${CHECKDATA} ${m}-class2b.data reference/${m}-class2b.data	\
         || errors=$(expr $errors + 1)
-    ${CHECKDATA} ${m}-class2.data2 reference/${m}-class2.data2	\
+    ${CHECKDATA} ${m}-class2b.data2 reference/${m}-class2b.data2	\
         || errors=$(expr $errors + 1)
-    [ $before -eq $errors ] && rm ${m}-class2.data ${m}-class2.data2 log.${m}-class2
+    [ $before -eq $errors ] && rm ${m}-class2b.data ${m}-class2b.data2 log.${m}-class2b
     counter=$(expr $counter + 4)
 done
 

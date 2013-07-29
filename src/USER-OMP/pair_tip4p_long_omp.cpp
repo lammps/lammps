641726ecd753f685caccc3525e9f5b4eba21fca4
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2013-07-29 23:20:35 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@10478 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/USER-OMP/pair_tip4p_long_omp.cpp
+++ src/USER-OMP/pair_tip4p_long_omp.cpp
@@ -101,7 +101,6 @@ void PairTIP4PLongOMP::compute(int eflag, int vflag)
 
     loop_setup_thr(ifrom, ito, tid, inum, nthreads);
     ThrData *thr = fix->get_thr(tid);
-    thr->timer(Timer::START);
     ev_setup_thr(eflag, vflag, nall, eatom, vatom, thr);
 
   if (!ncoultablebits) {
@@ -126,7 +125,6 @@ void PairTIP4PLongOMP::compute(int eflag, int vflag)
     } else eval<0,0,0,0>(ifrom, ito, thr);
   }
 
-    thr->timer(Timer::PAIR);
     reduce_thr(this, eflag, vflag, thr);
   } // end of omp parallel region
 }

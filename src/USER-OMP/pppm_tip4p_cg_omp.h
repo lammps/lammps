5b2056130a888b6530c9c0211736843cc3b473f9
Axel Kohlmeyer <akohlmey@gmail.com>
2013-08-01 16:33:11 +0200

    add serial and mt version of pppm/tip4p/cg

--- src/USER-OMP/pppm_tip4p_cg_omp.h
+++ src/USER-OMP/pppm_tip4p_cg_omp.h
@@ -13,22 +13,22 @@
 
 #ifdef KSPACE_CLASS
 
-KSpaceStyle(pppm/cg/omp,PPPMCGOMP)
+KSpaceStyle(pppm/tip4p/cg/omp,PPPMTIP4PCGOMP)
 
 #else
 
-#ifndef LMP_PPPM_CG_OMP_H
-#define LMP_PPPM_CG_OMP_H
+#ifndef LMP_PPPM_TIP4P_CG_OMP_H
+#define LMP_PPPM_TIP4P_CG_OMP_H
 
-#include "pppm_cg.h"
+#include "pppm_tip4p_cg.h"
 #include "thr_omp.h"
 
 namespace LAMMPS_NS {
 
-class PPPMCGOMP : public PPPMCG, public ThrOMP {
+class PPPMTIP4PCGOMP : public PPPMTIP4PCG, public ThrOMP {
  public:
-  PPPMCGOMP(class LAMMPS *, int, char **);
-  virtual ~PPPMCGOMP () {};
+  PPPMTIP4PCGOMP(class LAMMPS *, int, char **);
+  virtual ~PPPMTIP4PCGOMP () {};
   virtual void compute(int, int);
 
  protected:
@@ -38,19 +38,34 @@ class PPPMCGOMP : public PPPMCG, public ThrOMP {
   virtual void compute_gf_ik();
   virtual void compute_gf_ad();
 
+  virtual void particle_map();
   virtual void make_rho();
+
   virtual void fieldforce_ik();
   virtual void fieldforce_ad();
-  virtual void fieldforce_peratom();
+  // virtual void fieldforce_peratom();  XXX: need to benchmark first.
 
  private:
   void compute_rho1d_thr(FFT_SCALAR * const * const, const FFT_SCALAR &,
                          const FFT_SCALAR &, const FFT_SCALAR &);
   void compute_drho1d_thr(FFT_SCALAR * const * const, const FFT_SCALAR &,
 			  const FFT_SCALAR &, const FFT_SCALAR &);
+
+  void find_M_thr(const int, int &, int &, dbl3_t &);
+
+//  void slabcorr(int);  // XXX: not (yet) multi-threaded
+
 };
 
 }
 
 #endif
 #endif
+
+/* ERROR/WARNING messages:
+
+E: Kspace style pppm/tip4p/omp requires newton on
+
+Self-explanatory.
+
+*/

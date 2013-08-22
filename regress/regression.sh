a3d7d3af3479816ab5dfda8aa90aafe9053226cc
rjones <rjones@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2013-08-22 23:15:47 +0000

    name change
    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@10651 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- regress/regression.sh
+++ regress/regression.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cd /code/lammps-atc/regression
-./benchmark.py 4 12 min dipole >& latest
+cd /code/lammps-atc/regress
+./benchmark.py 4 12 min melt >& latest
 fail=`grep -c FAIL latest`
-addrs="rjones@sandia.gov jatempl@sandia.gov jzimmer@sandia.gov sjplimp@sandia.gov akohlmey@gmail.com" 
+addrs="rjones@sandia.gov jatempl@sandia.gov jzimmer@sandia.gov sjplimp@sandia.gov pscrozi@sandia.gov akohlmey@gmail.com" 
 if [ $fail == 0 ] ; then
   mhmail  $addrs -subject "LAMMPS regression passes" <  latest
 else

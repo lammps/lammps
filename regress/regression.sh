e5e09710b54cf8ed30d9fa0e25e7584cb91183c2
rjones <rjones@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2013-08-23 20:32:28 +0000

    update for remote mailing
    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@10684 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- regress/regression.sh
+++ regress/regression.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 cd /code/lammps-atc/regress
-./benchmark.py 4 12 min melt >& latest
+#./benchmark.py 4 12 min melt >& latest
 fail=`grep -c FAIL latest`
-addrs="rjones@sandia.gov jatempl@sandia.gov jzimmer@sandia.gov sjplimp@sandia.gov pscrozi@sandia.gov akohlmey@gmail.com" 
+addresses="rjones@sandia.gov jatempl@sandia.gov jzimmer@sandia.gov sjplimp@sandia.gov pscrozi@sandia.gov akohlmey@gmail.com" 
+subject="\"LAMMPS regression $fail tests failed\""
 if [ $fail == 0 ] ; then
-  mhmail  $addrs -subject "LAMMPS regression passes" <  latest
-else
-  mhmail  $addrs -subject "LAMMPS regression $fail tests failed" < latest
+  subject='"LAMMPS regression passed"'
 fi
+scp latest vikramarka.ca.sandia.gov:.
+#echo $subject
+#echo "mhmail $addresses -subject $subject < latest"
+ssh vikramarka.ca.sandia.gov "mhmail $addresses -subject $subject < latest"
 

f447886276f8db2120733cdd4a7265f72eca0ac0
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2013-11-22 22:15:48 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@11052 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/neigh_request.cpp
+++ src/neigh_request.cpp
@@ -72,8 +72,22 @@ NeighRequest::~NeighRequest()
 }
 
 /* ----------------------------------------------------------------------
+   archive request params that Neighbor may change after call to identical()
+------------------------------------------------------------------------- */
+
+void NeighRequest::archive()
+{
+  half_original = half;
+  half_from_full_original = half_from_full;
+  copy_original = copy;
+  otherlist_original = otherlist;
+}
+
+/* ----------------------------------------------------------------------
    compare this request to other request
-   return 1 if every variable is same, 0 if different in any way
+   identical means all params set by requestor are the same
+   compare to original values in other if Neighbor may have changed them
+   return 1 if identical, 0 if not
 ------------------------------------------------------------------------- */
 
 int NeighRequest::identical(NeighRequest *other)
@@ -88,14 +102,14 @@ int NeighRequest::identical(NeighRequest *other)
   if (compute != other->compute) same = 0;
   if (command != other->command) same = 0;
 
-  if (half != other->half) same = 0;
+  if (half != other->half_original) same = 0;
   if (full != other->full) same = 0;
   if (gran != other->gran) same = 0;
   if (granhistory != other->granhistory) same = 0;
   if (respainner != other->respainner) same = 0;
   if (respamiddle != other->respamiddle) same = 0;
   if (respaouter != other->respaouter) same = 0;
-  if (half_from_full != other->half_from_full) same = 0;
+  if (half_from_full != other->half_from_full_original) same = 0;
 
   if (newton != other->newton) same = 0;
   if (occasional != other->occasional) same = 0;
@@ -105,9 +119,9 @@ int NeighRequest::identical(NeighRequest *other)
   if (cudable != other->cudable) same = 0;
   if (omp != other->omp) same = 0;
 
-  if (copy != other->copy) same = 0;
+  if (copy != other->copy_original) same = 0;
   if (same_skip(other) == 0) same = 0;
-  if (otherlist != other->otherlist) same = 0;
+  if (otherlist != other->otherlist_original) same = 0;
 
   return same;
 }

889bae9bf8f07e8a0dff812d6a404f4c868fb994
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2014-01-29 23:37:38 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@11386 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/KSPACE/fix_tune_kspace.h
+++ src/KSPACE/fix_tune_kspace.h
@@ -1,4 +1,4 @@
-ndoc/* ----------------------------------------------------------------------
+/* ----------------------------------------------------------------------
    LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
    http://lammps.sandia.gov, Sandia National Laboratories
    Steve Plimpton, sjplimp@sandia.gov

d52900bb9d351d9057767f85230d2c9ff1fcbc2d
Axel Kohlmeyer <akohlmey@gmail.com>
2014-03-02 14:59:43 -0500

    implement CSVR thermostat as modification of fix temp/berendsen

--- src/fix_temp_csvr.h
+++ src/fix_temp_csvr.h
@@ -13,45 +13,32 @@
 
 #ifdef FIX_CLASS
 
-FixStyle(csvr,FixCSVR)
+FixStyle(temp/csvr,FixTempCSVR)
 
 #else
 
-#ifndef LMP_FIX_CSVR_H
-#define LMP_FIX_CSVR_H
+#ifndef LMP_FIX_TEMP_CSVR_H
+#define LMP_FIX_TEMP_CSVR_H
 
 #include "fix.h"
 
 namespace LAMMPS_NS {
 
-class FixCSVR : public Fix {
+class FixTempCSVR : public Fix {
  public:
-  FixCSVR(class LAMMPS *, int, char **);
-  virtual ~FixCSVR() {}
-
+  FixTempCSVR(class LAMMPS *, int, char **);
+  ~FixTempCSVR();
   int setmask();
+  void init();
+  void end_of_step();
   int modify_param(int, char **);
   void reset_target(double);
-
-  virtual void init();
-  virtual void initial_integrate(int);
-  virtual void final_integrate();
-  virtual void initial_integrate_respa(int, int, int);
-  virtual void final_integrate_respa(int, int);
-  virtual void reset_dt();
-  
   double compute_scalar();
   virtual void *extract(const char *, int &);
 
- protected:
-  class RanMars *random;
-
-  double t_start,t_stop,t_period,t_target;
-  double dtv,dtf;
-  double *step_respa;
-  int mass_require;
+ private:
   int which;
-
+  double t_start,t_stop,t_period,t_target;
   double energy;
   int tstyle,tvar;
   char *tstr;
@@ -59,6 +46,8 @@ class FixCSVR : public Fix {
   char *id_temp;
   class Compute *temperature;
   int tflag;
+
+  class RanMars *random;
 };
 
 }
@@ -73,5 +62,43 @@ E: Illegal ... command
 Self-explanatory.  Check the input script syntax and compare to the
 documentation for the command.  You can use -echo screen as a
 command-line option when running LAMMPS to see the offending line.
+
+E: Fix temp/csvr period must be > 0.0
+
+Self-explanatory.
+
+E: Variable name for fix temp/csvr does not exist
+
+Self-explanatory.
+
+E: Variable for fix temp/csvr is invalid style
+
+Only equal-style variables can be used.
+
+E: Temperature ID for fix temp/csvr does not exist
+
+Self-explanatory.
+
+E: Computed temperature for fix temp/csvr cannot be 0.0
+
+Self-explanatory.
+
+E: Fix temp/csvr variable returned negative temperature
+
+Self-explanatory.
+
+E: Could not find fix_modify temperature ID
+
+The compute ID for computing temperature does not exist.
+
+E: Fix_modify temperature ID does not compute temperature
+
+The compute ID assigned to the fix must compute temperature.
+
+W: Group for fix_modify temp != fix group
+
+The fix_modify command is specifying a temperature computation that
+computes a temperature on a different group of atoms than the fix
+itself operates on.  This is probably not what you want to do.
 
 */

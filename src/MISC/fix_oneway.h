481778cb31ea3433fde48491e560ebcfb50fafcd
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2014-03-10 15:18:54 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@11608 f3b2605a-c512-4ea7-a41b-209d697bcdaa

---
+++ src/MISC/fix_oneway.h
@@ -0,0 +1,54 @@
+/* -*- c++ -*- ----------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef FIX_CLASS
+
+FixStyle(oneway,FixOneWay)
+
+#else
+
+#ifndef LMP_FIX_ONEWAY_H
+#define LMP_FIX_ONEWAY_H
+
+#include "fix.h"
+
+namespace LAMMPS_NS {
+
+class FixOneWay : public Fix {
+ public:
+  FixOneWay(class LAMMPS *, int, char **);
+  virtual ~FixOneWay();
+  int setmask();
+  virtual void init();
+  virtual void end_of_step();
+
+ protected:
+  int direction;
+  int regionidx;
+  char *regionstr;
+};
+
+}
+
+#endif
+#endif
+
+/* ERROR/WARNING messages:
+
+E: Illegal ... command
+
+Self-explanatory.  Check the input script syntax and compare to the
+documentation for the command.  You can use -echo screen as a
+command-line option when running LAMMPS to see the offending line.
+
+*/

b97a4d3c3873ea881e1d0f18d02afef4fa393d68
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2014-05-22 13:59:06 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@12011 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/USER-FEP/Install.sh
+++ src/USER-FEP/Install.sh
@@ -34,15 +34,18 @@ action fix_adapt_fep.cpp
 action fix_adapt_fep.h
 action pair_coul_cut_soft.cpp
 action pair_coul_cut_soft.h
-action pair_coul_long_soft.cpp          pppm.cpp
-action pair_coul_long_soft.h            pppm.cpp
+action pair_coul_long_soft.cpp            pppm.cpp
+action pair_coul_long_soft.h              pppm.cpp
+action pair_lj_charmm_coul_long_soft.cpp  pppm.cpp
+action pair_lj_charmm_coul_long_soft.h    pppm.cpp
 action pair_lj_cut_coul_cut_soft.cpp
 action pair_lj_cut_coul_cut_soft.h
-action pair_lj_cut_coul_long_soft.cpp   pppm.cpp
-action pair_lj_cut_coul_long_soft.h     pppm.cpp
+action pair_lj_cut_coul_long_soft.cpp     pppm.cpp
+action pair_lj_cut_coul_long_soft.h       pppm.cpp
 action pair_lj_cut_soft.cpp
 action pair_lj_cut_soft.h
-action pair_lj_cut_tip4p_long_soft.cpp  pppm_tip4p.cpp
-action pair_lj_cut_tip4p_long_soft.h    pppm_tip4p.cpp
-action pair_tip4p_long_soft.cpp         pppm_tip4p.cpp
-action pair_tip4p_long_soft.h           pppm_tip4p.cpp
+action pair_lj_cut_tip4p_long_soft.cpp    pppm_tip4p.cpp
+action pair_lj_cut_tip4p_long_soft.h      pppm_tip4p.cpp
+action pair_tip4p_long_soft.cpp           pppm_tip4p.cpp
+action pair_tip4p_long_soft.h             pppm_tip4p.cpp
+

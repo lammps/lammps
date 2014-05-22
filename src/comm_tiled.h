a59a131024a26e68a2d0d36b86c30391af347b3d
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2014-05-22 14:38:26 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@12017 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/comm_tiled.h
+++ src/comm_tiled.h
@@ -41,9 +41,52 @@ class CommTiled : public Comm {
   void forward_comm_dump(class Dump *);    // forward comm from a Dump
   void reverse_comm_dump(class Dump *);    // reverse comm from a Dump
 
-  void forward_comm_array(int, double **);         // forward comm of array
+  void forward_comm_array(int, double **);          // forward comm of array
   int exchange_variable(int, double *, double *&);  // exchange on neigh stencil
   bigint memory_usage();
+
+ private:
+  int nswap;                    // # of swaps to perform = 2*dim
+  int *nsendproc,*nrecvproc;    // # of procs to send/recv to/from in each swap
+
+  int triclinic;                    // 0 if domain is orthog, 1 if triclinic
+  int map_style;                    // non-0 if global->local mapping is done
+  int size_forward;                 // # of per-atom datums in forward comm
+  int size_reverse;                 // # of datums in reverse comm
+  int size_border;                  // # of datums in forward border comm
+
+  int **sendnum,**recvnum;      // # of atoms to send/recv per swap/proc
+  int **sendproc,**recvproc;    // proc to send/recv to/from per swap/proc
+  int **size_forward_recv;      // # of values to recv in each forward swap/proc
+  int **firstrecv;              // where to put 1st recv atom per swap/proc
+  int **size_reverse_send;      // # to send in each reverse comm per swap/proc
+  int **size_reverse_recv;      // # to recv in each reverse comm per swap/proc
+
+  int **forward_recv_offset;  // forward comm offsets in buf_recv per swap/proc
+  int **reverse_recv_offset;  // reverse comm offsets in buf_recv per swap/proc
+
+  int ***sendlist;              // list of atoms to send per swap/proc
+  int **maxsendlist;            // max size of send list per swap/proc
+  int **pbc_flag;               // general flag for sending atoms thru PBC
+  int ***pbc;                   // dimension flags for PBC adjustments
+
+  double ***sendbox;            // bounding box of atoms to send per swap/proc
+
+  double *buf_send;             // send buffer for all comm
+  double *buf_recv;             // recv buffer for all comm
+  int maxsend,maxrecv;          // current size of send/recv buffer
+  int maxforward,maxreverse;    // max # of datums in forward/reverse comm
+
+  int bufextra;                 // extra space beyond maxsend in send buffer
+
+  MPI_Request *requests;
+  MPI_Status *statuses;
+
+  int comm_x_only,comm_f_only;      // 1 if only exchange x,f in for/rev comm
+
+  void grow_send(int, int);         // reallocate send buffer
+  void grow_recv(int);              // free/allocate recv buffer
+  void grow_list(int, int, int);    // reallocate sendlist for one swap/proc
 };
 
 }

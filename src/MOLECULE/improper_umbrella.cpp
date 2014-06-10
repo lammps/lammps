5e1cc8e250cfae386c86bcbb5d5bde607de49459
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2014-06-10 23:24:50 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@12115 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/MOLECULE/improper_umbrella.cpp
+++ src/MOLECULE/improper_umbrella.cpp
@@ -309,3 +309,13 @@ void ImproperUmbrella::read_restart(FILE *fp)
 
   for (int i = 1; i <= atom->nimpropertypes; i++) setflag[i] = 1;
 }
+
+/* ----------------------------------------------------------------------
+   proc 0 writes to data file
+------------------------------------------------------------------------- */
+
+void ImproperUmbrella::write_data(FILE *fp)
+{
+  for (int i = 1; i <= atom->nimpropertypes; i++)
+    fprintf(fp,"%d %g %g\n",i,k0[i],w0[i]/MY_PI*180.0);
+}

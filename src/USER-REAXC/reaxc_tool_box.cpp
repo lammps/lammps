a712b28bd66ed0efb733de89147cd74de9210ca1
Axel Kohlmeyer <akohlmey@gmail.com>
2014-09-29 13:40:00 -0400

    make reax/c honor $LAMMPS_POTENTIALS

--- src/USER-REAXC/reaxc_tool_box.cpp
+++ src/USER-REAXC/reaxc_tool_box.cpp
@@ -238,3 +238,76 @@ void sfree( void *ptr, const char *name )
   free( ptr );
   ptr = NULL;
 }
+
+/* ----------------------------------------------------------------------
+   strip off leading part of path, return just the filename
+------------------------------------------------------------------------- */
+
+static const char *potname(const char *path)
+{
+  const char *pot;
+
+  if (path == NULL) return NULL;
+
+#if defined(_WIN32)
+  // skip over the disk drive part of windows pathnames
+  if (isalpha(path[0]) && path[1] == ':') 
+    path += 2;
+#endif
+
+  for (pot = path; *path != '\0'; ++path) {
+#if defined(_WIN32)
+    if ((*path == '\\') || (*path == '/')) pot = path + 1;
+#else
+    if (*path == '/') pot = path + 1;
+#endif
+  }
+
+  return pot;
+}
+
+/* ----------------------------------------------------------------------
+   open a potential file as specified by name; failing that,
+   search in dir specified by env variable LAMMPS_POTENTIALS
+------------------------------------------------------------------------- */
+
+FILE *lmp_open_potential(const char *name)
+{
+  FILE *fp;
+
+  if (name == NULL) return NULL;
+
+  // attempt to open file directly
+  // if successful, return ptr
+
+  fp = fopen(name,"r");
+  if (fp) return fp;
+
+  // try the environment variable directory
+
+  const char *path = getenv("LAMMPS_POTENTIALS");
+  if (path == NULL) return NULL;
+
+  const char *pot = potname(name);
+  if (pot == NULL) return NULL;
+
+  size_t len1 = strlen(path);
+  size_t len2 = strlen(pot);
+  char *newpath = new char[len1+len2+2];
+
+  strcpy(newpath,path);
+#if defined(_WIN32)
+  newpath[len1] = '\\';
+  newpath[len1+1] = 0;
+#else
+  newpath[len1] = '/';
+  newpath[len1+1] = 0;
+#endif
+  strcat(newpath,pot);
+
+  fp = fopen(newpath,"r");
+  delete[] newpath;
+  return fp;
+}
+
+

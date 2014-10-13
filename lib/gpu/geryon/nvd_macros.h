ac961d025e9ca161cdd6f78e8dfd1e9ad1071852
Axel Kohlmeyer <akohlmey@gmail.com>
2014-10-13 17:20:59 -0400

    do not call MPI_Abort() the GPU library, if MPI is already shut down

--- lib/gpu/geryon/nvd_macros.h
+++ lib/gpu/geryon/nvd_macros.h
@@ -13,7 +13,12 @@
 
 #ifdef MPI_GERYON
 #include "mpi.h"
-#define NVD_GERYON_EXIT MPI_Abort(MPI_COMM_WORLD,-1)
+#define NVD_GERYON_EXIT do {                                               \
+  int is_final;                                                            \
+  MPI_Finalized(&is_final);                                                \
+  if (!is_final)                                                           \
+    MPI_Abort(MPI_COMM_WORLD,-1);                                          \
+  } while(0)
 #else
 #define NVD_GERYON_EXIT assert(0==1)
 #endif

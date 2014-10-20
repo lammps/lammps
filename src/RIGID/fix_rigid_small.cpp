2930738444f4c3b6d411696586c5b5bf1a5b385e
Axel Kohlmeyer <akohlmey@gmail.com>
2014-10-20 13:10:18 -0400

    need to resize per atom arrays after we know whether extended flags are used

--- src/RIGID/fix_rigid_small.cpp
+++ src/RIGID/fix_rigid_small.cpp
@@ -1738,6 +1738,7 @@ void FixRigidSmall::setup_bodies_static()
     if (atom->line_flag) orientflag = 1;
     if (atom->tri_flag) orientflag = 4;
     if (atom->mu_flag) dorientflag = 1;
+    grow_arrays(atom->nmax);
 
     for (i = 0; i < nlocal; i++) {
       eflags[i] = 0;

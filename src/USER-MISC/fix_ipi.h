11bd1a45ed40e2bc942a5766392aa16e624c0ba1
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2014-11-24 17:23:55 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@12775 f3b2605a-c512-4ea7-a41b-209d697bcdaa

---
+++ src/USER-MISC/fix_ipi.h
@@ -0,0 +1,45 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef FIX_CLASS
+
+FixStyle(ipi,FixIPI)
+
+#else
+
+#ifndef LMP_FIX_IPI_H
+#define LMP_FIX_IPI_H
+
+#include "fix.h"
+
+namespace LAMMPS_NS {
+
+class FixIPI : public Fix {
+ public:
+  FixIPI(class LAMMPS *, int, char **);
+  virtual ~FixIPI();
+  int setmask();
+  virtual void init();
+  virtual void initial_integrate(int);
+  virtual void final_integrate();
+
+ protected:
+  char host[1024]; int port; int inet, master, hasdata;
+  int ipisock, me; double *buffer; long bsize;
+  int kspace_flag;
+};
+
+}
+
+#endif
+#endif

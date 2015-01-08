7dd802cb67fd1c06a619a2fa69f9812fd5f81359
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2015-01-08 19:17:41 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@12899 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/VORONOI/Install.sh
+++ src/VORONOI/Install.sh
@@ -33,28 +33,32 @@ done
 if (test $1 = 1) then
 
   if (test -e ../Makefile.package) then
-    sed -i -e 's/[^ \t]*voronoi[^ \t]* //g' ../Makefile.package
+    sed -i -e 's/[^ \t]*voronoi[^ \t]* //' ../Makefile.package
+    sed -i -e 's|^PKG_INC =[ \t]*|&-I../../lib/voronoi/includelink |' ../Makefile.package
+    sed -i -e 's|^PKG_PATH =[ \t]*|&-L../../lib/voronoi/liblink |' ../Makefile.package
+    sed -i -e 's|^PKG_LIB =[ \t]*|&-lvoro++ |' ../Makefile.package
     sed -i -e 's|^PKG_SYSINC =[ \t]*|&$(voronoi_SYSINC) |' ../Makefile.package
     sed -i -e 's|^PKG_SYSLIB =[ \t]*|&$(voronoi_SYSLIB) |' ../Makefile.package
     sed -i -e 's|^PKG_SYSPATH =[ \t]*|&$(voronoi_SYSPATH) |' ../Makefile.package
   fi
 
   if (test -e ../Makefile.package.settings) then
-    sed -i -e '/^include.*VORONOI.*$/d' ../Makefile.package.settings
+    sed -i -e '/^include.*voronoi.*$/d' ../Makefile.package.settings
     # multiline form needed for BSD sed on Macs
     sed -i -e '4 i \
-include ..\/VORONOI\/Makefile.lammps
+include ..\/..\/lib\/voronoi\/Makefile.lammps
 ' ../Makefile.package.settings
   fi
 
 elif (test $1 = 0) then
 
   if (test -e ../Makefile.package) then
-    sed -i -e 's/[^ \t]*voronoi[^ \t]* //g' ../Makefile.package
+    sed -i -e 's/[^ \t]*voronoi[^ \t]* //' ../Makefile.package
+    sed -i -e 's/[^ \t]*voro++[^ \t]* //' ../Makefile.package
   fi
 
   if (test -e ../Makefile.package.settings) then
-    sed -i -e '/^include.*VORONOI.*$/d' ../Makefile.package.settings
+    sed -i -e '/^include.*voronoi.*$/d' ../Makefile.package.settings
   fi
 
 fi

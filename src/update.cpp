29beaea8899dc59cdac85a6d312e7239783aaedc
Axel Kohlmeyer <akohlmey@gmail.com>
2015-01-20 10:43:24 -0500

    fix bug in time unit conversion factor

--- src/update.cpp
+++ src/update.cpp
@@ -235,7 +235,7 @@ void Update::set_units(const char *style)
     force->hhmrr2e = 0.0;
     force->mvh2r = 0.0;
     force->angstrom = 1.88972612;
-    force->femtosecond = 0.0241888428;
+    force->femtosecond = 41.34137413;
     force->qelectron = 1.0;
 
     dt = 0.001;

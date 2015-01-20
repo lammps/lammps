1026597d2f4504e50d53af95ecc2614df1bee987
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2015-01-20 17:16:12 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@12962 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/USER-CUDA/pppm_cuda.cpp
+++ src/USER-CUDA/pppm_cuda.cpp
@@ -198,7 +198,6 @@ PPPMCuda::~PPPMCuda()
 
 void PPPMCuda::init()
 {
-
   cuda->shared_data.pppm.cudable_force=1;
 
     //if(cuda->finished_run) {PPPM::init(); return;}
@@ -473,8 +472,6 @@ void PPPMCuda::init()
 
   if (order == 0) error->all(FLERR,"PPPMCuda order has been reduced to 0");
 
-
-
   // decomposition of FFT mesh
   // global indices range from 0 to N-1
   // proc owns entire x-dimension, clump of columns in y,z dimensions
@@ -689,18 +686,21 @@ void PPPMCuda::compute(int eflag, int vflag)
   }
 
   // extend size of per-atom arrays if necessary
-  // and force update of device data, if charges can change
+  // force update of device data, if arrays resized or charges changed
+  // NOTE: also need to test for q changed ??
 
-  if ((cu_atom->update_nmax)||(old_nmax==0)||qdynamic) {
+  if (cu_atom->update_nmax || old_nmax == 0) {
     memory->destroy(part2grid);
     nmax = atom->nmax;
     memory->create(part2grid,nmax,3,"pppm:part2grid");
     delete cu_part2grid;
     delete [] adev_data_array;
     adev_data_array=new dev_array[1];
-    cu_part2grid = new cCudaData<int  , int   , yx > ((int*)part2grid,adev_data_array, nmax,3);
+    cu_part2grid = new cCudaData<int  , int   , yx > 
+      ((int*)part2grid,adev_data_array, nmax,3);
 
-    pppm_device_update(&cuda->shared_data,cu_part2grid->dev_data(),atom->nlocal,atom->nmax);
+    pppm_device_update(&cuda->shared_data,cu_part2grid->dev_data(),
+                       atom->nlocal,atom->nmax);
     old_nmax=nmax;
   }
   if(cu_atom->update_nlocal) {pppm_update_nlocal(cu_atom->nlocal);}
@@ -716,7 +716,6 @@ void PPPMCuda::compute(int eflag, int vflag)
 
   // find grid points for all my particles
   // map my particle charge onto my local 3d density grid
-
 
   my_gettime(CLOCK_REALTIME,&starttime);
 
@@ -769,7 +768,6 @@ void PPPMCuda::compute(int eflag, int vflag)
   // not necessary since all the calculations are done on one proc
 
   // calculate the force on my particles
-
 
   my_gettime(CLOCK_REALTIME,&starttime);
   fieldforce();

7188f4a72a068bc2e1cefc704fdad787677cdb68
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2015-01-30 22:23:58 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@13012 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/USER-FEP/pair_lj_cut_coul_cut_soft.cpp
+++ src/USER-FEP/pair_lj_cut_coul_cut_soft.cpp
@@ -297,7 +297,7 @@ double PairLJCutCoulCutSoft::init_one(int i, int j)
   lj4[i][j] = alphac  * (1.0 - lambda[i][j])*(1.0 - lambda[i][j]);
 
   if (offset_flag) {
-    double denlj = lj3[i][j] + pow(sigma[i][j] / cut_lj[i][j], 6.0);
+    double denlj = lj3[i][j] + pow(cut_lj[i][j] / sigma[i][j], 6.0);
     offset[i][j] = lj1[i][j] * 4.0 * epsilon[i][j] * (1.0/(denlj*denlj) - 1.0/denlj);
   } else offset[i][j] = 0.0;
 

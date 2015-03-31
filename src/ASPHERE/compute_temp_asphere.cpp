2cc7be920002f59ad113dd29db3a2486d1f22520
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2015-03-31 19:02:42 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@13346 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/ASPHERE/compute_temp_asphere.cpp
+++ src/ASPHERE/compute_temp_asphere.cpp
@@ -140,7 +140,7 @@ void ComputeTempAsphere::setup()
 
 void ComputeTempAsphere::dof_compute()
 {
-  fix_dof = modify->adjust_dof_fix(igroup);
+  adjust_dof_fix();
 
   // 6 dof for 3d, 3 dof for 2d
   // which dof are included also depends on mode
@@ -267,7 +267,7 @@ double ComputeTempAsphere::compute_scalar()
 
   MPI_Allreduce(&t,&scalar,1,MPI_DOUBLE,MPI_SUM,world);
   if (dynamic || tempbias == 2) dof_compute();
-  if (tfactor == 0.0 && scalar != 0.0) 
+  if (tfactor == 0.0 && atom->natoms != 0) 
     error->all(FLERR,"Temperature compute degrees of freedom < 0");
   scalar *= tfactor;
   return scalar;

cd6f885c6d2ba33e47b96d78f8afec46cbbe95a2
stamoor <stamoor@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2015-04-01 21:13:05 +0000

    Fixing memory error
    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@13358 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/SHOCK/fix_msst.cpp
+++ src/SHOCK/fix_msst.cpp
@@ -202,7 +202,7 @@ FixMSST::FixMSST(LAMMPS *lmp, int narg, char **arg) :
 
   // create a new compute potential energy compute
 
-  n = strlen(id) + 3;
+  n = strlen(id) + 4;
   id_pe = new char[n];
   strcpy(id_pe,id);
   strcat(id_pe,"_pe");

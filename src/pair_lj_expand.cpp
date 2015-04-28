5a88d7477a46c106e1ce4b7e559540f7f1f0684b
athomps <athomps@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2015-04-28 00:01:00 +0000

    Corrected error in etail and ptail
    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@13435 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/pair_lj_expand.cpp
+++ src/pair_lj_expand.cpp
@@ -269,26 +269,26 @@ double PairLJExpand::init_one(int i, int j)
 
     double sig2 = sigma[i][j]*sigma[i][j];
     double sig6 = sig2*sig2*sig2;
-    double shiftcut = shift[i][j] - cut[i][j];
-    double rc3  = shiftcut*shiftcut*shiftcut;
-    double rc4  = rc3*shiftcut;
-    double rc5  = rc4*shiftcut;
-    double rc6  = rc5*shiftcut;
+    double rcut = cut[i][j];
+    double rc3  = rcut*rcut*rcut;
+    double rc4  = rc3*rcut;
+    double rc5  = rc4*rcut;
+    double rc6  = rc5*rcut;
     double rc9  = rc6*rc3;
-    double rc10 = rc9*shiftcut;
-    double rc11 = rc10*shiftcut;
-    double rc12 = rc11*shiftcut;
+    double rc10 = rc9*rcut;
+    double rc11 = rc10*rcut;
+    double rc12 = rc11*rcut;
     double shift2 = shift[i][j]*shift[i][j];
     double shift3 = shift2*shift[i][j];
     etail_ij = 8.0*MY_PI*all[0]*all[1]*epsilon[i][j] *
-      sig6*((-1.0/(9.0*rc9) + shift[i][j]/(5.0*rc10) -
-             shift2/(11.0*rc11))*sig6 +
-            1.0/(3.0*rc3) - shift[i][j]/(2.0*rc4) + shift2/(5.0*rc5));
-    ptail_ij = 8.0*MY_PI*all[0]*all[1]*epsilon[i][j] *
-      sig6* ((-4.0/(3.0*rc9) + 18.0*shift[i][j]/(5.0*rc10) -
-             36.0*shift2/(11.0*rc11) + shift3/rc12)*sig6 +
-             2.0/rc3 - 9.0*shift[i][j]/(2.0*rc4) +
-             18.0*shift2/(5.0*rc5) - shift3/rc6)/3.0;
+      sig6*(sig6*(1.0/(9.0*rc9) + shift[i][j]/(5.0*rc10) +
+             shift2/(11.0*rc11)) -
+            (1.0/(3.0*rc3) + shift[i][j]/(2.0*rc4) + shift2/(5.0*rc5)));
+    ptail_ij = 16.0*MY_PI*all[0]*all[1]*epsilon[i][j] *
+      sig6*(sig6*(2.0/(9.0*rc9) + 3.0*shift[i][j]/(5.0*rc10) +
+		  6.0*shift2/(11.0*rc11) + shift3/(6.0*rc12)) -
+	    (1.0/(3.0*rc3) + 3.0*shift[i][j]/(4.0*rc4) +
+	     3.0*shift2/(5.0*rc5) + shift3/(6.0*rc6)));
   }
 
   return cut[i][j] + shift[i][j];

aef75ace77e7d1b71da42c81d1696c3469186401
athomps <athomps@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2015-05-15 22:41:51 +0000

    Added inside box check for fix_gcmc moves
    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@13476 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/domain.h
+++ src/domain.h
@@ -126,6 +126,7 @@ class Domain : protected Pointers {
   virtual void x2lamda(int);
   virtual void lamda2x(double *, double *);
   virtual void x2lamda(double *, double *);
+  int inside(double *);
   void x2lamda(double *, double *, double *, double *);
   void bbox(double *, double *, double *, double *);
   void box_corners();

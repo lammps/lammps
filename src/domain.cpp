f110488db0063af94c91cf7a8e501492735cd172
athomps <athomps@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2015-05-18 21:08:06 +0000

    Added inside_nonperiodic()
    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@13482 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/domain.cpp
+++ src/domain.cpp
@@ -618,6 +618,42 @@ int Domain::inside(double* x)
 }
 
 /* ----------------------------------------------------------------------
+   check that point is inside nonperiodic boundaries, in [lo,hi) sense
+   return 1 if true, 0 if false
+------------------------------------------------------------------------- */
+
+int Domain::inside_nonperiodic(double* x)
+{
+  double *lo,*hi,*period;
+  double delta[3];
+
+  if (xperiodic && yperiodic && zperiodic) return 1;
+ 
+  if (triclinic == 0) {
+    lo = boxlo;
+    hi = boxhi;
+    period = prd;
+  } else {
+    lo = boxlo_lamda;
+    hi = boxhi_lamda;
+    period = prd_lamda;
+
+    delta[0] = x[0] - boxlo[0];
+    delta[1] = x[1] - boxlo[1];
+    delta[2] = x[2] - boxlo[2];
+
+    x[0] = h_inv[0]*delta[0] + h_inv[5]*delta[1] + h_inv[4]*delta[2];
+    x[1] = h_inv[1]*delta[1] + h_inv[3]*delta[2];
+    x[2] = h_inv[2]*delta[2];
+  }
+
+  if (!xperiodic && (x[0] < lo[0] || x[0] >= hi[0])) return 0;
+  if (!yperiodic && (x[1] < lo[1] || x[1] >= hi[1])) return 0;
+  if (!zperiodic && (x[2] < lo[2] || x[2] >= hi[2])) return 0;
+  return 1;
+}
+
+/* ----------------------------------------------------------------------
    warn if image flags of any bonded atoms are inconsistent
    could be a problem when using replicate or fix rigid
 ------------------------------------------------------------------------- */

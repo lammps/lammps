a502ab3f8325f55aeff0ceeeab4669273c7e8303
Axel Kohlmeyer <akohlmey@gmail.com>
2015-06-13 21:18:12 -0400

    add box info to system output

--- src/info.cpp
+++ src/info.cpp
@@ -18,6 +18,7 @@
 #include "string.h"
 #include "info.h"
 #include "atom.h"
+#include "domain.h"
 #include "force.h"
 #include "group.h"
 #include "input.h"
@@ -27,14 +28,16 @@
 
 using namespace LAMMPS_NS;
 
+// same as in variable.cpp
+enum{INDEX,LOOP,WORLD,UNIVERSE,ULOOP,STRING,GETENV,
+     SCALARFILE,ATOMFILE,FORMAT,EQUAL,ATOM,PYTHON};
 static const char *varstyles[] = {
   "index", "loop", "world", "universe", "uloop", "string", "getenv",
   "file", "atomfile", "format", "equal", "atom", "python", "(unknown)"};
 
 static const char *mapstyles[] = { "none", "array", "hash" };
 
-enum{INDEX,LOOP,WORLD,UNIVERSE,ULOOP,STRING,GETENV,
-     SCALARFILE,ATOMFILE,FORMAT,EQUAL,ATOM,PYTHON};
+static const char bstyles[] = "pfsm";
 
 /* ---------------------------------------------------------------------- */
 
@@ -74,21 +77,31 @@ void Info::command(int narg, char **arg)
     fprintf(screen,"Units      = %s\n",update->unit_style);
     fprintf(screen,"Atom style = %s\n", atom->atom_style);
     fprintf(screen,"Atom map   = %s\n", mapstyles[atom->map_style]);
-    fprintf(screen,"Atoms     / types = " BIGINT_FORMAT " / %d\n",
-            atom->natoms, atom->ntypes);
+    if (atom->molecular > 0) {
+      const char *msg;
+      msg = (atom->molecular == 2) ? "template" : "standard";
+      fprintf(screen,"Molecule type = %s\n",msg);
+    }
+    fprintf(screen,"Atoms     | types | style: " BIGINT_FORMAT " | %d | %s\n",
+            atom->natoms, atom->ntypes, force->pair_style);
 
     if (atom->molecular > 0) {
+      const char *msg;
+      msg = force->bond_style ? force->bond_style : "none";
+      fprintf(screen,"Bonds     | types | style: " BIGINT_FORMAT " | %d | %s\n",
+              atom->nbonds, atom->nbondtypes, msg);
 
-      fprintf(screen,"Molecule type = %s\n",
-              (atom->molecular == 2) ? "template" : "standard");
-      fprintf(screen,"Bonds     / types = " BIGINT_FORMAT " / %d\n",
-              atom->nbonds, atom->nbondtypes);
-      fprintf(screen,"Angles    / types = " BIGINT_FORMAT " / %d\n",
-              atom->nangles, atom->nangletypes);
-      fprintf(screen,"Dihedrals / types = " BIGINT_FORMAT " / %d\n",
-              atom->ndihedrals, atom->ndihedraltypes);
-      fprintf(screen,"impropers / types = " BIGINT_FORMAT " / %d\n",
-              atom->nimpropers, atom->nimpropertypes);
+      msg = force->angle_style ? force->angle_style : "none";
+      fprintf(screen,"Angles    | types | style: " BIGINT_FORMAT " | %d | %s\n",
+              atom->nangles, atom->nangletypes, msg);
+
+      msg = force->dihedral_style ? force->dihedral_style : "none";
+      fprintf(screen,"Dihedrals | types | style: " BIGINT_FORMAT " | %d | %s\n",
+              atom->ndihedrals, atom->ndihedraltypes, msg);
+
+      msg = force->improper_style ? force->improper_style : "none";
+      fprintf(screen,"Impropers | types | style: " BIGINT_FORMAT " | %d | %s\n",
+              atom->nimpropers, atom->nimpropertypes, msg);
 
       const double * const special_lj   = force->special_lj;
       const double * const special_coul = force->special_coul;
@@ -99,6 +112,27 @@ void Info::command(int narg, char **arg)
                      special_coul[1],special_coul[2],special_coul[3]);
     }
 
+    fprintf(screen,"Kspace style = %s\n",
+            force->kspace ? force->kspace_style : "none");
+
+    if (domain->box_exist) {
+      fprintf(screen,"%s box: (%g x %g %g)\n",
+              domain->triclinic ? "Triclinic" : "Orthogonal",
+              domain->xprd, domain->yprd, domain->zprd);
+      fprintf(screen,"Boundaries = %c,%c %c,%c %c,%c\n",
+              bstyles[domain->boundary[0][0]],bstyles[domain->boundary[0][1]],
+              bstyles[domain->boundary[1][0]],bstyles[domain->boundary[1][1]],
+              bstyles[domain->boundary[2][0]],bstyles[domain->boundary[2][1]]);
+      fprintf(screen,"Xlo, zhi = %g, %g\n", domain->boxlo[0], domain->boxhi[0]);
+      fprintf(screen,"Ylo, zhi = %g, %g\n", domain->boxlo[1], domain->boxhi[1]);
+      fprintf(screen,"Zlo, zhi = %g, %g\n", domain->boxlo[2], domain->boxhi[2]);
+      if (domain->triclinic)
+        fprintf(screen,"Xy, xz, yz = %g, %g, %g\n",
+                domain->xy, domain->xz, domain->yz);
+
+    } else {
+      fputs("Box has not yet been created\n",screen);
+    }
   } else {
     error->all(FLERR,"Unknown info command style");
   }

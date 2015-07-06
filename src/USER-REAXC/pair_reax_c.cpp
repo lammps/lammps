f827b4419dbec6878a63e0c8927120eb774c066e
athomps <athomps@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2015-07-06 17:08:35 +0000

    Added DATE reporting for parameter file
    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@13534 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/USER-REAXC/pair_reax_c.cpp
+++ src/USER-REAXC/pair_reax_c.cpp
@@ -279,7 +279,16 @@ void PairReaxC::coeff( int nargs, char **args )
 
   // read ffield file
 
-  Read_Force_Field(args[2], &(system->reax_param), control);
+  char *file = args[2];
+  FILE *fp;
+  fp = force->open_potential(file);
+  if (fp != NULL) 
+    Read_Force_Field(fp, &(system->reax_param), control);
+  else if (comm->me == 0) {
+      char str[128];
+      sprintf(str,"Cannot open ReaxFF potential file %s",file);
+      error->one(FLERR,str);
+  }
 
   // read args that map atom types to elements in potential file
   // map[i] = which element the Ith atom type is, -1 if NULL

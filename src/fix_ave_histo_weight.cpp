173df37b38ad617faf6ada5aee6868eb6374d17a
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2015-07-22 16:31:13 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@13691 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/fix_ave_histo_weight.cpp
+++ src/fix_ave_histo_weight.cpp
@@ -11,6 +11,10 @@
    See the README file in the top-level LAMMPS directory.
 ------------------------------------------------------------------------- */
 
+/* ----------------------------------------------------------------------
+   Contributing author: Shawn Coleman (ARL)
+------------------------------------------------------------------------- */
+
 #include "stdlib.h"
 #include "string.h"
 #include "unistd.h"

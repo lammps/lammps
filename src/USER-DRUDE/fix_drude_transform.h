c51a96a2ddde9463441d3b0c569d671956f1ee0d
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2015-07-27 15:00:00 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@13731 f3b2605a-c512-4ea7-a41b-209d697bcdaa

---
+++ src/USER-DRUDE/fix_drude_transform.h
@@ -0,0 +1,52 @@
+/* -*- c++ -*- ----------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef FIX_CLASS
+
+FixStyle(drude/transform/direct,FixDrudeTransform<false>)
+FixStyle(drude/transform/inverse,FixDrudeTransform<true>)
+
+#else
+
+#ifndef LMP_FIX_DRUDE_TRANSFORM_H
+#define LMP_FIX_DRUDE_TRANSFORM_H
+
+#include "fix.h"
+#include "fix_drude.h"
+
+namespace LAMMPS_NS {
+
+template <bool inverse>
+class FixDrudeTransform : public Fix {
+ public:
+  FixDrudeTransform<inverse>(class LAMMPS *, int, char **);
+  ~FixDrudeTransform<inverse>();
+  int setmask();
+  void init();
+  void setup(int vflag);
+  void reduced_to_real();
+  void real_to_reduced();
+  void initial_integrate(int vflag);
+  void final_integrate();
+  int pack_forward_comm(int n, int *list, double *buf, int pbc_flag, int *pbc);
+  void unpack_forward_comm(int n, int first, double *buf);
+ protected:
+  double * mcoeff;
+  FixDrude * fix_drude;
+};
+
+}
+
+#endif
+#endif
+

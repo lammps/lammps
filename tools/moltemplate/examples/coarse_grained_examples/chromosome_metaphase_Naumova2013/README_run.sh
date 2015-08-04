57ed470003462d85385712b7e483fa5e21476cb4
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2015-08-04 18:48:13 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@13825 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- tools/moltemplate/examples/coarse_grained_examples/chromosome_metaphase_Naumova2013/README_run.sh
+++
@@ -1,7 +0,0 @@
-# Run lammps using the following 3 commands:
-# (assuming "lmp_linux" is the name of your LAMMPS binary)
-
-lmp_linux -i run.in.min
-lmp_linux -i run.in.stage1
-lmp_linux -i run.in.stage2
-

3bbba19e854a10f071cc997c12f868a397f3170f
Axel Kohlmeyer <akohlmey@gmail.com>
2015-10-27 07:07:38 -0400

    correct bug in cleanup script and update permissions accordingly

--- cleanup.sh
+++ cleanup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # do some cleaning up to fix permissions and remove cruft
 
-for f in `find ./ -name \*.orig -or -name \*~ -or -name \*.rej -print`
+for f in `find ./ -name \*.orig -print -or -name \*~ -print -or -name \*.rej -print`
 do rm -v $f
 done
 
-for f in `find ./ -name \*.cpp -or -name \*.c -or -name \*.h -print`
+for f in `find ./ -name \*.cpp -print -or -name \*.c -print -or -name \*.h -print`
 do chmod -x $f
 done
 

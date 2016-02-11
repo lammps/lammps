707dee68c9d13cc53bf60e75bafdc5800132e9c6
stamoor <stamoor@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2016-02-11 16:40:45 +0000

    Adding Kokkos error check
    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@14587 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/KOKKOS/kokkos.cpp
+++ src/KOKKOS/kokkos.cpp
@@ -159,7 +159,7 @@ void KokkosLMP::accelerator(int narg, char **arg)
       else if (strcmp(arg[iarg+1],"n2") == 0) neighflag = N2;
       else if (strcmp(arg[iarg+1],"full/cluster") == 0) neighflag = FULLCLUSTER;
       else error->all(FLERR,"Illegal package kokkos command");
-      if (neighflag = HALF && num_threads == 0 || ngpu == 0)
+      if (neighflag == HALF && num_threads > 1 || ngpu > 0)
         error->all(FLERR,"Must use Kokkos half/thread or full neighbor list with threads or GPU");
       iarg += 2;
     } else if (strcmp(arg[iarg],"binsize") == 0) {

872c75754fce7bd99d922e3f9133c14eeff2e321
Axel Kohlmeyer <akohlmey@gmail.com>
2016-02-15 00:23:55 -0500

    fix typos: pacakge -> package

--- src/comm.h
+++ src/comm.h
@@ -147,7 +147,7 @@ class Comm : protected Pointers {
 W: OMP_NUM_THREADS environment is not set.
 
 This environment variable must be set appropriately to use the
-USER-OMP pacakge.
+USER-OMP package.
 
 E: Illegal ... command
 

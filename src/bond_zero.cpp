577af878eaf4e09eca58a5b1dc0d3e5b3765b1e3
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2016-02-22 23:14:55 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@14664 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/bond_zero.cpp
+++ src/bond_zero.cpp
@@ -11,13 +11,9 @@
    See the README file in the top-level LAMMPS directory.
 ------------------------------------------------------------------------- */
 
-/* ----------------------------------------------------------------------
-   Contributing author: Carsten Svaneborg (SDU)
-------------------------------------------------------------------------- */
-
 #include <math.h>
 #include <stdlib.h>
-#include "angle_zero.h"
+#include "bond_zero.h"
 #include "atom.h"
 #include "force.h"
 #include "memory.h"
@@ -27,11 +23,11 @@ using namespace LAMMPS_NS;
 
 /* ---------------------------------------------------------------------- */
 
-AngleZero::AngleZero(LAMMPS *lmp) : Angle(lmp) {}
+BondZero::BondZero(LAMMPS *lmp) : Bond(lmp) {}
 
 /* ---------------------------------------------------------------------- */
 
-AngleZero::~AngleZero()
+BondZero::~BondZero()
 {
   if (allocated && !copymode) {
     memory->destroy(setflag);
@@ -40,7 +36,7 @@ AngleZero::~AngleZero()
 
 /* ---------------------------------------------------------------------- */
 
-void AngleZero::compute(int eflag, int vflag)
+void BondZero::compute(int eflag, int vflag)
 {
   if (eflag || vflag) ev_setup(eflag,vflag);
   else evflag = 0;
@@ -48,12 +44,12 @@ void AngleZero::compute(int eflag, int vflag)
 
 /* ---------------------------------------------------------------------- */
 
-void AngleZero::allocate()
+void BondZero::allocate()
 {
   allocated = 1;
-  int n = atom->nangletypes;
+  int n = atom->nbondtypes;
 
-  memory->create(setflag,n+1,"angle:setflag");
+  memory->create(setflag,n+1,"bond:setflag");
   for (int i = 1; i <= n; i++) setflag[i] = 0;
 }
 
@@ -61,13 +57,13 @@ void AngleZero::allocate()
    set coeffs for one or more types
 ------------------------------------------------------------------------- */
 
-void AngleZero::coeff(int narg, char **arg)
+void BondZero::coeff(int narg, char **arg)
 {
-  if (narg != 1) error->all(FLERR,"Incorrect args for angle coefficients");
+  if (narg != 1) error->all(FLERR,"Incorrect args for bond coefficients");
   if (!allocated) allocate();
 
   int ilo,ihi;
-  force->bounds(arg[0],atom->nangletypes,ilo,ihi);
+  force->bounds(arg[0],atom->nbondtypes,ilo,ihi);
 
   int count = 0;
   for (int i = ilo; i <= ihi; i++) {
@@ -75,12 +71,14 @@ void AngleZero::coeff(int narg, char **arg)
     count++;
   }
 
-  if (count == 0) error->all(FLERR,"Incorrect args for angle coefficients");
+  if (count == 0) error->all(FLERR,"Incorrect args for bond coefficients");
 }
 
-/* ---------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------
+   return an equilbrium bond length
+------------------------------------------------------------------------- */
 
-double AngleZero::equilibrium_angle(int i)
+double BondZero::equilibrium_distance(int i)
 {
   return 0.0;
 }
@@ -89,21 +87,22 @@ double AngleZero::equilibrium_angle(int i)
    proc 0 writes out coeffs to restart file
 ------------------------------------------------------------------------- */
 
-void AngleZero::write_restart(FILE *fp) {}
+void BondZero::write_restart(FILE *fp) {}
 
 /* ----------------------------------------------------------------------
    proc 0 reads coeffs from restart file, bcasts them
 ------------------------------------------------------------------------- */
 
-void AngleZero::read_restart(FILE *fp)
+void BondZero::read_restart(FILE *fp)
 {
   allocate();
-  for (int i = 1; i <= atom->nangletypes; i++) setflag[i] = 1;
+  for (int i = 1; i <= atom->nbondtypes; i++) setflag[i] = 1;
 }
 
 /* ---------------------------------------------------------------------- */
 
-double AngleZero::single(int type, int i1, int i2, int i3)
+double BondZero::single(int type, double rsq, int i, int j,
+                        double &fforce)
 {
   return 0.0;
 }

40a438575d6f23a6e1030ff07d220502d3ca473a
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2016-03-22 13:44:25 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@14776 f3b2605a-c512-4ea7-a41b-209d697bcdaa

---
+++ src/USER-OMP/pair_morse_smooth_linear_omp.h
@@ -0,0 +1,62 @@
+/* -*- c++ -*- ----------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef PAIR_CLASS
+
+PairStyle(morse/smooth/linear/omp,PairMorseSmoothLinearOMP)
+
+#else
+
+#ifndef LMP_PAIR_MORSE_SMOOTH_LINEAR_OMP_H
+#define LMP_PAIR_MORSE_SMOOTH_LINEAR_OMP_H
+
+#include "pair_morse_smooth_linear.h"
+#include "thr_omp.h"
+
+namespace LAMMPS_NS {
+
+class PairMorseSmoothLinearOMP : public PairMorseSmoothLinear, public ThrOMP {
+ public:
+  PairMorseSmoothLinearOMP(class LAMMPS *);
+
+  virtual void compute(int, int);
+  virtual double memory_usage();
+
+ private:
+  template <int EVFLAG, int EFLAG, int NEWTON_PAIR>
+  void eval( int ifrom, int ito, ThrData * const thr );
+};
+
+}
+
+#endif
+#endif
+
+/* ERROR/WARNING messages:
+
+E: Illegal ... command
+
+Self-explanatory.  Check the input script syntax and compare to the
+documentation for the command.  You can use -echo screen as a
+command-line option when running LAMMPS to see the offending line.
+
+E: Incorrect args for pair coefficients
+
+Self-explanatory.  Check the input script or data file.
+
+E: All pair coeffs are not set
+
+All pair coefficients must be set in the data file or by the
+pair_coeff command before running a simulation.
+
+*/

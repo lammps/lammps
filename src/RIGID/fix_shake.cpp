5253f2aae1802e34e141f02c78a6da71bd57270e
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2016-05-10 22:26:37 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@15019 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/RIGID/fix_shake.cpp
+++ src/RIGID/fix_shake.cpp
@@ -17,6 +17,7 @@
 #include <string.h>
 #include <stdio.h>
 #include "fix_shake.h"
+#include "fix_rattle.h"
 #include "atom.h"
 #include "atom_vec.h"
 #include "molecule.h"
@@ -56,7 +57,6 @@ FixShake::FixShake(LAMMPS *lmp, int narg, char **arg) :
   virial_flag = 1;
   create_attribute = 1;
   dof_flag = 1;
-
   // error check
 
   molecular = atom->molecular;
@@ -70,6 +70,9 @@ FixShake::FixShake(LAMMPS *lmp, int narg, char **arg) :
   shake_atom = NULL;
   shake_type = NULL;
   xshake = NULL;
+
+  ftmp = NULL; 
+  vtmp = NULL;
 
   grow_arrays(atom->nmax);
   atom->add_callback(0);
@@ -206,7 +209,6 @@ FixShake::FixShake(LAMMPS *lmp, int narg, char **arg) :
   // SHAKE vs RATTLE
 
   rattle = 0;
-  vflag_post_force = 0;
 
   // identify all SHAKE clusters
 
@@ -255,6 +257,9 @@ FixShake::~FixShake()
   memory->destroy(shake_atom);
   memory->destroy(shake_type);
   memory->destroy(xshake);
+  memory->destroy(ftmp);
+  memory->destroy(vtmp);
+
 
   delete [] bond_flag;
   delete [] angle_flag;
@@ -433,30 +438,27 @@ void FixShake::setup(int vflag)
       next_output = (ntimestep/output_every)*output_every + output_every;
   } else next_output = -1;
 
-  // half timestep constraint on pre-step, full timestep thereafter
 
-  if (strstr(update->integrate_style,"verlet")) {
-    respa   = 0;
-    dtv     = update->dt;
-    dtfsq   = 0.5 * update->dt * update->dt * force->ftm2v;
-    FixShake::post_force(vflag);
-    if (!rattle) dtfsq = update->dt * update->dt * force->ftm2v;
+  // set respa to 0 if verlet is used and to 1 otherwise
 
-  } else {
-    respa  = 1;
-    dtv = step_respa[0];
-    dtf_innerhalf = 0.5 * step_respa[0] * force->ftm2v;
-    dtf_inner = dtf_innerhalf;
+  if (strstr(update->integrate_style,"verlet")) 
+    respa = 0;
+  else
+    respa = 1;
 
-    // apply correction to all rRESPA levels
 
-    for (int ilevel = 0; ilevel < nlevels_respa; ilevel++) {
-      ((Respa *) update->integrate)->copy_flevel_f(ilevel);
-      FixShake::post_force_respa(vflag,ilevel,loop_respa[ilevel]-1);
-      ((Respa *) update->integrate)->copy_f_flevel(ilevel);
-    }
-    if (!rattle) dtf_inner = step_respa[0] * force->ftm2v;
-  }
+  // correct geometry of cluster if necessary
+
+  correct_coordinates(vflag);
+
+  // remove velocities along any bonds
+
+  correct_velocities();
+
+  // precalculate constraining forces for first integration step
+
+  shake_end_of_step(vflag);
+
 }
 
 /* ----------------------------------------------------------------------
@@ -499,7 +501,7 @@ void FixShake::pre_neighbor()
         atom2 = atom->map(shake_atom[i][1]);
         if (atom1 == -1 || atom2 == -1) {
           char str[128];
-          sprintf(str,"Shake atoms " TAGINT_FORMAT " " TAGINT_FORMAT
+          sprintf(str,"Shake atoms " TAGINT_FORMAT " " TAGINT_FORMAT 
                   " missing on proc %d at step " BIGINT_FORMAT,
                   shake_atom[i][0],shake_atom[i][1],me,update->ntimestep);
           error->one(FLERR,str);
@@ -511,7 +513,7 @@ void FixShake::pre_neighbor()
         atom3 = atom->map(shake_atom[i][2]);
         if (atom1 == -1 || atom2 == -1 || atom3 == -1) {
           char str[128];
-          sprintf(str,"Shake atoms "
+          sprintf(str,"Shake atoms " 
                   TAGINT_FORMAT " " TAGINT_FORMAT " " TAGINT_FORMAT
                   " missing on proc %d at step " BIGINT_FORMAT,
                   shake_atom[i][0],shake_atom[i][1],shake_atom[i][2],
@@ -526,8 +528,8 @@ void FixShake::pre_neighbor()
         atom4 = atom->map(shake_atom[i][3]);
         if (atom1 == -1 || atom2 == -1 || atom3 == -1 || atom4 == -1) {
           char str[128];
-          sprintf(str,"Shake atoms "
-                  TAGINT_FORMAT " " TAGINT_FORMAT " "
+          sprintf(str,"Shake atoms " 
+                  TAGINT_FORMAT " " TAGINT_FORMAT " " 
                   TAGINT_FORMAT " " TAGINT_FORMAT
                   " missing on proc %d at step " BIGINT_FORMAT,
                   shake_atom[i][0],shake_atom[i][1],
@@ -570,9 +572,8 @@ void FixShake::post_force(int vflag)
     else if (shake_flag[m] == 4) shake4(m);
     else shake3angle(m);
   }
-
+  
   // store vflag for coordinate_constraints_end_of_step()
-
   vflag_post_force = vflag;
 }
 
@@ -620,7 +621,6 @@ void FixShake::post_force_respa(int vflag, int ilevel, int iloop)
   }
 
   // store vflag for coordinate_constraints_end_of_step()
-
   vflag_post_force = vflag;
 }
 
@@ -670,7 +670,7 @@ void FixShake::find_clusters()
   tagint tagprev;
   double massone;
   tagint *buf;
-
+  
   if (me == 0 && screen) {
     if (!rattle) fprintf(screen,"Finding SHAKE clusters ...\n");
     else fprintf(screen,"Finding RATTLE clusters ...\n");
@@ -685,7 +685,7 @@ void FixShake::find_clusters()
   double *rmass = atom->rmass;
   int **nspecial = atom->nspecial;
   tagint **special = atom->special;
-
+  
   int *molindex = atom->molindex;
   int *molatom = atom->molatom;
 
@@ -956,7 +956,7 @@ void FixShake::find_clusters()
   comm->ring(size,sizeof(tagint),buf,2,ring_nshake,buf);
 
   // store partner info returned to me
-
+  
   m = 0;
   while (m < size) {
     i = atom->map(buf[m]);
@@ -2289,7 +2289,7 @@ int FixShake::bondtype_findset(int i, tagint n1, tagint n2, int setflag)
     tagint *batom = atommols[imol]->bond_atom[iatom];
     btype = atommols[imol]->bond_type[iatom];
     nbonds = atommols[imol]->num_bond[iatom];
-
+    
     for (m = 0; m < nbonds; m++) {
       if (n1 == tag[i] && n2 == batom[m]+tagprev) break;
       if (n1 == batom[m]+tagprev && n2 == tag[i]) break;
@@ -2346,7 +2346,7 @@ int FixShake::angletype_findset(int i, tagint n1, tagint n2, int setflag)
     tagint *aatom3 = atommols[imol]->angle_atom3[iatom];
     atype = atommols[imol]->angle_type[iatom];
     nangles = atommols[imol]->num_angle[iatom];
-
+    
     for (m = 0; m < nangles; m++) {
       if (n1 == aatom1[m]+tagprev && n2 == aatom3[m]+tagprev) break;
       if (n1 == aatom3[m]+tagprev && n2 == aatom1[m]+tagprev) break;
@@ -2397,6 +2397,11 @@ void FixShake::grow_arrays(int nmax)
   memory->grow(shake_type,nmax,3,"shake:shake_type");
   memory->destroy(xshake);
   memory->create(xshake,nmax,3,"shake:xshake");
+  memory->destroy(ftmp);
+  memory->create(ftmp,nmax,3,"shake:ftmp");
+  memory->destroy(vtmp);
+  memory->create(vtmp,nmax,3,"shake:vtmp");
+
 }
 
 /* ----------------------------------------------------------------------
@@ -2456,7 +2461,7 @@ void FixShake::update_arrays(int i, int atom_offset)
     shake_atom[i][0] += atom_offset;
     shake_atom[i][1] += atom_offset;
     shake_atom[i][2] += atom_offset;
-  } else if (flag == 2) {
+  } else if (flag == 2) { 
     shake_atom[i][0] += atom_offset;
     shake_atom[i][1] += atom_offset;
   } else if (flag == 3) {
@@ -2602,7 +2607,7 @@ int FixShake::unpack_exchange(int nlocal, double *buf)
 
 /* ---------------------------------------------------------------------- */
 
-int FixShake::pack_forward_comm(int n, int *list, double *buf,
+int FixShake::pack_forward_comm(int n, int *list, double *buf, 
                                 int pbc_flag, int *pbc)
 {
   int i,j,m;
@@ -2683,25 +2688,116 @@ void *FixShake::extract(const char *str, int &dim)
 
 
 /* ----------------------------------------------------------------------
-   wrapper method for end_of_step fixes which modify the coordinates
+   Add coordinate constraining forces; this method is called
+   at the end of a timestep.
 ------------------------------------------------------------------------- */
 
-void FixShake::coordinate_constraints_end_of_step() {
+void FixShake::shake_end_of_step(int vflag) {
+
   if (!respa) {
+    dtv     = update->dt;
     dtfsq   = 0.5 * update->dt * update->dt * force->ftm2v;
-    FixShake::post_force(vflag_post_force);
+    FixShake::post_force(vflag);
     if (!rattle) dtfsq = update->dt * update->dt * force->ftm2v;
-  }
-  else {
+
+  } else {
+    dtv = step_respa[0];
     dtf_innerhalf = 0.5 * step_respa[0] * force->ftm2v;
     dtf_inner = dtf_innerhalf;
+
     // apply correction to all rRESPA levels
+
     for (int ilevel = 0; ilevel < nlevels_respa; ilevel++) {
       ((Respa *) update->integrate)->copy_flevel_f(ilevel);
-      FixShake::post_force_respa(vflag_post_force,ilevel,loop_respa[ilevel]-1);
+      FixShake::post_force_respa(vflag,ilevel,loop_respa[ilevel]-1);
       ((Respa *) update->integrate)->copy_f_flevel(ilevel);
     }
     if (!rattle) dtf_inner = step_respa[0] * force->ftm2v;
   }
 }
 
+
+
+/* ----------------------------------------------------------------------
+   wrapper method for end_of_step fixes which modify velocities
+------------------------------------------------------------------------- */
+
+void FixShake::correct_velocities() { 
+
+}
+
+
+/* ----------------------------------------------------------------------
+   Calculate constraining forces based on the current configuration
+   and change coordinates.
+------------------------------------------------------------------------- */
+
+void FixShake::correct_coordinates(int vflag) { 
+   
+  // save current forces and velocities so that you 
+  // initialise them to zero such that FixShake::unconstrained_coordinate_update has no effect 
+
+  for (int j=0; j<nlocal; j++) {
+    for (int k=0; k<3; k++) {
+
+      // store current value of forces and velocities
+
+      ftmp[j][k] = f[j][k];
+      vtmp[j][k] = v[j][k];
+
+      // set f and v to zero for SHAKE
+
+      v[j][k] = 0;
+      f[j][k] = 0;
+    }
+  }
+
+  // call SHAKE to correct the coordinates which were updated without constraints
+  // IMPORTANT: use 1 as argument and thereby enforce velocity Verlet
+
+  dtfsq   = 0.5 * update->dt * update->dt * force->ftm2v;
+  FixShake::post_force(vflag);
+
+  // integrate coordiantes: x' = xnp1 + dt^2/2m_i * f, where f is the constraining force
+  // NOTE: After this command, the coordinates geometry of the molecules will be correct! 
+
+  double dtfmsq; 
+  if (rmass) {  
+    for (int i = 0; i < nlocal; i++) {  
+      dtfmsq = dtfsq/ rmass[i];  
+      x[i][0] = x[i][0] + dtfmsq*f[i][0];  
+      x[i][1] = x[i][1] + dtfmsq*f[i][1];  
+      x[i][2] = x[i][2] + dtfmsq*f[i][2];  
+    }  
+  }  
+  else {  
+    for (int i = 0; i < nlocal; i++) {  
+      dtfmsq = dtfsq / mass[type[i]];  
+      x[i][0] = x[i][0] + dtfmsq*f[i][0];  
+      x[i][1] = x[i][1] + dtfmsq*f[i][1];  
+      x[i][2] = x[i][2] + dtfmsq*f[i][2];  
+    }  
+  }
+
+  // copy forces and velocities back
+
+  for (int j=0; j<nlocal; j++) {
+    for (int k=0; k<3; k++) {
+      f[j][k] = ftmp[j][k];
+      v[j][k] = vtmp[j][k];
+    }
+  }
+
+  if (!rattle) dtfsq = update->dt * update->dt * force->ftm2v;
+
+  // communicate changes
+  // NOTE: for compatibility xshake is temporarily set to x, such that pack/unpack_forward 
+  //       can be used for communicating the coordinates. 
+
+  double **xtmp = xshake;
+  xshake = x;
+  if (nprocs > 1) {
+    comm->forward_comm_fix(this);
+  }
+  xshake = xtmp;
+}

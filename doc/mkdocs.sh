73c70d98dbf3bf4c846796f47aadfe7aa6f3f2b3
Axel Kohlmeyer <akohlmey@gmail.com>
2016-05-10 17:16:10 -0400

    update document building for LAMMPS-ICMS branch

--- doc/mkdocs.sh
+++ doc/mkdocs.sh
@@ -2,28 +2,27 @@
 
 # update equations
 
-for s in doc/Eqs/*.tex 
+for s in src/Eqs/*.tex 
 do \
 	n=`basename $s .tex`
-	if [ ! -e doc/Eqs/${n}.jpg ] || [ $s -nt doc/Eqs/${n}.jpg ]
+	if [ ! -e src/Eqs/${n}.jpg ] || [ $s -nt src/Eqs/${n}.jpg ]
 	then
 		pdflatex $s
-		pdftoppm ${n}.pdf | pnmcrop | pnmtojpeg > doc/Eqs/${n}.jpg
+		pdftoppm ${n}.pdf | pnmcrop | pnmtojpeg > src/Eqs/${n}.jpg
         rm -f ${n}.aux ${n}.log ${n}.pdf
 	fi
 done
 
-# build txt2html tool
-make -C txt2html
-
 # convert all .txt files to html
-./txt2html/txt2html -b doc/*.txt
+make html
 
 # check if we have any new html files,
 # that are not yet in the book file.
-for s in `echo doc/*.txt | sed -e 's,\.txt,\.html,g'`
+for s in `echo src/*.txt | sed -e 's,src/,html/,g' -e 's,\.txt,\.html,g'`
 do \
   grep -q $s lammps.book || echo doc file $s missing in lammps.book
 done
 htmldoc --batch lammps.book
+
+git co -- html src
 

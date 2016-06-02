195c521b8a8c830371aa4762c675e564bd0dd666
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2016-06-02 14:14:17 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@15104 f3b2605a-c512-4ea7-a41b-209d697bcdaa

---
+++ src/fix_deprecated.h
@@ -0,0 +1,51 @@
+/* -*- c++ -*- ----------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef FIX_CLASS
+
+// list all deprecated and removed fix styles here
+
+FixStyle(ave/spatial,FixDeprecated)
+FixStyle(ave/spatial/sphere,FixDeprecated)
+
+#else
+
+#ifndef LMP_FIX_DEPRECATED_H
+#define LMP_FIX_DEPRECATED_H
+
+#include "fix.h"
+
+namespace LAMMPS_NS {
+
+class FixDeprecated : public Fix {
+ public:
+  FixDeprecated(class LAMMPS *, int, char **);
+  ~FixDeprecated() {}
+  int setmask() {return 0;}
+  void init() {}
+};
+
+}
+
+#endif
+#endif
+
+/* ERROR/WARNING messages:
+
+E: The fix ave/spatial command has been removed from LAMMPS
+
+It has been replaced by the more flexible fix ave/chunk and compute
+chunk/atom commands.  All the fix ave/spatial keywords and options are
+available in those two newer commands.
+
+*/

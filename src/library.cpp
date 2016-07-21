ae7af38eacfaeefc4de8076a76a8b9b53083fb92
Axel Kohlmeyer <akohlmey@gmail.com>
2016-07-21 19:41:35 -0400

    give access to nbonds, nangles, ndihedral, nimpropers in Atom via lammps_extract_global()

--- src/library.cpp
+++ src/library.cpp
@@ -149,6 +149,10 @@ void *lammps_extract_global(void *ptr, char *name)
   if (strcmp(name,"xz") == 0) return (void *) &lmp->domain->xz;
   if (strcmp(name,"yz") == 0) return (void *) &lmp->domain->yz;
   if (strcmp(name,"natoms") == 0) return (void *) &lmp->atom->natoms;
+  if (strcmp(name,"nbonds") == 0) return (void *) &lmp->atom->nbonds;
+  if (strcmp(name,"nangles") == 0) return (void *) &lmp->atom->nangles;
+  if (strcmp(name,"ndihedrals") == 0) return (void *) &lmp->atom->ndihedrals;
+  if (strcmp(name,"nimpropers") == 0) return (void *) &lmp->atom->nimpropers;
   if (strcmp(name,"nlocal") == 0) return (void *) &lmp->atom->nlocal;
   if (strcmp(name,"ntimestep") == 0) return (void *) &lmp->update->ntimestep;
 

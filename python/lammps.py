57d7a4a6ad5ffad17b17eab2cddeecf4979bfb4e
Richard Berger <richard.berger@temple.edu>
2016-08-10 14:35:48 -0400

    Add utility function to IPyLammps for embedding videos

--- python/lammps.py
+++ python/lammps.py
@@ -698,3 +698,7 @@ class IPyLammps(PyLammps):
     self.write_dump(*cmd_args)
     from IPython.core.display import Image
     return Image('snapshot.png')
+
+  def video(self, filename):
+    from IPython.display import HTML
+    return HTML("<video controls><source src=\"" + filename + "\"></video>")

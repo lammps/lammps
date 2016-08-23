d007faca515fe70965968faca45ba473d105b488
stamoor <stamoor@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2016-08-23 16:07:26 +0000

    Fixing Kokkos output for number of OpenMP threads
    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@15488 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/finish.cpp
+++ src/finish.cpp
@@ -162,10 +162,17 @@ void Finish::end(int flag)
       if (screen) fprintf(screen,fmt2,cpu_loop,nprocs,nthreads);
       if (logfile) fprintf(logfile,fmt2,cpu_loop,nprocs,nthreads);
 #else
-      const char fmt2[] =
-        "%.1f%% CPU use with %d MPI tasks x no OpenMP threads\n";
-      if (screen) fprintf(screen,fmt2,cpu_loop,nprocs);
-      if (logfile) fprintf(logfile,fmt2,cpu_loop,nprocs);
+      if (lmp->kokkos) {
+        const char fmt2[] =
+          "%.1f%% CPU use with %d MPI tasks x %d OpenMP threads\n";
+        if (screen) fprintf(screen,fmt2,cpu_loop,nprocs,lmp->kokkos->num_threads);
+        if (logfile) fprintf(logfile,fmt2,cpu_loop,nprocs,lmp->kokkos->num_threads);
+      } else {
+        const char fmt2[] =
+          "%.1f%% CPU use with %d MPI tasks x no OpenMP threads\n";
+        if (screen) fprintf(screen,fmt2,cpu_loop,nprocs);
+        if (logfile) fprintf(logfile,fmt2,cpu_loop,nprocs);
+      }
 #endif
 
     }

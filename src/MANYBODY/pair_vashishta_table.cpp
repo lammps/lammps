ad2ccf6f27253f3aed45ae386c8387070061541c
Axel Kohlmeyer <akohlmey@gmail.com>
2016-09-08 17:22:03 -0400

    implement vashishta/table/omp in USER-OMP
    (cherry picked from commit 4bb42be3cc09b4773f750821bb61a857bf6821bc)

--- src/MANYBODY/pair_vashishta_table.cpp
+++ src/MANYBODY/pair_vashishta_table.cpp
@@ -201,7 +201,7 @@ void PairVashishtaTable::compute(int eflag, int vflag)
         f[k][1] += fk[1];
         f[k][2] += fk[2];
 
-	if (evflag) ev_tally3(i,j,k,evdwl,0.0,fj,fk,delr1,delr2);
+        if (evflag) ev_tally3(i,j,k,evdwl,0.0,fj,fk,delr1,delr2);
       }
     }
   }

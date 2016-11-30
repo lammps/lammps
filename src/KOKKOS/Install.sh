6e6776f39635b1b69dab532bced2b0d95f150d62
Tim Mattox <timothy.mattox@engilitycorp.com>
2016-11-30 16:25:07 -0500

    Finish moving/integrating atom_vec_dpd_kokkos into the Kokkos package

--- src/KOKKOS/Install.sh
+++ src/KOKKOS/Install.sh
@@ -47,6 +47,8 @@ action atom_vec_bond_kokkos.cpp atom_vec_bond.cpp
 action atom_vec_bond_kokkos.h atom_vec_bond.h
 action atom_vec_charge_kokkos.cpp
 action atom_vec_charge_kokkos.h
+action atom_vec_dpd_kokkos.cpp atom_vec_dpd.cpp
+action atom_vec_dpd_kokkos.h atom_vec_dpd.h
 action atom_vec_full_kokkos.cpp atom_vec_full.cpp
 action atom_vec_full_kokkos.h atom_vec_full.h
 action atom_vec_kokkos.cpp

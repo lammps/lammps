73394800954d3201781a16a7af0215318ac19e7b
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2016-12-17 00:46:28 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@15952 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/neigh_list.cpp
+++ src/neigh_list.cpp
@@ -187,20 +187,16 @@ void NeighList::setup_pages(int pgsize_caller, int oneatom_caller)
 
 /* ----------------------------------------------------------------------
    grow per-atom data to allow for nlocal/nall atoms
-   skipped if a copy list
    for parent lists:
      also trigger grow in child list(s) which are not built themselves
      gran calls grow() in granhistory
      respaouter calls grow() in respainner, respamiddle
    triggered by neighbor list build
+   not called if a copy list
 ------------------------------------------------------------------------- */
 
 void NeighList::grow(int nlocal, int nall)
 {
-  // skip if a copy list
-
-  if (copy) return;
-
   // trigger grow() in children before possible return
 
   if (listgranhistory) listgranhistory->grow(nlocal,nall);

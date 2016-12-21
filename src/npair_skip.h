4732f905216845b55a41a5cce7cca380f4668383
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2016-12-21 16:44:36 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@15956 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/npair_skip.h
+++ src/npair_skip.h
@@ -15,7 +15,8 @@
 
 NPairStyle(skip,
            NPairSkip,
-           NP_SKIP | NP_HALF | NP_FULL | NP_NSQ | NP_BIN | NP_MULTI | 
+           NP_SKIP | NP_HALF | NP_FULL | NP_HALFFULL |
+           NP_NSQ | NP_BIN | NP_MULTI | 
            NP_NEWTON | NP_NEWTOFF | NP_ORTHO | NP_TRI)
 
 #else

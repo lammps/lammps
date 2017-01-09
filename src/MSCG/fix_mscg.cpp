de83ad9df14db058dc026aeffbe2865f2a915699
sjplimp <sjplimp@f3b2605a-c512-4ea7-a41b-209d697bcdaa>
2017-01-09 20:11:26 +0000

    git-svn-id: svn://svn.icms.temple.edu/lammps-ro/trunk@16003 f3b2605a-c512-4ea7-a41b-209d697bcdaa

--- src/MSCG/fix_mscg.cpp
+++ src/MSCG/fix_mscg.cpp
@@ -11,6 +11,10 @@
    See the README file in the top-level LAMMPS directory.
 ------------------------------------------------------------------------- */
 
+/* ----------------------------------------------------------------------
+   Contributing authors: Lauren Abbott (Sandia)
+------------------------------------------------------------------------- */
+
 #include <mpi.h>
 #include <string.h>
 #include <stdlib.h>

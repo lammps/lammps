61cff85435f440df9398266d6169442fc18ad13a
Axel Kohlmeyer <akohlmey@gmail.com>
2017-01-13 14:35:35 -0500

    avoid not only division by zero, but also computing variance for short runs with insufficient resolution

--- src/finish.cpp
+++ src/finish.cpp
@@ -912,7 +912,7 @@ void mpi_timings(const char *label, Timer *t, enum Timer::ttype tt,
   time_cpu = tmp/nprocs*100.0;
 
   // % variance from the average as measure of load imbalance
-  if ((time > 0.0) && ((time_sq/time - time) > 1.0e-10))
+  if ((time > 0.001) && ((time_sq/time - time) > 1.0e-10))
     time_sq = sqrt(time_sq/time - time)*100.0;
   else
     time_sq = 0.0;
@@ -964,7 +964,7 @@ void omp_times(FixOMP *fix, const char *label, enum Timer::ttype which,
   time_std /= nthreads;
   time_total /= nthreads;
 
-  if ((time_avg > 0.0) && ((time_std/time_avg -time_avg) > 1.0e-10))
+  if ((time_avg > 0.001) && ((time_std/time_avg -time_avg) > 1.0e-10))
     time_std = sqrt(time_std/time_avg - time_avg)*100.0;
   else
     time_std = 0.0;

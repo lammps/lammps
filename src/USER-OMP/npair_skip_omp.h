31976d1dee2dcbbacddd97e0a5f194d8cefe8814
Axel Kohlmeyer <akohlmey@gmail.com>
2017-01-23 19:20:05 -0500

    skip list definition was missing NP_HALFFULL flag

--- src/USER-OMP/npair_skip_omp.h
+++ src/USER-OMP/npair_skip_omp.h
@@ -18,7 +18,8 @@
 
 NPairStyle(skip/omp,
            NPairSkip,
-           NP_SKIP | NP_HALF | NP_FULL | NP_NSQ | NP_BIN | NP_MULTI |
+           NP_SKIP | NP_HALF | NP_FULL | NP_HALFFULL |
+           NP_NSQ | NP_BIN | NP_MULTI |
            NP_NEWTON | NP_NEWTOFF | NP_ORTHO | NP_TRI | NP_OMP)
 
 NPairStyle(skip/half/respa/omp,

d6800405a518d957e438ee959852b2bf38c9997e
Richard Berger <richard.berger@temple.edu>
2017-03-07 00:57:23 -0500

    Fix more typos in examples

--- python/examples/trivial.py
+++ python/examples/trivial.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python -i
-# preceeding line should have path for Python on your machine
+# preceding line should have path for Python on your machine
 
 # trivial.py
 # Purpose: run a LAMMPS input script via Python

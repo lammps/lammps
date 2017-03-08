fc23f9cfe897f383db0fa48ce28ea7a2dceb34e8
Stan Moore <stamoor@sandia.gov>
2017-03-08 13:07:52 -0700

    Disable allocation of per-atom arrays in ev_setup for USER-DPD Kokkos styles

--- src/KOKKOS/pair_table_rx_kokkos.cpp
+++ src/KOKKOS/pair_table_rx_kokkos.cpp
@@ -627,7 +627,7 @@ void PairTableRXKokkos<DeviceType>::compute_style(int eflag_in, int vflag_in)
 
   if (neighflag == FULL) no_virial_fdotr_compute = 1;
 
-  if (eflag || vflag) ev_setup(eflag,vflag);
+  if (eflag || vflag) ev_setup(eflag,vflag,0);
   else evflag = vflag_fdotr = 0;
 
   if (eflag_atom) {

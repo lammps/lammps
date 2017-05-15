a624ed90c18ce650ce75d12181aa1dd5cb738586
Julien Guénolé <julien.guenole@fau.de>
2017-05-15 14:48:19 +0200

    add min_adaptglok from Wolfram

---
+++ src/min_adaptglok.h
@@ -0,0 +1,45 @@
+/* -*- c++ -*- ----------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef MINIMIZE_CLASS
+
+MinimizeStyle(adaptglok,MinAdaptGlok)
+
+#else
+
+#ifndef LMP_MIN_ADAPTGLOK_H
+#define LMP_MIN_ADAPTGLOK_H
+
+#include "min.h"
+
+namespace LAMMPS_NS {
+
+class MinAdaptGlok : public Min {
+ public:
+  MinAdaptGlok(class LAMMPS *);
+  ~MinAdaptGlok() {}
+  void init();
+  void setup_style();
+  void reset_vectors();
+  int iterate(int);
+
+ private:
+  double dt,dtmax;
+  double alpha;
+  bigint last_negative;
+};
+
+}
+
+#endif
+#endif

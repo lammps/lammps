18dee3f78ed2537f3044d757691b526f4778ca7e
Stefan Paquay <stefanpaquay@gmail.com>
2017-06-06 16:03:09 -0400

    Added Gaussian bump. Updated e-mail address.

--- src/USER-MANIFOLD/manifold_gaussian_bump.h
+++ src/USER-MANIFOLD/manifold_gaussian_bump.h
@@ -24,7 +24,10 @@
    testing purposes) and a wave-y plane.
    See the README file for more info.
 
-   Stefan Paquay, s.paquay@tue.nl
+   Stefan Paquay, spaquay@brandeis.edu
+   Brandeis University, Waltham, MA, USA.
+
+   This package was mainly developed at
    Applied Physics/Theory of Polymers and Soft Matter,
    Eindhoven University of Technology (TU/e), The Netherlands
 

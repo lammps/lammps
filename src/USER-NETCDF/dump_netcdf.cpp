bdf03757e686ef7de037113beea8d4ffed91249c
Lars Pastewka <lars.pastewka@kit.edu>
2017-06-08 23:20:21 +0200

    MAINT: Simplified GPL headers.

--- src/USER-NETCDF/dump_netcdf.cpp
+++ src/USER-NETCDF/dump_netcdf.cpp
@@ -1,25 +1,3 @@
-/* ======================================================================
-   LAMMPS NetCDF dump style
-   https://github.com/pastewka/lammps-netcdf
-   Lars Pastewka, lars.pastewka@kit.edu
-
-   Copyright (2011-2013) Fraunhofer IWM
-   Copyright (2014) Karlsruhe Institute of Technology
-
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 2 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-   ====================================================================== */
-
 /* ----------------------------------------------------------------------
    LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
    http://lammps.sandia.gov, Sandia National Laboratories
@@ -31,6 +9,10 @@
    the GNU General Public License.
 
    See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+/* ----------------------------------------------------------------------
+   Contributing author: Lars Pastewka (University of Freiburg)
 ------------------------------------------------------------------------- */
 
 #if defined(LMP_HAS_NETCDF)

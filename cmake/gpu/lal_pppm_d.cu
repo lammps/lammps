acbc60319f6eb3ad15007ed568b876f1b8575785
Christoph Junghans <junghans@lanl.gov>
2017-07-18 18:43:51 -0600

    GPU: clean up part 2

--- cmake/gpu/lal_pppm_d.cu
+++ cmake/gpu/lal_pppm_d.cu
@@ -1,6 +1,4 @@
-#ifdef CMAKE_GPU
 #define grdtyp double
 #define grdtyp4 double4
-#endif
 
 #include "lal_pppm.cu"
--- cmake/gpu/lal_pppm_f.cu
+++ cmake/gpu/lal_pppm_f.cu
@@ -1,6 +1,4 @@
-#ifdef CMAKE_GPU
 #define grdtyp float
 #define grdtyp4 float4
-#endif
 
 #include "lal_pppm.cu"

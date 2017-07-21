8ed881947ff91ef1e87f0fa8a7abb04f50a1dc78
Stan Moore <stamoor@sandia.gov>
2017-07-21 14:28:13 -0600

    Fix execution space in manybody potentials

--- src/KOKKOS/pair_tersoff_kokkos.cpp
+++ src/KOKKOS/pair_tersoff_kokkos.cpp
@@ -76,7 +76,7 @@ void PairTersoffKokkos<DeviceType>::allocate()
 
   k_params = Kokkos::DualView<params_ters***,Kokkos::LayoutRight,DeviceType>
 	  ("PairTersoff::paramskk",n+1,n+1,n+1);
-  paramskk = k_params.d_view;
+  paramskk = k_params.template view<DeviceType>();
 }
 
 /* ----------------------------------------------------------------------

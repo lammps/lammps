9a5723123ff040085bbf43dcb44563023112f57f
Anders Hafreager <andershaf@gmail.com>
2017-11-21 23:43:52 -0800

    Set peratom_freq in fix ave/chunk

--- src/fix_ave_chunk.cpp
+++ src/fix_ave_chunk.cpp
@@ -63,6 +63,7 @@ FixAveChunk::FixAveChunk(LAMMPS *lmp, int narg, char **arg) :
   strcpy(idchunk,arg[6]);
 
   global_freq = nfreq;
+  peratom_freq = nfreq;
   no_change_box = 1;
 
   // expand args if any have wildcard character "*"

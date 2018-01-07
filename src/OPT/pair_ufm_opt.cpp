11cddd8798e14b7a8cffb855833aeef00825ab2b
Axel Kohlmeyer <akohlmey@gmail.com>
2018-01-06 19:42:40 -0500

    explicitly include math.h

--- src/OPT/pair_ufm_opt.cpp
+++ src/OPT/pair_ufm_opt.cpp
@@ -18,6 +18,7 @@
  ------------------------------------------------------------------------- */
 
 #include <stdlib.h>
+#include <math.h>
 #include "pair_ufm_opt.h"
 #include "atom.h"
 #include "force.h"

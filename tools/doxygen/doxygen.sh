86ed55599d7325d34f36b5e463170248d5852977
Dr. Nandor Tamaskovics <tamas@tu-freiberg.de>
2018-01-16 17:28:58 +0100

    The bash shell script doxygen.sh has been revised.

--- tools/doxygen/doxygen.sh
+++ tools/doxygen/doxygen.sh
@@ -21,107 +21,87 @@ FIG2DEV=`which fig2dev` ;
 DOXYGEN=`which doxygen` ;
 
 
-error=0;
-
-
 if [[ -d ${LAMMPSSRC} && -d ${LAMMPSDEVELOPERDOC} && -d ${LAMMPSDOC} && -f ${LAMMPSDOCMAKE} && -d ${LAMMPSDOXYGEN} && -f ${LAMMPSDOXYFILE} ]] ;
  then
    cd ${LAMMPSSRC} ;
    make no-all ;
    cd ${LAMMPSBASE} ;
  else
-  error=1 ;
-  echo "Cannot configure LAMMPS sources - Please run doxygen.sh from the LAMMPS root directory."
+   echo "Cannot configure LAMMPS sources - Please run doxygen.sh from the LAMMPS root directory." >&2 ;
+   exit 1 ;
  fi ;
 
 
-if [ ${error} == 0 ] ;
+if [ ! -x ${FIG2DEV} ] ;
   then
-    if [[ ! -x ${FIG2DEV} ]] ;
+    echo "${FIG2DEV} not found - Please install ${FIG2DEV} for Your operating system." >&2 ;
+    exit 1 ;
+  fi ;
+
+
+if [ -d ${LAMMPSDEVELOPERDOC} ] ;
+  then
+    ${FIG2DEV} -L png ${LAMMPSDEVELOPERDOC}/classes.fig > tools/doxygen/classes.png ;
+    ${FIG2DEV} -L eps ${LAMMPSDEVELOPERDOC}/classes.fig > tools/doxygen/classes.eps ;
+  else
+    echo "LAMMPS developer documentation not found - Please control Your LAMMPS installation." >&2 ;
+    exit 1 ;
+  fi ;
+
+
+if [ ! -x ${DOXYGEN} ] ;
+  then
+    echo "doxygen not found - Please install doxygen for Your operating system." >&2 ;
+    exit 1 ;
+  fi ;
+
+
+if [[ -d ${LAMMPSSRC} && -f ${LAMMPSDOXYFILE} && -f ${LAMMPSDEVELOPERDOXFILE} ]] ;
+  then
+    ICS=' ';
+    read -r -a array < ${LAMMPSSRC}/version.h ;
+    version=`echo ${array[2]} ${array[3]} ${array[4]} | sed s/\"//g`;
+    cp -av ${LAMMPSDOXYFILE} ${DOXYFILE} ;
+    sed -i "s/LAMMPS_VERSION/${version}/g"  ${DOXYFILE} ;
+    cp -av ${LAMMPSDEVELOPERDOXFILE} ${DEVELOPERDOXFILE} ;
+    sed -i "s/LAMMPS_VERSION/${version}/g"  ${DEVELOPERDOXFILE} ;
+    ${DOXYGEN} ${DOXYFILE} ;
+  else
+    echo "Doxyfile prototype not found - Please control Your LAMMPS installation." >&2 ;
+    exit 1 ;
+  fi ;
+
+
+if [[ -d ${LAMMPSDOC} && -f ${LAMMPSDOCMAKE} ]] ;
+  then
+    cd ${LAMMPSDOC} ;
+    for d in ${LAMMPSDOCTMP} ;
+      do
+        rm -vrf $d;
+      done ;
+    make clean ;
+    make html ;
+    make pdf ;
+    cd ${LAMMPSBASE} ;
+    echo "${LAMMPSDOC} exists." ;
+  else
+    echo "Cannot build LAMMPS native documentation - Please run doxygen.sh from the LAMMPS root directory."i >&2 ;
+    exit 1 ;
+  fi ;
+
+
+if [ -d ${LAMMPSDOCHTML} ] ;
+  then
+    if [ -d ${LAMMPSDOXYGENDOCMANUAL} ] ;
       then
-        error=1 ;
-        echo "${FIG2DEV} not found - Please install ${FIG2DEV} for Your operating system." ;
+        rm -vrf ${LAMMPSDOXYGENDOCMANUAL} ;
+        echo "${LAMMPSDOXYGENDOCMANUAL} removed." ;
       fi ;
-  fi ;
-
-
-if [ ${error} == 0 ] ;
-  then
-    if [[ -d ${LAMMPSDEVELOPERDOC} ]] ;
-      then
-        ${FIG2DEV} -L png ${LAMMPSDEVELOPERDOC}/classes.fig > tools/doxygen/classes.png ;
-        ${FIG2DEV} -L eps ${LAMMPSDEVELOPERDOC}/classes.fig > tools/doxygen/classes.eps ;
-      else
-        error=1 ;
-        echo "LAMMPS developer documentation not found - Please control Your LAMMPS installation." ;
-      fi;
-  fi;
-
-
-if [ ${error} == 0 ] ;
-  then
-    if [[ ! -x ${DOXYGEN} ]] ;
-      then
-        error=1;
-        echo "doxygen not found - Please install doxygen for Your operating system." ;
-      fi ;
-  fi ;
-
-
-if [ ${error} == 0 ] ;
-  then
-    if [[ -d ${LAMMPSSRC} && -f ${LAMMPSDOXYFILE} && -f ${LAMMPSDEVELOPERDOXFILE} ]] ;
-      then
-        ICS=' ';
-        read -r -a array < ${LAMMPSSRC}/version.h ;
-        version=`echo ${array[2]} ${array[3]} ${array[4]} | sed s/\"//g`;
-        cp -av ${LAMMPSDOXYFILE} ${DOXYFILE} ;
-        sed -i "s/LAMMPS_VERSION/${version}/g"  ${DOXYFILE} ;
-        cp -av ${LAMMPSDEVELOPERDOXFILE} ${DEVELOPERDOXFILE} ;
-        sed -i "s/LAMMPS_VERSION/${version}/g"  ${DEVELOPERDOXFILE} ;
-        ${DOXYGEN} ${DOXYFILE} ;
-      else
-        echo "Doxyfile prototype not found - Please control Your LAMMPS installation." ;
-      fi ;
-  fi ;
-
-
-if [ ${error} == 0 ] ;
-  then
-    if [[ -d ${LAMMPSDOC} && -f ${LAMMPSDOCMAKE} ]] ;
-      then
-        cd ${LAMMPSDOC} ;
-        if [ -d ${LAMMPSDOCTMP} ] ;
-         then
-          rm -vrf ${LAMMPSDOCTMP} ;
-         fi ;
-        make clean ;
-        make html ;
-        make pdf ;
-        cd ${LAMMPSBASE} ;
-      echo "${LAMMPSDOC} exists." ;
-    else
-      error=1 ;
-      echo "Cannot build LAMMPS native documentation - Please run doxygen.sh from the LAMMPS root directory." ;
-    fi ;
-  fi ;
-
-
-if [ ${error} == 0 ] ;
-  then
-    if [[ ${error} == 0 && -d ${LAMMPSDOCHTML} ]] ;
-      then
-        if [ -d ${LAMMPSDOXYGENDOCMANUAL} ] ;
-          then
-            rm -vrf ${LAMMPSDOXYGENDOCMANUAL} ;
-            echo "${LAMMPSDOXYGENDOCMANUAL} removed." ;
-          fi ;
-          cp -av ${LAMMPSDOCHTML} ${LAMMPSDOXYGENDOCMANUAL} ;
-	  cp -av ${LAMMPSDOC}/*.pdf ${LAMMPSDOXYGENDOCMANUAL} ;
-          echo "${LAMMPSDOXYGENDOCMANUAL} copied." ;
-        else
-          error=1 ;
-          echo "Cannot include LAMMPS native documentation into the doxygen documentation - Please run doxygen.sh from the LAMMPS root directory." ;
-    fi ;
+    cp -av ${LAMMPSDOCHTML} ${LAMMPSDOXYGENDOCMANUAL} ;
+    cp -av ${LAMMPSDOC}/*.pdf ${LAMMPSDOXYGENDOCMANUAL} ;
+    echo "${LAMMPSDOXYGENDOCMANUAL} copied." ;
+  else
+    echo "Cannot include LAMMPS native documentation into the doxygen documentation - Please run doxygen.sh from the LAMMPS root directory." >&2 ;
+    exit 1 ;
   fi ;
 

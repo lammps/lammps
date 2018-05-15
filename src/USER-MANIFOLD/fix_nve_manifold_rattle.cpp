a0fe1221b50fa847371f9b1ff12ca86c44a74f89
Stefan Paquay <stefanpaquay@gmail.com>
2018-05-15 13:27:23 -0400

    Removed some debug code from fix_nve_manifold_rattle.

--- src/USER-MANIFOLD/fix_nve_manifold_rattle.cpp
+++ src/USER-MANIFOLD/fix_nve_manifold_rattle.cpp
@@ -513,9 +513,9 @@ void FixNVEManifoldRattle::rattle_manifold_x(double *x, double *v,
 
   const double c_inv = 1.0 / c;
 
-  if( comm->me == 0 ){
-    fprintf( screen, "Performing RATTLE on atom on proc 0!\n" );
-  }
+  //if( comm->me == 0 ){
+  //  fprintf( screen, "Performing RATTLE on atom on proc 0!\n" );
+  //}
 
   while ( 1 ) {
     v[0] = vt[0] - l*no_dt[0];

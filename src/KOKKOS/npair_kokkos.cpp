13d562e969aa31aa524e4b8ea7ff9b55c83dd153
Stan Moore <stamoor@sandia.gov>
2018-07-06 12:06:09 -0600

    Removed wrong which variable

--- src/KOKKOS/npair_kokkos.cpp
+++ src/KOKKOS/npair_kokkos.cpp
@@ -686,6 +686,7 @@ void NeighborKokkosExecute<DeviceType>::
 {
   /* if necessary, goto next page and add pages */
   int n = 0;
+  int which = 0;
   int moltemplate;
   if (molecular == 2) moltemplate = 1;
   else moltemplate = 0;
@@ -807,7 +808,6 @@ void NeighborKokkosExecute<DeviceType>::
 {
   /* if necessary, goto next page and add pages */
   int n = 0;
-  int which = 0;
 
   // get subview of neighbors of i
 

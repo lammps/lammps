c442166dede75d2b06ea0d4cd71dce6477163794
Gareth Tribello <gareth.tribello@gmail.com>
2018-07-12 16:22:40 +0100

    Tidied up example directory for PLUMED

--- examples/USER/plumed/clear-files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Data from first set of checks
-rm bck.* plmd_energy lammps_energy mq_plumed mq_lammps lammps.xyz plumed.xyz p.log
-# Data from checks on restraints
-rm bck.* p.log lammps_restraint plumed_restraint
-# Data from checks on virial
-rm bck.* lammps_energy lammps.xyz log.lammps plmd_volume p.log plmd_volume_without_restraint plmd_volume_with_restraint

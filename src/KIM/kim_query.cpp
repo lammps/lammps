741a7fe630d5e445163115c897db5aba74455f7f
Axel Kohlmeyer <akohlmey@gmail.com>
2018-08-20 18:56:40 -0400

    final touches to support compiling with libcurl transparently

--- src/KIM/kim_query.cpp
+++ src/KIM/kim_query.cpp
@@ -57,36 +57,55 @@
 
 #include <mpi.h>
 #include <cstring>
+#include <string>
 #include "kim_query.h"
 #include "comm.h"
 #include "error.h"
 #include "input.h"
 #include "variable.h"
 
+#include <sys/types.h>
+#include <curl/curl.h>
+
 using namespace LAMMPS_NS;
 
-static char *do_query(char *, char *, int, MPI_Comm);
+#if defined(LMP_KIM_CURL)
+
+struct WriteBuf {
+  char *dataptr;
+  size_t sizeleft;
+};
+
+static char *do_query(char *, int, char **, int, MPI_Comm);
+static size_t write_callback(void *, size_t, size_t, void *);
+
+#endif
 
 /* ---------------------------------------------------------------------- */
 
 void KimQuery::command(int narg, char **arg)
 {
-  char *model, *property, *varname, *value;
+  char *varname, *function, *value;
 
-  if (narg != 3) error->all(FLERR,"Illegal kim_query command");
+  if (narg < 2) error->all(FLERR,"Illegal kim_query command");
 
-  model = arg[0];
-  property = arg[1];
-  varname = arg[2];
+  varname = arg[0];
+  function = arg[1];
 
-  value = do_query(model, property, comm->me, world);
+#if defined(LMP_KIM_CURL)
+
+  value = do_query(function, narg-2, arg+2, comm->me, world);
 
   // check for valid result
+  // on error the content of "value" is a '\0' byte
+  // as the first element, and then the error message
+  // that was returned by the web server
 
-  int len = strlen(value) + 1;
-  if (len == 1) {
-    // TODO: store more detailed error message after \0 byte.
-    error->all(FLERR,"Query of OpenKIM database failed");
+  if (0 == strlen(value)) {
+    char errmsg[512];
+
+    sprintf(errmsg,"OpenKIM query failed: %s",value+1);
+        error->all(FLERR,errmsg);
   }
 
   char **varcmd = new char*[3];
@@ -98,26 +117,119 @@ void KimQuery::command(int narg, char **arg)
   
   delete[] varcmd;
   delete[] value;
+#else
+  error->all(FLERR,"Cannot use 'kim_query' command when KIM package "
+             "is compiled without support for libcurl");
+#endif
 }
 
+#if defined(LMP_KIM_CURL)
 
-char *do_query(char *model, char *property, int rank, MPI_Comm comm)
+// copy data to the user provided data structure, optionally in increments
+
+size_t write_callback(void *data, size_t size, size_t nmemb, void *userp)
+{
+  struct WriteBuf *buf = (struct WriteBuf *)userp;
+  size_t buffer_size = size*nmemb;
+
+  // copy chunks into the buffer for as long as there is space left
+  if (buf->sizeleft) {
+    size_t copy_this_much = buf->sizeleft;
+    if (copy_this_much > buffer_size)
+      copy_this_much = buffer_size;
+    memcpy(buf->dataptr, data, copy_this_much);
+
+    buf->dataptr += copy_this_much;
+    buf->sizeleft -= copy_this_much;
+    return copy_this_much;
+  }
+  return 0; // done
+}
+
+char *do_query(char *qfunction, int narg, char **arg, int rank, MPI_Comm comm)
 {
   char value[512], *retval;
 
-  // only run query from rank 0
-  if (rank == 0) {
+  // run the web query from rank 0 only
 
-    // fake query
-    strcpy(value,(const char*)"4.25");
+  if (rank == 0) {
+    CURL *handle;
+    CURLcode res;
+
+    // set up and clear receive buffer
+
+    struct WriteBuf buf;
+    buf.dataptr = value;
+    buf.sizeleft = 511;
+    memset(value,0,512);
+
+    // create curl web query instance
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    handle = curl_easy_init();
+
+    if (handle) {
+      std::string url("https://query.openkim.org/api/");
+      url += qfunction;
+
+      std::string query(arg[0]);
+      for (int i=1; i < narg; ++i) {
+        query += '&';
+        query += arg[i];
+      }
+
+#if LMP_DEBUG_CURL
+      curl_easy_setopt(handle, CURLOPT_VERBOSE, 1L);
+#endif
+
+#if defined(LMP_NO_SSL_CHECK)
+      // disable verifying SSL certificate and host name
+      curl_easy_setopt(handle, CURLOPT_SSL_VERIFYPEER, 0L);
+      curl_easy_setopt(handle, CURLOPT_SSL_VERIFYHOST, 0L);
+#endif
+
+      curl_easy_setopt(handle, CURLOPT_URL, url.c_str());
+      curl_easy_setopt(handle, CURLOPT_FOLLOWLOCATION, 1L);
+      curl_easy_setopt(handle, CURLOPT_POSTFIELDS, query.c_str());
+
+      curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION,write_callback);
+      curl_easy_setopt(handle, CURLOPT_WRITEDATA,&buf);
+
+      // perform OpenKIM query and check for errors
+      res = curl_easy_perform(handle);
+      if (res != CURLE_OK) {
+        // on error we return an "empty" string but add error message after it
+        value[0]= '\0';
+        strcpy(value+1,curl_easy_strerror(res));
+      }
+      curl_easy_cleanup(handle);
+    }
+    curl_global_cleanup();
   }
   MPI_Bcast(value, 512, MPI_CHAR, 0, comm);
 
-  // must make a proper copy of the query, as the stack allocation
-  // will go out of scope
+  // we must make a proper copy of the query, as the stack allocation
+  // for "value" will go out of scope. a valid query has a '[' as
+  // the first character. skip over it (and the last character ']', too)
+  // an error messages starts with a '\0' character. copy that and
+  // the remaining string, as that is the error message.
 
-  int len = strlen(value) + 1;
-  retval = new char[len];
-  strcpy(retval,value);
+  if (value[0] == '[') {
+    int len = strlen(value)-1;
+    retval = new char[len];
+    value[len] = '\0';
+    strcpy(retval,value+1);
+  } else if (value[0] == '\0') {
+    int len = strlen(value+1)+2;
+    retval = new char[len];
+    retval[0] = '\0';
+    strcpy(retval+1,value+1);
+  } else {
+    // unknown response type. we should not get here.
+    // copy response without modifications.
+    int len = strlen(value)+1;
+    retval = new char[len];
+    strcpy(retval,value);
+  }
   return retval;
 }
+#endif

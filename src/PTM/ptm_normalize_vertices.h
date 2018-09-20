c705e8d0e6a3b1a739f2ddf6cd1ee40a3241d40a
pmla <pete.mahler.larsen@gmail.com>
2018-09-19 20:46:48 -0400

    renamed files for LAMMPS build system compatibility

---
+++ src/PTM/ptm_normalize_vertices.h
@@ -0,0 +1,8 @@
+#ifndef PTM_NORMALIZE_VERTICES_H
+#define PTM_NORMALIZE_VERTICES_H
+
+void subtract_barycentre(int num, double (*points)[3], double (*normalized)[3]);
+double normalize_vertices(int num, double (*points)[3], double (*normalized)[3]);
+
+#endif
+

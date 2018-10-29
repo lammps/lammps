bdf37649057de199bba3d557acba95815a16d1aa
Axel Kohlmeyer <akohlmey@gmail.com>
2018-10-28 21:31:40 -0400

    update user-plumed Install.sh file to fit better into LAMMPS, adjust to changes in lib folder

--- src/USER-PLUMED/Install.sh
+++ src/USER-PLUMED/Install.sh
@@ -4,29 +4,27 @@
 if (test $1 = 1) then
 
   if (test -e ../Makefile.package) then
-    sed -i -e 's|^PKG_LIB =[ \t]*|& $(PLUMED_LOAD) |' ../Makefile.package
-    if ( ! test -e ../../lib/plumed/plumed2*/src/lib/Plumed.inc.static ) then
+    sed -i -e 's|^PKG_LIB =[ \t]*|& -lplumedWrapper -ldl |' ../Makefile.package
+    if ( ! test -e ../../lib/plumed/liblink/plumed/src/lib/Plumed.inc.static ) then
         sed -i -e 's|^PKG_SYSINC =[ \t]*|& -D__PLUMED_HAS_DLOPEN |' ../Makefile.package
     fi
+    sed -i -e 's|^PKG_INC =[ \t]*|&-I../../lib/plumed/includelink |' ../Makefile.package
+    sed -i -e 's|^PKG_PATH =[ \t]*|&-L../../lib/plumed/liblink |' ../Makefile.package
   fi
 
   if (test -e ../Makefile.package.settings) then
     # This is for statically linking plumed2
-    if ( test -e ../../lib/plumed/plumed2*/src/lib/Plumed.inc.static ) then  
-       fname=`ls ../../lib/plumed/plumed2*/src/lib/Plumed.inc.static` 
+    if ( test -e ../../lib/plumed/liblink/src/lib/Plumed.inc.static ) then  
+       fname=../../lib/plumed/liblink/src/lib/Plumed.inc.static 
        sed -i -e '4 i \
 include '$fname'
 ' ../Makefile.package.settings
-       dname=`ls ../../lib/plumed/plumed2*/src/wrapper/Plumed.h`
-       ln -s USER-PLUMED/$dname ../Plumed.h
     # This is for linking plumed2 as a runtime library  -- this is the default behavior
     else 
     # multiline form needed for BSD sed on Macs
        sed -i -e '4 i \
 PLUMED_LOAD=-ldl
 ' ../Makefile.package.settings
-       cp Plumed.h ..
-       cp Plumed.cpp ..
     fi
   fi
 
@@ -36,21 +34,12 @@ PLUMED_LOAD=-ldl
 elif (test $1 = 0) then
 
   if (test -e ../Makefile.package) then
-    sed -i -e 's/[^ \t]* \$(PLUMED_LOAD)[^ \t]* //' ../Makefile.package
-    sed -i -e 's/[^ \t]* -D__PLUMED_HAS_DLOPEN[^ \t]* //' ../Makefile.package
-  fi
-
-  if (test -e ../Makefile.package.settings) then
-    fname=`ls ../../lib/plumed/plumed2*/src/lib/Plumed.inc.static`
-    sed -i -e "\:include $fname: d" ../Makefile.package.settings 
-    sed -i -e '/PLUMED_LOAD=-ldl/d' ../Makefile.package.settings
+    sed -i -e 's/[^ \t]*-lplumedWrapper -ldl[^ \t]* //' ../Makefile.package
+    sed -i -e 's/[^ \t]*-D__PLUMED_HAS_DLOPEN[^ \t]* //' ../Makefile.package
+    sed -i -e 's|[^ \t]*-I../../lib/plumed/includelink[^ \t]* ||' ../Makefile.package
+    sed -i -e 's|[^ \t]*-L../../lib/plumed/liblink[^ \t]* ||' ../Makefile.package
   fi
 
   rm -f ../fix_plumed.cpp
   rm -f ../fix_plumed.h
-  rm -f ../Plumed.h
-  if ( test -e ../Plumed.h ) then
-       rm -f ../Plumed.cpp
-  fi
-
 fi

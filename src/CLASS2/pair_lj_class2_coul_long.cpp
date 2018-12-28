3a7350575cd964dca1876d5574558a0dbacdde1b
Axel Kohlmeyer <akohlmey@gmail.com>
2018-12-28 17:38:55 -0500

    tweak coeff argument parsing settings for lj/class2/coul/long

--- src/CLASS2/pair_lj_class2_coul_long.cpp
+++ src/CLASS2/pair_lj_class2_coul_long.cpp
@@ -252,8 +252,9 @@ void PairLJClass2CoulLong::settings(int narg, char **arg)
 
 void PairLJClass2CoulLong::coeff(int narg, char **arg)
 {
-  if (narg < 4 || narg > 6)
+  if (narg < 4 || narg > 5)
     error->all(FLERR,"Incorrect args for pair coefficients");
+
   if (!allocated) allocate();
 
   int ilo,ihi,jlo,jhi;

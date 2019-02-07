622eb4790220dfca53e40edf537ee4782cd94db6
Axel Kohlmeyer <akohlmey@gmail.com>
2019-02-07 01:16:48 -0500

    some small tweaks, fixes for typos, and url corrections for the manual

--- doc/utils/sphinx-config/conf.py
+++ doc/utils/sphinx-config/conf.py
@@ -221,7 +221,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   ('Manual', 'LAMMPS.tex', 'LAMMPS Documentation',
-   'Steve Plimpton', 'manual'),
+   'The LAMMPS Developers', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of

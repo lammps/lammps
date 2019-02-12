e582c0d4e5eec1b5e22954e3249719e69827deb2
Jan Janßen <jan-janssen@users.noreply.github.com>
2019-02-12 08:20:54 +0100

    Update lammps.py

--- python/lammps.py
+++ python/lammps.py
@@ -85,7 +85,7 @@ class lammps(object):
     # fall back to loading with a relative path,
     #   typically requires LD_LIBRARY_PATH to be set appropriately
 
-    if 'liblammps.dylib' in os.listdir(modpath):
+    if any([f.startswith('liblammps') and f.endswith('.dylib') for f in os.listdir(modpath)]):
       lib_ext = ".dylib"
     else:
       lib_ext = ".so"

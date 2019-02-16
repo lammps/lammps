959da9de013d29d9e58bb4d2c4b6d8a48308bdc5
Rupert Nash <r.nash@epcc.ed.ac.uk>
2019-02-16 14:49:44 +0000

    Create a compute for the momentum of a group of atoms

---
+++ src/compute_momentum.cpp
@@ -0,0 +1,59 @@
+
+#include <mpi.h>
+#include "compute_momentum.h"
+#include "atom.h"
+#include "update.h"
+#include "force.h"
+#include "domain.h"
+#include "group.h"
+#include "error.h"
+
+using namespace LAMMPS_NS;
+
+ComputeMomentum::ComputeMomentum(LAMMPS *lmp, int narg, char **arg) :
+  Compute(lmp, narg, arg)
+{
+  if (narg != 3) error->all(FLERR,"Illegal compute momentum command");
+
+  vector_flag = 1;
+  size_vector = 3;
+  extvector = 1;
+  vector = new double[size_vector];
+}
+
+ComputeMomentum::~ComputeMomentum() {
+  delete[] vector;
+}
+
+void ComputeMomentum::init()
+{
+}
+
+void ComputeMomentum::compute_vector()
+{
+  invoked_vector = update->ntimestep;
+
+  double **v = atom->v;
+  double *rmass = atom->rmass;
+  double *mass = atom->mass;
+  int *mask = atom->mask;
+  int *type = atom->type;
+  int nlocal = atom->nlocal;
+
+  double mom[3] = {0.0, 0.0, 0.0};
+
+  if (rmass) {
+    for (int i = 0; i < nlocal; i++)
+      if (mask[i] & groupbit) {
+	for(int j = 0; j < 3; ++j)
+	  mom[j] += rmass[i] * v[i][j];
+      }
+  } else {
+    for (int i = 0; i < nlocal; i++)
+      if (mask[i] & groupbit)
+	for(int j = 0; j < 3; ++j)
+	  mom[j] += mass[type[i]] * v[i][j];    
+  }
+
+  MPI_Allreduce(&mom, vector, 3, MPI_DOUBLE, MPI_SUM, world);
+}

1179beb54330b061d84ecaea61e2586ae09b72e7
Norbert Podhorszki <pnorbert@ornl.gov>
2019-02-20 14:42:28 -0500

    Makefile build support for USER-ADIOS and build documentation

--- src/USER-ADIOS/Install.sh
+++ src/USER-ADIOS/Install.sh
@@ -30,60 +30,64 @@ done
 
 if (test $1 = 1) then
 
-#  if (test -z "$ADIOS_DIR") then
-#    if command -v adios_config; then 
-#        ADIOS_DIR=`adios_config -d`
-#    else
-#      echo "ERROR: ADIOS_DIR environment variable needs to point to ADIOS" \
-#           " installation directory or adios_config should be in PATH"
-#    fi
-#  fi
-#  ADIOS_INC=-I${ADIOS_DIR}/include
-#  ADIOS_LIB=`${ADIOS_DIR}/bin/adios_config -l`
-#  
-#  echo "adios_SYSINC=${ADIOS_INC}
-#adios_SYSLIB=${ADIOS_LIB}
-#adios_SYSPATH=${ADIOS_DIR}
-#" > ../Makefile.adios 
-
-
-  if (test -e ../Makefile.package) then
-    sed -i -e 's/[^ \t]*adios[^ \t]* //g' ../Makefile.package
-    sed -i -e 's/-DLMP_ADIOS //g' ../Makefile.package
-    sed -i -e '/^adios_SYS.*$/d' ../Makefile.package
-#    sed -i -e '4 i \
-#adios_SYSINC='"${ADIOS_INC}"'
-#' ../Makefile.package
-#    sed -i -e '5 i \
-#adios_SYSLIB='"${ADIOS_LIB}"'
-#' ../Makefile.package
-#    sed -i -e '6 i \
-#adios_SYSPATH='"${ADIOS_DIR}"'
-#' ../Makefile.package
-    sed -i -e 's|^PKG_INC =[ \t]*|&-DLMP_ADIOS |' ../Makefile.package
-    sed -i -e 's|^PKG_SYSINC =[ \t]*|&$(adios_SYSINC) |' ../Makefile.package
-    sed -i -e 's|^PKG_SYSLIB =[ \t]*|&$(adios_SYSLIB) |' ../Makefile.package
-    sed -i -e 's|^PKG_SYSPATH =[ \t]*|&$(adios_SYSPATH) |' ../Makefile.package
+  CONFIGSCRIPT=none
+  if ( test `which adios2-config` ) then
+    CONFIGSCRIPT=adios2-config
+  elif ( ! test -z "$ADIOS2_DIR" ) then
+    if ( test `which $ADIOS2_DIR/bin/adios2-config` ) then
+      CONFIGSCRIPT=$ADIOS2_DIR/bin/adios2-config
+    else
+      echo "ERROR: ADIOS2_DIR environment variable is set but" \
+           "\$ADIOS2_DIR/bin/adios2-config does not exist"
+    fi
+  elif ( ! test -z "$ADIOS_DIR" ) then
+    if ( test `which $ADIOS_DIR/bin/adios2-config` ) then
+      CONFIGSCRIPT=$ADIOS_DIR/bin/adios2-config
+    else
+      echo "ERROR: ADIOS_DIR environment variable is set but" \
+           "\$ADIOS_DIR/bin/adios2-config does not exist"
+    fi
+  else
+    echo "ERROR: ADIOS2_DIR environment variable must point to ADIOS 2.x" \
+         "installation directory or adios2-config should be in PATH"
   fi
 
-  if (test -e ../Makefile.package.settings) then
-    sed -i -e '/^include.*adios.*$/d' ../Makefile.package.settings
-    # multiline form needed for BSD sed on Macs
-    sed -i -e '4 i \
-include ..\/..\/lib\/adios\/Makefile.lammps
+  if [ "$CONFIGSCRIPT" != "none" ]; then 
+    ADIOS2_INC=`$CONFIGSCRIPT --cxx-flags`
+    ADIOS2_LIB=`$CONFIGSCRIPT --cxx-libs`
+  
+    echo "adios_SYSINC=${ADIOS2_INC}
+adios_SYSLIB=${ADIOS2_LIB}
+" > Makefile.lammps
+
+
+    if (test -e ../Makefile.package) then
+      sed -i -e 's/[^ \t]*adios[^ \t]* //g' ../Makefile.package
+      sed -i -e '/^adios_SYS.*$/d' ../Makefile.package
+      sed -i -e 's|^PKG_SYSINC =[ \t]*|&$(adios_SYSINC) |' ../Makefile.package
+      sed -i -e 's|^PKG_SYSLIB =[ \t]*|&$(adios_SYSLIB) |' ../Makefile.package
+    fi
+
+    if (test -e ../Makefile.package.settings) then
+      sed -i -e '/^include.*ADIOS.*$/d' ../Makefile.package.settings
+      # multiline form needed for BSD sed on Macs
+      sed -i -e '4 i \
+include ../USER-ADIOS/Makefile.lammps
 ' ../Makefile.package.settings
+    fi
   fi
 
 elif (test $1 = 0) then
 
   if (test -e ../Makefile.package) then
     sed -i -e 's/[^ \t]*adios[^ \t]* //g' ../Makefile.package
-    sed -i -e 's/-DLMP_ADIOS //g' ../Makefile.package
     sed -i -e '/^adios_SYS.*$/d' ../Makefile.package
   fi
 
   if (test -e ../Makefile.package.settings) then
-    sed -i -e '/^include.*adios.*$/d' ../Makefile.package.settings
+    sed -i -e '/^include.*ADIOS.*$/d' ../Makefile.package.settings
   fi
+
+  rm -f Makefile.lammps
 
 fi

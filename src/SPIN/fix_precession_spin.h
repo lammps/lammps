a726362888c430734faf9d499ccb03ca3b383146
julient31 <julien.tranchida1@gmail.com>
2019-03-25 12:57:42 -0600

    Commit JT 032519 - adding all min files into new branch - preparing branch for pull request of spin minimizer

--- src/SPIN/fix_precession_spin.h
+++ src/SPIN/fix_precession_spin.h
@@ -33,6 +33,7 @@ class FixPrecessionSpin : public Fix {
   int setmask();
   void init();
   void setup(int);
+  void min_setup(int);
   void post_force(int);
   void post_force_respa(int, int, int);
   void min_post_force(int);

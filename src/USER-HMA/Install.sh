02373c4c3b932db416869b07da68207d439ecc9b
Andrew Schultz <ajs42@buffalo.edu>
2019-03-29 15:22:44 -0400

    Move HMA bits into USER-HMA package

---
+++ src/USER-HMA/Install.sh
@@ -0,0 +1,31 @@
+# Install/Uninstall package files in LAMMPS
+# mode = 0/1/2 for uninstall/install/update
+
+mode=$1
+
+# enforce using portable C locale
+LC_ALL=C
+export LC_ALL
+
+# arg1 = file, arg2 = file it depends on
+
+action () {
+  if (test $mode = 0) then
+    rm -f ../$1
+  elif (! cmp -s $1 ../$1) then
+    if (test -z "$2" || test -e ../$2) then
+      cp $1 ..
+      if (test $mode = 2) then
+        echo "  updating src/$1"
+      fi
+    fi
+  elif (test -n "$2") then
+    if (test ! -e ../$2) then
+      rm -f ../$1
+    fi
+  fi
+}
+
+# package files without dependencies
+action compute_hma.h
+action compute_hma.cpp

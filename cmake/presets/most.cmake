5a751254262718bc8f3a4293c3311b923b82a735
Axel Kohlmeyer <akohlmey@gmail.com>
2019-04-02 14:30:34 -0400

    add some popular packages requiring libraries to most.cmake preset

--- cmake/presets/most.cmake
+++ cmake/presets/most.cmake
@@ -1,14 +1,14 @@
-# preset that turns on a wide range of packages none of which require
-# external libraries. Some more unusual packages are removed as well.
-# The resulting binary should be able to run most inputs.
+# preset that turns on a wide range of packages, some of which require
+# external libraries. Compared to all_on.cmake some more unusual packages
+# are removed. The resulting binary should be able to run most inputs.
 
 set(ALL_PACKAGES ASPHERE CLASS2 COLLOID CORESHELL DIPOLE
         GRANULAR KSPACE MANYBODY MC MISC MOLECULE OPT PERI
-        QEQ REPLICA RIGID SHOCK SRD
+        PYTHON QEQ REPLICA RIGID SHOCK SRD VORONOI
         USER-CGDNA USER-CGSDK USER-COLVARS USER-DIFFRACTION USER-DPD
         USER-DRUDE USER-FEP USER-MEAMC USER-MESO
-        USER-MISC USER-MOFFF USER-OMP USER-PHONON USER-REAXC
-        USER-SPH USER-UEF USER-YAFF)
+        USER-MISC USER-MOFFF USER-OMP USER-PLUMED USER-PHONON USER-REAXC
+        USER-SPH USER-SMD USER-UEF USER-YAFF)
 
 foreach(PKG ${ALL_PACKAGES})
   set(PKG_${PKG} ON CACHE BOOL "" FORCE)

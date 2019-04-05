d17553d8d294d6a6598526f5dcfaa3305a783fde
Axel Kohlmeyer <akohlmey@gmail.com>
2019-04-05 16:52:28 -0400

    add preset to make it easier to use clang with OpenMP and MPI

---
+++ cmake/presets/clang.cmake
@@ -0,0 +1,16 @@
+# preset that will enable clang/clang++ with support for MPI and OpenMP (on Linux boxes)
+
+set(CMAKE_CXX_COMPILER "clang++" CACHE STRING "" FORCE)
+set(CMAKE_C_COMPILER "clang" CACHE STRING "" FORCE)
+set(CMAKE_CXX_FLAGS "-Wall -Wextra -g -O2 -DNDEBG" CACHE STRING "" FORCE)
+set(MPI_CXX "clang++" CACHE STRING "" FORCE)
+set(MPI_CXX_COMPILER "mpicxx" CACHE STRING "" FORCE)
+
+set(OpenMP_C "clang" CACHE STRING "" FORCE)
+set(OpenMP_C_FLAGS "-fopenmp" CACHE STRING "" FORCE)
+set(OpenMP_C_LIB_NAMES "omp" CACHE STRING "" FORCE)
+set(OpenMP_CXX "clang++" CACHE STRING "" FORCE)
+set(OpenMP_CXX_FLAGS "-fopenmp" CACHE STRING "" FORCE)
+set(OpenMP_CXX_LIB_NAMES "omp" CACHE STRING "" FORCE)
+set(OpenMP_omp_LIBRARY "/usr/lib64/libomp.so" CACHE PATH "" FORCE)
+

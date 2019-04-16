69e7a2a237dee6bd160e19c6594549db51ce3d94
Steven Strong <strong1@uchicago.edu>
2019-04-16 11:32:47 -0500

    add e3b water model

---
+++ src/USER-MISC/compute_pe_e3b.cpp
@@ -0,0 +1,86 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+
+Adapted from USER-TALLY/compute_pe_tally.cpp by Steven E Strong
+Also used code from compute_pe.cpp
+------------------------------------------------------------------------- */
+
+#include "compute_pe_e3b.h"
+#include "pair_e3b.h"
+
+#include "pair.h"
+#include "update.h"
+#include "error.h"
+#include "force.h"
+
+using namespace LAMMPS_NS;
+
+/* ---------------------------------------------------------------------- */
+
+ComputePEE3B::ComputePEE3B(LAMMPS *lmp, int narg, char **arg) :
+  Compute(lmp, narg, arg), e3b(NULL)
+{
+  //        0  1   2
+  //compute ID grp pe/e3b
+  if (narg != 3) error->all(FLERR,"Illegal compute pe/e3b command");
+
+  scalar_flag = 1;
+  vector_flag = 1;
+  size_vector = 4; //etotA,etotB,etotC,etot2
+  extvector = extscalar = 1;
+  timeflag = 1;
+
+  peflag = 1;                   // we need Pair::ev_tally() to be run
+
+  invoked_vector = invoked_scalar = -1;
+  vector = new double[size_vector];
+}
+
+/* ---------------------------------------------------------------------- */
+
+ComputePEE3B::~ComputePEE3B()
+{
+  delete[] vector;
+}
+
+/* ---------------------------------------------------------------------- */
+
+void ComputePEE3B::init() {
+  Pair *pair = force->pair_match("e3b",false,0);
+  if (pair==NULL)
+    error->all(FLERR,"This compute must be used with pair_style e3b");
+
+  e3b = (PairE3B *) pair;
+  if (e3b==NULL)
+    error->all(FLERR,"something went wrong");
+}
+
+/* ---------------------------------------------------------------------- */
+
+void ComputePEE3B::compute_vector()
+{
+  invoked_vector = update->ntimestep;
+  if (update->eflag_global != invoked_scalar)
+    error->all(FLERR,"Energy was not tallied on needed timestep");
+
+  // sum energies across procs
+  MPI_Allreduce(e3b->etot,vector,4,MPI_DOUBLE,MPI_SUM,world);
+}
+
+double ComputePEE3B::compute_scalar() {
+  invoked_scalar = update->ntimestep;
+  if (invoked_scalar != invoked_vector)
+    compute_vector();
+
+  scalar = vector[0]+vector[1]+vector[2]+vector[3];
+  return scalar;
+}
---
+++ src/USER-MISC/pair_e3b.h
@@ -0,0 +1,74 @@
+/* -*- c++ -*- ----------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef PAIR_CLASS
+
+PairStyle(e3b,PairE3B)
+
+#else
+
+#ifndef LMP_PAIR_E3B_H
+#define LMP_PAIR_E3B_H
+
+#include "pair.h"
+#include "compute_pe_e3b.h"
+
+namespace LAMMPS_NS {
+
+class PairE3B : public Pair {
+ public:
+  PairE3B(class LAMMPS *);
+  virtual ~PairE3B();
+  virtual void compute(int, int);
+  void settings(int, char **);
+  virtual void coeff(int, char **);
+  virtual double init_one(int, int);
+  virtual void init_style();
+
+  //allow compute pe/e3b to access etot vector
+  friend void ComputePEE3B::compute_vector();
+
+protected:
+  //potential parameters
+  int typeO;
+  double ea,eb,ec;      //three body energies
+  double k3;            //three body exponential decay (units inverse length)
+  double rs,rc3,rc2;    //rs: switching cutuff, rc3: cutoff for 3-body
+  double e2,k2;         //2-body energy and exp decay
+  double cutmax;   //max cutoff of all interactions
+  double rc2sq,rc3sq,rc3deltaSq;
+  double sc_denom,sc_num;
+
+  //list of indexes of Os and Hs in each pair
+  int pairmax,pairPerAtom;      // size of pair list
+  int **pairO,***pairH;         // pair lists
+  double ***exps,****del3,***fpair3,*sumExp;
+  int maxID;  //size of global sumExp array
+  size_t nbytes;     //size of sumExp array in bytes
+  int natoms;        //to make sure number of atoms is constant
+
+  double etot[4]; //etotA,etotB,etotC,etot2
+
+  virtual void allocate();
+  void allocateE3B();
+  bool allocatedE3B;
+  //for reading settings from pair_style input
+  bool checkKeyword(const char *,const char *,const int, const int);
+  void checkInputs(const double &bondL);
+  void presetParam(const int flag,bool &repeatFlag,double &bondL);
+  tagint find_maxID();
+};
+}
+
+#endif
+#endif

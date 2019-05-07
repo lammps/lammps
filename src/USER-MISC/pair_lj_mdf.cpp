8c193b8c426bffce94ae58cd280f4ae100f5db68
Axel Kohlmeyer <akohlmey@gmail.com>
2019-05-07 19:28:19 -0400

    add support for write_data/write_coeff to pair style lj/mdf

--- src/USER-MISC/pair_lj_mdf.cpp
+++ src/USER-MISC/pair_lj_mdf.cpp
@@ -349,6 +349,29 @@ void PairLJMDF::read_restart_settings(FILE *fp)
   MPI_Bcast(&mix_flag,1,MPI_INT,0,world);
 }
 
+/* ----------------------------------------------------------------------
+   proc 0 writes to data file
+------------------------------------------------------------------------- */
+
+void PairLJMDF::write_data(FILE *fp)
+{
+  for (int i = 1; i <= atom->ntypes; i++)
+    fprintf(fp,"%d %g %g\n",i,epsilon[i][i],sigma[i][i]);
+}
+
+/* ----------------------------------------------------------------------
+   proc 0 writes all pairs to data file
+------------------------------------------------------------------------- */
+
+void PairLJMDF::write_data_all(FILE *fp)
+{
+  for (int i = 1; i <= atom->ntypes; i++)
+    for (int j = i; j <= atom->ntypes; j++)
+      fprintf(fp,"%d %d %g %g %g %g\n",
+              i,j,epsilon[i][j],sigma[i][j],
+              cut_inner[i][j],cut[i][j]);
+}
+
 /* ---------------------------------------------------------------------- */
 
 double PairLJMDF::single(int /*i*/, int /*j*/, int itype, int jtype,

4cee333c07acecc6ac25aaa30fd06bedb85522c5
Axel Kohlmeyer <akohlmey@gmail.com>
2019-06-11 07:12:39 -0400

    need to use C++ compiler when checking for includes
    this is the same bugfix as in PR #1504

--- cmake/Modules/Packages/USER-INTEL.cmake
+++ cmake/Modules/Packages/USER-INTEL.cmake
@@ -1,6 +1,6 @@
 if(PKG_USER-INTEL)
   include(CheckIncludeFile)
-  check_include_file(immintrin.h FOUND_IMMINTRIN)
+  check_include_file_cxx(immintrin.h FOUND_IMMINTRIN)
   if(NOT FOUND_IMMINTRIN)
     message(FATAL_ERROR "immintrin.h header not found, Intel package won't work without it")
   endif()

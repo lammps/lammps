3611cfdaf8c153750ce8cc45ac239c1145eb0a2b
mkanski <michal.kanski@live.com>
2019-06-14 19:58:31 +0200

    All styles support setting molecules' orientation

--- src/create_atoms.cpp
+++ src/create_atoms.cpp
@@ -176,8 +176,6 @@ void CreateAtoms::command(int narg, char **arg)
       } else error->all(FLERR,"Illegal create_atoms command");
       iarg += 3;
     } else if (strcmp(arg[iarg],"rotate") == 0) {
-      if (style != SINGLE)
-        error->all(FLERR,"Cannot use create_atoms rotate unless single style");
       if (iarg+5 > narg) error->all(FLERR,"Illegal create_atoms command");
       double thetaone;
       double axisone[3];
@@ -678,7 +676,9 @@ void CreateAtoms::add_random()
         coord[1] >= sublo[1] && coord[1] < subhi[1] &&
         coord[2] >= sublo[2] && coord[2] < subhi[2]) {
       if (mode == ATOM) atom->avec->create_atom(ntype,xone);
-      else add_molecule(xone);
+      else if (quatone[0] == 0 && quatone[1] == 0 && quatone[2] == 0)
+        add_molecule(xone);
+      else add_molecule(xone, quatone);
     }
   }
 
@@ -832,7 +832,9 @@ void CreateAtoms::add_lattice()
           // add the atom or entire molecule to my list of atoms
 
           if (mode == ATOM) atom->avec->create_atom(basistype[m],x);
-          else add_molecule(x);
+          else if (quatone[0] == 0 && quatone[1] == 0 && quatone[2] == 0)
+            add_molecule(x);
+          else add_molecule(x,quatone);
         }
       }
     }

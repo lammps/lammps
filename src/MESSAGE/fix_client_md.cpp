0bc648e23e3a495be12eeaaebc6f779ee2abb56f
Steve Plimpton <sjplimp@sandia.gov>
2019-06-18 14:30:30 -0600

    add message quit protocol, to allow for non-quantum MD steps

--- src/MESSAGE/fix_client_md.cpp
+++ src/MESSAGE/fix_client_md.cpp
@@ -80,21 +80,6 @@ FixClientMD::FixClientMD(LAMMPS *lmp, int narg, char **arg) :
 FixClientMD::~FixClientMD()
 {
   memory->destroy(xpbc);
-
-  CSlib *cs = (CSlib *) lmp->cslib;
-
-  // all-done message to server
-
-  cs->send(-1,0);
-
-  int nfield;
-  int *fieldID,*fieldtype,*fieldlen;
-  cs->recv(nfield,fieldID,fieldtype,fieldlen);
-
-  // clean-up
-
-  delete cs;
-  lmp->cslib = NULL;
 }
 
 /* ---------------------------------------------------------------------- */

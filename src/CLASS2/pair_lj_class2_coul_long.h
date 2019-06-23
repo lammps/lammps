3b6cc29f64a201f3a9083ed1a62e49e35ef3f46f
Evangelos Voyiatzis <evoyiatzis@gmail.com>
2019-06-23 20:16:25 +0200

    Implementation of inner/middle/outer compute methods for lj/class2/coul/long

--- src/CLASS2/pair_lj_class2_coul_long.h
+++ src/CLASS2/pair_lj_class2_coul_long.h
@@ -2,12 +2,10 @@
    LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
    http://lammps.sandia.gov, Sandia National Laboratories
    Steve Plimpton, sjplimp@sandia.gov
-
    Copyright (2003) Sandia Corporation.  Under the terms of Contract
    DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
    certain rights in this software.  This software is distributed under
    the GNU General Public License.
-
    See the README file in the top-level LAMMPS directory.
 ------------------------------------------------------------------------- */
 
@@ -31,7 +29,7 @@ class PairLJClass2CoulLong : public Pair {
   virtual void compute(int, int);
   virtual void settings(int, char **);
   void coeff(int, char **);
-  virtual void init_style();
+  void init_style();
   virtual double init_one(int, int);
   void write_restart(FILE *);
   void read_restart(FILE *);
@@ -40,6 +38,10 @@ class PairLJClass2CoulLong : public Pair {
   void write_data(FILE *);
   void write_data_all(FILE *);
   double single(int, int, int, int, double, double, double, double &);
+ 
+  void compute_inner();
+  void compute_middle();
+  void compute_outer(int, int);
   void *extract(const char *, int &);
 
  protected:
@@ -49,6 +51,7 @@ class PairLJClass2CoulLong : public Pair {
   double **epsilon,**sigma;
   double **lj1,**lj2,**lj3,**lj4,**offset;
   double g_ewald;
+  double *cut_respa;
 
   virtual void allocate();
 };
@@ -59,23 +62,17 @@ class PairLJClass2CoulLong : public Pair {
 #endif
 
 /* ERROR/WARNING messages:
-
 E: Illegal ... command
-
 Self-explanatory.  Check the input script syntax and compare to the
 documentation for the command.  You can use -echo screen as a
 command-line option when running LAMMPS to see the offending line.
-
 E: Incorrect args for pair coefficients
-
 Self-explanatory.  Check the input script or data file.
-
 E: Pair style lj/class2/coul/long requires atom attribute q
-
 The atom style defined does not have this attribute.
-
 E: Pair style requires a KSpace style
-
 No kspace style is defined.
-
+E: Pair cutoff < Respa interior cutoff
+One or more pairwise cutoffs are too short to use with the specified
+rRESPA cutoffs.
 */

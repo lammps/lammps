50e76ff7a1369efdb1c6238d0c937d802db10f6f
Axel Kohlmeyer <akohlmey@gmail.com>
2019-06-25 05:59:29 -0400

    restore virtual keyword and empty lines in comments

--- src/CLASS2/pair_lj_class2_coul_long.h
+++ src/CLASS2/pair_lj_class2_coul_long.h
@@ -2,10 +2,12 @@
    LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
    http://lammps.sandia.gov, Sandia National Laboratories
    Steve Plimpton, sjplimp@sandia.gov
+
    Copyright (2003) Sandia Corporation.  Under the terms of Contract
    DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
    certain rights in this software.  This software is distributed under
    the GNU General Public License.
+
    See the README file in the top-level LAMMPS directory.
 ------------------------------------------------------------------------- */
 
@@ -29,7 +31,7 @@ class PairLJClass2CoulLong : public Pair {
   virtual void compute(int, int);
   virtual void settings(int, char **);
   void coeff(int, char **);
-  void init_style();
+  virtual void init_style();
   virtual double init_one(int, int);
   void write_restart(FILE *);
   void read_restart(FILE *);
@@ -62,17 +64,28 @@ class PairLJClass2CoulLong : public Pair {
 #endif
 
 /* ERROR/WARNING messages:
+
 E: Illegal ... command
+
 Self-explanatory.  Check the input script syntax and compare to the
 documentation for the command.  You can use -echo screen as a
 command-line option when running LAMMPS to see the offending line.
+
 E: Incorrect args for pair coefficients
+
 Self-explanatory.  Check the input script or data file.
+
 E: Pair style lj/class2/coul/long requires atom attribute q
+
 The atom style defined does not have this attribute.
+
 E: Pair style requires a KSpace style
+
 No kspace style is defined.
+
 E: Pair cutoff < Respa interior cutoff
+
 One or more pairwise cutoffs are too short to use with the specified
 rRESPA cutoffs.
+
 */

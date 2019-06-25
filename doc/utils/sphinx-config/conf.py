11569e1dc5b283cde8520477e054093ddfbb1997
Anne Gunn <agunn@sherprog.com>
2019-06-25 08:19:57 -0600

    Add folders to the Sphinx configuration exclude list. Attempting to reduce error noise in the official build.

--- doc/utils/sphinx-config/conf.py
+++ doc/utils/sphinx-config/conf.py
@@ -88,7 +88,13 @@ release = ''
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = [
+    '_build',
+    'convertedRst',
+    'modifiedRst',
+    'originalTxt',
+    'safeRst'
+]
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.

a44f2cc3bbfd3ca9b6cbda34e5b3f0862c9dff15
charlie sievers <charliesievers@charlies-MacBook-Pro.local>
2019-09-23 11:48:54 -0700

    changed flag name in fix_langevin

--- src/fix_langevin.cpp
+++ src/fix_langevin.cpp
@@ -96,7 +96,7 @@ FixLangevin::FixLangevin(LAMMPS *lmp, int narg, char **arg) :
   for (int i = 1; i <= atom->ntypes; i++) ratio[i] = 1.0;
   ascale = 0.0;
   gjfflag = 0;
-  fsflag = 0;
+  osflag = 0;
   nvalues = 0;
   oflag = 0;
   tallyflag = 0;

1074884f741c20ac7b5c5b1459bbc4dbfba18ea3
julient31 <julien.tranchida1@gmail.com>
2019-11-04 09:04:43 -0700

    Commit JT 110419 - removed fmax uninitialized variable from min_cg.cpp - removed tabs from comments - initialized fdotf variables

--- src/min_sd.cpp
+++ src/min_sd.cpp
@@ -78,11 +78,14 @@ int MinSD::iterate(int maxiter)
 
     // force tolerance criterion
 
-    if (normstyle == MAX) fdotf = fnorm_max();		// max force norm
-    else if (normstyle == INF) fdotf = fnorm_inf();	// infinite force norm
-    else if (normstyle == TWO) fdotf = fnorm_sqr();	// Euclidean force 2-norm
-    else error->all(FLERR,"Illegal min_modify command");
-    if (fdotf < update->ftol*update->ftol) return FTOL;
+    fdotf = 0.0;
+    if (update->ftol > 0.0) {
+      if (normstyle == MAX) fdotf = fnorm_max();        // max force norm
+      else if (normstyle == INF) fdotf = fnorm_inf();   // infinite force norm
+      else if (normstyle == TWO) fdotf = fnorm_sqr();   // Euclidean force 2-norm
+      else error->all(FLERR,"Illegal min_modify command");
+      if (fdotf < update->ftol*update->ftol) return FTOL;
+    }
 
     // set new search direction h to f = -Grad(x)
 

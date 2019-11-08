599a189545b184c5d159bdde29ecc380b7eff6e7
lucienPan <lucien.pan0927@gmail.com>
2019-11-08 17:28:49 -0500

    Patch of class2 dihedral

--- src/CLASS2/dihedral_class2.cpp
+++ src/CLASS2/dihedral_class2.cpp
@@ -2,10 +2,12 @@
    LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
    http://lammps.sandia.gov, Sandia National Laboratories
    Steve Plimpton, sjplimp@sandia.gov
+
    Copyright (2003) Sandia Corporation.  Under the terms of Contract
    DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
    certain rights in this software.  This software is distributed under
    the GNU General Public License.
+
    See the README file in the top-level LAMMPS directory.
 ------------------------------------------------------------------------- */
 
@@ -962,3 +964,4 @@ void DihedralClass2::write_data(FILE *fp)
             at_f1_2[i],at_f2_2[i],at_f3_2[i],
             at_theta0_1[i]*180.0/MY_PI,at_theta0_2[i]*180.0/MY_PI);
 }
+

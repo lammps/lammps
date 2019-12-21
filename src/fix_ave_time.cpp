5268ceaf080e819f83404dfde57a84e25883d363
cdt1802 <pak37@cam.ac.uk>
2019-12-21 12:36:20 +0000

    initial commit

--- src/fix_ave_time.cpp
+++ src/fix_ave_time.cpp
@@ -1028,7 +1028,7 @@ void FixAveTime::options(int iarg, int narg, char **arg)
   offlist = NULL;
   overwrite = 0;
   format_user = NULL;
-  format = (char *) " %g";
+  format = (char *) " %f";
   title1 = NULL;
   title2 = NULL;
   title3 = NULL;

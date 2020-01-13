206aa77532a836cf4c4774abc128d34d2832a782
Axel Kohlmeyer <akohlmey@gmail.com>
2020-01-13 11:22:42 -0500

    fix one more whitespace issue

--- src/KOKKOS/pack_kokkos.h
+++ src/KOKKOS/pack_kokkos.h
@@ -6,7 +6,7 @@
 
    Copyright (2014) Sandia Corporation.  Under the terms of Contract
    DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
-   certain rights in this software.  This software is distributed under 
+   certain rights in this software.  This software is distributed under
    the GNU General Public License.
 
    See the README file in the top-level SPARTA directory.

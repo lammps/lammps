07c5adc57ac4f71eb6bf37a675170becaf873b90
Axel Kohlmeyer <akohlmey@gmail.com>
2020-02-14 16:07:34 -0500

    we need more specific matches to correctly handle TIME and TIMESTEP items

--- src/reader_native.cpp
+++ src/reader_native.cpp
@@ -17,6 +17,7 @@
 #include "atom.h"
 #include "memory.h"
 #include "error.h"
+#include "utils.h"
 
 using namespace LAMMPS_NS;
 
@@ -58,13 +59,13 @@ int ReaderNative::read_time(bigint &ntimestep)
 
   // skip over unit and time information, if present.
 
-  if (strstr(line,"ITEM: UNITS") == line)
+  if (utils::strmatch(line,"^\\s*ITEM: UNITS\\s*$"))
     read_lines(2);
 
-  if (strstr(line,"ITEM: TIME") == line)
+  if (utils::strmatch(line,"^\\s*ITEM: TIME\\s*$"))
     read_lines(2);
 
-  if (strstr(line,"ITEM: TIMESTEP") != line)
+  if (!utils::strmatch(line,"^\\s*ITEM: TIMESTEP\\s*$"))
     error->one(FLERR,"Dump file is incorrectly formatted");
 
   read_lines(1);

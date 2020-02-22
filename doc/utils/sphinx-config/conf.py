09ef55ac26ddd0f7afb2e9979a81c1054500cba6
Axel Kohlmeyer <akohlmey@gmail.com>
2020-02-22 06:14:39 -0500

    include small logo image in navigation bar. update copyright date

--- doc/utils/sphinx-config/conf.py
+++ doc/utils/sphinx-config/conf.py
@@ -60,7 +60,7 @@ master_doc = 'Manual'
 
 # General information about the project.
 project = 'LAMMPS'
-copyright = '2013 Sandia Corporation'
+copyright = '2003-2020 Sandia Corporation'
 
 def get_lammps_version():
     import os
@@ -142,7 +142,7 @@ html_title = "LAMMPS documentation"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = 'lammps-logo.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32

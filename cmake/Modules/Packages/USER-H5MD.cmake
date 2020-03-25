58c9c4c64bc02d279b163cfb766ca6a4060f107d
Christoph Junghans <junghans@lanl.gov>
2020-03-25 10:14:42 -0600

    USER-H5MD.cmake: make hdf5 incldir public

--- cmake/Modules/Packages/USER-H5MD.cmake
+++ cmake/Modules/Packages/USER-H5MD.cmake
@@ -3,5 +3,5 @@ if(PKG_USER-H5MD)
 
   find_package(HDF5 REQUIRED)
   target_link_libraries(h5md PRIVATE ${HDF5_LIBRARIES})
-  target_include_directories(h5md PRIVATE ${HDF5_INCLUDE_DIRS})
+  target_include_directories(h5md PUBLIC ${HDF5_INCLUDE_DIRS})
 endif()

d202b8d707e86449afd39e33a2b7f873b77a2a36
Axel Kohlmeyer <akohlmey@gmail.com>
2020-03-31 01:36:07 -0400

    add some more docstrings to the lammps.py file

--- python/lammps.py
+++ python/lammps.py
@@ -103,6 +103,28 @@ class NeighList:
             yield self.get(ii)
 
 class lammps(object):
+  """Create an instance of the LAMMPS python class
+
+  This is a Python wrapper class that exposes the LAMMPS C-library
+  interface to Python.  It either requires that LAMMPS has been compiled
+  as shared library which is then dynamically loaded via the ctypes
+  Python module or that this module called from a Python function that
+  is called from a Python interpreter embedded into a LAMMPS executable.
+  When the class is instantiated it calls the :ref:`lammps_open()
+  <lammps_open>` function of the LAMMPS C-library interface, which in
+  turn will create an instance of the :ref:`LAMMPS class
+  <lammps_ns_lammps>`.
+
+  :param name: "machine" name of the shared LAMMPS libary ("mpi" loads ``liblammps_mpi.so``, "" loads ``liblammps.so``)
+  :type  name: string
+  :param idx: list of command line arguments to be passed to the :ref:`lammps_open() <lammps_open>` function.
+  :type  idx: list
+  :param ptr: pointer to a LAMMPS C++ class when called from an embedded Python interpreter. None means load symbols from shared library
+  :type  ptr: pointer
+  :param comm: MPI communicator
+  :type  comm: MPI_Comm
+
+  """
 
   # detect if Python is using version of mpi4py that can pass a communicator
 
@@ -116,7 +138,7 @@ class lammps(object):
 
   # create instance of LAMMPS
 
-  def __init__(self,name="",cmdargs=None,ptr=None,comm=None):
+  def __init__(self,name='',cmdargs=None,ptr=None,comm=None):
     self.comm = comm
     self.opened = 0
 
@@ -305,20 +327,42 @@ class lammps(object):
       self.opened = 0
 
   def close(self):
+    """Explicitly delete a LAMMPS instance through the C-library interface
+
+    This is a wrapper around the :ref:`lammps_close() <lammps_close>` function of the C-library interface.
+    """
     if self.opened: self.lib.lammps_close(self.lmp)
     self.lmp = None
     self.opened = 0
 
   def finalize(self):
+    """Shut down the MPI communication through the library interface by calling :ref:`lammps_finalize() <lammps_finalize>`.
+    """
     if self.opened: self.lib.lammps_close(self.lmp)
     self.lmp = None
     self.opened = 0
     self.lib.lammps_finalize()
 
   def version(self):
+    """Return a numerical representation of the LAMMPS version in use.
+
+    This is a wrapper around the :ref:`lammps_close() <lammps_close>` function of the C-library interface.
+
+    :return: version number
+    :rtype:  int
+    """
     return self.lib.lammps_version(self.lmp)
 
   def file(self,file):
+    """Read LAMMPS commands from a file
+
+    This is a wrapper around the :ref:`lammps_file() <lammps_file>` function of the C-library interface.
+    It will open the file with the name/path `file` and process the LAMMPS commands line by line until
+    the end. The function will return when the end of the file is reached.
+    
+    :param file: Name of the file/path with LAMMPS commands
+    :type file:  string
+    """
     if file: file = file.encode()
     self.lib.lammps_file(self.lmp,file)
 

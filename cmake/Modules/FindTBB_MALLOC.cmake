b83c98db14a634f855f94e70a10e5dd333f55814
Christoph Junghans <junghans@lanl.gov>
2020-04-03 09:50:28 -0600

    USER-INTEL.cmake: clean up, create imported targets

--- cmake/Modules/FindTBB_MALLOC.cmake
+++ cmake/Modules/FindTBB_MALLOC.cmake
@@ -15,13 +15,22 @@ find_library(TBB_MALLOC_LIBRARY NAMES tbbmalloc PATHS $ENV{TBBROOT}/lib/intel64/
                                                       $ENV{TBBROOT}/lib/intel64/gcc4.4
                                                       $ENV{TBBROOT}/lib/intel64/gcc4.1)
 
-set(TBB_MALLOC_LIBRARIES ${TBB_MALLOC_LIBRARY})
-set(TBB_MALLOC_INCLUDE_DIRS ${TBB_MALLOC_INCLUDE_DIR})
-
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set TBB_MALLOC_FOUND to TRUE
 # if all listed variables are TRUE
 
 find_package_handle_standard_args(TBB_MALLOC DEFAULT_MSG TBB_MALLOC_LIBRARY TBB_MALLOC_INCLUDE_DIR)
+
+if(TBB_MALLOC_FOUND)
+  set(TBB_MALLOC_LIBRARIES ${TBB_MALLOC_LIBRARY})
+  set(TBB_MALLOC_INCLUDE_DIRS ${TBB_MALLOC_INCLUDE_DIR})
+
+  if(NOT TARGET TBB::TBB_MALLOC)
+    add_library(TBB::TBB_MALLOC UNKNOWN IMPORTED)
+    set_target_properties(TBB::TBB_MALLOC PROPERTIES
+      IMPORTED_LOCATION "${TBB_MALLOC_LIBRARY}"
+      INTERFACE_INCLUDE_DIRECTORIES "${TBB_MALLOC_INCLUDE_DIR}")
+  endif()
+endif()
 
 mark_as_advanced(TBB_MALLOC_INCLUDE_DIR TBB_MALLOC_LIBRARY )

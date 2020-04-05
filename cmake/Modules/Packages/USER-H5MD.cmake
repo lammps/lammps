1988a2a72563f955793229bfb1211458df07d728
Christoph Junghans <junghans@lanl.gov>
2020-04-05 14:58:54 -0600

    cmake: make USER-H5MD.cmake consistent

--- cmake/Modules/Packages/USER-H5MD.cmake
+++ cmake/Modules/Packages/USER-H5MD.cmake
@@ -1,7 +1,5 @@
-if(PKG_USER-H5MD)
-  enable_language(C)
+enable_language(C)
 
-  find_package(HDF5 REQUIRED)
-  target_link_libraries(h5md PRIVATE ${HDF5_LIBRARIES})
-  target_include_directories(h5md PUBLIC ${HDF5_INCLUDE_DIRS})
-endif()
+find_package(HDF5 REQUIRED)
+target_link_libraries(h5md PRIVATE ${HDF5_LIBRARIES})
+target_include_directories(h5md PUBLIC ${HDF5_INCLUDE_DIRS})

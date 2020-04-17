2a68637ca3b58e92747a12b524d9189286fb42ad
To Quy-Dong <55282601+toquydong@users.noreply.github.com>
2020-04-17 08:46:24 +0200

    Update random_park.cpp
    Add some random number generator for gaussian, rayleigh and bessel distribution.

--- src/random_park.cpp
+++ src/random_park.cpp
@@ -16,6 +16,7 @@
 #include "random_park.h"
 #include <cmath>
 #include "error.h"
+#include "math_const.h"
 
 using namespace LAMMPS_NS;
 
@@ -132,4 +133,55 @@ void RanPark::reset(int ibase, double *coord)
 int RanPark::state()
 {
   return seed;
+}
+/* ----------------------------------------------------------------------
+   Gaussian RN
+------------------------------------------------------------------------- */
+
+double RanMars::gaussian(double mu, double sigma)
+{
+  double v1;
+  v1 = mu+sigma*gaussian();
+  return v1;
+}
+
+/* ----------------------------------------------------------------------
+   Rayleigh RN
+------------------------------------------------------------------------- */
+
+double RanMars::rayleigh(double sigma)
+{
+  double first,v1;
+
+  if (sigma <= 0) error->all(FLERR,"Invalid Rayleigh parameter");
+
+  v1 = uniform();
+  first = sigma*sqrt(-2.0*log(v1));
+  return first;
+}
+
+/* ----------------------------------------------------------------------
+   Bessel exponential RN
+------------------------------------------------------------------------- */
+
+double RanMars::besselexp(double theta, double alpha, double cp)
+{
+  double first,v1,v2;
+
+  if (theta < 0.0 || alpha < 0.0 || alpha < 1.0)
+    error->all(FLERR,"Invalid Bessel exponential distribution parameters");
+
+  v1 = uniform();
+  v2 = uniform();
+
+  if (cp < 0.0)
+    first = sqrt((1.0-alpha)*cp*cp - 2.0*alpha*theta*log(v1) +
+                 2.0*sqrt(-2.0*theta*(1.0-alpha)*alpha*log(v1)) *
+                 cos(2.0*MathConst::MY_PI*v2)*cp);
+  else
+    first = -sqrt((1.0-alpha)*cp*cp - 2.0*alpha*theta*log(v1) -
+                  2.0*sqrt(-2.0*theta*(1.0-alpha)*alpha*log(v1)) *
+                  cos(2.0*MathConst::MY_PI*v2)*cp);
+
+  return first;
 }

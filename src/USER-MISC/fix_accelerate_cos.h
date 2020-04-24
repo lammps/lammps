0545de51374d305a79942f766e56ae7112320fd7
Zheng Gong <unimisia@gmail.com>
2020-04-24 20:23:14 +0200

    clean up comments in fix_accelerate_cos.h

--- src/USER-MISC/fix_accelerate_cos.h
+++ src/USER-MISC/fix_accelerate_cos.h
@@ -54,21 +54,4 @@ Self-explanatory.  Check the input script syntax and compare to the
 documentation for the command.  You can use -echo screen as a
 command-line option when running LAMMPS to see the offending line.
 
-E: Region ID for fix setforce does not exist
-
-Self-explanatory.
-
-E: Variable name for fix setforce does not exist
-
-Self-explanatory.
-
-E: Variable for fix setforce is invalid style
-
-Only equal-style variables can be used.
-
-E: Cannot use non-zero forces in an energy minimization
-
-Fix setforce cannot be used in this manner.  Use fix addforce
-instead.
-
 */

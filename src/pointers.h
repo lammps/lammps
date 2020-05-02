a747395c87183c058df42b182fe5658d7c36f55a
Axel Kohlmeyer <akohlmey@gmail.com>
2020-05-01 20:11:08 -0400

    remove decorations of Pointers class members

--- src/pointers.h
+++ src/pointers.h
@@ -81,31 +81,31 @@ class Pointers {
   virtual ~Pointers() {}
 
  protected:
-  LAMMPS *lmp;                  //!< pointer to LAMMPS class
-  Memory *&memory;              //!< reference to Memory class instance in LAMMPS class
-  Error *&error;                //!< reference to Error class instance in LAMMPS class
-  Universe *&universe;          //!< reference to Universe class instance in LAMMPS class
-  Input *&input;                //!< reference to Input class instance in LAMMPS class
+  LAMMPS *lmp;
+  Memory *&memory;
+  Error *&error;
+  Universe *&universe;
+  Input *&input;
 
-  Atom *&atom;                  //!< reference to Atom class instance in LAMMPS class
-  Update *&update;              //!< reference to Update class instance in LAMMPS class
-  Neighbor *&neighbor;          //!< reference to Neighbor class instance in LAMMPS class
-  Comm *&comm;                  //!< reference to Comm class instance in LAMMPS class
-  Domain *&domain;              //!< reference to Domain class instance in LAMMPS class
-  Force *&force;                //!< reference to Force class instance in LAMMPS class
-  Modify *&modify;              //!< reference to Modify class instance in LAMMPS class
-  Group *&group;                //!< reference to Group class instance in LAMMPS class
-  Output *&output;              //!< reference to Output class instance in LAMMPS class
-  Timer *&timer;                //!< reference to Timer class instance in LAMMPS class
+  Atom *&atom;
+  Update *&update;
+  Neighbor *&neighbor;
+  Comm *&comm;
+  Domain *&domain;
+  Force *&force;
+  Modify *&modify;
+  Group *&group;
+  Output *&output;
+  Timer *&timer;
 
-  MPI_Comm &world;              //!< reference to MPI "world" communicator in LAMMPS class
-  FILE *&infile;                //!< reference to input file pointer in LAMMPS class
-  FILE *&screen;                //!< reference to screen output file pointer in LAMMPS class
-  FILE *&logfile;               //!< reference to log file output file pointer in LAMMPS class
+  MPI_Comm &world;
+  FILE *&infile;
+  FILE *&screen;
+  FILE *&logfile;
 
-  class AtomKokkos *&atomKK;    //!< reference to AtomKokkos class instance in LAMMPS class
-  class MemoryKokkos *&memoryKK; //!< reference to AemoryKokkos class instance in LAMMPS class
-  class Python *&python;         //!< reference to Python class instance in LAMMPS class
+  class AtomKokkos *&atomKK;
+  class MemoryKokkos *&memoryKK;
+  class Python *&python;
 };
 
 }

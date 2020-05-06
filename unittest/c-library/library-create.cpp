7c3a3e08ebf4c1e9d01130a333b0be8264c41323
Axel Kohlmeyer <akohlmey@gmail.com>
2020-05-06 12:31:35 -0400

    add tests for lammps_open_no_mpi() and lammps_open_fortran()

--- unittest/c-library/library-create.cpp
+++ unittest/c-library/library-create.cpp
@@ -61,6 +61,61 @@ TEST(lammps_open, with_args) {
     EXPECT_STREQ(output.substr(0,16).c_str(), "Total wall time:");
 }
 
+TEST(lammps_open_no_mpi, no_screen) {
+    const char *args[] = {"liblammps",
+                          "-log", "none",
+                          "-screen", "none",
+                          "-nocite"};
+    char **argv = (char **)args;
+    int argc = sizeof(args)/sizeof(char *);
+
+    ::testing::internal::CaptureStdout();
+    void *alt_ptr;
+    void *handle = lammps_open_no_mpi(argc, argv, &alt_ptr);
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_STREQ(output.c_str(),"");
+    EXPECT_EQ(handle,alt_ptr);
+    LAMMPS_NS::LAMMPS *lmp = (LAMMPS_NS::LAMMPS *)handle;
+
+    EXPECT_EQ(lmp->world, MPI_COMM_WORLD);
+    EXPECT_EQ(lmp->infile, stdin);
+    EXPECT_EQ(lmp->screen, nullptr);
+    EXPECT_EQ(lmp->logfile, nullptr);
+    EXPECT_EQ(lmp->citeme, nullptr);
+    ::testing::internal::CaptureStdout();
+    lammps_close(handle);
+    output = testing::internal::GetCapturedStdout();
+    EXPECT_STREQ(output.c_str(), "");
+}
+
+TEST(lammps_open_fortran, no_args) {
+
+    // MPI is already initialized
+    MPI_Comm mycomm;
+    MPI_Comm_split(MPI_COMM_WORLD, 0, 1, &mycomm);
+    int fcomm = MPI_Comm_c2f(mycomm);
+    ::testing::internal::CaptureStdout();
+    void *handle = lammps_open_fortran(0, NULL, fcomm, NULL);
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_STREQ(output.substr(0,6).c_str(),"LAMMPS");
+    LAMMPS_NS::LAMMPS *lmp = (LAMMPS_NS::LAMMPS *)handle;
+
+    // MPI STUBS uses no real communicators
+#if !defined(MPI_STUBS)
+    EXPECT_NE(lmp->world, MPI_COMM_WORLD);
+#endif
+
+    EXPECT_EQ(lmp->world, mycomm);
+    EXPECT_EQ(lmp->infile, stdin);
+    EXPECT_EQ(lmp->screen, stdout);
+    EXPECT_NE(lmp->logfile, nullptr);
+    EXPECT_NE(lmp->citeme, nullptr);
+    ::testing::internal::CaptureStdout();
+    lammps_close(handle);
+    output = testing::internal::GetCapturedStdout();
+    EXPECT_STREQ(output.substr(0,16).c_str(), "Total wall time:");
+}
+
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);

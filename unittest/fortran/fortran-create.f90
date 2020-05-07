e458402c18d247ad38d7b83e3cb1012bce963b39
Axel Kohlmeyer <akohlmey@gmail.com>
2020-05-07 00:02:51 -0400

    add fortran test with split communicator and arguments

--- unittest/fortran/fortran-create.f90
+++ unittest/fortran/fortran-create.f90
@@ -1,30 +1,57 @@
 MODULE keepdata
   USE liblammps
   TYPE(LAMMPS) :: lmp
+  INTEGER :: mycomm
 END MODULE keepdata
 
 
-FUNCTION f_lammps_open_noargs() BIND(C, name="f_lammps_open_noargs")
+FUNCTION f_lammps_open_no_args() BIND(C, name="f_lammps_open_no_args")
   USE ISO_C_BINDING, ONLY: c_ptr
   USE liblammps
   USE keepdata,      ONLY: lmp
   IMPLICIT NONE
-  TYPE(c_ptr) :: f_lammps_open_noargs
+  TYPE(c_ptr) :: f_lammps_open_no_args
   
-  CHARACTER(len=*), DIMENSION(1), PARAMETER :: args = &
-      [ CHARACTER(len=12) :: 'liblammps' ]
-
   lmp = lammps()
-  f_lammps_open_noargs = lmp%handle
-END FUNCTION f_lammps_open_noargs
+  f_lammps_open_no_args = lmp%handle
+END FUNCTION f_lammps_open_no_args
 
+FUNCTION f_lammps_open_with_args() BIND(C, name="f_lammps_open_with_args")
+  USE ISO_C_BINDING, ONLY: c_ptr
+  USE MPI,           ONLY: MPI_COMM_WORLD, mpi_comm_split
+  USE liblammps
+  USE keepdata,      ONLY: lmp,mycomm
+  IMPLICIT NONE
+  TYPE(c_ptr) :: f_lammps_open_with_args
+  INTEGER     :: color, key, ierr
+  
+  CHARACTER(len=*), DIMENSION(4), PARAMETER :: args = &
+      [ CHARACTER(len=12) :: 'liblammps', '-log', 'none', '-nocite' ]
+
+  color = 2
+  key = 1
+  CALL mpi_comm_split(MPI_COMM_WORLD, color, key, mycomm, ierr)
+  lmp = lammps(args,mycomm)
+  f_lammps_open_with_args = lmp%handle
+END FUNCTION f_lammps_open_with_args
 
 SUBROUTINE f_lammps_close() BIND(C, name="f_lammps_close")
+  USE ISO_C_BINDING, ONLY: c_null_ptr
   USE liblammps
   USE keepdata, ONLY: lmp
   IMPLICIT NONE
 
-  call lmp%close()
+  CALL lmp%close()
+  lmp%handle = c_null_ptr
 END SUBROUTINE f_lammps_close
+  
+FUNCTION f_lammps_get_comm() BIND(C, name="f_lammps_get_comm")
+  USE liblammps
+  USE keepdata, ONLY: mycomm
+  IMPLICIT NONE
+  INTEGER :: f_lammps_get_comm
+
+  f_lammps_get_comm = mycomm
+END FUNCTION f_lammps_get_comm
   
 
--- unittest/fortran/wrap-create.cpp
+++ unittest/fortran/wrap-create.cpp
@@ -9,13 +9,15 @@
 
 // prototypes for fortran reverse wrapper functions
 extern "C" {
-    void *f_lammps_open_noargs();
+    void *f_lammps_open_no_args();
+    void *f_lammps_open_with_args();
     void f_lammps_close();
+    int f_lammps_get_comm();
 }
 
-TEST(fortran_open, noargs) {
+TEST(fortran_open, no_args) {
     ::testing::internal::CaptureStdout();
-    void *handle = f_lammps_open_noargs();
+    void *handle = f_lammps_open_no_args();
     std::string output = testing::internal::GetCapturedStdout();
     EXPECT_STREQ(output.substr(0,6).c_str(),"LAMMPS");
     int mpi_init=0;
@@ -26,6 +28,28 @@ TEST(fortran_open, noargs) {
     EXPECT_EQ(lmp->infile, stdin);
     EXPECT_EQ(lmp->screen, stdout);
     EXPECT_NE(lmp->citeme, nullptr);
+    ::testing::internal::CaptureStdout();
+    f_lammps_close();
+    output = testing::internal::GetCapturedStdout();
+    EXPECT_STREQ(output.substr(0,16).c_str(), "Total wall time:");
+}
+
+TEST(fortran_open, with_args) {
+    ::testing::internal::CaptureStdout();
+    void *handle = f_lammps_open_with_args();
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_STREQ(output.substr(0,6).c_str(),"LAMMPS");
+    LAMMPS_NS::LAMMPS *lmp = (LAMMPS_NS::LAMMPS *)handle;
+    EXPECT_EQ(lmp->infile, stdin);
+    EXPECT_EQ(lmp->screen, stdout);
+    EXPECT_EQ(lmp->logfile, nullptr);
+    EXPECT_EQ(lmp->citeme, nullptr);
+
+    int f_comm = f_lammps_get_comm();
+    MPI_Comm mycomm = MPI_Comm_f2c(f_comm);
+    EXPECT_NE(lmp->world, MPI_COMM_WORLD);
+    EXPECT_EQ(lmp->world, mycomm);
+    
     ::testing::internal::CaptureStdout();
     f_lammps_close();
     output = testing::internal::GetCapturedStdout();

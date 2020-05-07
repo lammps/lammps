e53838699e311c1cb26f6c59edbd43e9f1e0ccf0
Axel Kohlmeyer <akohlmey@gmail.com>
2020-05-07 19:41:02 -0400

    implement suggestions from @junghans

--- unittest/python/python-open.py
+++ unittest/python/python-open.py
@@ -1,13 +1,5 @@
 
 import sys
-
-if len(sys.argv) < 2:
-    sys.exit("usage: %s <directory/with/lammps.py>" % sys.argv[0])
-
-sys.path.insert(0,sys.argv[1])
-
-from lammps import *
-from ctypes import *
+from lammps import lammps
 
 lmp=lammps()
-

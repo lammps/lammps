072aabbe0d468a728caa11aca2e8b22e7d3792c8
Axel Kohlmeyer <akohlmey@gmail.com>
2020-05-08 00:37:13 -0400

    convert python test to use unittest module

--- unittest/python/python-open.py
+++ unittest/python/python-open.py
@@ -1,17 +1,37 @@
 
 import sys
+import unittest
 from lammps import lammps
+from io import StringIO
+from contextlib import redirect_stdout
 
-lmp=lammps()
-lmp=lammps(cmdargs=['-nocite','-sf','opt','-log','none'])
+class PythonOpen(unittest.TestCase):
 
-has_mpi=False
-try:
-    from mpi4py import MPI
-    has_mpi=True
-except:
-    sys.exit(0)
+    def setUp(self):
+        self.has_mpi=False
+        try:
+            from mpi4py import MPI
+            self.has_mpi=True
+        except:
+            pass
 
-mycomm=MPI.Comm.Split(MPI.COMM_WORLD, 0, 1)
-lmp=lammps(comm=mycomm)
+    def testNoArgs(self):
+        """Create LAMMPS instance without any arguments"""
 
+        lmp=lammps()
+        lmp.__del__()
+
+    def testWithArgs(self):
+        """Create LAMMPS instance with a few arguments"""
+        lmp=lammps(cmdargs=['-nocite','-sf','opt','-log','none'])
+        lmp.__del__()
+
+    def testWithMPI(self):
+        if self.has_mpi:
+            from mpi4py import MPI
+            mycomm=MPI.Comm.Split(MPI.COMM_WORLD, 0, 1)
+            lmp=lammps(comm=mycomm)
+            lmp.__del__()
+
+if __name__ == "__main__":
+    unittest.main()

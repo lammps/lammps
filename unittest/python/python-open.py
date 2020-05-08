17b661b9cb62fbac32531db61bed35ad24d93019
Axel Kohlmeyer <akohlmey@gmail.com>
2020-05-07 21:01:23 -0400

    add a few more ways to initialize a lammps object

--- unittest/python/python-open.py
+++ unittest/python/python-open.py
@@ -3,3 +3,15 @@ import sys
 from lammps import lammps
 
 lmp=lammps()
+lmp=lammps(cmdargs=['-nocite','-sf','opt','-log','none'])
+
+has_mpi=False
+try:
+    from mpi4py import MPI
+    has_mpi=True
+except:
+    sys.exit(0)
+
+mycomm=MPI.Comm.Split(MPI.COMM_WORLD, 0, 1)
+lmp=lammps(comm=mycomm)
+

9808e6c8409586b7348e3b56f09d5825b67cf7e1
Axel Kohlmeyer <akohlmey@gmail.com>
2020-05-09 12:00:41 -0400

    add assertions to pythonopen tests

--- unittest/python/python-open.py
+++ unittest/python/python-open.py
@@ -2,15 +2,16 @@
 import sys,os,unittest
 from lammps import lammps
 
+has_mpi=False
+try:
+    from mpi4py import MPI
+    has_mpi=True
+except:
+    pass
+
 class PythonOpen(unittest.TestCase):
 
     def setUp(self):
-        self.has_mpi=False
-        try:
-            from mpi4py import MPI
-            self.has_mpi=True
-        except:
-            pass
         self.machine=None
         if 'LAMMPS_MACHINE_NAME' in os.environ:
             self.machine=os.environ['LAMMPS_MACHINE_NAME']
@@ -19,21 +20,27 @@ class PythonOpen(unittest.TestCase):
         """Create LAMMPS instance without any arguments"""
 
         lmp=lammps(name=self.machine)
-        lmp.__del__()
+        self.assertIsNot(lmp.lmp,None)
+        self.assertEqual(lmp.opened,1)
+        self.assertEqual(has_mpi,lmp.has_mpi4py)
+        lmp.close()
+        self.assertIsNone(lmp.lmp,None)
+        self.assertEqual(lmp.opened,0)
 
     def testWithArgs(self):
         """Create LAMMPS instance with a few arguments"""
         lmp=lammps(name=self.machine,
                    cmdargs=['-nocite','-sf','opt','-log','none'])
-        lmp.close()
-        lmp.__del__()
+        self.assertIsNot(lmp.lmp,None)
+        self.assertEqual(lmp.opened,1)
 
+    @unittest.skipIf(not has_mpi,"Skipping MPI test since mpi4py is not found")
     def testWithMPI(self):
-        if self.has_mpi:
-            from mpi4py import MPI
-            mycomm=MPI.Comm.Split(MPI.COMM_WORLD, 0, 1)
-            lmp=lammps(name=self.machine,comm=mycomm)
-            lmp.__del__()
+        mycomm=MPI.Comm.Split(MPI.COMM_WORLD, 0, 1)
+        lmp=lammps(name=self.machine,comm=mycomm)
+        self.assertIsNot(lmp.lmp,None)
+        self.assertEqual(lmp.opened,1)
+        lmp.close()
 
 if __name__ == "__main__":
     unittest.main()

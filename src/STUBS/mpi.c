6e536087897c5b49d309760bdab63b252224762e
Axel Kohlmeyer <akohlmey@gmail.com>
2020-05-10 14:08:54 -0400

    a new communicator should have a different number than the old

--- src/STUBS/mpi.c
+++ src/STUBS/mpi.c
@@ -353,7 +353,7 @@ int MPI_Get_count(MPI_Status *status, MPI_Datatype datatype, int *count)
 
 int MPI_Comm_split(MPI_Comm comm, int color, int key, MPI_Comm *comm_out)
 {
-  *comm_out = comm;
+  *comm_out = comm+1;
   return 0;
 }
 
@@ -361,7 +361,7 @@ int MPI_Comm_split(MPI_Comm comm, int color, int key, MPI_Comm *comm_out)
 
 int MPI_Comm_dup(MPI_Comm comm, MPI_Comm *comm_out)
 {
-  *comm_out = comm;
+  *comm_out = comm+1;
   return 0;
 }
 

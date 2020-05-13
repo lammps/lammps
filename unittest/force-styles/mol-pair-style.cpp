9c25cfa887a5a785caf78c05c40aeb44b63cfb2c
Axel Kohlmeyer <akohlmey@gmail.com>
2020-05-13 07:54:58 -0400

    yaml file generation now captures initial state

--- unittest/force-styles/mol-pair-style.cpp
+++ unittest/force-styles/mol-pair-style.cpp
@@ -2,6 +2,8 @@
 
 #include "lammps.h"
 #include "atom.h"
+#include "force.h"
+#include "pair.h"
 #include "input.h"
 #include <mpi.h>
 #include <cstdio>
@@ -19,6 +21,10 @@
 typedef struct {
     double x,y,z;
 } coord_t;
+
+typedef struct {
+    double xx,yy,zz,xy,xz,yz;
+} stress_t;
 
 enum state_value {
     START,
@@ -42,11 +48,16 @@ public:
     bool has_global_stress;
     bool has_atom_energy;
     bool has_atom_stress;
+    std::vector<std::string> pre_commands;
+    std::vector<std::string> post_commands;
     std::string input_file;
     std::string pair_style;
     std::vector<std::string> pair_coeff;
     int natoms;
-    std::vector<coord_t> init_force;
+    std::vector<coord_t> init_forces;
+    double init_vdwl;
+    double init_coul;
+    stress_t init_stress;
     TestConfig() : pair_newton_on(false),
                    pair_newton_off(false),
                    has_global_energy(false),
@@ -55,11 +66,42 @@ public:
                    has_atom_stress(false),
                    input_file(""),
                    pair_style("zero"),
-                   natoms(0) {
+                   natoms(0),
+                   init_vdwl(0),
+                   init_coul(0),
+                   init_stress({0,0,0,0,0,0}) {
+        pre_commands.clear();
+        post_commands.clear();
         pair_coeff.clear();
-        init_force.clear();
+        init_forces.clear();
     }
 } test_config;
+
+
+LAMMPS_NS::LAMMPS *init_lammps(int argc, char **argv, const TestConfig &cfg)
+{
+    LAMMPS_NS::LAMMPS *lmp;
+
+    int flag;
+    MPI_Initialized(&flag);
+    if (!flag) MPI_Init(&argc,&argv);
+
+    lmp = new LAMMPS_NS::LAMMPS(argc, argv, MPI_COMM_WORLD);
+    for (std::size_t i=0; i < cfg.pre_commands.size(); ++i)
+        lmp->input->one(cfg.pre_commands[i].c_str());
+    lmp->input->file(cfg.input_file.c_str());
+    std::string cmd("pair_style ");
+    cmd += cfg.pair_style;
+    lmp->input->one(cmd.c_str());
+    for (std::size_t i=0; i < cfg.pair_coeff.size(); ++i) {
+        cmd = "pair_coeff " + cfg.pair_coeff[i];
+        lmp->input->one(cmd.c_str());
+    }
+    for (std::size_t i=0; i < cfg.post_commands.size(); ++i)
+        lmp->input->one(cfg.post_commands[i].c_str());
+    lmp->input->one("run 0 post no");
+    return lmp;
+}
 
 int consume_event(struct parser_state *s, yaml_event_t *event)
 {
@@ -197,6 +239,28 @@ int parse(const char *infile)
                     test_config.has_atom_energy =
                         get_bool_from_data(event.data.scalar.value,
                                            event.data.scalar.length);
+                } else if (state.key == "pre_commands") {
+                    test_config.pre_commands.clear();
+                    std::string data  = (char *)event.data.scalar.value;
+                    std::size_t first = 0;
+                    std::size_t found = data.find("\n");
+                    while (found != std::string::npos) {
+                        std::string line(data.substr(first,found));
+                        test_config.pre_commands.push_back(line);
+                        data = data.substr(found+1);
+                        found = data.find("\n");
+                    }
+                } else if (state.key == "post_commands") {
+                    test_config.post_commands.clear();
+                    std::string data  = (char *)event.data.scalar.value;
+                    std::size_t first = 0;
+                    std::size_t found = data.find("\n");
+                    while (found != std::string::npos) {
+                        std::string line(data.substr(first,found));
+                        test_config.post_commands.push_back(line);
+                        data = data.substr(found+1);
+                        found = data.find("\n");
+                    }
                 } else if (state.key == "input_file") {
                     test_config.input_file = (char *)event.data.scalar.value;
                 } else if (state.key == "pair_style") {
@@ -212,9 +276,36 @@ int parse(const char *infile)
                         data = data.substr(found+1);
                         found = data.find("\n");
                     }
-                } else
-                    std::cerr << "accepted: " << state.key
-                          << " = " << event.data.scalar.value << std::endl;
+                } else if (state.key == "natoms") {
+                    test_config.natoms = atoi((char *)event.data.scalar.value);
+                } else if (state.key == "init_vdwl") {
+                    test_config.init_vdwl = atof((char *)event.data.scalar.value);
+                } else if (state.key == "init_coul") {
+                    test_config.init_coul = atof((char *)event.data.scalar.value);
+                } else if (state.key == "init_stress") {
+                    stress_t stress;
+                    sscanf((char *)event.data.scalar.value,
+                           "%lg %lg %lg %lg %lg %lg",
+                           &stress.xx, &stress.yy, &stress.zz,
+                           &stress.xy, &stress.xz, &stress.yz);
+                    test_config.init_stress = stress;
+                } else if (state.key == "init_forces") {
+                    test_config.init_forces.clear();
+                    test_config.init_forces.resize(test_config.natoms+1);
+                    std::string data  = (char *)event.data.scalar.value;
+                    std::size_t first = 0;
+                    std::size_t found = data.find("\n");
+                    while (found != std::string::npos) {
+                        std::string line(data.substr(first,found));
+                        coord_t xyz;
+                        int tag;
+                        sscanf(line.c_str(), "%d %lg %lg %lg", &tag, &xyz.x, &xyz.y, &xyz.z);
+                        test_config.init_forces[tag] = xyz;
+                        data = data.substr(found+1);
+                        found = data.find("\n");
+                    }
+                } else std::cerr << "Ignoring unknown key/value pair: " << state.key
+                                 << " = " << event.data.scalar.value << std::endl;
             }
             yaml_event_delete(&event);
         } while (state.state != STOP);
@@ -242,6 +333,59 @@ void emit_boolean(yaml_emitter_t *emitter, yaml_event_t *event,
                                  (yaml_char_t *) out.c_str(),
                                  out.size(), 1, 0,
                                  YAML_PLAIN_SCALAR_STYLE);
+    yaml_emitter_emit(emitter, event);
+}
+
+void emit_double(yaml_emitter_t *emitter, yaml_event_t *event,
+                 const std::string &key, const double value)
+{
+    yaml_scalar_event_initialize(event, NULL,
+                                 (yaml_char_t *)YAML_STR_TAG,
+                                 (yaml_char_t *) key.c_str(),
+                                 key.size(), 1, 0,
+                                 YAML_PLAIN_SCALAR_STYLE);
+    yaml_emitter_emit(emitter, event);
+    char buf[256];
+    snprintf(buf,256,"%.15g",value);
+    yaml_scalar_event_initialize(event, NULL,
+                                 (yaml_char_t *)YAML_STR_TAG,
+                                 (yaml_char_t *)buf,
+                                 strlen(buf), 1, 0,
+                                 YAML_PLAIN_SCALAR_STYLE);
+    yaml_emitter_emit(emitter, event);
+}
+
+void emit_string(yaml_emitter_t *emitter, yaml_event_t *event,
+                 const std::string &key, const std::string &value)
+{
+    yaml_scalar_event_initialize(event, NULL,
+                                 (yaml_char_t *)YAML_STR_TAG,
+                                 (yaml_char_t *) key.c_str(),
+                                 key.size(), 1, 0,
+                                 YAML_PLAIN_SCALAR_STYLE);
+    yaml_emitter_emit(emitter, event);
+    yaml_scalar_event_initialize(event, NULL,
+                                 (yaml_char_t *)YAML_STR_TAG,
+                                 (yaml_char_t *)value.c_str(),
+                                 value.size(), 1, 0,
+                                 YAML_PLAIN_SCALAR_STYLE);
+    yaml_emitter_emit(emitter, event);
+}
+
+void emit_block(yaml_emitter_t *emitter, yaml_event_t *event,
+                const std::string &key, const std::string &value)
+{
+    yaml_scalar_event_initialize(event, NULL,
+                                 (yaml_char_t *)YAML_STR_TAG,
+                                 (yaml_char_t *) key.c_str(),
+                                 key.size(), 1, 0,
+                                 YAML_PLAIN_SCALAR_STYLE);
+    yaml_emitter_emit(emitter, event);
+    yaml_scalar_event_initialize(event, NULL,
+                                 (yaml_char_t *)YAML_STR_TAG,
+                                 (yaml_char_t *)value.c_str(),
+                                 value.size(), 1, 0,
+                                 YAML_LITERAL_SCALAR_STYLE);
     yaml_emitter_emit(emitter, event);
 }
 
@@ -281,49 +425,54 @@ void generate(const char *outfile)
     emit_boolean(&emitter, &event,
                  "atom_energy", test_config.has_atom_energy);
 
-    yaml_scalar_event_initialize(&event, NULL,
-                                 (yaml_char_t *)YAML_STR_TAG,
-                                 (yaml_char_t *) "pair_style",
-                                 strlen("pair_style"), 1, 0,
-                                 YAML_PLAIN_SCALAR_STYLE);
-    yaml_emitter_emit(&emitter, &event);
-    yaml_scalar_event_initialize(&event, NULL,
-                                 (yaml_char_t *)YAML_STR_TAG,
-                                 (yaml_char_t *)test_config.pair_style.c_str(),
-                                 test_config.pair_style.size(), 1, 0,
-                                 YAML_PLAIN_SCALAR_STYLE);
-    yaml_emitter_emit(&emitter, &event);
-
     std::string block("");
+    for (std::size_t i=0; i < test_config.pre_commands.size(); ++i) {
+        block += test_config.pre_commands[i] + "\n";
+    }
+    emit_block(&emitter, &event, "pre_commands", block);
+    block.clear();
+    for (std::size_t i=0; i < test_config.post_commands.size(); ++i) {
+        block += test_config.post_commands[i] + "\n";
+    }
+    emit_block(&emitter, &event, "post_commands", block);
+    emit_string(&emitter, &event, "input_file", test_config.input_file);
+
+    emit_string(&emitter, &event, "pair_style", test_config.pair_style);
+    block.clear();
     for (std::size_t i=0; i < test_config.pair_coeff.size(); ++i) {
         block += test_config.pair_coeff[i] + "\n";
     }
-    yaml_scalar_event_initialize(&event, NULL,
-                                 (yaml_char_t *)YAML_STR_TAG,
-                                 (yaml_char_t *) "pair_coeff",
-                                 strlen("pair_coeff"), 1, 0,
-                                 YAML_PLAIN_SCALAR_STYLE);
-    yaml_emitter_emit(&emitter, &event);
-    yaml_scalar_event_initialize(&event, NULL,
-                                 (yaml_char_t *)YAML_STR_TAG,
-                                 (yaml_char_t *)block.c_str(),
-                                 block.size(), 1, 0,
-                                 YAML_LITERAL_SCALAR_STYLE);
-    yaml_emitter_emit(&emitter, &event);
+    emit_block(&emitter, &event, "pair_coeff", block);
+    
+    const char *args[] = {"MolPairStyle", "-log", "none", "-echo", "screen", "-nocite" };
+    char **argv = (char **)args;
+    int argc = sizeof(args)/sizeof(char *);
+    LAMMPS_NS::LAMMPS *lmp = init_lammps(argc,argv,test_config);
 
-    yaml_scalar_event_initialize(&event, NULL,
-                                 (yaml_char_t *)YAML_STR_TAG,
-                                 (yaml_char_t *) "input_file",
-                                 strlen("input_file"), 1, 0,
-                                 YAML_PLAIN_SCALAR_STYLE);
-    yaml_emitter_emit(&emitter, &event);
-    yaml_scalar_event_initialize(&event, NULL,
-                                 (yaml_char_t *)YAML_STR_TAG,
-                                 (yaml_char_t *)test_config.input_file.c_str(),
-                                 test_config.input_file.size(), 1, 0,
-                                 YAML_PLAIN_SCALAR_STYLE);
-    yaml_emitter_emit(&emitter, &event);
+    const int natoms = lmp->atom->natoms;
+    const int bufsize = 256;
+    char buf[bufsize];
+    snprintf(buf,bufsize,"%d", natoms);
+    emit_string(&emitter, &event, "natoms", buf);
+    emit_double(&emitter, &event, "init_vdwl", lmp->force->pair->eng_vdwl);
+    emit_double(&emitter, &event, "init_coul", lmp->force->pair->eng_coul);
 
+    double *stress = lmp->force->pair->virial;
+    snprintf(buf,bufsize,"%.15g %.15g %.15g %.15g %.15g %.15g",
+             stress[0],stress[1],stress[2],stress[3],stress[4],stress[5]);
+    emit_string(&emitter, &event, "init_stress", buf);
+
+    block = "";
+    double **f = lmp->atom->f;
+    LAMMPS_NS::tagint *tag = lmp->atom->tag;
+    for (int i=0; i < natoms; ++i) {
+        snprintf(buf,bufsize,"%d %.15g %.15g %.15g\n",
+                 (int)tag[i], f[i][0], f[i][1], f[i][2]);
+        block += std::string(buf);
+    }
+    emit_block(&emitter, &event, "init_forces", block);
+
+    delete lmp;
     yaml_mapping_end_event_initialize(&event);
     yaml_emitter_emit(&emitter, &event);
     yaml_document_end_event_initialize(&event, 0);

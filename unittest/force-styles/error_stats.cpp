d9147171aff0685c280f38d4f690d2fee4409ce4
Axel Kohlmeyer <akohlmey@gmail.com>
2020-05-19 18:44:12 -0400

    add copyright header

--- unittest/force-styles/error_stats.cpp
+++ unittest/force-styles/error_stats.cpp
@@ -1,9 +1,20 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
 
 #include "error_stats.h"
 #include <iostream>
 #include <string>
 #include <cmath>
-
 
 void ErrorStats::reset() {
     num = 0;

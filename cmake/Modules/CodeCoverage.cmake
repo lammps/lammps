8cf0f341b66f9b90ebc8a3176bdf5f6ba953cc11
Richard Berger <richard.berger@temple.edu>
2020-05-20 17:51:29 -0400

    Move coverage html into folder

--- cmake/Modules/CodeCoverage.cmake
+++ cmake/Modules/CodeCoverage.cmake
@@ -18,11 +18,21 @@ if(ENABLE_COVERAGE)
             COMMENT "Generating XML Coverage Report..."
         )
 
+        set(COVERAGE_HTML_DIR ${CMAKE_BINARY_DIR}/coverage_html)
+
+        add_custom_target(coverage_html_folder
+            COMMAND ${CMAKE_COMMAND} -E make_directory ${COVERAGE_HTML_DIR})
+
         add_custom_target(
             gen_coverage_html
-            COMMAND ${GCOVR_BINARY} -s  --html --html-details -r ${ABSOLUTE_LAMMPS_SOURCE_DIR} --object-directory=${CMAKE_BINARY_DIR} -o coverage.html
+            COMMAND ${GCOVR_BINARY} -s  --html --html-details -r ${ABSOLUTE_LAMMPS_SOURCE_DIR} --object-directory=${CMAKE_BINARY_DIR} -o ${COVERAGE_HTML_DIR}/index.html
             WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
             COMMENT "Generating HTML Coverage Report..."
+        )
+        add_dependencies(gen_coverage_html coverage_html_folder)
+
+        add_custom_target(clean_coverage_html
+            ${CMAKE_COMMAND} -E remove_directory ${COVERAGE_HTML_DIR}
         )
     endif()
 endif()

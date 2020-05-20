bd2d74b2cfb3cac90cefe57c69d21c8b4b4abf60
iafoss <iafoss@yandex.ru>
2020-05-19 23:26:23 -0400

    update

--- src/USER-MESONT/atom_vec_mesont.h
+++ src/USER-MESONT/atom_vec_mesont.h
@@ -29,45 +29,6 @@ namespace LAMMPS_NS {
 class AtomVecMesoNT : public AtomVec {
  public:
   AtomVecMesoNT(class LAMMPS *);
-  virtual ~AtomVecMesoNT() {}
-  void grow(int);
-  void grow_reset();
-  void copy(int, int, int);
-  virtual int pack_comm(int, int *, double *, int, int *);
-  virtual int pack_comm_vel(int, int *, double *, int, int *);
-  virtual void unpack_comm(int, int, double *);
-  virtual void unpack_comm_vel(int, int, double *);
-  int pack_reverse(int, int, double *);
-  void unpack_reverse(int, int *, double *);
-  virtual int pack_border(int, int *, double *, int, int *);
-  virtual int pack_border_vel(int, int *, double *, int, int *);
-  virtual void unpack_border(int, int, double *);
-  virtual void unpack_border_vel(int, int, double *);
-  int pack_border_hybrid(int, int *, double *);
-  int unpack_border_hybrid(int, int, double *);
-  virtual int pack_exchange(int, double *);
-  virtual int unpack_exchange(double *);
-  int size_restart();
-  int pack_restart(int, double *);
-  int unpack_restart(double *);
-  void create_atom(int, double *);
-  void data_atom(double *, imageint, char **);
-  int data_atom_hybrid(int, char **);
-  void pack_data(double **);
-  int pack_data_hybrid(int, double *);
-  void write_data(FILE *, int, double **);
-  int write_data_hybrid(FILE *, double *);
-  bigint memory_usage();
-
- protected:
-  tagint *tag;
-  int *type,*mask;
-  imageint *image;
-  double **x,**v,**f;
-  double *rmass, *radius, *length;
-  int *buckling;
-  tagint **bond_nt;
-  tagint *molecule;
 };
 
 }
@@ -76,17 +37,5 @@ class AtomVecMesoNT : public AtomVec {
 #endif
 
 /* ERROR/WARNING messages:
-
-E: Invalid atom_style command
-
-Self-explanatory.
-
-E: Invalid atom type in Atoms section of data file
-
-Self-explanatory.
-
-E: Per-processor system is too big
-
-Self-explanatory.
 
 */

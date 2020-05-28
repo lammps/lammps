2eaf710488e0c2550d608562f6d4a7ac440f6089
Richard Berger <richard.berger@temple.edu>
2020-05-28 08:01:54 -0400

    Hide stdout during tests

--- unittest/formats/test_potential_file_reader.cpp
+++ unittest/formats/test_potential_file_reader.cpp
@@ -25,34 +25,50 @@ protected:
         const char *args[] = {"PotentialFileReaderTest", "-log", "none", "-echo", "screen", "-nocite" };
         char **argv = (char **)args;
         int argc = sizeof(args)/sizeof(char *);
+        ::testing::internal::CaptureStdout();
         lmp = new LAMMPS(argc, argv, MPI_COMM_WORLD);
+        ::testing::internal::GetCapturedStdout();
     }
 
     void TearDown() override {
+        ::testing::internal::CaptureStdout();
         delete lmp;
+        ::testing::internal::GetCapturedStdout();
     }
 };
 
 TEST_F(PotenialFileReaderTest, Si) {
+    ::testing::internal::CaptureStdout();
     PotentialFileReader reader(lmp, "Si.sw", "Stillinger-Weber");
+    ::testing::internal::GetCapturedStdout();
+
     auto line = reader.next_line(PairSW::Param::NPARAMS_PER_LINE);
     ASSERT_EQ(utils::count_words(line), PairSW::Param::NPARAMS_PER_LINE);
 }
 
 TEST_F(PotenialFileReaderTest, Comb) {
+    ::testing::internal::CaptureStdout();
     PotentialFileReader reader(lmp, "ffield.comb", "COMB");
+    ::testing::internal::GetCapturedStdout();
+
     auto line = reader.next_line(PairComb::Param::NPARAMS_PER_LINE);
     ASSERT_EQ(utils::count_words(line), PairComb::Param::NPARAMS_PER_LINE);
 }
 
 TEST_F(PotenialFileReaderTest, Comb3) {
+    ::testing::internal::CaptureStdout();
     PotentialFileReader reader(lmp, "ffield.comb3", "COMB3");
+    ::testing::internal::GetCapturedStdout();
+
     auto line = reader.next_line(PairComb3::Param::NPARAMS_PER_LINE);
     ASSERT_EQ(utils::count_words(line), PairComb3::Param::NPARAMS_PER_LINE);
 }
 
 TEST_F(PotenialFileReaderTest, Tersoff) {
+    ::testing::internal::CaptureStdout();
     PotentialFileReader reader(lmp, "Si.tersoff", "Tersoff");
+    ::testing::internal::GetCapturedStdout();
+
     auto line = reader.next_line(PairTersoff::Param::NPARAMS_PER_LINE);
     ASSERT_EQ(utils::count_words(line), PairTersoff::Param::NPARAMS_PER_LINE);
 }

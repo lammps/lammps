2efb079171050e96ea30e7766c0d2ccc8d631e3d
Axel Kohlmeyer <akohlmey@gmail.com>
2020-06-06 17:29:03 -0400

    fix error handling with exceptions

--- src/library.cpp
+++ src/library.cpp
@@ -3009,9 +3009,9 @@ int lammps_get_last_error_message(void *handle, char * buffer, int buf_size) {
   LAMMPS *  lmp = (LAMMPS *) handle;
   Error * error = lmp->error;
 
-  if(error->get_last_error()) {
+  if(error->get_last_error() != "") {
     int error_type = error->get_last_error_type();
-    strncpy(buffer, error->get_last_error().c_str(), buffer_size-1);
+    strncpy(buffer, error->get_last_error().c_str(), buf_size-1);
     error->set_last_error(NULL, ERROR_NONE);
     return error_type;
   }

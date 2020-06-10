d02363b8fbc157e07ab195aeaffab7f34c1b7c11
Steve Plimpton <sjplimp@sandia.gov>
2020-06-10 16:33:05 -0600

    adding a reset_ids sort option

--- src/fix.h
+++ src/fix.h
@@ -64,6 +64,7 @@ class Fix : protected Pointers {
   int maxexchange;               // max # of per-atom values for Comm::exchange()
   int maxexchange_dynamic;       // 1 if fix sets maxexchange dynamically
   int pre_exchange_migrate;      // 1 if fix migrates atoms in pre_exchange()
+  int stores_ids;                 // 1 if fix stores atom IDs
 
   int scalar_flag;               // 0/1 if compute_scalar() function exists
   int vector_flag;               // 0/1 if compute_vector() function exists

3db944deccad66ad5a75b1ae3c39843e5ce65541
Axel Kohlmeyer <akohlmey@gmail.com>
2020-06-13 01:54:58 -0400

    apply clang-format settings to the c++ sources in the unittest tree

--- unittest/force-styles/error_stats.cpp
+++ unittest/force-styles/error_stats.cpp
@@ -12,32 +12,36 @@
 ------------------------------------------------------------------------- */
 
 #include "error_stats.h"
+#include <cmath>
 #include <iostream>
 #include <string>
-#include <cmath>
 
-void ErrorStats::reset() {
+void ErrorStats::reset()
+{
     num = 0;
     maxidx = -1;
-    sum = sumsq = maxerr =0.0;
+    sum = sumsq = maxerr = 0.0;
 }
 
-void ErrorStats::add(const double &val) {
+void ErrorStats::add(const double &val)
+{
     ++num;
     if (val > maxerr) {
         maxidx = num;
         maxerr = val;
     }
     sum += val;
-    sumsq += val*val;
+    sumsq += val * val;
 }
 
-double ErrorStats::avg() const {
-    return (num > 0) ? sum/num : 0.0;
+double ErrorStats::avg() const
+{
+    return (num > 0) ? sum / num : 0.0;
 }
 
-double ErrorStats::dev() const {
-    return (num > 0) ? sqrt(sumsq/num - sum/num*sum/num) : 0.0;
+double ErrorStats::dev() const
+{
+    return (num > 0) ? sqrt(sumsq / num - sum / num * sum / num) : 0.0;
 }
 
 std::ostream &operator<<(std::ostream &out, const ErrorStats &stats)
@@ -46,9 +50,7 @@ std::ostream &operator<<(std::ostream &out, const ErrorStats &stats)
     const std::streamsize width = out.width(10);
     const std::streamsize prec = out.precision(3);
 
-    out << std::scientific
-        << "Average: " << stats.avg()
-        << " StdDev: " << stats.dev()
+    out << std::scientific << "Average: " << stats.avg() << " StdDev: " << stats.dev()
         << " MaxErr: " << stats.max();
 
     out.precision(prec);
@@ -57,4 +59,3 @@ std::ostream &operator<<(std::ostream &out, const ErrorStats &stats)
 
     return out << " @ item: " << stats.idx();
 }
-

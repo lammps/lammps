ddcc104df653b427f042de02ffe6cf9b744451e5
Axel Kohlmeyer <akohlmey@gmail.com>
2020-06-23 09:21:47 -0400

    must also reissue hybrid pair style commands after restarts

--- unittest/force-styles/pair_style.cpp
+++ unittest/force-styles/pair_style.cpp
@@ -32,6 +32,7 @@
 #include "modify.h"
 #include "pair.h"
 #include "universe.h"
+#include "utils.h"
 
 #include <cctype>
 #include <cstdio>
@@ -159,7 +160,8 @@ void restart_lammps(LAMMPS *lmp, const TestConfig &cfg)
     command("clear");
     command("read_restart " + cfg.basename + ".restart");
 
-    if (!lmp->force->pair) {
+    // need to re-issue pair style with non-restartable and hybrid pair styles
+    if ((!lmp->force->pair) || (utils::strmatch(cfg.pair_style,"^hybrid"))) {
         command("pair_style " + cfg.pair_style);
     }
     if (!lmp->force->pair->restartinfo || !lmp->force->pair->writedata) {

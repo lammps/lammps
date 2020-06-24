5ea9b1b38d439185db9f868205e55b0c5eb6ed9a
Axel Kohlmeyer <akohlmey@gmail.com>
2020-06-24 09:33:19 -0400

    simplify detection and application of valgrind suppressions

--- cmake/Modules/Testing.cmake
+++ cmake/Modules/Testing.cmake
@@ -5,18 +5,10 @@ option(ENABLE_TESTING "Enable testing" OFF)
 if(ENABLE_TESTING)
   find_program(VALGRIND_BINARY NAMES valgrind)
   set(VALGRIND_DEFAULT_OPTIONS "--leak-check=full --show-leak-kinds=all --track-origins=yes")
-
-  if(BUILD_MPI)
-    set(VALGRIND_DEFAULT_OPTIONS "${VALGRIND_DEFAULT_OPTIONS} --suppressions=${LAMMPS_TOOLS_DIR}/valgrind/OpenMP.supp")
-  endif()
-
-  if(BUILD_OMP)
-    set(VALGRIND_DEFAULT_OPTIONS "${VALGRIND_DEFAULT_OPTIONS} --suppressions=${LAMMPS_TOOLS_DIR}/valgrind/OpenMP.supp")
-  endif()
-
-  if(PKG_PYTHON)
-    set(VALGRIND_DEFAULT_OPTIONS "${VALGRIND_DEFAULT_OPTIONS} --suppressions=${LAMMPS_TOOLS_DIR}/valgrind/Python3.supp")
-  endif()
+  file(GLOB VALGRIND_SUPPRESSION_FILES ${LAMMPS_TOOLS_DIR}/valgrind/[^.]*.supp)
+  foreach(SUPP ${VALLGRIND_SUPPRESSION_FILES})
+    set(VALGRIND_DEFAULT_OPTIONS "${VALGRIND_DEFAULT_OPTIONS} --suppressions=${SUPP}")
+  endforeach()
 
   set(MEMORYCHECK_COMMAND "${VALGRIND_BINARY}" CACHE FILEPATH "Memory Check Command")
   set(MEMORYCHECK_COMMAND_OPTIONS "${VALGRIND_DEFAULT_OPTIONS}" CACHE STRING "Memory Check Command Options")

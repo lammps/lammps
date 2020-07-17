2be20f424f4d156c8811e41044c1250293917109
Axel Kohlmeyer <akohlmey@gmail.com>
2020-07-16 21:13:47 -0400

    simplify

--- src/KIM/kim_interactions.h
+++ src/KIM/kim_interactions.h
@@ -77,7 +77,7 @@ class KimInteractions : protected Pointers {
  private:
   void do_setup(int, char **);
   int species_to_atomic_no(const std::string &species) const;
-  void KIM_SET_TYPE_PARAMETERS(char const *const input_line) const;
+  void KIM_SET_TYPE_PARAMETERS(const std::string &input_line) const;
 };
 
 }

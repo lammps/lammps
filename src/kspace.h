b162cefc5fcd2740efae4af9c0c6c02be30b3085
Axel Kohlmeyer <akohlmey@gmail.com>
2020-07-19 12:12:50 -0400

    for ewald we also have evflag_atom that needs to be initialized

--- src/kspace.h
+++ src/kspace.h
@@ -198,7 +198,7 @@ class KSpace : protected Pointers {
   void pair_check();
   void ev_init(int eflag, int vflag, int alloc = 1) {
     if (eflag||vflag) ev_setup(eflag, vflag, alloc);
-    else evflag = eflag_either = eflag_global = eflag_atom = vflag_either = vflag_global = vflag_atom = 0;
+    else evflag = evflag_atom = eflag_either = eflag_global = eflag_atom = vflag_either = vflag_global = vflag_atom = 0;
   }
   void ev_setup(int, int, int alloc = 1);
   double estimate_table_accuracy(double, double);

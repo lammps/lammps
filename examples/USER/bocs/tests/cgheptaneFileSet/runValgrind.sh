e11a3c47993e5d73e4b44903adc5c215f3cb5029
Anne Gunn <agunn@sherprog.com>
2020-08-10 08:46:09 -0600

    Remove "test" example files.

--- examples/USER/bocs/tests/cgheptaneFileSet/runValgrind.sh
+++
@@ -1,9 +0,0 @@
-now="$(date)" 
-printf "Start at %s\n" "$now"
-
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --log-file="valgrind.cgheptane.cubic200.log" /mnt/c/openSource/eagunn/lammps/build/lmp -in cgheptane.cubic200.lmp
-
-now="$(date)" 
-printf "Finish at %s\n" "$now"
-
- 

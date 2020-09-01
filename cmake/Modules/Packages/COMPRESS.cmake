60b98f6b91ef31a1a31690e9700d2a0039f59d17
Richard Berger <richard.berger@temple.edu>
2020-09-01 13:52:56 -0400

    Require libzstd>=1.4 for Zstd dump styles

--- cmake/Modules/Packages/COMPRESS.cmake
+++ cmake/Modules/Packages/COMPRESS.cmake
@@ -2,7 +2,7 @@ find_package(ZLIB REQUIRED)
 target_link_libraries(lammps PRIVATE ZLIB::ZLIB)
 
 find_package(PkgConfig REQUIRED)
-pkg_check_modules(Zstd IMPORTED_TARGET libzstd)
+pkg_check_modules(Zstd IMPORTED_TARGET libzstd>=1.4)
 
 if(Zstd_FOUND)
     target_compile_definitions(lammps PRIVATE -DLAMMPS_ZSTD)

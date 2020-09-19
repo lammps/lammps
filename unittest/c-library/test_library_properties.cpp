c3e27a72a81cf4775e8a23066c486e72d286cb4a
Christoph Junghans <junghans@lanl.gov>
2020-09-19 15:15:46 -0600

    fix LibraryProperties.version test

--- unittest/c-library/test_library_properties.cpp
+++ unittest/c-library/test_library_properties.cpp
@@ -53,7 +53,7 @@ protected:
 
 TEST_F(LibraryProperties, version)
 {
-    EXPECT_GE(20200824, lammps_version(lmp));
+    EXPECT_GE(20200918, lammps_version(lmp));
 };
 
 TEST_F(LibraryProperties, memory_usage)

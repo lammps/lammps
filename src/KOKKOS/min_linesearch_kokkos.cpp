ceaaf149eb095e65491855895cac39695807c36e
Stan Moore <stamoor@sandia.gov>
2020-10-23 08:53:53 -0600

    Whitespace cleanup

--- src/KOKKOS/min_linesearch_kokkos.cpp
+++ src/KOKKOS/min_linesearch_kokkos.cpp
@@ -61,6 +61,7 @@ MinLineSearchKokkos::~MinLineSearchKokkos()
   delete [] gextra;
   delete [] hextra;
 }
+
 /* ---------------------------------------------------------------------- */
 
 void MinLineSearchKokkos::init()

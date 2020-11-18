6d0c8e71de6dd3d2b760ad237b4aee41962260ce
Richard Berger <richard.berger@temple.edu>
2020-11-18 17:51:04 -0500

    Correct assignment of centroidstressflag

--- src/pair_hybrid.cpp
+++ src/pair_hybrid.cpp
@@ -416,10 +416,10 @@ void PairHybrid::flags()
 
   for (m = 0; m < nstyles; m++) {
     if (styles[m]->centroidstressflag == CENTROID_NOTAVAIL)
-      centroidstressflag == CENTROID_NOTAVAIL;
+      centroidstressflag = CENTROID_NOTAVAIL;
     if (centroidstressflag == CENTROID_SAME &&
 	styles[m]->centroidstressflag == CENTROID_AVAIL)
-      centroidstressflag == CENTROID_AVAIL;
+      centroidstressflag = CENTROID_AVAIL;
   }
 }
 

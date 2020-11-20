f42e907d30596b89b3553f17451e435c5f5b287d
Jacob Gissinger <jrgiss05@gmail.com>
2020-11-19 19:06:33 -0500

    update terminology

--- src/USER-REACTION/fix_bond_react.cpp
+++ src/USER-REACTION/fix_bond_react.cpp
@@ -1330,7 +1330,7 @@ void FixBondReact::superimpose_algorithm()
         //if (hang_catch > onemol->natoms*4)
         if (hang_catch > atom->nlocal*30) {
           error->one(FLERR,"Bond/react: Excessive iteration of superimpose algorithm. "
-              "Please check that all pre-reaction template atoms are linked to a bonding atom, "
+              "Please check that all pre-reaction template atoms are linked to an initiator atom, "
               "via at least one path that does not involve edge atoms.");
         }
       }

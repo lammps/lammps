653c29624658154f09cda260aa1a0585f38d6520
Axel Kohlmeyer <akohlmey@gmail.com>
2021-02-09 12:29:14 -0500

    document __version__ module member and version check

--- python/lammps/__init__.py
+++ python/lammps/__init__.py
@@ -1,3 +1,13 @@
+"""
+LAMMPS module global members:
+
+.. data:: __version__
+
+   Numerical representation of the LAMMPS version this
+   module was taken from.  Has the same format as the
+   result of :py:func:`lammps.version`.
+"""
+
 from .constants import *
 from .core import *
 from .data import *

f8a5991416ddcc2a4b8622c6ab7657b2d9c11cf1
Axel Kohlmeyer <akohlmey@gmail.com>
2021-02-10 17:09:56 -0500

    rearrange output a little bit

--- src/citeme.cpp
+++ src/citeme.cpp
@@ -18,21 +18,13 @@
 using namespace LAMMPS_NS;
 
 static const char cite_separator[] =
-  "\nCITE-CITE-CITE-CITE-CITE-CITE-CITE-CITE-CITE-CITE-CITE-CITE-CITE\n\n";
+  "CITE-CITE-CITE-CITE-CITE-CITE-CITE-CITE-CITE-CITE-CITE-CITE-CITE\n\n";
 
 static const char cite_nagline[] =
-  "Your LAMMPS simulation uses code contributions which should be cited.\n"
-  "Please see https://lammps.sandia.gov/doc/Intro_citing.html for more\n"
-  "information on citing LAMMPS itself.\n";
+  "Your simulation uses code contributions which should be cited:\n";
 
-static const char cite_short[] =
-  "A short list of the features is given below.\n\n";
-
-static const char cite_full[] =
-  "Below is a list of the full references in BibTeX format.\n\n";
-
-static const char cite_file[] = "Please see the {} {} "
-  "for detailed references in BibTeX format.\n";
+static const char cite_file[] = "The {} {} lists these citations in "
+                                "BibTeX format.\n\n";
 
 /* ---------------------------------------------------------------------- */
 
@@ -52,7 +44,6 @@ CiteMe::CiteMe(LAMMPS *lmp, int _screen, int _logfile, const char *_file)
     fp = fopen(_file,"w");
     if (fp) {
       fputs(cite_nagline,fp);
-      fputs(cite_full,fp);
       fflush(fp);
     } else {
       utils::logmesg(lmp, "Unable to open citation file '" + citefile
@@ -89,46 +80,45 @@ void CiteMe::add(const char *ref)
   }
 
   if (scrbuffer.empty()) {
+    scrbuffer += "\n";
     scrbuffer += cite_separator;
     scrbuffer += cite_nagline;
-    if (!citefile.empty()) scrbuffer += fmt::format(cite_file,"file",citefile);
-    if (screen_flag == VERBOSE) scrbuffer += cite_full;
-    if (screen_flag == TERSE) {
-      if (logfile_flag == VERBOSE)
-        scrbuffer += fmt::format(cite_file,"log","file");
-      scrbuffer += cite_short;
-    }
+    if (screen_flag == VERBOSE) scrbuffer += "\n";
   }
+
   if (logbuffer.empty()) {
+    logbuffer += "\n";
     logbuffer += cite_separator;
     logbuffer += cite_nagline;
-    if (!citefile.empty()) logbuffer += fmt::format(cite_file,"file",citefile);
-    if (logfile_flag == VERBOSE) logbuffer += cite_full;
-    if (logfile_flag == TERSE) {
-      if (screen_flag == VERBOSE)
-        scrbuffer += fmt::format(cite_file,"screen","output");
-      logbuffer += cite_short;
-    }
+    if (logfile_flag == VERBOSE) logbuffer += "\n";
   }
 
   std::string reference = ref;
   std::size_t found = reference.find_first_of("\n");
   std::string header = reference.substr(0,found+1);
-  if (screen_flag == VERBOSE) scrbuffer += reference;
-  if (screen_flag == TERSE) scrbuffer += header;
-  if (logfile_flag == VERBOSE) logbuffer += reference;
-  if (logfile_flag == TERSE) logbuffer += header;
+  if (screen_flag == VERBOSE) scrbuffer += "- " + reference;
+  if (screen_flag == TERSE) scrbuffer += "- " + header;
+  if (logfile_flag == VERBOSE) logbuffer += "- " + reference;
+  if (logfile_flag == TERSE) logbuffer += "- " + header;
 }
 
 void CiteMe::flush()
 {
   if (comm->me == 0) {
     if (!scrbuffer.empty()) {
+      if (!citefile.empty())
+        scrbuffer += fmt::format(cite_file,"file",citefile);
+      if (logfile_flag == VERBOSE)
+        scrbuffer += fmt::format(cite_file,"log","file");
       scrbuffer += cite_separator;
       if (screen) fputs(scrbuffer.c_str(),screen);
       scrbuffer.clear();
     }
     if (!logbuffer.empty()) {
+      if (!citefile.empty())
+        logbuffer += fmt::format(cite_file,"file",citefile);
+      if (screen_flag == VERBOSE)
+        scrbuffer += fmt::format(cite_file,"screen","output");
       logbuffer += cite_separator;
       if (logfile) fputs(logbuffer.c_str(),logfile);
       logbuffer.clear();

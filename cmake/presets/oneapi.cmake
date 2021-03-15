e6d8209b9d97748bb147498b508201ab2e5de959
Axel Kohlmeyer <akohlmey@gmail.com>
2021-03-15 00:06:58 -0400

    use new intel LLVM based fortran compiler as well

--- cmake/presets/oneapi.cmake
+++ cmake/presets/oneapi.cmake
@@ -1,8 +1,8 @@
-# preset that will enable Intel compilers with support for MPI and OpenMP (on Linux boxes)
+# preset that will enable the LLVM based Intel compilers with support for MPI and OpenMP (on Linux boxes)
 
 set(CMAKE_CXX_COMPILER "icpx" CACHE STRING "" FORCE)
 set(CMAKE_C_COMPILER "icx" CACHE STRING "" FORCE)
-set(CMAKE_Fortran_COMPILER "ifort" CACHE STRING "" FORCE)
+set(CMAKE_Fortran_COMPILER "ifx" CACHE STRING "" FORCE)
 set(MPI_CXX "icpx" CACHE STRING "" FORCE)
 set(MPI_CXX_COMPILER "mpicxx" CACHE STRING "" FORCE)
 unset(HAVE_OMP_H_INCLUDE CACHE)

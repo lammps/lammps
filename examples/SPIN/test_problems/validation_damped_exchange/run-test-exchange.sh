39dc3e69ba716d63298cc8aca7e973268a94f8fe
julient31 <julien.tranchida1@gmail.com>
2021-03-30 10:35:25 -0600

    Commit before pull

--- examples/SPIN/test_problems/validation_damped_exchange/run-test-exchange.sh
+++ examples/SPIN/test_problems/validation_damped_exchange/run-test-exchange.sh
@@ -3,9 +3,15 @@
 # clean old res
 rm res_*.dat
 
-# compute Lammps 
-./../../../../src/lmp_serial \
-  -in test-spin-precession.in 
+# test standard Lammps 
+# ./../../../../src/lmp_serial \
+#   -in test-spin-precession.in 
+
+# test spin/kk with Kokkos Lammps
+mpirun -np 1 ../../../../src/lmp_kokkos_mpi_only \
+  -k on -sf kk -in test-spin-precession.in
+
+# extract data from Lammps run
 in="$(grep -n Step log.lammps | awk -F ':' '{print $1}')"
 en="$(grep -n Loop log.lammps | awk -F ':' '{print $1}')"
 in="$(echo "$in+1" | bc -l)"

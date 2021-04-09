97977e3e68d1e60e84c3883cfa8ec7f986eef730
Axel Kohlmeyer <akohlmey@gmail.com>
2021-04-09 09:44:23 -0400

    incorrect check for additional argument in pair_modify nofdotr

--- src/pair.cpp
+++ src/pair.cpp
@@ -204,7 +204,6 @@ void Pair::modify_params(int narg, char **arg)
       else error->all(FLERR,"Illegal pair_modify command");
       iarg += 2;
     } else if (strcmp(arg[iarg],"nofdotr") == 0) {
-      if (iarg+2 > narg) error->all(FLERR,"Illegal pair_modify command");
       no_virial_fdotr_compute = 1;
       ++iarg;
     } else error->all(FLERR,"Illegal pair_modify command");

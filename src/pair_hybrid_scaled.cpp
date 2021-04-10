0d325f22219239a52d3b2b7ca8344749814b51bc
Axel Kohlmeyer <akohlmey@gmail.com>
2021-04-09 22:50:16 -0400

    error out when scale factor variables do not exist

--- src/pair_hybrid_scaled.cpp
+++ src/pair_hybrid_scaled.cpp
@@ -71,6 +71,9 @@ void PairHybridScaled::compute(int eflag, int vflag)
     double *vals = new double[nvars];
     for (i = 0; i < nvars; ++i) {
       j = input->variable->find(scalevars[i].c_str());
+      if (j < 0)
+        error->all(FLERR,fmt::format("Variable '{}' not found when updating "
+                                     "scale factors",scalevars[i]));
       vals[i] = input->variable->compute_equal(j);
     }
     for (i = 0; i < nstyles; ++i) {

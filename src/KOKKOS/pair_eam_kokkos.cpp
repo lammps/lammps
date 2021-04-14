1d6f24f0df075be9f8ee80d33f64e84b91d489e9
Nick Curtis <nicholas.curtis@amd.com>
2021-04-14 16:50:37 -0500

    hide additional printf
    Change-Id: I200c85013b9d0163847194ad4b18ac33af65a823

--- src/KOKKOS/pair_eam_kokkos.cpp
+++ src/KOKKOS/pair_eam_kokkos.cpp
@@ -754,7 +754,9 @@ void PairEAMKokkos<DeviceType>::operator()(g<NEIGHFLAG,NEWTON_PAIR,EVFLAG>, cons
    for (int i = threadIdx.y; i < m_max*7; i+=blockDim.y){
       int j = i%7;
       int m = i/7;
+#ifdef HIP_PRINTF_WORKAROUND
       if ( d_z2r_spline(0,m,j) < -99955.65 ) printf("C: m=%d, j=%d, i = %d\n",m,j,i);
+#endif
       A[m][j] = d_z2r_spline(0,m,j);
    }
     __syncthreads();

c11e9cc84925f4b8889d093aeb328f77395295ee
Axel Kohlmeyer <akohlmey@gmail.com>
2021-04-18 22:59:55 -0400

    remove unused defines and enumerators

--- src/USER-REAXC/reaxff_defs.h
+++ src/USER-REAXC/reaxff_defs.h
@@ -33,28 +33,9 @@
 
 #define constPI        3.14159265
 #define C_ele          332.06371
-//#define K_B         503.398008   // kcal/mol/K
-#define K_B             0.831687   // amu A^2 / ps^2 / K
-#define F_CONV          1e6 / 48.88821291 / 48.88821291   // --> amu A / ps^2
-#define E_CONV          0.002391   // amu A^2 / ps^2 --> kcal/mol
 #define EV_to_KCALpMOL 14.400000   // ElectronVolt --> KCAL per MOLe
 #define KCALpMOL_to_EV 23.02       // 23.060549 //KCAL per MOLe --> ElectronVolt
-#define ECxA_to_DEBYE   4.803204   // elem. charge * Ang -> debye
-#define CAL_to_JOULES   4.184000   // CALories --> JOULES
-#define JOULES_to_CAL   1/4.184000 // JOULES --> CALories
-#define AMU_to_GRAM     1.6605e-24
-#define ANG_to_CM       1e-8
-#define AVOGNR          6.0221367e23
-#define P_CONV          1e-24 * AVOGNR * JOULES_to_CAL
 
-#define MAX_STR             1024
-#define MAX_LINE            1024
-#define MAX_TOKENS          1024
-#define MAX_TOKEN_LEN       1024
-
-#define ALMOST_ZERO    1e-10
-#define NEG_INF       -1e10
-#define NO_BOND        1e-3  // 0.001
 #define HB_THRESHOLD   1e-2  // 0.01
 
 #define REAX_MIN_CAP    50
@@ -68,29 +49,17 @@
 #define REAX_SAFER_ZONE 1.4
 #define DANGER_ZONE     0.90
 #define LOOSE_ZONE      0.75
-#define MAX_3BODY_PARAM 5
-#define MAX_4BODY_PARAM 5
-
-#define MASTER_NODE 0
 
 #define MAXREAXBOND 24 /* used in fix_reaxc_bonds.cpp and pair_reaxc.cpp */
 #define MAXSPECBOND 24 /* used in fix_reaxc_species.cpp and pair_reaxc.cpp */
 
-/************* crucial for reaxff_types.h *********/
-
-#define REAX_MAX_STR            1024
 #define REAX_MAX_3BODY_PARAM    5
 #define REAX_MAX_4BODY_PARAM    5
 
 namespace ReaxFF
 {
   /******************* ENUMERATORS *************************/
-
-  enum lists { BONDS, OLD_BONDS, THREE_BODIES,
-    HBONDS, FAR_NBRS, DBOS, DDELTAS, LIST_N };
-
-  enum message_tags {NONE, INIT_DESCS, ATOM_LINES, BOND_LINES, ANGLE_LINES};
-
+  enum lists { BONDS, THREE_BODIES, HBONDS, FAR_NBRS, LIST_N };
   enum interactions {TYP_VOID, TYP_BOND, TYP_THREE_BODY,
     TYP_HBOND, TYP_FAR_NEIGHBOR, TYP_DBO, TYP_DDELTA, TYP_N};
 }

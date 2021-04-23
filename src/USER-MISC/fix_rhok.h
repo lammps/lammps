df7fe4431fe4ae4e9eeea4a6e86531c3bf18c879
Axel Kohlmeyer <akohlmey@gmail.com>
2021-04-22 21:26:07 -0400

    make LAMMPS homepage URL and C++ indicator comments consistent

--- src/USER-MISC/fix_rhok.h
+++ src/USER-MISC/fix_rhok.h
@@ -1,6 +1,6 @@
 /* -*- c++ -*- ----------------------------------------------------------
    LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
-   http://lammps.sandia.gov, Sandia National Laboratories
+   https://lammps.sandia.gov/, Sandia National Laboratories
    Steve Plimpton, sjplimp@sandia.gov
    Copyright (2003) Sandia Corporation.  Under the terms of Contract
    DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains

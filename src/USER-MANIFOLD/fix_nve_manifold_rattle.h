1ac3991aad39dcd85503de4c273413230c384d61
Axel Kohlmeyer <akohlmey@gmail.com>
2021-04-22 21:37:54 -0400

    Lammps -> LAMMPS

--- src/USER-MANIFOLD/fix_nve_manifold_rattle.h
+++ src/USER-MANIFOLD/fix_nve_manifold_rattle.h
@@ -1,5 +1,5 @@
 /* -*- c++ -*- ----------------------------------------------------------
-   Lammps - Large-scale Atomic/Molecular Massively Parallel Simulator
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
    https://lammps.sandia.gov/, Sandia National Laboratories
    Steve Plimpton, sjplimp@sandia.gov
 

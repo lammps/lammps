64fa70f8d2995793d7e2c8a410a89d4ffab4ab29
taylor-a-barnes <taylor.a.barnes@gmail.com>
2021-04-27 13:25:11 -0400

    Add USER-MDI README file

--- examples/USER/mdi/Script.sh
+++ examples/USER/mdi/Script.sh
@@ -14,15 +14,3 @@ wait
 python driver.py -mdi "-name driver -role DRIVER -method TCP -port 8021" &
 mpiexec -n 2 ../../../src/lmp_mdi -mdi "-name LAMMPS -role ENGINE -method TCP -port 8021 -hostname localhost" -in lammps.in > lammps.out &
 wait
-
-
-# MPI, running LAMMPS on one proc
-
-mpiexec -n 1 python driver.py -mdi "-name driver -role DRIVER -method MPI" : \
-        -n 1 ../../../src/lmp_mdi -mdi "-name LAMMPS -role ENGINE -method MPI -out lammps.out" -in lammps.in
-
-
-# MPI, running LAMMPS on two procs
-
-mpirun -n 1 python driver.py -mdi "-name driver -role DRIVER -method MPI" : \
-       -n 2 ../../../src/lmp_mdi -mdi "-name LAMMPS -role ENGINE -method MPI -out lammps.out" -in lammps.in

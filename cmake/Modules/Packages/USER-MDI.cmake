d9505a45279480d87e3e3078a0cf47cbe197b983
Axel Kohlmeyer <akohlmey@gmail.com>
2021-05-13 08:57:14 -0400

    cmake tweaks

--- cmake/Modules/Packages/USER-MDI.cmake
+++ cmake/Modules/Packages/USER-MDI.cmake
@@ -1,36 +1,32 @@
-find_package(mdi)
+find_package(mdi QUIET)
 if(${mdi_FOUND})
   set(DOWNLOAD_MDI_DEFAULT OFF)
 else()
   set(DOWNLOAD_MDI_DEFAULT ON)
 endif()
 option(DOWNLOAD_MDI "Download and compile the MDI library instead of using an already installed one" ${DOWNLOAD_MDI_DEFAULT})
+
 if(DOWNLOAD_MDI)
   message(STATUS "MDI download requested - we will build our own")
-  set(mdi_URL "https://github.com/MolSSI-MDI/MDI_Library/archive/v1.2.9.tar.gz" CACHE STRING "URL for MDI tarball")
-  set(mdi_MD5 "ddfa46d6ee15b4e59cfd527ec7212184" CACHE STRING "MD5 checksum for MDI tarball")
-  mark_as_advanced(mdi_URL)
-  mark_as_advanced(mdi_MD5)
+  set(MDI_URL "https://github.com/MolSSI-MDI/MDI_Library/archive/v1.2.9.tar.gz" CACHE STRING "URL for MDI tarball")
+  set(MDI_MD5 "ddfa46d6ee15b4e59cfd527ec7212184" CACHE STRING "MD5 checksum for MDI tarball")
+  mark_as_advanced(MDI_URL)
+  mark_as_advanced(MDI_MD5)
 
   set(LAMMPS_LIB_MDI_BIN_DIR ${LAMMPS_LIB_BINARY_DIR}/mdi)
 
   include(ExternalProject)
   message(STATUS "Building mdi.")
   ExternalProject_Add(mdi_external
-      URL     ${mdi_URL}
-      URL_MD5 ${mdi_MD5}
+      URL     ${MDI_URL}
+      URL_MD5 ${MDI_MD5}
       UPDATE_COMMAND ""
-      CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${LAMMPS_LIB_MDI_BIN_DIR}
+      CMAKE_ARGS ${CMAKE_REQUEST_PIC}
+                 -DCMAKE_INSTALL_PREFIX=${LAMMPS_LIB_MDI_BIN_DIR}
                  -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-                 -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
                  -DCMAKE_INSTALL_LIBDIR=${CMAKE_INSTALL_LIBDIR}
                  -DCMAKE_INSTALL_INCLUDEDIR=${CMAKE_INSTALL_INCLUDEDIR}
                  -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}
-                 -DENABLE_OPENMP=${ENABLE_OPENMP}
-                 -DENABLE_XHOST=${ENABLE_XHOST}
-                 -DBUILD_FPIC=${BUILD_FPIC}
-                 -DENABLE_GENERIC=${ENABLE_GENERIC}
-                 -DLIBC_INTERJECT=${LIBC_INTERJECT}
                  -Dlanguage=C
       CMAKE_CACHE_ARGS -DCMAKE_C_FLAGS:STRING=${CMAKE_C_FLAGS}
                        -DCMAKE_CXX_FLAGS:STRING=${CMAKE_CXX_FLAGS}
@@ -52,7 +48,9 @@ else()
 
   find_package(mdi)
   if(NOT mdi_FOUND)
-    message(FATAL_ERROR "MDI library not found. Help CMake to find it by setting mdi_LIBRARY and mdi_INCLUDE_DIR, or set DOWNLOAD_MDI=ON to download it")
+    message(FATAL_ERROR "MDI library not found. Help CMake to find it "
+      "by setting mdi_LIBRARY and mdi_INCLUDE_DIR, or set DOWNLOAD_MDI=ON "
+      "to download and compile it")
   endif()
 
   # Link the lammps library against MDI
@@ -60,9 +58,9 @@ else()
   target_link_libraries(lammps PRIVATE ${mdi_LIBRARY})
 
   # Link the lammps executable against MDI
-  target_include_directories(lmp PRIVATE ${mdi_INCLUDE_DIR})
-  target_link_libraries(lmp PRIVATE ${mdi_LIBRARY})
-
+  #target_include_directories(lmp PRIVATE ${mdi_INCLUDE_DIR})
+  #target_link_libraries(lmp PRIVATE ${mdi_LIBRARY}
 endif()
 
-add_definitions(-DLMP_USER_MDI=1)
+target_compile_definitions(lammps PRIVATE -DLMP_USER_MDI)
+target_compile_definitions(lmp PRIVATE -DLMP_USER_MDI)

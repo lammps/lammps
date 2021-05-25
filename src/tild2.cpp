729055b4c028eaa648920cd5ed0b00865a5de6c8
tabedzki <tabedzki@seas.upenn.edu>
2021-05-25 19:35:40 -0400

    Commented out the line that gives pressure=0 and the fix for it

--- src/tild2.cpp
+++ src/tild2.cpp
@@ -305,10 +305,11 @@ void TILD::init()
   // don't invoke allocate peratom() or group(), will be allocated when needed (which is when?)
 
   allocate();
+  // calculate_rho0();
 
-  // change number density to tild density 
-  double volume = domain->xprd * domain->yprd * domain->zprd;
-  force->nktv2p *= rho0 * volume / atom->natoms;
+  // // change number density to tild density 
+  // double volume = domain->xprd * domain->yprd * domain->zprd;
+  // force->nktv2p *= rho0 * volume / atom->natoms;
 
   compute_rho_coeff();
 }

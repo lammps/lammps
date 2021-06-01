400b55e6d54fa1a1e96d9e1f615e82510fceeb02
Richard Berger <richard.berger@temple.edu>
2021-06-01 17:51:06 -0400

    Remove mentions of Pypar

--- python/examples/matplotlib_plot.py
+++ python/examples/matplotlib_plot.py
@@ -28,10 +28,10 @@ nsteps = int(sys.argv[3])
 compute = sys.argv[4]
 
 me = 0
-# uncomment if running in parallel via Pypar
-#import pypar
-#me = pypar.rank()
-#nprocs = pypar.size()
+# uncomment this if running in parallel via mpi4py
+#from mpi4py import MPI
+#me = MPI.COMM_WORLD.Get_rank()
+#nprocs = MPI.COMM_WORLD.Get_size()
 
 from lammps import lammps
 lmp = lammps()
@@ -82,11 +82,8 @@ while ntimestep < nsteps:
 
 lmp.command("run 0 pre no post yes")
 
-# uncomment if running in parallel via Pypar
+# uncomment if running in parallel via mpi4py
 #print("Proc %d out of %d procs has" % (me,nprocs), lmp)
-#pypar.finalize()
 
-if sys.version_info[0] == 3:
-    input("Press Enter to exit...")
-else:
-    raw_input("Press Enter to exit...")
+if me == 0:
+  input("Press Enter to exit...")

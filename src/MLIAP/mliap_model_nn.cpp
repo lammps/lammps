aa2940448e47c59a4a8de5330cb7ce02dc94dcd1
macstein <31542628+macstein@users.noreply.github.com>
2021-06-12 19:49:32 -0700

    Update mliap_model_nn.cpp
    set nonlinearflag in NN model

--- src/MLIAP/mliap_model_nn.cpp
+++ src/MLIAP/mliap_model_nn.cpp
@@ -41,6 +41,7 @@ MLIAPModelNN::MLIAPModelNN(LAMMPS* lmp, char* coefffilename) :
   activation = nullptr;
   scale = nullptr;
   if (coefffilename) read_coeffs(coefffilename);
+  nonlinearflag = 1;
 }
 
 /* ---------------------------------------------------------------------- */

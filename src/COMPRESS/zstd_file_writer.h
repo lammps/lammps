d041e5cba0f4b78d1af5d6863ad9320550b31dbe
Axel Kohlmeyer <akohlmey@gmail.com>
2021-07-06 13:39:07 -0400

    we require at least zstd API version 1.4

--- src/COMPRESS/zstd_file_writer.h
+++ src/COMPRESS/zstd_file_writer.h
@@ -25,6 +25,11 @@
 #include <string>
 #include <zstd.h>
 
+#if ZSTD_VERSION_NUMBER < 10400
+#error must have at least zstd version 1.4 to compile with -DLAMMPS_ZSTD
+#endif
+
+
 namespace LAMMPS_NS {
 
 class ZstdFileWriter : public FileWriter {

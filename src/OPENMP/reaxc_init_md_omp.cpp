33ac10df7ab87e10b8bdc64fcbd639748657701e
Stan Moore <stamoor@sandia.gov>
2021-07-09 14:00:19 -0600

    Missed one instance of SUCCESS

--- src/OPENMP/reaxc_init_md_omp.cpp
+++ src/OPENMP/reaxc_init_md_omp.cpp
@@ -111,7 +111,7 @@ int Init_ListsOMP(reax_system *system, control_params *control,
   free(hb_top);
   free(bond_top);
 
-  return SUCCESS;
+  return REAXC_SUCCES;
 }
 
 /* ---------------------------------------------------------------------- */

0cb09786b6291b983bccb40af78c485e204fbc9b
Axel Kohlmeyer <akohlmey@gmail.com>
2021-07-19 01:02:52 -0400

    need to make this functions virtual since they are overridded by morse/soft

--- src/pair_morse.h
+++ src/pair_morse.h
@@ -30,17 +30,17 @@ class PairMorse : public Pair {
   virtual ~PairMorse();
   virtual void compute(int, int);
 
-  void settings(int, char **);
-  void coeff(int, char **);
-  double init_one(int, int);
-  void write_restart(FILE *);
-  void read_restart(FILE *);
-  void write_restart_settings(FILE *);
-  void read_restart_settings(FILE *);
-  void write_data(FILE *);
-  void write_data_all(FILE *);
-  double single(int, int, int, int, double, double, double, double &);
-  void *extract(const char *, int &);
+  virtual void settings(int, char **);
+  virtual void coeff(int, char **);
+  virtual double init_one(int, int);
+  virtual void write_restart(FILE *);
+  virtual void read_restart(FILE *);
+  virtual void write_restart_settings(FILE *);
+  virtual void read_restart_settings(FILE *);
+  virtual void write_data(FILE *);
+  virtual void write_data_all(FILE *);
+  virtual double single(int, int, int, int, double, double, double, double &);
+  virtual void *extract(const char *, int &);
 
  protected:
   double cut_global;

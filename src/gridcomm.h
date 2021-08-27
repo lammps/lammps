e93ae9ba12c66743ce5694ed72b8c87cf830dd7c
Axel Kohlmeyer <akohlmey@gmail.com>
2021-08-27 19:56:33 -0400

    apply clang-format

--- src/gridcomm.h
+++ src/gridcomm.h
@@ -20,7 +20,7 @@ namespace LAMMPS_NS {
 
 class GridComm : protected Pointers {
  public:
-  enum { KSPACE = 0, FIX = 1};     // calling classes
+  enum { KSPACE = 0, FIX = 1 };    // calling classes
 
   GridComm(class LAMMPS *, MPI_Comm, int, int, int, int, int, int, int, int, int, int, int, int,
            int, int, int);
@@ -184,14 +184,10 @@ class GridComm : protected Pointers {
   int ghost_adjacent_regular();
   int ghost_adjacent_tiled();
 
-  template <class T>
-  void forward_comm_regular(T *, int, int, int, void *, void *, MPI_Datatype);
-  template <class T>
-  void forward_comm_tiled(T *, int, int, int, void *, void *, MPI_Datatype);
-  template <class T>
-  void reverse_comm_regular(T *, int, int, int, void *, void *, MPI_Datatype);
-  template <class T>
-  void reverse_comm_tiled(T *, int, int, int, void *, void *, MPI_Datatype);
+  template <class T> void forward_comm_regular(T *, int, int, int, void *, void *, MPI_Datatype);
+  template <class T> void forward_comm_tiled(T *, int, int, int, void *, void *, MPI_Datatype);
+  template <class T> void reverse_comm_regular(T *, int, int, int, void *, void *, MPI_Datatype);
+  template <class T> void reverse_comm_tiled(T *, int, int, int, void *, void *, MPI_Datatype);
 
   virtual void grow_swap();
   void grow_overlap();

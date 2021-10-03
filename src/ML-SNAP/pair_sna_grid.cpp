8bddc801df643bf69f8c1a4c3798646dc46a130f
Aidan Thompson <athomps@sandia.gov>
2021-10-03 17:55:55 -0600

    First successful compile of pair style

---
+++ src/ML-SNAP/pair_sna_grid.cpp
@@ -0,0 +1,182 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#include "pair_grid.h"
+#include "pair_sna_grid.h"
+#include "sna.h"
+#include "atom.h"
+#include "update.h"
+#include "modify.h"
+#include "neighbor.h"
+#include "neigh_list.h"
+#include "neigh_request.h"
+#include "force.h"
+#include "pair.h"
+#include "domain.h"
+#include "comm.h"
+#include "memory.h"
+#include "error.h"
+#include "tokenizer.h"
+
+#include <cmath>
+#include <cstring>
+
+using namespace LAMMPS_NS;
+
+PairSNAGrid::PairSNAGrid(LAMMPS *lmp) :
+  PairGrid(lmp), cutsq(nullptr),
+  radelem(nullptr), wjelem(nullptr)
+{
+}
+
+/* ---------------------------------------------------------------------- */
+
+PairSNAGrid::~PairSNAGrid()
+{
+  memory->destroy(radelem);
+  memory->destroy(wjelem);
+  memory->destroy(cutsq);
+  delete snaptr;
+
+  if (chemflag) memory->destroy(map);
+}
+
+/* ---------------------------------------------------------------------- */
+
+void PairSNAGrid::init()
+{
+  if (force->pair == nullptr)
+    error->all(FLERR,"Pair sna/grid requires a pair style be defined");
+
+  if (cutmax > force->pair->cutforce)
+    error->all(FLERR,"Pair sna/grid cutoff is longer than pairwise cutoff");
+
+  // need an occasional full neighbor list
+
+  int irequest = neighbor->request(this,instance_me);
+  neighbor->requests[irequest]->pair = 0;
+  neighbor->requests[irequest]->compute = 1;
+  neighbor->requests[irequest]->half = 0;
+  neighbor->requests[irequest]->full = 1;
+  neighbor->requests[irequest]->occasional = 1;
+
+  snaptr->init();
+}
+
+/* ---------------------------------------------------------------------- */
+
+void PairSNAGrid::init_list(int /*id*/, NeighList *ptr)
+{
+  list = ptr;
+}
+
+/* ---------------------------------------------------------------------- */
+
+void PairSNAGrid::compute(int eflag, int vflag)
+{
+
+  // compute sna for each gridpoint
+
+  double** const x = atom->x;
+  const int* const mask = atom->mask;
+  int * const type = atom->type;
+  const int ntotal = atom->nlocal + atom->nghost;
+
+  // insure rij, inside, and typej are of size jnum
+  
+  snaptr->grow_rij(ntotal);
+
+  for (int iz = nzlo; iz <= nzhi; iz++)
+    for (int iy = nylo; iy <= nyhi; iy++)
+      for (int ix = nxlo; ix <= nxhi; ix++) {
+	double xgrid[3];
+	grid2x(ix, iy, iz, xgrid);
+	const double xtmp = xgrid[0];
+	const double ytmp = xgrid[1];
+	const double ztmp = xgrid[2];
+
+	// currently, all grid points are type 1
+	
+	const int itype = 1;
+	int ielem = 0;
+	if (chemflag)
+	  ielem = map[itype];
+	const double radi = radelem[itype];
+
+	// rij[][3] = displacements between atom I and those neighbors
+	// inside = indices of neighbors of I within cutoff
+	// typej = types of neighbors of I within cutoff
+
+	int ninside = 0;
+	for (int j = 0; j < ntotal; j++) {
+
+	  const double delx = xtmp - x[j][0];
+	  const double dely = ytmp - x[j][1];
+	  const double delz = ztmp - x[j][2];
+	  const double rsq = delx*delx + dely*dely + delz*delz;
+	  int jtype = type[j];
+	  int jelem = 0;
+	  if (chemflag)
+	    jelem = map[jtype];
+	  if (rsq < cutsq[jtype][jtype] && rsq > 1e-20) {
+	    snaptr->rij[ninside][0] = delx;
+	    snaptr->rij[ninside][1] = dely;
+	    snaptr->rij[ninside][2] = delz;
+	    snaptr->inside[ninside] = j;
+	    snaptr->wj[ninside] = wjelem[jtype];
+	    snaptr->rcutij[ninside] = 2.0*radelem[jtype]*rcutfac;
+	    snaptr->element[ninside] = jelem; // element index for multi-element snap
+	    ninside++;
+	  }
+	}
+
+	snaptr->compute_ui(ninside, ielem);
+	snaptr->compute_zi();
+	snaptr->compute_bi(ielem);
+
+	// linear contributions
+
+	for (int icoeff = 0; icoeff < ncoeff; icoeff++)
+	  gridlocal[ndesc_base+icoeff][iz][iy][ix] = 
+	    snaptr->blist[icoeff];
+
+	// quadratic contributions
+
+	if (quadraticflag) {
+	  int ncount = ncoeff;
+	  for (int icoeff = 0; icoeff < ncoeff; icoeff++) {
+	    double bveci = snaptr->blist[icoeff];
+	    gridlocal[ndesc_base+ncount++][iz][iy][ix] = 
+	      0.5*bveci*bveci;
+	    for (int jcoeff = icoeff+1; jcoeff < ncoeff; jcoeff++)
+	      gridlocal[ndesc_base+ncount++][iz][iy][ix] = 
+		bveci*snaptr->blist[jcoeff];
+	  }
+	}
+      }
+}
+
+
+/* ----------------------------------------------------------------------
+   memory usage
+------------------------------------------------------------------------- */
+
+double PairSNAGrid::memory_usage()
+{
+  double nbytes = snaptr->memory_usage();    // SNA object
+  int n = atom->ntypes+1;
+  nbytes += (double)n*sizeof(int);            // map
+
+  return nbytes;
+}
+
---
+++ src/ML-SNAP/pair_sna_grid.h
@@ -0,0 +1,76 @@
+/* -*- c++ -*- ----------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   https://www.lammps.org/, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef PAIR_CLASS
+// clang-format off
+PairStyle(sna/grid, PairSNAGrid);
+// clang-format on
+#else
+
+#ifndef LMP_PAIR_SNA_GRID_H
+#define LMP_PAIR_SNA_GRID_H
+
+#include "pair_grid.h"
+
+namespace LAMMPS_NS {
+
+class PairSNAGrid : public PairGrid {
+ public:
+  PairSNAGrid(class LAMMPS *);
+  ~PairSNAGrid();
+
+  void init();
+  void init_list(int, class NeighList *);
+  void compute(int, int);
+  double memory_usage();
+
+ private:
+  int ncoeff;
+  double **cutsq;
+  class NeighList *list;
+  double rcutfac;
+  double *radelem;
+  double *wjelem;
+  int *map;    // map types to [0,nelements)
+  int nelements, chemflag;
+  class SNA *snaptr;
+  double cutmax;
+  int quadraticflag;
+};
+
+}
+
+#endif
+#endif
+
+/* ERROR/WARNING messages:
+
+E: Illegal ... command
+
+Self-explanatory.  Check the input script syntax and compare to the
+documentation for the command.  You can use -echo screen as a
+command-line option when running LAMMPS to see the offending line.
+
+E: Compute sna/grid/local requires a pair style be defined
+
+Self-explanatory.
+
+E: Compute sna/grid/local cutoff is longer than pairwise cutoff
+
+Self-explanatory.
+
+W: More than one compute sna/grid/local
+
+Self-explanatory.
+
+*/

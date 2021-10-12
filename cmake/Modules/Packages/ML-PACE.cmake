77d830bf3a5a7553019f73708a26b40384c9effe
Yury Lysogorskiy <yura.lysogorskii@gmail.com>
2021-10-12 18:15:31 +0200

    update YAML-CPP library target name to yaml-cpp-pace

--- cmake/Modules/Packages/ML-PACE.cmake
+++ cmake/Modules/Packages/ML-PACE.cmake
@@ -1,18 +1,18 @@
 
-set(PACELIB_URL "https://github.com/ICAMS/lammps-user-pace/archive/refs/tags/v.2021.9.28.tar.gz" CACHE STRING "URL for PACE evaluator library sources")
-set(PACELIB_MD5 "f98363bb98adc7295ea63974738c2a1b" CACHE STRING "MD5 checksum of PACE evaluator library tarball")
-mark_as_advanced(PACELIB_URL)
-mark_as_advanced(PACELIB_MD5)
+#set(PACELIB_URL "https://github.com/ICAMS/lammps-user-pace/archive/refs/tags/v.2021.9.28.tar.gz" CACHE STRING "URL for PACE evaluator library sources")
+#set(PACELIB_MD5 "f98363bb98adc7295ea63974738c2a1b" CACHE STRING "MD5 checksum of PACE evaluator library tarball")
+#mark_as_advanced(PACELIB_URL)
+#mark_as_advanced(PACELIB_MD5)
 
 # download library sources to build folder
-file(DOWNLOAD ${PACELIB_URL} ${CMAKE_BINARY_DIR}/libpace.tar.gz SHOW_PROGRESS EXPECTED_HASH MD5=${PACELIB_MD5})
+#file(DOWNLOAD ${PACELIB_URL} ${CMAKE_BINARY_DIR}/libpace.tar.gz SHOW_PROGRESS EXPECTED_HASH MD5=${PACELIB_MD5})
 
 # uncompress downloaded sources
-execute_process(
-  COMMAND ${CMAKE_COMMAND} -E remove_directory lammps-user-pace*
-  COMMAND ${CMAKE_COMMAND} -E tar xzf libpace.tar.gz
-  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-)
+#execute_process(
+#  COMMAND ${CMAKE_COMMAND} -E remove_directory lammps-user-pace*
+#  COMMAND ${CMAKE_COMMAND} -E tar xzf libpace.tar.gz
+#  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+#)
 
 file(GLOB lib-pace ${CMAKE_BINARY_DIR}/lammps-user-pace-*)
 add_subdirectory(${lib-pace}/yaml-cpp build-yaml-cpp)
@@ -27,5 +27,5 @@ set_target_properties(pace PROPERTIES CXX_EXTENSIONS ON OUTPUT_NAME lammps_pace$
 target_include_directories(pace PUBLIC ${PACE_EVALUATOR_INCLUDE_DIR} ${YAML_CPP_INCLUDE_DIR})
 
 target_link_libraries(lammps PRIVATE pace)
-target_link_libraries(lammps PRIVATE yaml-cpp)
+target_link_libraries(lammps PRIVATE yaml-cpp-pace)
 

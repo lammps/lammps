5e102e1bfe884a9fdb39d2084da5e0dbf3c5cff2
Yury Lysogorskiy <yura.lysogorskii@gmail.com>
2021-10-18 18:43:38 +0200

    ML-PACE.cmake: find_package(yaml-cpp 0.6.3 EXACT QUITE) first, otherwise dowload from github/yaml-cpp tag 0.6.3

--- cmake/Modules/Packages/ML-PACE.cmake
+++ cmake/Modules/Packages/ML-PACE.cmake
@@ -1,3 +1,53 @@
+# try to find system libyaml-cpp v.0.6.3 library
+find_package(yaml-cpp 0.6.3 EXACT QUIET)
+
+
+if(yaml-cpp_FOUND)
+    message(STATUS "Library yaml-cpp(v0.6.3)  found")    
+    #message(STATUS "YAML_CPP_LIBRARY_DIRS=${YAML_CPP_LIBRARY_DIRS}")
+    
+#    get_cmake_property(_variableNames VARIABLES)
+#    list (SORT _variableNames)
+#    foreach (_variableName ${_variableNames})
+#        message(STATUS "${_variableName}=${${_variableName}}")
+#    endforeach()
+
+    find_path(YAML_CPP_INCLUDE_DIR
+            NAMES yaml.h
+            PATHS ${YAML_CPP_INCLUDE_DIRS})
+
+    if(NOT YAML_CPP_LIBRARIES)
+        find_library(YAML_CPP_LIBRARY
+                NAMES yaml-cpp
+                PATHS ${YAML_CPP_LIBRARY_DIRS})
+    else()
+        set(YAML_CPP_LIBRARY ${YAML_CPP_LIBRARIES})
+    endif()
+else()
+    message(STATUS "Library yaml-cpp(v0.6.3) not found, downloading")
+    
+    set(YAML_063_URL "https://github.com/jbeder/yaml-cpp/archive/refs/tags/yaml-cpp-0.6.3.tar.gz" CACHE STRING "URL for yaml-cpp (v.0.6.3) library sources")    
+    set(YAML_063_MD5 "b45bf1089a382e81f6b661062c10d0c2" CACHE STRING "MD5 checksum of yaml-cpp (v.0.6.3) library tarball")
+    mark_as_advanced(YAML_063_URL)
+    mark_as_advanced(YAML_063_MD5)
+
+    # download library sources to build folder
+    file(DOWNLOAD ${YAML_063_URL} ${CMAKE_BINARY_DIR}/yaml-cpp-0.6.3.tar.gz SHOW_PROGRESS EXPECTED_HASH MD5=${YAML_063_MD5})
+    
+    execute_process(
+        COMMAND ${CMAKE_COMMAND} -E remove_directory yaml-cpp-yaml-cpp-0.6.3*
+        COMMAND ${CMAKE_COMMAND} -E tar xzf yaml-cpp-0.6.3.tar.gz
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+    )
+    
+    set(YAML_DIR ${CMAKE_BINARY_DIR}/yaml-cpp-yaml-cpp-0.6.3)
+    
+    add_subdirectory(${YAML_DIR} build-yaml-cpp EXCLUDE_FROM_ALL)
+    set(YAML_CPP_INCLUDE_DIR ${YAML_DIR}/include)
+    set(YAML_CPP_LIBRARY yaml-cpp)    
+endif()
+
+
 
 set(PACELIB_URL "https://github.com/ICAMS/lammps-user-pace/archive/refs/tags/v.2021.9.28.upd1.tar.gz" CACHE STRING "URL for PACE evaluator library sources")
 set(PACELIB_MD5 "ec75bc491edd75e10560cdbf129d91a7" CACHE STRING "MD5 checksum of PACE evaluator library tarball")
@@ -16,25 +66,8 @@ execute_process(
 
 file(GLOB lib-pace ${CMAKE_BINARY_DIR}/lammps-user-pace-*)
 
-# try to find system libyaml-cpp v.0.6.3 library
-find_package(yaml-cpp 0.6.3 QUIET)
-
-find_path(YAML_CPP_INCLUDE_DIR
-        NAMES yaml.h
-        PATHS ${YAML_CPP_INCLUDE_DIRS})
-
-find_library(YAML_CPP_LIBRARY
-        NAMES yaml-cpp
-        PATHS ${YAML_CPP_LIBRARY_DIRS})
-
-# if system library not found - using downloaded
-if(${YAML_CPP_LIBRARY} STREQUAL "" OR ${YAML_CPP_INCLUDE_DIR} STREQUAL "" OR
-        ${YAML_CPP_INCLUDE_DIR} STREQUAL "YAML_CPP_INCLUDE_DIR-NOTFOUND")
-    message("-- Library yaml-fcc v0.6.3  not found, using downloaded copy")
-    add_subdirectory(${lib-pace}/yaml-cpp build-yaml-cpp)
-    set(YAML_CPP_INCLUDE_DIR ${lib-pace}/yaml-cpp/include)
-    set(YAML_CPP_LIBRARY yaml-cpp-pace)
-endif()
+message(STATUS "YAML_CPP_INCLUDE_DIR=${YAML_CPP_INCLUDE_DIR}")
+message(STATUS "YAML_CPP_LIBRARY=${YAML_CPP_LIBRARY}")
 
 file(GLOB PACE_EVALUATOR_INCLUDE_DIR ${lib-pace}/ML-PACE)
 file(GLOB PACE_EVALUATOR_SOURCES ${lib-pace}/ML-PACE/*.cpp)

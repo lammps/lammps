4edd5238b1cd4074bb80aac249f9e0a77f221a41
Axel Kohlmeyer <akohlmey@gmail.com>
2021-10-22 13:21:45 -0400

    improve putenv() and unsetenv() implementation on windows by using _putenv_s()

--- src/platform.cpp
+++ src/platform.cpp
@@ -428,11 +428,11 @@ int platform::putenv(const std::string &vardef)
 
   auto found = vardef.find_first_of('=');
 #ifdef _WIN32
-  // must assign a value to variable with _putenv()
+  // must assign a value to variable with _putenv_s()
   if (found == std::string::npos)
-    return _putenv(utils::strdup(vardef + "=1"));
+    return _putenv_s(vardef.c_str(), "1");
   else
-    return _putenv(utils::strdup(vardef));
+    return _putenv_s(vardef.substr(0, found).c_str(), vardef.substr(found+1).c_str());
 #else
   if (found == std::string::npos)
     return setenv(vardef.c_str(), "", 1);
@@ -449,15 +449,12 @@ int platform::putenv(const std::string &vardef)
 int platform::unsetenv(const std::string &variable)
 {
   if (variable.size() == 0) return -1;
-
 #ifdef _WIN32
-  // empty _putenv() definition deletes variable on Windows
-  // must not have an equal sign as that will result in creating an environment variable
-  if (variable.find_first_of('=') != std::string::npos) return -1;
-  auto var = utils::strdup(variable);
-  int rv = _putenv(var);
-  delete[] var;
-  return rv;
+  // emulate POSIX semantics by returning -1 on trying to unset non-existing variable
+  const char *ptr = getenv(variable.c_str());
+  if (!ptr) return -1;
+  // empty _putenv_s() definition deletes variable
+  return _putenv_s(variable.c_str(),"");
 #else
   return ::unsetenv(variable.c_str());
 #endif

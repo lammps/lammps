c911cd52bbf1406eb8aa5c17cedd75ffdd1df434
Axel Kohlmeyer <akohlmey@gmail.com>
2021-10-27 08:24:07 -0400

    whitespace

--- cmake/Modules/Packages/ML-PACE.cmake
+++ cmake/Modules/Packages/ML-PACE.cmake
@@ -5,7 +5,7 @@ mark_as_advanced(PACELIB_URL)
 mark_as_advanced(PACELIB_MD5)
 
 # download library sources to build folder
-file(DOWNLOAD ${PACELIB_URL} ${CMAKE_BINARY_DIR}/libpace.tar.gz EXPECTED_HASH MD5=${PACELIB_MD5}) #SHOW_PROGRESS 
+file(DOWNLOAD ${PACELIB_URL} ${CMAKE_BINARY_DIR}/libpace.tar.gz EXPECTED_HASH MD5=${PACELIB_MD5}) #SHOW_PROGRESS
 
 # uncompress downloaded sources
 execute_process(

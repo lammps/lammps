d178a5ffa39a35ce67c2faa7c3e4b23826325e42
Oliver Henrich <oliver.henrich@strath.ac.uk>
2021-11-17 13:12:44 +0000

    Extended test script

--- examples/PACKAGES/cgdna/examples/test.sh
+++ examples/PACKAGES/cgdna/examples/test.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
-DATE='2Jul21'
-LMPDIR=/Users/ohenrich/Work/code/lammps
+DATE='27Oct21'
+TOL=1e-8
 
+LMPDIR=/Users/ohenrich/Work/code/lammps
 SRCDIR=$LMPDIR/src
 EXDIR=$LMPDIR/examples/PACKAGES/cgdna/examples
 
@@ -11,6 +12,9 @@ if [ $# -eq 1 ] && [ $1 = run ]; then
 
   cd $SRCDIR
   make clean-all
+  make purge
+  make pu
+  make ps
   make -j8 mpi
 
   ######################################################
@@ -24,15 +28,27 @@ if [ $# -eq 1 ] && [ $1 = run ]; then
 
   mpirun -np 1 ./lmp_mpi < in.duplex1 > /dev/null
   mv log.lammps log.$DATE.duplex1.g++.1
-  grep etot log.$DATE.duplex1.g++.1 > e_test.dat
-  grep etot ../log*1 > e_old.dat
-  ndiff -relerr 1e-8 e_test.dat e_old.dat
+  grep etot log.$DATE.duplex1.g++.1 > e_test.1.dat
+  grep etot ../log*1 > e_old.1.dat
+  ndiff -relerr $TOL e_test.1.dat e_old.1.dat
+  if [ $? -eq 0 ];
+  then 
+      echo "# 1 MPI-task passed" 
+  else 
+      echo "# 1 MPI-task unsuccessful" 
+  fi
 
   mpirun -np 4 ./lmp_mpi < in.duplex1 > /dev/null
   mv log.lammps log.$DATE.duplex1.g++.4
-  grep etot log.$DATE.duplex1.g++.4 > e_test.dat
-  grep etot ../log*4 > e_old.dat
-  ndiff -relerr 1e-8 e_test.dat e_old.dat
+  grep etot log.$DATE.duplex1.g++.4 > e_test.4.dat
+  grep etot ../log*4 > e_old.4.dat
+  ndiff -relerr $TOL e_test.4.dat e_old.4.dat
+  if [ $? -eq 0 ];
+  then 
+      echo "# 4 MPI-tasks passed" 
+  else 
+      echo "# 4 MPI-tasks unsuccessful" 
+  fi
 
   ######################################################
 
@@ -47,15 +63,27 @@ if [ $# -eq 1 ] && [ $1 = run ]; then
 
   mpirun -np 1 ./lmp_mpi < in.duplex2 > /dev/null
   mv log.lammps log.$DATE.duplex2.g++.1
-  grep etot log.$DATE.duplex2.g++.1 > e_test.dat
-  grep etot ../log*1 > e_old.dat
-  ndiff -relerr 1e-8 e_test.dat e_old.dat
+  grep etot log.$DATE.duplex2.g++.1 > e_test.1.dat
+  grep etot ../log*1 > e_old.1.dat
+  ndiff -relerr $TOL e_test.1.dat e_old.1.dat
+  if [ $? -eq 0 ];
+  then 
+      echo "# 1 MPI-task passed" 
+  else 
+      echo "# 1 MPI-task unsuccessful" 
+  fi
 
   mpirun -np 4 ./lmp_mpi < in.duplex2 > /dev/null
   mv log.lammps log.$DATE.duplex2.g++.4
-  grep etot log.$DATE.duplex2.g++.4 > e_test.dat
-  grep etot ../log*4 > e_old.dat
-  ndiff -relerr 1e-8 e_test.dat e_old.dat
+  grep etot log.$DATE.duplex2.g++.4 > e_test.4.dat
+  grep etot ../log*4 > e_old.4.dat
+  ndiff -relerr $TOL e_test.4.dat e_old.4.dat
+  if [ $? -eq 0 ];
+  then 
+      echo "# 4 MPI-tasks passed" 
+  else 
+      echo "# 4 MPI-tasks unsuccessful" 
+  fi
 
   ######################################################
 
@@ -70,15 +98,27 @@ if [ $# -eq 1 ] && [ $1 = run ]; then
 
   mpirun -np 1 ./lmp_mpi < in.duplex1 > /dev/null
   mv log.lammps log.$DATE.duplex1.g++.1
-  grep etot log.$DATE.duplex1.g++.1 > e_test.dat
-  grep etot ../log*1 > e_old.dat
-  ndiff -relerr 1e-8 e_test.dat e_old.dat
+  grep etot log.$DATE.duplex1.g++.1 > e_test.1.dat
+  grep etot ../log*1 > e_old.1.dat
+  ndiff -relerr $TOL e_test.1.dat e_old.1.dat
+  if [ $? -eq 0 ];
+  then 
+      echo "# 1 MPI-task passed" 
+  else 
+      echo "# 1 MPI-task unsuccessful" 
+  fi
 
   mpirun -np 4 ./lmp_mpi < in.duplex1 > /dev/null
   mv log.lammps log.$DATE.duplex1.g++.4
-  grep etot log.$DATE.duplex1.g++.4 > e_test.dat
-  grep etot ../log*4 > e_old.dat
-  ndiff -relerr 1e-8 e_test.dat e_old.dat
+  grep etot log.$DATE.duplex1.g++.4 > e_test.4.dat
+  grep etot ../log*4 > e_old.4.dat
+  ndiff -relerr $TOL e_test.4.dat e_old.4.dat
+  if [ $? -eq 0 ];
+  then 
+      echo "# 4 MPI-tasks passed" 
+  else 
+      echo "# 4 MPI-tasks unsuccessful" 
+  fi
 
   ######################################################
 
@@ -93,15 +133,27 @@ if [ $# -eq 1 ] && [ $1 = run ]; then
 
   mpirun -np 1 ./lmp_mpi < in.duplex2 > /dev/null
   mv log.lammps log.$DATE.duplex2.g++.1
-  grep etot log.$DATE.duplex2.g++.1 > e_test.dat
-  grep etot ../log*1 > e_old.dat
-  ndiff -relerr 1e-8 e_test.dat e_old.dat
+  grep etot log.$DATE.duplex2.g++.1 > e_test.1.dat
+  grep etot ../log*1 > e_old.1.dat
+  ndiff -relerr $TOL e_test.1.dat e_old.1.dat
+  if [ $? -eq 0 ];
+  then 
+      echo "# 1 MPI-task passed" 
+  else 
+      echo "# 1 MPI-task unsuccessful" 
+  fi
 
   mpirun -np 4 ./lmp_mpi < in.duplex2 > /dev/null
   mv log.lammps log.$DATE.duplex2.g++.4
-  grep etot log.$DATE.duplex2.g++.4 > e_test.dat
-  grep etot ../log*4 > e_old.dat
-  ndiff -relerr 1e-8 e_test.dat e_old.dat
+  grep etot log.$DATE.duplex2.g++.4 > e_test.4.dat
+  grep etot ../log*4 > e_old.4.dat
+  ndiff -relerr $TOL e_test.4.dat e_old.4.dat
+  if [ $? -eq 0 ];
+  then 
+      echo "# 4 MPI-tasks passed" 
+  else 
+      echo "# 4 MPI-tasks unsuccessful" 
+  fi
 
   ######################################################
 
@@ -116,15 +168,27 @@ if [ $# -eq 1 ] && [ $1 = run ]; then
 
   mpirun -np 1 ./lmp_mpi < in.duplex3 > /dev/null
   mv log.lammps log.$DATE.duplex3.g++.1
-  grep etot log.$DATE.duplex3.g++.1 > e_test.dat
-  grep etot ../log*1 > e_old.dat
-  ndiff -relerr 1e-8 e_test.dat e_old.dat
+  grep etot log.$DATE.duplex3.g++.1 > e_test.1.dat
+  grep etot ../log*1 > e_old.1.dat
+  ndiff -relerr $TOL e_test.1.dat e_old.1.dat
+  if [ $? -eq 0 ];
+  then 
+      echo "# 1 MPI-task passed" 
+  else 
+      echo "# 1 MPI-task unsuccessful" 
+  fi
 
   mpirun -np 4 ./lmp_mpi < in.duplex3 > /dev/null
   mv log.lammps log.$DATE.duplex3.g++.4
-  grep etot  log.$DATE.duplex3.g++.4 > e_test.dat
-  grep etot ../log*4 > e_old.dat
-  ndiff -relerr 1e-8 e_test.dat e_old.dat
+  grep etot  log.$DATE.duplex3.g++.4 > e_test.4.dat
+  grep etot ../log*4 > e_old.4.dat
+  ndiff -relerr $TOL e_test.4.dat e_old.4.dat
+  if [ $? -eq 0 ];
+  then 
+      echo "# 4 MPI-tasks passed" 
+  else 
+      echo "# 4 MPI-tasks unsuccessful" 
+  fi
 
   ######################################################
 
@@ -141,27 +205,51 @@ if [ $# -eq 1 ] && [ $1 = run ]; then
 
   mpirun -np 1 ./lmp_mpi < in.duplex4.4type > /dev/null
   mv log.lammps log.$DATE.duplex4.4type.g++.1
-  grep etot log.$DATE.duplex4.4type.g++.1 > e_test.dat
-  grep etot ../log*4type*1 > e_old.dat
-  ndiff -relerr 1e-8 e_test.dat e_old.dat
+  grep etot log.$DATE.duplex4.4type.g++.1 > e_test.4type.1.dat
+  grep etot ../log*4type*1 > e_old.4type.1.dat
+  ndiff -relerr $TOL e_test.4type.1.dat e_old.4type.1.dat
+  if [ $? -eq 0 ];
+  then 
+      echo "# 1 MPI-task 4 types passed" 
+  else 
+      echo "# 1 MPI-task 4 types unsuccessful" 
+  fi
 
   mpirun -np 4 ./lmp_mpi < in.duplex4.4type > /dev/null
   mv log.lammps log.$DATE.duplex4.4type.g++.4
-  grep etot log.$DATE.duplex4.4type.g++.4 > e_test.dat
-  grep etot ../log*4type*4 > e_old.dat
-  ndiff -relerr 1e-8 e_test.dat e_old.dat
+  grep etot log.$DATE.duplex4.4type.g++.4 > e_test.4type.4.dat
+  grep etot ../log*4type*4 > e_old.4type.4.dat
+  ndiff -relerr $TOL e_test.4type.4.dat e_old.4type.4.dat
+  if [ $? -eq 0 ];
+  then 
+      echo "# 4 MPI-tasks 4 types passed" 
+  else 
+      echo "# 4 MPI-tasks 4 types unsuccessful" 
+  fi
 
   mpirun -np 1 ./lmp_mpi < in.duplex4.8type > /dev/null
   mv log.lammps log.$DATE.duplex4.8type.g++.1
-  grep etot log.$DATE.duplex4.8type.g++.1 > e_test.dat
-  grep etot ../log*8type*1 > e_old.dat
-  ndiff -relerr 1e-8 e_test.dat e_old.dat
+  grep etot log.$DATE.duplex4.8type.g++.1 > e_test.8type.1.dat
+  grep etot ../log*8type*1 > e_old.8type.1.dat
+  ndiff -relerr $TOL e_test.8type.1.dat e_old.8type.1.dat
+  if [ $? -eq 0 ];
+  then 
+      echo "# 1 MPI-task 8 types passed" 
+  else 
+      echo "# 1 MPI-task 8 types unsuccessful" 
+  fi
 
   mpirun -np 4 ./lmp_mpi < in.duplex4.8type > /dev/null
   mv log.lammps log.$DATE.duplex4.8type.g++.4
-  grep etot log.$DATE.duplex4.8type.g++.4 > e_test.dat
-  grep etot ../log*8type*4 > e_old.dat
-  ndiff -relerr 1e-8 e_test.dat e_old.dat
+  grep etot log.$DATE.duplex4.8type.g++.4 > e_test.8type.4.dat
+  grep etot ../log*8type*4 > e_old.8type.4.dat
+  ndiff -relerr $TOL e_test.8type.4.dat e_old.8type.4.dat
+  if [ $? -eq 0 ];
+  then 
+      echo "# 4 MPI-tasks 8 types passed" 
+  else 
+      echo "# 4 MPI-tasks 8 types unsuccessful" 
+  fi
 
   ######################################################
 
@@ -176,15 +264,27 @@ if [ $# -eq 1 ] && [ $1 = run ]; then
 
   mpirun -np 1 ./lmp_mpi < in.dsring > /dev/null
   mv log.lammps log.$DATE.dsring.g++.1
-  grep etot log.$DATE.dsring.g++.1 > e_test.dat
-  grep etot ../log*1 > e_old.dat
-  ndiff -relerr 1e-8 e_test.dat e_old.dat
+  grep etot log.$DATE.dsring.g++.1 > e_test.1.dat
+  grep etot ../log*1 > e_old.1.dat
+  ndiff -relerr $TOL e_test.1.dat e_old.1.dat
+  if [ $? -eq 0 ];
+  then 
+      echo "# 1 MPI-task passed" 
+  else 
+      echo "# 1 MPI-task unsuccessful" 
+  fi
 
   mpirun -np 4 ./lmp_mpi < in.dsring > /dev/null
   mv log.lammps log.$DATE.dsring.g++.4
-  grep etot log.$DATE.dsring.g++.4 > e_test.dat
-  grep etot ../log*4 > e_old.dat
-  ndiff -relerr 1e-8 e_test.dat e_old.dat
+  grep etot log.$DATE.dsring.g++.4 > e_test.4.dat
+  grep etot ../log*4 > e_old.4.dat
+  ndiff -relerr $TOL e_test.4.dat e_old.4.dat
+  if [ $? -eq 0 ];
+  then 
+      echo "# 4 MPI-tasks passed" 
+  else 
+      echo "# 4 MPI-tasks unsuccessful" 
+  fi
 
   ######################################################
 
@@ -199,15 +299,27 @@ if [ $# -eq 1 ] && [ $1 = run ]; then
 
   mpirun -np 1 ./lmp_mpi < in.duplex2 > /dev/null
   mv log.lammps log.$DATE.duplex2.g++.1
-  grep etot log.$DATE.duplex2.g++.1 > e_test.dat
-  grep etot ../log*1 > e_old.dat
-  ndiff -relerr 1e-8 e_test.dat e_old.dat
+  grep etot log.$DATE.duplex2.g++.1 > e_test.1.dat
+  grep etot ../log*1 > e_old.1.dat
+  ndiff -relerr $TOL e_test.1.dat e_old.1.dat
+  if [ $? -eq 0 ];
+  then 
+      echo "# 1 MPI-task passed" 
+  else 
+      echo "# 1 MPI-task unsuccessful" 
+  fi
 
   mpirun -np 4 ./lmp_mpi < in.duplex2 > /dev/null
   mv log.lammps log.$DATE.duplex2.g++.4
-  grep etot log.$DATE.duplex2.g++.4 > e_test.dat
-  grep etot ../log*4 > e_old.dat
-  ndiff -relerr 1e-8 e_test.dat e_old.dat
+  grep etot log.$DATE.duplex2.g++.4 > e_test.4.dat
+  grep etot ../log*4 > e_old.4.dat
+  ndiff -relerr $TOL e_test.4.dat e_old.4.dat
+  if [ $? -eq 0 ];
+  then 
+      echo "# 4 MPI-tasks passed" 
+  else 
+      echo "# 4 MPI-tasks unsuccessful" 
+  fi
 
   ######################################################
   echo '# Done'

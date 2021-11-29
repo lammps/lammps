49cfd6c4866deb27258dba5fe5a0150a2ba09fce
Yaser Afshar <ya.afshar@gmail.com>
2021-11-28 18:49:50 -0600

    Update the LabelMap to use unordered_map
    Update the LabelMap to use unordered_map and optimize search of
    elements in average constant-time complexity. It also enforce
    enforce unique labels.

--- src/label_map.h
+++ src/label_map.h
@@ -16,15 +16,21 @@
 
 #include "pointers.h"  // IWYU pragma: export
 
+#include <unordered_map>
+
 namespace LAMMPS_NS {
 
 class LabelMap : protected Pointers {
  public:
-   int natomtypes,nbondtypes,nangletypes;
-   int ndihedraltypes,nimpropertypes;
+   int natomtypes,nbondtypes,nangletypes,ndihedraltypes,nimpropertypes;
    std::string id;
    std::vector<std::string> typelabel,btypelabel,atypelabel;
    std::vector<std::string> dtypelabel,itypelabel;
+   std::unordered_map<std::string, int> typelabel_map;
+   std::unordered_map<std::string, int> btypelabel_map;
+   std::unordered_map<std::string, int> atypelabel_map;
+   std::unordered_map<std::string, int> dtypelabel_map;
+   std::unordered_map<std::string, int> itypelabel_map;
 
    // per-type data struct mapping this label map to another
 
@@ -42,13 +48,11 @@ class LabelMap : protected Pointers {
    ~LabelMap();
 
    void allocate_type_labels();
-   void modify_lmap(int, char **);
-   void merge_lmap(class LabelMap *, int);                                   // copy another lmap into this one
-   void create_lmap2lmap(class LabelMap *, int);                             // index mapping between two lmaps
-   int find_or_create(const std::string &, std::vector<std::string> &, int); // look up type or create new type
-   int find(const std::string &, int);                                       // find numeric type of type label
-   int search(const std::string &, const std::vector<std::string> &, int);   // look up type index
-   int is_complete(int);                                                     // check if all types are assigned
+   void modify_lmap(int, char **);                // labelmap command in the input script
+   void merge_lmap(class LabelMap *, int);        // copy another lmap into this one
+   void create_lmap2lmap(class LabelMap *, int);  // index mapping between two lmaps
+   int find(const std::string &, int);            // find numeric type of type label
+   int is_complete(int);                          // check if all types are assigned
 
    // input/output for atom class label map
 
@@ -57,6 +61,8 @@ class LabelMap : protected Pointers {
    void write_restart(FILE *);
 
  private:
+   int find_or_create(const std::string &, std::vector<std::string> &, std::unordered_map<std::string, int> &); // look up type or create new type
+   int search(const std::string &, const std::unordered_map<std::string, int> &); // look up type index
    char *read_string(FILE *);
    void write_string(const std::string &, FILE *);
    int read_int(FILE *);

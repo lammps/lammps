3262140b65f84509f0586d684a13948f7a8d9485
Axel Kohlmeyer <akohlmey@gmail.com>
2021-12-27 10:35:38 -0500

    more detailed unit tests. do not fail if ncdump is missing.

--- unittest/formats/test_dump_netcdf.cpp
+++ unittest/formats/test_dump_netcdf.cpp
@@ -19,9 +19,11 @@
 #include "output.h"
 #include "thermo.h"
 #include "utils.h"
+#include "version.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
+#include <algorithm>
 #include <string>
 
 using ::testing::Eq;
@@ -97,13 +99,139 @@ TEST_F(DumpNetCDFTest, run0_plain)
     if (NCDUMP_BINARY) {
         auto converted_file = convert_binary_to_text(dump_file);
         auto lines          = read_lines(converted_file);
-        auto words          = utils::split_words(lines[0]);
+        auto header         = utils::split_words(lines[0]);
         ASSERT_EQ(lines.size(), 233);
-        ASSERT_THAT(words[0], Eq("netcdf"));
-        ASSERT_THAT(words[1]+".nc", Eq(dump_file));
-        words = utils::split_words(lines[3]);
-        ASSERT_THAT(words[0], Eq("atom"));
-        ASSERT_THAT(words[2], Eq("32"));
+        ASSERT_THAT(header[0], Eq("netcdf"));
+        ASSERT_THAT(header[1] + ".nc", Eq(dump_file));
+
+        // check dimensions section
+        auto section = std::find(lines.begin(), lines.end(), "dimensions:");
+        for (auto line = ++section; line < lines.end(); ++line) {
+            auto words = utils::split_words(*line);
+            if ((words.size() < 1) || (words[0] == "variables:")) break;
+            if (words[0] == "atom") ASSERT_THAT(words[2], Eq("32"));
+            if (words[0] == "label") ASSERT_THAT(words[2], Eq("10"));
+            if (words[0] == "Voigt") ASSERT_THAT(words[2], Eq("6"));
+            if (words[0] == "spatial") ASSERT_THAT(words[2], Eq("3"));
+        }
+
+        // check variables section
+        section = std::find(lines.begin(), lines.end(), "variables:");
+        for (auto line = ++section; line < lines.end(); ++line) {
+            auto words = utils::split_words(*line);
+            if ((words.size() < 2) || (words[0] == "data:")) break;
+            if (words[0] == "time:units") ASSERT_THAT(words[2], Eq("lj"));
+            if (words[0] == "time:scale_factor") ASSERT_THAT(words[2], Eq("0.005f"));
+            if (words[0] == "cell_origin:units") ASSERT_THAT(words[2], Eq("lj"));
+            if (words[0] == "cell_angles:units") ASSERT_THAT(words[2], Eq("degree"));
+            if (words[1] == "id(frame,") ASSERT_THAT(words[2], Eq("atom)"));
+            if (words[1] == "type(frame,") ASSERT_THAT(words[2], Eq("atom)"));
+            if (words[1] == "proc(frame,") ASSERT_THAT(words[2], Eq("atom)"));
+            if (words[1] == "procp1(frame,") ASSERT_THAT(words[2], Eq("atom)"));
+            if (words[1] == "mass(frame,") ASSERT_THAT(words[2], Eq("atom)"));
+            if (words[1] == "ix(frame,") ASSERT_THAT(words[2], Eq("atom)"));
+            if (words[1] == "iy(frame,") ASSERT_THAT(words[2], Eq("atom)"));
+            if (words[1] == "iz(frame,") ASSERT_THAT(words[2], Eq("atom)"));
+            if (words[0] == ":Conventions") ASSERT_THAT(words[2], Eq("AMBER"));
+            if (words[0] == ":ConventionVersion") ASSERT_THAT(words[2], Eq("1.0"));
+            if (words[0] == ":program") ASSERT_THAT(words[2], Eq("LAMMPS"));
+            if (words[0] == ":programVersion") ASSERT_THAT(words[2], Eq(LAMMPS_VERSION));
+        }
+
+        // check data section
+        section = std::find(lines.begin(), lines.end(), "data:");
+        for (auto line = ++section; line < lines.end(); ++line) {
+            auto words = utils::split_words(*line);
+            if (words.size() > 0) {
+                if (words[0] == "spatial") ASSERT_THAT(words[2], Eq("xyz"));
+                if (words[0] == "cell_spatial") ASSERT_THAT(words[2], Eq("abc"));
+                if (words[0] == "cell_origin") {
+                    ++line;
+                    words = utils::split_words(*line);
+                    ASSERT_THAT(words[0], Eq("0,"));
+                    ASSERT_THAT(words[1], Eq("0,"));
+                    ASSERT_THAT(words[2], Eq("0"));
+                }
+                if (words[0] == "cell_lengths") {
+                    ++line;
+                    words = utils::split_words(*line);
+                    ASSERT_THAT(words[0], Eq("3.359192,"));
+                    ASSERT_THAT(words[1], Eq("3.359192,"));
+                    ASSERT_THAT(words[2], Eq("3.359192"));
+                }
+                if (words[0] == "cell_angles") {
+                    ++line;
+                    words = utils::split_words(*line);
+                    ASSERT_THAT(words[0], Eq("90,"));
+                    ASSERT_THAT(words[1], Eq("90,"));
+                    ASSERT_THAT(words[2], Eq("90"));
+                }
+                if (words[0] == "id") {
+                    ++line;
+                    words = utils::split_words(*line);
+                    ASSERT_THAT(words[0], Eq("1,"));
+                    ASSERT_THAT(words[1], Eq("2,"));
+                    ASSERT_THAT(words[2], Eq("3,"));
+                    ASSERT_THAT(words[3], Eq("4,"));
+                    ASSERT_THAT(words[4], Eq("5,"));
+                    ASSERT_THAT(words[5], Eq("6,"));
+                    ASSERT_THAT(words[6], Eq("7,"));
+                    ASSERT_THAT(words[7], Eq("8,"));
+                    ASSERT_THAT(words[8], Eq("9,"));
+                    ASSERT_THAT(words[9], Eq("10,"));
+                }
+                if (words[0] == "mass") {
+                    ++line;
+                    words = utils::split_words(*line);
+                    ASSERT_THAT(words[0], Eq("1,"));
+                    ASSERT_THAT(words[1], Eq("1,"));
+                    ASSERT_THAT(words[2], Eq("1,"));
+                    ASSERT_THAT(words[3], Eq("1,"));
+                    ASSERT_THAT(words[4], Eq("1,"));
+                    ASSERT_THAT(words[5], Eq("1,"));
+                    ASSERT_THAT(words[6], Eq("1,"));
+                    ASSERT_THAT(words[7], Eq("1,"));
+                    ASSERT_THAT(words[8], Eq("1,"));
+                    ASSERT_THAT(words[9], Eq("1,"));
+                }
+                if (words[0] == "coordinates") {
+                    ++line;
+                    words = utils::split_words(*line);
+                    ASSERT_THAT(words[0], Eq("0,"));
+                    ASSERT_THAT(words[1], Eq("0,"));
+                    ASSERT_THAT(words[2], Eq("0,"));
+                    ++line;
+                    words = utils::split_words(*line);
+                    ASSERT_THAT(words[0], Eq("0.8397981,"));
+                    ASSERT_THAT(words[1], Eq("0.8397981,"));
+                    ASSERT_THAT(words[2], Eq("0,"));
+                    ++line;
+                    words = utils::split_words(*line);
+                    ASSERT_THAT(words[0], Eq("0.8397981,"));
+                    ASSERT_THAT(words[1], Eq("0,"));
+                    ASSERT_THAT(words[2], Eq("0.8397981,"));
+                    ++line;
+                    words = utils::split_words(*line);
+                    ASSERT_THAT(words[0], Eq("0,"));
+                    ASSERT_THAT(words[1], Eq("0.8397981,"));
+                    ASSERT_THAT(words[2], Eq("0.8397981,"));
+                }
+                if (words[0] == "ix") {
+                    ++line;
+                    words = utils::split_words(*line);
+                    ASSERT_THAT(words[0], Eq("0,"));
+                    ASSERT_THAT(words[1], Eq("0,"));
+                    ASSERT_THAT(words[2], Eq("0,"));
+                    ASSERT_THAT(words[3], Eq("0,"));
+                    ASSERT_THAT(words[4], Eq("0,"));
+                    ASSERT_THAT(words[5], Eq("0,"));
+                    ASSERT_THAT(words[6], Eq("0,"));
+                    ASSERT_THAT(words[7], Eq("0,"));
+                    ASSERT_THAT(words[8], Eq("0,"));
+                    ASSERT_THAT(words[9], Eq("0,"));
+                }
+            }
+        }
         delete_file(converted_file);
     }
     delete_file(dump_file);
@@ -112,7 +240,7 @@ TEST_F(DumpNetCDFTest, run0_plain)
 TEST_F(DumpNetCDFTest, run0_mpi)
 {
     if (!lammps_has_style(lmp, "dump", "netcdf/mpiio")) GTEST_SKIP();
-    auto dump_file = dump_filename("run0");
+    auto dump_file = dump_filename("mpi0");
     auto fields    = "id type proc procp1 mass x y z ix iy iz xu yu zu vx vy vz fx fy fz";
     set_style("netcdf/mpiio");
     generate_dump(dump_file, fields, "", 0);
@@ -121,13 +249,139 @@ TEST_F(DumpNetCDFTest, run0_mpi)
     if (NCDUMP_BINARY) {
         auto converted_file = convert_binary_to_text(dump_file);
         auto lines          = read_lines(converted_file);
-        auto words          = utils::split_words(lines[0]);
+        auto header         = utils::split_words(lines[0]);
         ASSERT_EQ(lines.size(), 234);
-        ASSERT_THAT(words[0], Eq("netcdf"));
-        ASSERT_THAT(words[1]+".nc", Eq(dump_file));
-        words = utils::split_words(lines[3]);
-        ASSERT_THAT(words[0], Eq("atom"));
-        ASSERT_THAT(words[2], Eq("32"));
+        ASSERT_THAT(header[0], Eq("netcdf"));
+        ASSERT_THAT(header[1] + ".nc", Eq(dump_file));
+
+        // check dimensions section
+        auto section = std::find(lines.begin(), lines.end(), "dimensions:");
+        for (auto line = ++section; line < lines.end(); ++line) {
+            auto words = utils::split_words(*line);
+            if ((words.size() < 1) || (words[0] == "variables:")) break;
+            if (words[0] == "atom") ASSERT_THAT(words[2], Eq("32"));
+            if (words[0] == "label") ASSERT_THAT(words[2], Eq("10"));
+            if (words[0] == "Voigt") ASSERT_THAT(words[2], Eq("6"));
+            if (words[0] == "spatial") ASSERT_THAT(words[2], Eq("3"));
+        }
+
+        // check variables section
+        section = std::find(lines.begin(), lines.end(), "variables:");
+        for (auto line = ++section; line < lines.end(); ++line) {
+            auto words = utils::split_words(*line);
+            if ((words.size() < 2) || (words[0] == "data:")) break;
+            if (words[0] == "time:units") ASSERT_THAT(words[2], Eq("lj"));
+            if (words[0] == "time:scale_factor") ASSERT_THAT(words[2], Eq("0.005f"));
+            if (words[0] == "cell_origin:units") ASSERT_THAT(words[2], Eq("lj"));
+            if (words[0] == "cell_angles:units") ASSERT_THAT(words[2], Eq("degree"));
+            if (words[1] == "id(frame,") ASSERT_THAT(words[2], Eq("atom)"));
+            if (words[1] == "type(frame,") ASSERT_THAT(words[2], Eq("atom)"));
+            if (words[1] == "proc(frame,") ASSERT_THAT(words[2], Eq("atom)"));
+            if (words[1] == "procp1(frame,") ASSERT_THAT(words[2], Eq("atom)"));
+            if (words[1] == "mass(frame,") ASSERT_THAT(words[2], Eq("atom)"));
+            if (words[1] == "ix(frame,") ASSERT_THAT(words[2], Eq("atom)"));
+            if (words[1] == "iy(frame,") ASSERT_THAT(words[2], Eq("atom)"));
+            if (words[1] == "iz(frame,") ASSERT_THAT(words[2], Eq("atom)"));
+            if (words[0] == ":Conventions") ASSERT_THAT(words[2], Eq("AMBER"));
+            if (words[0] == ":ConventionVersion") ASSERT_THAT(words[2], Eq("1.0"));
+            if (words[0] == ":program") ASSERT_THAT(words[2], Eq("LAMMPS"));
+            if (words[0] == ":programVersion") ASSERT_THAT(words[2], Eq(LAMMPS_VERSION));
+        }
+
+        // check data section
+        section = std::find(lines.begin(), lines.end(), "data:");
+        for (auto line = ++section; line < lines.end(); ++line) {
+            auto words = utils::split_words(*line);
+            if (words.size() > 0) {
+                if (words[0] == "spatial") ASSERT_THAT(words[2], Eq("xyz"));
+                if (words[0] == "cell_spatial") ASSERT_THAT(words[2], Eq("abc"));
+                if (words[0] == "cell_origin") {
+                    ++line;
+                    words = utils::split_words(*line);
+                    ASSERT_THAT(words[0], Eq("0,"));
+                    ASSERT_THAT(words[1], Eq("0,"));
+                    ASSERT_THAT(words[2], Eq("0"));
+                }
+                if (words[0] == "cell_lengths") {
+                    ++line;
+                    words = utils::split_words(*line);
+                    ASSERT_THAT(words[0], Eq("3.359192,"));
+                    ASSERT_THAT(words[1], Eq("3.359192,"));
+                    ASSERT_THAT(words[2], Eq("3.359192"));
+                }
+                if (words[0] == "cell_angles") {
+                    ++line;
+                    words = utils::split_words(*line);
+                    ASSERT_THAT(words[0], Eq("90,"));
+                    ASSERT_THAT(words[1], Eq("90,"));
+                    ASSERT_THAT(words[2], Eq("90"));
+                }
+                if (words[0] == "id") {
+                    ++line;
+                    words = utils::split_words(*line);
+                    ASSERT_THAT(words[0], Eq("1,"));
+                    ASSERT_THAT(words[1], Eq("2,"));
+                    ASSERT_THAT(words[2], Eq("3,"));
+                    ASSERT_THAT(words[3], Eq("4,"));
+                    ASSERT_THAT(words[4], Eq("5,"));
+                    ASSERT_THAT(words[5], Eq("6,"));
+                    ASSERT_THAT(words[6], Eq("7,"));
+                    ASSERT_THAT(words[7], Eq("8,"));
+                    ASSERT_THAT(words[8], Eq("9,"));
+                    ASSERT_THAT(words[9], Eq("10,"));
+                }
+                if (words[0] == "mass") {
+                    ++line;
+                    words = utils::split_words(*line);
+                    ASSERT_THAT(words[0], Eq("1,"));
+                    ASSERT_THAT(words[1], Eq("1,"));
+                    ASSERT_THAT(words[2], Eq("1,"));
+                    ASSERT_THAT(words[3], Eq("1,"));
+                    ASSERT_THAT(words[4], Eq("1,"));
+                    ASSERT_THAT(words[5], Eq("1,"));
+                    ASSERT_THAT(words[6], Eq("1,"));
+                    ASSERT_THAT(words[7], Eq("1,"));
+                    ASSERT_THAT(words[8], Eq("1,"));
+                    ASSERT_THAT(words[9], Eq("1,"));
+                }
+                if (words[0] == "coordinates") {
+                    ++line;
+                    words = utils::split_words(*line);
+                    ASSERT_THAT(words[0], Eq("0,"));
+                    ASSERT_THAT(words[1], Eq("0,"));
+                    ASSERT_THAT(words[2], Eq("0,"));
+                    ++line;
+                    words = utils::split_words(*line);
+                    ASSERT_THAT(words[0], Eq("0.8397981,"));
+                    ASSERT_THAT(words[1], Eq("0.8397981,"));
+                    ASSERT_THAT(words[2], Eq("0,"));
+                    ++line;
+                    words = utils::split_words(*line);
+                    ASSERT_THAT(words[0], Eq("0.8397981,"));
+                    ASSERT_THAT(words[1], Eq("0,"));
+                    ASSERT_THAT(words[2], Eq("0.8397981,"));
+                    ++line;
+                    words = utils::split_words(*line);
+                    ASSERT_THAT(words[0], Eq("0,"));
+                    ASSERT_THAT(words[1], Eq("0.8397981,"));
+                    ASSERT_THAT(words[2], Eq("0.8397981,"));
+                }
+                if (words[0] == "ix") {
+                    ++line;
+                    words = utils::split_words(*line);
+                    ASSERT_THAT(words[0], Eq("0,"));
+                    ASSERT_THAT(words[1], Eq("0,"));
+                    ASSERT_THAT(words[2], Eq("0,"));
+                    ASSERT_THAT(words[3], Eq("0,"));
+                    ASSERT_THAT(words[4], Eq("0,"));
+                    ASSERT_THAT(words[5], Eq("0,"));
+                    ASSERT_THAT(words[6], Eq("0,"));
+                    ASSERT_THAT(words[7], Eq("0,"));
+                    ASSERT_THAT(words[8], Eq("0,"));
+                    ASSERT_THAT(words[9], Eq("0,"));
+                }
+            }
+        }
         delete_file(converted_file);
     }
     delete_file(dump_file);

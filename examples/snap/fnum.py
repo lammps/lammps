82b7b2f3ea641daaff87dd2659dc881bbcc585ef
Aidan Thompson <athomps@sandia.gov>
2022-01-04 14:02:29 -0700

    Simplified access of force errors

--- examples/snap/fnum.py
+++ examples/snap/fnum.py
@@ -1,8 +1,5 @@
 import lammps
 
-def sqerr(a,b):
-    return (a[0]-b[0])**2+(a[1]-b[1])**2+(a[2]-b[2])**2
-
 infile = "in.fnum"
 
 fdeltalist = [1.0e-2,1.0e-3,1.0e-4,1.0e-5,1.0e-6,1.0e-7,1.0e-8,1.0e-9,1.0e-10]
@@ -12,13 +9,6 @@ for fdelta in fdeltalist:
     cmdlist = ["-screen","none","-var","fdelta",f'{fdelta}']
     lmp = lammps.lammps(cmdargs = cmdlist)
     lmp.file(infile)
-    nlocal = lmp.extract_global("nlocal")
-
-    f = lmp.extract_atom("f")
-    fnum = lmp.extract_fix("fnum", lammps.LMP_STYLE_ATOM, lammps.LMP_TYPE_ARRAY)
-
-    sumsq = 0
-    for i in range(nlocal):
-        sumsq += sqerr(fnum[i],f[i])
-    rmse = (sumsq/nlocal)**0.5
+    faverrsq = lmp.extract_compute("faverrsq", lammps.LMP_STYLE_GLOBAL, lammps.LMP_TYPE_SCALAR)
+    rmse = faverrsq**0.5
     print(f"{fdelta} {rmse}")

093c54d8ed451e4f602a610f0abd95d9bdfbff43
Axel Kohlmeyer <akohlmey@gmail.com>
2022-02-07 14:42:07 -0500

    update to use settings from upstream

--- lib/kokkos/core/src/desul/atomics/Generic.hpp
+++ lib/kokkos/core/src/desul/atomics/Generic.hpp
@@ -10,7 +10,7 @@ SPDX-License-Identifier: (BSD-3-Clause)
 #define DESUL_ATOMICS_GENERIC_HPP_
 
 #include <type_traits>
-#if defined(__GNUC__)
+#if defined(__GNUC__) && (!defined(__clang__))
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wstrict-aliasing"
 #endif
@@ -688,7 +688,7 @@ DESUL_INLINE_FUNCTION bool atomic_compare_exchange_weak(T* const dest,
 #include <desul/atomics/GCC.hpp>
 #include <desul/atomics/HIP.hpp>
 #include <desul/atomics/OpenMP.hpp>
-#if defined(__GNUC__)
+#if defined(__GNUC__) && (!defined(__clang__))
 #pragma GCC diagnostic pop
 #endif
 #endif

a5fe8b0581af63f8c6ed988feee00c6d3007c461
Axel Kohlmeyer <akohlmey@gmail.com>
2022-02-11 22:01:04 -0500

    update OpenCL loader to latest OpenCL spec sources

--- cmake/Modules/OpenCLLoader.cmake
+++ cmake/Modules/OpenCLLoader.cmake
@@ -1,6 +1,6 @@
 message(STATUS "Downloading and building OpenCL loader library")
-set(OPENCL_LOADER_URL "${LAMMPS_THIRDPARTY_URL}/opencl-loader-2021.09.18.tar.gz" CACHE STRING "URL for OpenCL loader tarball")
-set(OPENCL_LOADER_MD5 "3b3882627964bd02e5c3b02065daac3c" CACHE STRING "MD5 checksum of OpenCL loader tarball")
+set(OPENCL_LOADER_URL "${LAMMPS_THIRDPARTY_URL}/opencl-loader-2022.01.04.tar.gz" CACHE STRING "URL for OpenCL loader tarball")
+set(OPENCL_LOADER_MD5 "1f23f7ee9934b2d102dcff68517cb7a8" CACHE STRING "MD5 checksum of OpenCL loader tarball")
 mark_as_advanced(OPENCL_LOADER_URL)
 mark_as_advanced(OPENCL_LOADER_MD5)
 

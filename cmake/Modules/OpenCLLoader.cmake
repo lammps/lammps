78d149f1187baa99754b2d72310ad21352bd0b43
Axel Kohlmeyer <akohlmey@gmail.com>
2022-02-12 13:21:10 -0500

    update to download improved loader sources that make installing optional

--- cmake/Modules/OpenCLLoader.cmake
+++ cmake/Modules/OpenCLLoader.cmake
@@ -1,9 +1,10 @@
 message(STATUS "Downloading and building OpenCL loader library")
 set(OPENCL_LOADER_URL "${LAMMPS_THIRDPARTY_URL}/opencl-loader-2022.01.04.tar.gz" CACHE STRING "URL for OpenCL loader tarball")
-set(OPENCL_LOADER_MD5 "1f23f7ee9934b2d102dcff68517cb7a8" CACHE STRING "MD5 checksum of OpenCL loader tarball")
+set(OPENCL_LOADER_MD5 "8d3a801e87a2c6653bf0e27707063914" CACHE STRING "MD5 checksum of OpenCL loader tarball")
 mark_as_advanced(OPENCL_LOADER_URL)
 mark_as_advanced(OPENCL_LOADER_MD5)
 
+set(INSTALL_LIBOPENCL OFF CACHE BOOL "" FORCE)
 set(BUILD_SHARED_LIBS OFF)
 include(ExternalCMakeProject)
 ExternalCMakeProject(opencl_loader ${OPENCL_LOADER_URL} ${OPENCL_LOADER_MD5} opencl-loader . "")

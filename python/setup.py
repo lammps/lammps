891d3c8296fd9158209c9d6b37ad800718ed3c77
Axel Kohlmeyer <akohlmey@gmail.com>
2022-02-24 17:04:16 -0500

    update setup.py to be compatible with building "pure" and "binary" wheels

--- python/setup.py
+++ python/setup.py
@@ -1,6 +1,7 @@
 # this only installs the LAMMPS python package
 # it assumes the LAMMPS shared library is already installed
 from setuptools import setup
+from setuptools.dist import Distribution
 from sys import version_info
 import os,time
 LAMMPS_PYTHON_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -21,10 +22,27 @@ def get_lammps_version():
         t = time.strptime("".join(line[start_pos:end_pos].split()), "%d%b%Y")
         return "{}.{}.{}".format(t.tm_year,t.tm_mon,t.tm_mday)
 
+class BinaryDistribution(Distribution):
+    """Wrapper to enforce creating a binary package"""
+    def has_ext_modules(foo):
+        return True
+
+libpath = os.environ.get("LAMMPS_SHARED_LIB")
+
 if version_info.major >= 3:
     pkgs = ['lammps', 'lammps.mliap']
 else:
     pkgs = ['lammps']
+
+with open("README", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+if libpath:
+    pkgdata = {'lammps': [ libpath ]}
+    bdist = BinaryDistribution
+else:
+    pkgdata = {}
+    bdist = Distribution
 
 setup(
     name = "lammps",
@@ -32,7 +50,21 @@ setup(
     author = "Steve Plimpton",
     author_email = "sjplimp@sandia.gov",
     url = "https://www.lammps.org",
+    project_urls = {
+        "Bug Tracker": "https://github.com/lammps/lammps/issues",
+    },
     description = "LAMMPS Molecular Dynamics Python package",
+    long_description = long_description,
+    long_description_content_type = "text/plain",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+    ],
     license = "GPL",
-    packages=pkgs,
+    packages = pkgs,
+    package_data = pkgdata,
+    distclass = bdist,
 )

eaa6fb17af9f0f0bd917bad2d7e3c856f746cb7a
Aidan Thompson <athomps@sandia.gov>
2022-03-03 16:47:33 -0700

    Relaxed the error check on molecule moves in parallel. Molecule exchange is okay.

--- src/MC/fix_gcmc.cpp
+++ src/MC/fix_gcmc.cpp
@@ -681,11 +681,13 @@ void FixGCMC::init()
     }
   }
 
-  // Current implementation is broken using
-  // full_flag on molecules on more than one processor.
-  // Print error if this is the current mode
-  if (full_flag && (exchmode == EXCHMOL || movemode == MOVEMOL) && comm->nprocs > 1)
-    error->all(FLERR,"fix gcmc does currently not support full_energy option with molecules on more than 1 MPI process.");
+  // current implementation is broken using
+  // full_flag and translation/rotation of molecules
+  // on more than one processor.
+
+  if (full_flag && movemode == MOVEMOL && comm->nprocs > 1)
+    error->all(FLERR,"fix gcmc does currently not support full_energy "
+	       "option with molecule MC moves on more than 1 MPI process.");
 
 }
 

5dbad2354a9233b41baa696a45adca198453b9d9
Axel Kohlmeyer <akohlmey@gmail.com>
2022-03-05 20:18:40 -0500

    use new neighbor list request API

--- src/pair_lj_cut_coul_cut.cpp
+++ src/pair_lj_cut_coul_cut.cpp
@@ -254,7 +254,7 @@ void PairLJCutCoulCut::init_style()
   if (!atom->q_flag)
     error->all(FLERR,"Pair style lj/cut/coul/cut requires atom attribute q");
 
-  neighbor->request(this,instance_me);
+  neighbor->add_request(this,NeighConst::REQ_DEFAULT);
 }
 
 /* ----------------------------------------------------------------------

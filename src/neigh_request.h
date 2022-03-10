799ab965d96878ae1f2408052c46d1ffb2530b5f
Axel Kohlmeyer <akohlmey@gmail.com>
2022-03-09 23:25:26 -0500

    make neighbor list request flags protected and add selected friends

--- src/neigh_request.h
+++ src/neigh_request.h
@@ -19,7 +19,15 @@
 namespace LAMMPS_NS {
 
 class NeighRequest : protected Pointers {
- public:
+ friend class Neighbor;
+ friend class NBin;
+ friend class NeighList;
+ friend class NPair;
+ friend class NStencil;
+ friend class NeighborKokkos;
+ friend class NPairSkipIntel;
+ friend class FixIntel;
+ protected:
   int index;                 // index of which neigh request this is
   void *requestor;           // class that made request
   int requestor_instance;    // instance of that class (only Fix, Compute, Pair)
@@ -110,7 +118,7 @@ class NeighRequest : protected Pointers {
   int index_pair;       // index of NPair class assigned to this request
 
   // methods
-
+ public:
   NeighRequest(class LAMMPS *);
   NeighRequest(class LAMMPS *, int, void *, int);
   NeighRequest(NeighRequest *);

29ada4e263ca49cad26783fec7907629693a9369
J Todd <joeatodd@gmail.com>
2022-03-25 11:19:59 +0000

    Update description header

--- cmake/presets/kokkos-sycl.cmake
+++ cmake/presets/kokkos-sycl.cmake
@@ -1,6 +1,5 @@
-# preset that enables KOKKOS and selects CUDA compilation with OpenMP
-# enabled as well. This preselects CC 5.0 as default GPU arch, since
-# that is compatible with all higher CC, but not the default CC 3.5
+# preset that enables KOKKOS and selects SYCL compilation with OpenMP
+# enabled as well. Also sets some performance related compiler flags.
 set(PKG_KOKKOS ON CACHE BOOL "" FORCE)
 set(Kokkos_ENABLE_SERIAL ON CACHE BOOL "" FORCE)
 set(Kokkos_ENABLE_OPENMP ON CACHE BOOL "" FORCE)

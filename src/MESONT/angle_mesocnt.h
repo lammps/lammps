36272b3267d5a987ff5477837cbb87be5aa3481c
phankl <philipp.kloza@gmail.com>
2022-04-11 11:01:56 +0100

    added buckling angle_style header file

---
+++ src/MESONT/angle_mesocnt.h
@@ -0,0 +1,54 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   https://www.lammps.org/, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef ANGLE_CLASS
+AngleStyle(mesocnt, AngleMesoCNT);
+#else
+
+#ifndef LMP_ANGLE_MESOCNT_H
+#define LMP_ANGLE_MESOCNT_H
+
+#include "angle.h"
+
+namespace LAMMPS_NS {
+
+class AngleMesoCNT : public Angle {
+ public:
+  AngleMesoCNT(class LAMMPS *);
+  ~AngleMesoCNT() override;
+  void compute(int, int) override;
+  void coeff(int, char **) override;
+  double equilibrium_angle(int) override;
+  void write_restart(FILE *) override;
+  void read_restart(FILE *) override;
+  void write_data(FILE *) override;
+  double single(int, int, int, int) override;
+
+ protected:
+  double *kh, *kb, *thetab;
+
+  virtual void allocate();
+};
+
+}    // namespace LAMMPS_NS
+
+#endif
+#endif
+
+/* ERROR/WARNING messages:
+
+E: Incorrect args for angle coefficients
+
+Self-explanatory.  Check the input script or data file.
+
+*/

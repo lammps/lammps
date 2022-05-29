3f87b88d579f7866fd48de7157453039a8f1a1ba
jrgissing <jrgiss05@gmail.com>
2022-05-29 10:43:36 -0400

    check that bond is actually in map

--- src/REACTION/fix_bond_react.cpp
+++ src/REACTION/fix_bond_react.cpp
@@ -2278,6 +2278,7 @@ double FixBondReact::rxnfunction(const std::string& rxnfunc, const std::string& 
     double perbondval;
     std::set<tagint> aset;
     std::string computeid = varid;
+    std::map<std::set<tagint>,int>::iterator it;
 
     if (computeid.substr(0,2) != "c_") error->one(FLERR,"Bond/react: Reaction special function compute "
                                          "name should begin with 'c_'");
@@ -2304,7 +2305,10 @@ double FixBondReact::rxnfunction(const std::string& rxnfunc, const std::string& 
     if (cperbond->invoked_local != lmp->update->ntimestep)
       cperbond->compute_local();
 
-    ibond = cperbond->atoms2bond.find(aset)->second;
+    it = cperbond->atoms2bond.find(aset);
+    if (it == cperbond->atoms2bond.end()) error->one(FLERR,"Bond/react: Unable to locate bond referenced by "
+                                            "reaction special function 'rxnbond'");
+    ibond = it->second;
     perbondval = cperbond->vector_local[ibond];
     return perbondval;
   }

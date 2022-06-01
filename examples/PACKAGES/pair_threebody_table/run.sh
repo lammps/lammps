8e9071b01f00c44d41060e08d3ceb234e9ccdd47
Axel Kohlmeyer <akohlmey@gmail.com>
2022-06-01 14:22:53 -0400

    move and update threebody/table examples

--- examples/PACKAGES/pair_threebody_table/run.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash -e
-
-#run the LAMMPS simulation of the first part of the tutorial (needs a current LAMMPS version compiled with the user pair_style 3b/table)
-lmp < spce.in > spce.out
-
-#run the LAMMPS simulation of the second part of the tutorial (needs a current LAMMPS version compiled with the user pair_style 3b/table)
-lmp < spce_2.in > spce_2.out
-

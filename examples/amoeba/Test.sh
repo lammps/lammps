f58d413fbd0162c5d99e8f64f7dd2b86fd1713e2
Axel Kohlmeyer <akohlmey@gmail.com>
2022-07-05 06:59:31 -0400

    Update test examples and script to be more in line with other LAMMPS examples

--- examples/amoeba/Test.sh
+++ examples/amoeba/Test.sh
@@ -1,66 +1,34 @@
-# run test problems
+#!/bin/sh
+# run test problems.
+EXE=${1-../../src/lmp_mpi}
+DATE=$(date +%1d%b%y)
 
 # dimer
 
-../../src/lmp_mpi < in.water_dimer.amoeba.test
-mv log.lammps log.water_dimer.amoeba.1.test
-mv dump.water_dimer dump.water_dimer.amoeba.1.test
+${EXE} -in in.water_dimer.amoeba -log log.${DATE}.water_dimer.amoeba.g++.1
+mpirun -np 4 ${EXE} -in in.water_dimer.amoeba -log log.${DATE}.water_dimer.amoeba.g++.4
 
-mpirun -np 4 ../../src/lmp_mpi < in.water_dimer.amoeba.test
-mv log.lammps log.water_dimer.amoeba.4.test
-mv dump.water_dimer dump.water_dimer.amoeba.4.test
-
-../../src/lmp_mpi < in.water_dimer.hippo.test
-mv log.lammps log.water_dimer.hippo.1.test
-mv dump.water_dimer dump.water_dimer.hippo.1.test
-
-mpirun -np 4 ../../src/lmp_mpi < in.water_dimer.hippo.test
-mv log.lammps log.water_dimer.hippo.4.test
-mv dump.water_dimer dump.water_dimer.hippo.4.test
+${EXE} -in in.water_dimer.hippo -log log.${DATE}.water_dimer.hippo.g++.1
+mpirun -np 4 ${EXE} -in in.water_dimer.hippo -log log.${DATE}.water_dimer.hippo.g++.4
 
 # hexamer
 
-../../src/lmp_mpi < in.water_hexamer.amoeba.test
-mv log.lammps log.water_hexamer.amoeba.1.test
-mv dump.water_hexamer dump.water_hexamer.amoeba.1.test
+${EXE} -in in.water_hexamer.amoeba -log log.${DATE}.water_hexamer.amoeba.g++.1
+mpirun -np 4 ${EXE} -in in.water_hexamer.amoeba -log log.${DATE}.water_hexamer.amoeba.g++.4
 
-mpirun -np 4 ../../src/lmp_mpi < in.water_hexamer.amoeba.test
-mv log.lammps log.water_hexamer.amoeba.4.test
-mv dump.water_hexamer dump.water_hexamer.amoeba.4.test
-
-../../src/lmp_mpi < in.water_hexamer.hippo.test
-mv log.lammps log.water_hexamer.hippo.1.test
-mv dump.water_hexamer dump.water_hexamer.hippo.1.test
-
-mpirun -np 4 ../../src/lmp_mpi < in.water_hexamer.hippo.test
-mv log.lammps log.water_hexamer.hippo.4.test
-mv dump.water_hexamer dump.water_hexamer.hippo.4.test
+${EXE} -in in.water_hexamer.hippo -log log.${DATE}.water_hexamer.hippo.g++.1
+mpirun -np 4 ${EXE} -in in.water_hexamer.hippo -log log.${DATE}.water_hexamer.hippo.g++.4
 
 # water box
 
-../../src/lmp_mpi < in.water_box.amoeba.test
-mv log.lammps log.water_box.amoeba.1.test
-mv dump.water_box dump.water_box.amoeba.1.test
+${EXE} -in in.water_box.amoeba -log log.${DATE}.water_box.amoeba.g++.1
+mpirun -np 4 ${EXE} -in in.water_box.amoeba -log log.${DATE}.water_box.amoeba.g++.4
 
-mpirun -np 32 ../../src/lmp_mpi < in.water_box.amoeba.test
-mv log.lammps log.water_box.amoeba.32.test
-mv dump.water_box dump.water_box.amoeba.32.test
-
-../../src/lmp_mpi < in.water_box.hippo.test
-mv log.lammps log.water_box.hippo.1.test
-mv dump.water_box dump.water_box.hippo.1.test
-
-mpirun -np 32 ../../src/lmp_mpi < in.water_box.hippo.test
-mv log.lammps log.water_box.hippo.32.test
-mv dump.water_box dump.water_box.hippo.32.test
+${EXE} -in in.water_box.hippo -log log.${DATE}.water_box.hippo.g++.1
+mpirun -np 4 ${EXE} -in in.water_box.hippo -log log.${DATE}.water_box.hippo.g++.4
 
 # ubiquitin
 
-../../src/lmp_mpi < in.ubiquitin.test
-mv log.lammps log.ubiquitin.1.test
-mv dump.ubiquitin dump.ubiquitin.1.test
-
-mpirun -np 32 ../../src/lmp_mpi < in.ubiquitin.test
-mv log.lammps log.ubiquitin.32.test
-mv dump.ubiquitin dump.ubiquitin.32.test
+${EXE} -in in.ubiquitin -log log.${DATE}.ubiquitin.g++.1
+mpirun -np 4 ${EXE} -in in.ubiquitin -log log.${DATE}.ubiquitin.g++.4
 

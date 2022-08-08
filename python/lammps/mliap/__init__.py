fd2ab2a8b9c42c6226f181fd29bf44261e7a0621
chemshift <dsema@mit.edu>
2022-08-08 12:08:46 -0600

    Update __init__.py

--- python/lammps/mliap/__init__.py
+++ python/lammps/mliap/__init__.py
@@ -6,19 +6,18 @@ import sysconfig
 import ctypes
 import platform
 
-library_dir = sysconfig.get_config_vars('prefix')[0]
 py_ver = sysconfig.get_config_vars('VERSION')[0]
 OS_name = platform.system()
 
 if OS_name == "Linux":
     SHLIB_SUFFIX = '.so'
-    library = library_dir + '/lib/' + 'libpython' + py_ver + SHLIB_SUFFIX
+    library = 'libpython' + py_ver + SHLIB_SUFFIX
 elif OS_name == "Darwin":
     SHLIB_SUFFIX = '.dylib'
-    library = library_dir + '/lib/' + 'libpython' + py_ver + SHLIB_SUFFIX
+    library = 'libpython' + py_ver + SHLIB_SUFFIX
 elif OS_name == "Windows":
     SHLIB_SUFFIX = '.dll'
-    library = library_dir + '\\' + 'python' + py_ver + SHLIB_SUFFIX
+    library = 'python' + py_ver + SHLIB_SUFFIX
 
 try:
     pylib = ctypes.CDLL(library)

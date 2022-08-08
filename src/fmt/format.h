7e0cad7d9e602b6b223b02e1b5ad0954ec3d84db
Axel Kohlmeyer <akohlmey@gmail.com>
2022-08-08 15:40:19 -0400

    import fix from upstream

--- src/fmt/format.h
+++ src/fmt/format.h
@@ -389,12 +389,11 @@ class uint128_fallback {
       hi_ += (lo_ < n ? 1 : 0);
       return *this;
     }
-// LAMMPS customization: XLCClang does not support __builtin_addcll()
-#if FMT_HAS_BUILTIN(__builtin_addcll) && !(defined(__xlc__) && defined(__clang__))
+#if FMT_HAS_BUILTIN(__builtin_addcll) && !defined(__ibmxl__)
     unsigned long long carry;
     lo_ = __builtin_addcll(lo_, n, 0, &carry);
     hi_ += carry;
-#elif FMT_HAS_BUILTIN(__builtin_ia32_addcarryx_u64)
+#elif FMT_HAS_BUILTIN(__builtin_ia32_addcarryx_u64) && !defined(__ibmxl__)
     unsigned long long result;
     auto carry = __builtin_ia32_addcarryx_u64(0, lo_, n, &result);
     lo_ = result;

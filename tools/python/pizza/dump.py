1f37087156ba2de28dd57acc2215f4fdba1b611f
Axel Kohlmeyer <akohlmey@gmail.com>
2022-08-20 21:42:45 -0400

    python 2/3 portability changes for log class from Pizza.py

--- tools/python/pizza/dump.py
+++ tools/python/pizza/dump.py
@@ -319,8 +319,8 @@ class dump:
     # if fail, try next file
     # if new snapshot time stamp already exists, read next snapshot
 
-    while 1:
-      f = open(self.flist[self.nextfile],'rb')
+    while True:
+      f = open(self.flist[self.nextfile],'r')
       f.seek(self.eof)
       snap = self.read_snapshot(f)
       if not snap:

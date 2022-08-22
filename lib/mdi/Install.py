ab7ae9324f7c0c88dbc29420c07d9ee028c01531
Steve Plimpton <sjplimp@sandia.gov>
2022-08-22 13:15:48 -0600

    update version of MDI library

--- lib/mdi/Install.py
+++ lib/mdi/Install.py
@@ -32,7 +32,7 @@ make lib-mdi args="-m mpi" # build MDI lib with same settings as in the mpi Make
 
 # settings
 
-version = "1.4.2"
+version = "1.4.4"
 url = "https://github.com/MolSSI-MDI/MDI_Library/archive/v%s.tar.gz" % version
 
 # known checksums for different MDI versions. used to validate the download.

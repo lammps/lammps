0d88f4ccf28f2f6ed2b90232a7ec7a531ac8ac58
Axel Kohlmeyer <akohlmey@gmail.com>
2022-08-28 12:31:00 -0400

    consistent naming and style

--- unittest/commands/test_simple_commands.cpp
+++ unittest/commands/test_simple_commands.cpp
@@ -552,9 +552,8 @@ int main(int argc, char **argv)
     MPI_Init(&argc, &argv);
     ::testing::InitGoogleMock(&argc, argv);
 
-    if (platform::mpi_vendor() == "Open MPI" && !LAMMPS_NS::Info::has_exceptions())
-        std::cout << "Warning: using OpenMPI without exceptions. "
-                     "Death tests will be skipped\n";
+    if (platform::mpi_vendor() == "Open MPI" && !Info::has_exceptions())
+        std::cout << "Warning: using OpenMPI without exceptions. Death tests will be skipped\n";
 
     // handle arguments passed via environment variable
     if (const char *var = getenv("TEST_ARGS")) {

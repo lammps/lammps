0ed0d757ecc56ac9b9e72c2c7b3e6d4f87e555df
Axel Kohlmeyer <akohlmey@gmail.com>
2022-09-24 21:08:43 -0400

    refactor unified LJ example input to embed the python code into the input

--- examples/mliap/pickle_mliap_unified_lj_Ar.py
+++
@@ -1,9 +0,0 @@
-import lammps
-import lammps.mliap
-
-from lammps.mliap.mliap_unified_lj import MLIAPUnifiedLJ
-
-
-if __name__ == '__main__':
-    unified = MLIAPUnifiedLJ(["Ar"])
-    unified.pickle('mliap_unified_lj_Ar.pkl')

f3a6038f51fd992bb6c15283a2e8e97be1a625c1
Axel Kohlmeyer <akohlmey@gmail.com>
2022-10-24 19:08:44 -0400

    update developer contact info

--- src/KOKKOS/mliap_so3_kokkos.cpp
+++ src/KOKKOS/mliap_so3_kokkos.cpp
@@ -1,18 +1,18 @@
 /* ----------------------------------------------------------------------
- LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
- https://www.lammps.org/, Sandia National Laboratories
- Steve Plimpton, sjplimp@sandia.gov
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   https://www.lammps.org/, Sandia National Laboratories
+   LAMMPS Development team: developers@lammps.org
 
- Copyright (2003) Sandia Corporation.  Under the terms of Contract
- DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
- certain rights in this software.  This software is distributed under
- the GNU General Public License.
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
 
- See the README file in the top-level LAMMPS directory.
+   See the README file in the top-level LAMMPS directory.
  ------------------------------------------------------------------------- */
 
 /* ----------------------------------------------------------------------
- Contributing authors: Byungkyun Kang (University of Nevada, Las Vegas)
+   Contributing authors: Byungkyun Kang (University of Nevada, Las Vegas)
    Contributing author: Matt Bettencourt (NVIDIA)
  ------------------------------------------------------------------------- */
 

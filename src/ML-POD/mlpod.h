cd5bd9f3789a2ef7242f8d09d473d2e21ca85856
Axel Kohlmeyer <akohlmey@gmail.com>
2022-11-30 19:08:18 -0500

    add C++ marker for emacs

--- src/ML-POD/mlpod.h
+++ src/ML-POD/mlpod.h
@@ -1,4 +1,4 @@
-/* ----------------------------------------------------------------------
+/* -*- c++ -*- ----------------------------------------------------------
    LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
    https://www.lammps.org/ Sandia National Laboratories
    LAMMPS development team: developers@lammps.org

6fe8b84c23c3b86a141c82cd77d27f7a5f47d273
Axel Kohlmeyer <akohlmey@gmail.com>
2022-11-30 19:31:00 -0500

    correct updating Makefile.package.setting edits, fix a few broken ones.

--- src/MDI/Install.sh
+++ src/MDI/Install.sh
@@ -49,7 +49,7 @@ if (test $1 = 1) then
   fi
 
   if (test -e ../Makefile.package.settings) then
-    sed -i -e '/^include.*mdi.*$/d' ../Makefile.package.settings
+    sed -i -e '/^[ \t]*include.*mdi.*$/d' ../Makefile.package.settings
     # multiline form needed for BSD sed on Macs
     sed -i -e '4 i \
 include ..\/..\/lib\/mdi\/Makefile.lammps
@@ -65,7 +65,7 @@ elif (test $1 = 0) then
   fi
 
   if (test -e ../Makefile.package.settings) then
-    sed -i -e '/^include.*mdi.*$/d' ../Makefile.package.settings
+    sed -i -e '/^[ \t]*include.*mdi.*$/d' ../Makefile.package.settings
   fi
 
 fi

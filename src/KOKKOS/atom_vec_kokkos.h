9b3cc46ccf652fb7dd9bde43bc38eaf26d2bc6b1
Axel Kohlmeyer <akohlmey@gmail.com>
2022-12-14 16:15:24 -0500

    add override keyword

--- src/KOKKOS/atom_vec_kokkos.h
+++ src/KOKKOS/atom_vec_kokkos.h
@@ -36,7 +36,7 @@ union d_ubuf {
 class AtomVecKokkos : virtual public AtomVec {
  public:
   AtomVecKokkos(class LAMMPS *);
-  ~AtomVecKokkos();
+  ~AtomVecKokkos() override;
 
   virtual void sync(ExecutionSpace space, unsigned int mask) = 0;
   virtual void modified(ExecutionSpace space, unsigned int mask) = 0;

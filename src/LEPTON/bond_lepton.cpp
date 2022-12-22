5da8242690f837f8380c691b87cb05b70fe4df3e
Axel Kohlmeyer <akohlmey@gmail.com>
2022-12-22 02:13:51 -0500

    add bond style lepton

---
+++ src/LEPTON/bond_lepton.cpp
@@ -0,0 +1,322 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   https://www.lammps.org/, Sandia National Laboratories
+   LAMMPS development team: developers@lammps.org
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+/* ----------------------------------------------------------------------
+   Contributing author: Axel Kohlmeyer (Temple U)
+------------------------------------------------------------------------- */
+
+#include "bond_lepton.h"
+
+#include "atom.h"
+#include "comm.h"
+#include "error.h"
+#include "force.h"
+#include "memory.h"
+#include "neighbor.h"
+
+#include <cctype>
+#include <cmath>
+#include <cstring>
+
+#include "LMP_Lepton.h"
+
+using namespace LAMMPS_NS;
+
+/* ---------------------------------------------------------------------- */
+
+BondLepton::BondLepton(LAMMPS *_lmp) : Bond(_lmp), r0(nullptr), type2expression(nullptr)
+{
+  writedata = 1;
+  reinitflag = 0;
+}
+
+/* ---------------------------------------------------------------------- */
+
+BondLepton::~BondLepton()
+{
+  if (allocated) {
+    memory->destroy(setflag);
+    memory->destroy(r0);
+    memory->destroy(type2expression);
+  }
+}
+
+/* ---------------------------------------------------------------------- */
+
+void BondLepton::compute(int eflag, int vflag)
+{
+  ev_init(eflag, vflag);
+  ev_init(eflag, vflag);
+  if (evflag) {
+    if (eflag) {
+      if (force->newton_bond)
+        eval<1, 1, 1>();
+      else
+        eval<1, 1, 0>();
+    } else {
+      if (force->newton_bond)
+        eval<1, 0, 1>();
+      else
+        eval<1, 0, 0>();
+    }
+  } else {
+    if (force->newton_bond)
+      eval<0, 0, 1>();
+    else
+      eval<0, 0, 0>();
+  }
+}
+
+/* ---------------------------------------------------------------------- */
+template <int EVFLAG, int EFLAG, int NEWTON_BOND> void BondLepton::eval()
+{
+  std::vector<LMP_Lepton::CompiledExpression> bondforce;
+  std::vector<LMP_Lepton::CompiledExpression> bondpot;
+  for (const auto &expr : expressions) {
+    auto parsed = LMP_Lepton::Parser::parse(expr);
+    bondforce.emplace_back(parsed.differentiate("r").createCompiledExpression());
+    if (EFLAG) bondpot.emplace_back(parsed.createCompiledExpression());
+  }
+
+  const double *const *const x = atom->x;
+  double *const *const f = atom->f;
+  const int *const *const bondlist = neighbor->bondlist;
+  const int nbondlist = neighbor->nbondlist;
+  const int nlocal = atom->nlocal;
+
+  for (int n = 0; n < nbondlist; n++) {
+    const int i1 = bondlist[n][0];
+    const int i2 = bondlist[n][1];
+    const int type = bondlist[n][2];
+
+    const double delx = x[i1][0] - x[i2][0];
+    const double dely = x[i1][1] - x[i2][1];
+    const double delz = x[i1][2] - x[i2][2];
+
+    const double rsq = delx * delx + dely * dely + delz * delz;
+    const double r = sqrt(rsq);
+    const double dr = r - r0[type];
+    const int idx = type2expression[type];
+
+    // force and energy
+
+    double fbond = 0.0;
+    if (r > 0.0) {
+      double &r_for = bondforce[idx].getVariableReference("r");
+      r_for = dr;
+      fbond = -bondforce[idx].evaluate() / r;
+    }
+
+    // apply force to each of 2 atoms
+
+    if (NEWTON_BOND || i1 < nlocal) {
+      f[i1][0] += delx * fbond;
+      f[i1][1] += dely * fbond;
+      f[i1][2] += delz * fbond;
+    }
+
+    if (NEWTON_BOND || i2 < nlocal) {
+      f[i2][0] -= delx * fbond;
+      f[i2][1] -= dely * fbond;
+      f[i2][2] -= delz * fbond;
+    }
+
+    double ebond = 0.0;
+    if (EFLAG) {
+      double &r_pot = bondpot[idx].getVariableReference("r");
+      r_pot = dr;
+      ebond = bondpot[idx].evaluate();
+    }
+    if (EVFLAG) ev_tally(i1, i2, nlocal, NEWTON_BOND, ebond, fbond, delx, dely, delz);
+  }
+}
+
+/* ---------------------------------------------------------------------- */
+
+void BondLepton::allocate()
+{
+  allocated = 1;
+  const int np1 = atom->nbondtypes + 1;
+
+  memory->create(r0, np1, "bond:r0");
+  memory->create(type2expression, np1, "bond:type2expression");
+  memory->create(setflag, np1, "bond:setflag");
+  for (int i = 1; i < np1; i++) setflag[i] = 0;
+}
+
+/* ----------------------------------------------------------------------
+   set coeffs for one type
+------------------------------------------------------------------------- */
+
+void BondLepton::coeff(int narg, char **arg)
+{
+  if (narg != 3) error->all(FLERR, "Incorrect number of args for bond coefficients");
+  if (!allocated) allocate();
+
+  int ilo, ihi;
+  utils::bounds(FLERR, arg[0], 1, atom->nbondtypes, ilo, ihi, error);
+
+  double r0_one = utils::numeric(FLERR, arg[1], false, lmp);
+
+  // remove whitespace and quotes from expression string and then
+  // check if the expression can be parsed and evaluated without error
+  std::string exp_one;
+  for (const auto &c : std::string(arg[2]))
+    if (!isspace(c) && (c != '"') && (c != '\'')) exp_one.push_back(c);
+
+  try {
+    auto parsed = LMP_Lepton::Parser::parse(exp_one);
+    auto bondpot = parsed.createCompiledExpression();
+    auto bondforce = parsed.differentiate("r").createCompiledExpression();
+    double &r_pot = bondpot.getVariableReference("r");
+    double &r_for = bondforce.getVariableReference("r");
+    r_for = r_pot = 1.0;
+    bondpot.evaluate();
+    bondforce.evaluate();
+  } catch (std::exception &e) {
+    error->all(FLERR, e.what());
+  }
+
+  std::size_t idx = 0;
+  for (const auto &exp : expressions) {
+    if (exp == exp_one) break;
+    ++idx;
+  }
+
+  // not found, add to list
+  if ((expressions.size() == 0) || (idx == expressions.size())) expressions.push_back(exp_one);
+
+  int count = 0;
+  for (int i = ilo; i <= ihi; i++) {
+    r0[i] = r0_one;
+    type2expression[i] = idx;
+    setflag[i] = 1;
+    count++;
+  }
+
+  if (count == 0) error->all(FLERR, "Incorrect args for bond coefficients");
+}
+
+/* ----------------------------------------------------------------------
+   return an equilbrium bond length
+------------------------------------------------------------------------- */
+
+double BondLepton::equilibrium_distance(int i)
+{
+  return r0[i];
+}
+
+/* ----------------------------------------------------------------------
+   proc 0 writes to restart file
+------------------------------------------------------------------------- */
+
+void BondLepton::write_restart(FILE *fp)
+{
+  fwrite(&r0[1], sizeof(double), atom->nbondtypes, fp);
+  fwrite(&type2expression[1], sizeof(int), atom->nbondtypes, fp);
+
+  int num = expressions.size();
+  int maxlen = 0;
+  for (const auto &exp : expressions) maxlen = MAX(maxlen, (int) exp.size());
+  ++maxlen;
+
+  fwrite(&num, sizeof(int), 1, fp);
+  fwrite(&maxlen, sizeof(int), 1, fp);
+  for (const auto &exp : expressions) {
+    int n = exp.size() + 1;
+    fwrite(&n, sizeof(int), 1, fp);
+    fwrite(exp.c_str(), sizeof(char), n, fp);
+  }
+}
+
+/* ----------------------------------------------------------------------
+   proc 0 reads from restart file, bcasts
+------------------------------------------------------------------------- */
+
+void BondLepton::read_restart(FILE *fp)
+{
+  allocate();
+
+  if (comm->me == 0) {
+    utils::sfread(FLERR, &r0[1], sizeof(double), atom->nbondtypes, fp, nullptr, error);
+    utils::sfread(FLERR, &type2expression[1], sizeof(int), atom->nbondtypes, fp, nullptr, error);
+  }
+  MPI_Bcast(&r0[1], atom->nbondtypes, MPI_DOUBLE, 0, world);
+  MPI_Bcast(&type2expression[1], atom->nbondtypes, MPI_INT, 0, world);
+  for (int i = 1; i <= atom->nbondtypes; i++) setflag[i] = 1;
+
+  int num, maxlen, len;
+  if (comm->me == 0) {
+    utils::sfread(FLERR, &num, sizeof(int), 1, fp, nullptr, error);
+    utils::sfread(FLERR, &maxlen, sizeof(int), 1, fp, nullptr, error);
+  }
+  MPI_Bcast(&num, 1, MPI_INT, 0, world);
+  MPI_Bcast(&maxlen, 1, MPI_INT, 0, world);
+
+  char *buf = new char[maxlen];
+
+  for (int i = 0; i < num; ++i) {
+    if (comm->me == 0) {
+      utils::sfread(FLERR, &len, sizeof(int), 1, fp, nullptr, error);
+      utils::sfread(FLERR, buf, sizeof(char), len, fp, nullptr, error);
+    }
+    MPI_Bcast(buf, maxlen, MPI_CHAR, 0, world);
+    expressions.push_back(buf);
+  }
+
+  delete[] buf;
+}
+
+/* ----------------------------------------------------------------------
+   proc 0 writes to data file
+------------------------------------------------------------------------- */
+
+void BondLepton::write_data(FILE *fp)
+{
+  for (int i = 1; i <= atom->nbondtypes; i++)
+    fprintf(fp, "%d %g %s\n", i, r0[i], expressions[type2expression[i]].c_str());
+}
+
+/* ---------------------------------------------------------------------- */
+
+double BondLepton::single(int type, double rsq, int /*i*/, int /*j*/, double &fforce)
+{
+  const double r = sqrt(rsq);
+  const double dr = r - r0[type];
+
+  auto parsed = LMP_Lepton::Parser::parse(expressions[type2expression[type]]);
+  auto bondpot = parsed.createCompiledExpression();
+  auto bondforce = parsed.differentiate("r").createCompiledExpression();
+  double &r_for = bondforce.getVariableReference("r");
+  double &r_pot = bondpot.getVariableReference("r");
+  r_for = r_pot = dr;
+
+  // force and energy
+
+  fforce = 0.0;
+  double ebond = 0.0;
+  if (r > 0.0) {
+    fforce = -bondforce.evaluate() / r;
+    ebond = bondpot.evaluate();
+  }
+  return ebond;
+}
+
+/* ---------------------------------------------------------------------- */
+
+void *BondLepton::extract(const char *str, int &dim)
+{
+  dim = 1;
+  if (strcmp(str, "r0") == 0) return (void *) r0;
+  return nullptr;
+}
---
+++ src/OPENMP/bond_lepton_omp.cpp
@@ -0,0 +1,148 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   https://www.lammps.org/, Sandia National Laboratories
+   LAMMPS development team: developers@lammps.org
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+/* ----------------------------------------------------------------------
+   Contributing author: Axel Kohlmeyer (Temple U)
+------------------------------------------------------------------------- */
+
+#include "bond_lepton_omp.h"
+#include "atom.h"
+#include "comm.h"
+#include "force.h"
+#include "neighbor.h"
+#include "omp_compat.h"
+
+#include <cmath>
+
+#include "LMP_Lepton.h"
+#include "suffix.h"
+using namespace LAMMPS_NS;
+
+/* ---------------------------------------------------------------------- */
+
+BondLeptonOMP::BondLeptonOMP(class LAMMPS *lmp) : BondLepton(lmp), ThrOMP(lmp, THR_BOND)
+{
+  suffix_flag |= Suffix::OMP;
+}
+
+/* ---------------------------------------------------------------------- */
+
+void BondLeptonOMP::compute(int eflag, int vflag)
+{
+  ev_init(eflag, vflag);
+
+  const int nall = atom->nlocal + atom->nghost;
+  const int nthreads = comm->nthreads;
+  const int inum = neighbor->nbondlist;
+
+#if defined(_OPENMP)
+#pragma omp parallel LMP_DEFAULT_NONE LMP_SHARED(eflag, vflag)
+#endif
+  {
+    int ifrom, ito, tid;
+
+    loop_setup_thr(ifrom, ito, tid, inum, nthreads);
+    ThrData *thr = fix->get_thr(tid);
+    thr->timer(Timer::START);
+    ev_setup_thr(eflag, vflag, nall, eatom, vatom, nullptr, thr);
+
+    if (inum > 0) {
+      if (evflag) {
+        if (eflag) {
+          if (force->newton_bond)
+            eval<1, 1, 1>(ifrom, ito, thr);
+          else
+            eval<1, 1, 0>(ifrom, ito, thr);
+        } else {
+          if (force->newton_bond)
+            eval<1, 0, 1>(ifrom, ito, thr);
+          else
+            eval<1, 0, 0>(ifrom, ito, thr);
+        }
+      } else {
+        if (force->newton_bond)
+          eval<0, 0, 1>(ifrom, ito, thr);
+        else
+          eval<0, 0, 0>(ifrom, ito, thr);
+      }
+    }
+    thr->timer(Timer::BOND);
+    reduce_thr(this, eflag, vflag, thr);
+  }    // end of omp parallel region
+}
+
+/* ---------------------------------------------------------------------- */
+
+template <int EVFLAG, int EFLAG, int NEWTON_BOND>
+void BondLeptonOMP::eval(int nfrom, int nto, ThrData *const thr)
+{
+  std::vector<LMP_Lepton::CompiledExpression> bondforce;
+  std::vector<LMP_Lepton::CompiledExpression> bondpot;
+  for (const auto &expr : expressions) {
+    auto parsed = LMP_Lepton::Parser::parse(expr);
+    bondforce.emplace_back(parsed.differentiate("r").createCompiledExpression());
+    if (EFLAG) bondpot.emplace_back(parsed.createCompiledExpression());
+  }
+
+  const auto *_noalias const x = (dbl3_t *) atom->x[0];
+  auto *_noalias const f = (dbl3_t *) thr->get_f()[0];
+  const int3_t *_noalias const bondlist = (int3_t *) neighbor->bondlist[0];
+  const int nlocal = atom->nlocal;
+
+  for (int n = nfrom; n < nto; n++) {
+    const int i1 = bondlist[n].a;
+    const int i2 = bondlist[n].b;
+    const int type = bondlist[n].t;
+
+    const double delx = x[i1].x - x[i2].x;
+    const double dely = x[i1].y - x[i2].y;
+    const double delz = x[i1].z - x[i2].z;
+
+    const double rsq = delx * delx + dely * dely + delz * delz;
+    const double r = sqrt(rsq);
+    const double dr = r - r0[type];
+    const int idx = type2expression[type];
+
+    // force and energy
+
+    double fbond = 0.0;
+    if (r > 0.0) {
+      double &r_for = bondforce[idx].getVariableReference("r");
+      r_for = dr;
+      fbond = -bondforce[idx].evaluate() / r;
+    }
+
+    // apply force to each of 2 atoms
+
+    if (NEWTON_BOND || i1 < nlocal) {
+      f[i1].x += delx * fbond;
+      f[i1].y += dely * fbond;
+      f[i1].z += delz * fbond;
+    }
+
+    if (NEWTON_BOND || i2 < nlocal) {
+      f[i2].x -= delx * fbond;
+      f[i2].y -= dely * fbond;
+      f[i2].z -= delz * fbond;
+    }
+
+    double ebond = 0.0;
+    if (EFLAG) {
+      double &r_pot = bondpot[idx].getVariableReference("r");
+      r_pot = dr;
+      ebond = bondpot[idx].evaluate();
+    }
+    if (EVFLAG)
+      ev_tally_thr(this, i1, i2, nlocal, NEWTON_BOND, ebond, fbond, delx, dely, delz, thr);
+  }
+}

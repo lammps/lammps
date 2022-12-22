91c498c4130109c5c5b9b179e1710da5f616d4e1
Axel Kohlmeyer <akohlmey@gmail.com>
2022-12-22 16:32:17 -0500

    suppres explicit exports/import in Lepton lib

--- cmake/Modules/Packages/LEPTON.cmake
+++ cmake/Modules/Packages/LEPTON.cmake
@@ -2,7 +2,7 @@ set(LEPTON_SOURCE_DIR ${LAMMPS_LIB_SOURCE_DIR}/lepton)
 
 file(GLOB LEPTON_SOURCES ${LEPTON_SOURCE_DIR}/src/[^.]*.cpp)
 add_library(lmplepton STATIC ${LEPTON_SOURCES})
-target_compile_definitions(lmplepton PRIVATE -DLEPTON_BUILDING_STATIC_LIBRARY)
+target_compile_definitions(lmplepton PUBLIC -DLEPTON_BUILDING_STATIC_LIBRARY=1)
 set_target_properties(lmplepton PROPERTIES OUTPUT_NAME lammps_lmplepton${LAMMPS_MACHINE})
 target_include_directories(lmplepton PUBLIC ${LEPTON_SOURCE_DIR}/include)
 target_link_libraries(lammps PRIVATE lmplepton)

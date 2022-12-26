32347792ad2933c202eaf5565e8d52a971056f6c
Axel Kohlmeyer <akohlmey@gmail.com>
2022-12-26 16:46:59 -0500

    follow the usual convention and call dihedral angle phi

--- tools/tabulate/dihedral_harmonic_tabulate.py
+++ tools/tabulate/dihedral_harmonic_tabulate.py
@@ -5,11 +5,11 @@ from tabulate import DihedralTabulate
 ################################################################################
 import math
 
-def harmonic_energy(theta):
+def harmonic_energy(phi):
     k = 100.0
     # the force constant in LAMMPS is in energy per radians^2 so convert from degrees to radians
     deg2rad = math.pi / 180.0
-    f = k * (1 - math.cos(2.0 * deg2rad * theta))
+    f = k * (1 - math.cos(2.0 * deg2rad * phi))
     return f
 
 ################################################################################

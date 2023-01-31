f8859c5fcac0a5a8bfdff346d2dfa42f4d734d56
Axel Kohlmeyer <akohlmey@gmail.com>
2023-01-31 16:35:06 -0500

    implement download fallback URLs pointing to download.lammps.org for CMake

--- cmake/Modules/Packages/KOKKOS.cmake
+++ cmake/Modules/Packages/KOKKOS.cmake
@@ -53,8 +53,10 @@ if(DOWNLOAD_KOKKOS)
   set(KOKKOS_MD5 "0ec97fc0c356dd65bd2487defe81a7bf" CACHE STRING "MD5 checksum of KOKKOS tarball")
   mark_as_advanced(KOKKOS_URL)
   mark_as_advanced(KOKKOS_MD5)
+  GetFallbackURL(KOKKOS_URL KOKKOS_FALLBACK)
+
   ExternalProject_Add(kokkos_build
-    URL     ${KOKKOS_URL}
+    URL     ${KOKKOS_URL} ${KOKKOS_FALLBACK}
     URL_MD5 ${KOKKOS_MD5}
     CMAKE_ARGS ${KOKKOS_LIB_BUILD_ARGS}
     BUILD_BYPRODUCTS <INSTALL_DIR>/lib/libkokkoscore.a <INSTALL_DIR>/lib/libkokkoscontainers.a

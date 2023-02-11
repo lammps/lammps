188ee5af1555f94661f59cb90600861c045b1073
Axel Kohlmeyer <akohlmey@gmail.com>
2023-02-11 18:59:52 -0500

    use MAXBIGINT_DOUBLE which does not overflow when casting back to bigint

--- src/variable.cpp
+++ src/variable.cpp
@@ -2689,8 +2689,8 @@ double Variable::collapse_tree(Tree *tree)
     else if (update->ntimestep < ivalue2) {
       bigint offset = update->ntimestep - ivalue1;
       tree->value = ivalue1 + (offset/ivalue3)*ivalue3 + ivalue3;
-      if (tree->value > ivalue2) tree->value = (double) MAXBIGINT;
-    } else tree->value = (double) MAXBIGINT;
+      if (tree->value > ivalue2) tree->value = (double) MAXBIGINT_DOUBLE;
+    } else tree->value = (double) MAXBIGINT_DOUBLE;
     return tree->value;
   }
 
@@ -2726,11 +2726,11 @@ double Variable::collapse_tree(Tree *tree)
         if (istep > ivalue5) {
           offset = ivalue5 - ivalue1;
           istep = ivalue1 + (offset/ivalue3)*ivalue3 + ivalue3;
-          if (istep > ivalue2) istep = MAXBIGINT;
+          if (istep > ivalue2) istep = MAXBIGINT_DOUBLE;
         }
       }
-    } else istep = MAXBIGINT;
-    tree->value = istep;
+    } else istep = MAXBIGINT_DOUBLE;
+    tree->value = (double)istep;
     return tree->value;
   }
 
@@ -3028,8 +3028,8 @@ double Variable::eval_tree(Tree *tree, int i)
     else if (update->ntimestep < ivalue2) {
       bigint offset = update->ntimestep - ivalue1;
       arg = ivalue1 + (offset/ivalue3)*ivalue3 + ivalue3;
-      if (arg > ivalue2) arg = (double) MAXBIGINT;
-    } else arg = (double) MAXBIGINT;
+      if (arg > ivalue2) arg = (double) MAXBIGINT_DOUBLE;
+    } else arg = (double) MAXBIGINT_DOUBLE;
     return arg;
   }
 
@@ -3060,10 +3060,10 @@ double Variable::eval_tree(Tree *tree, int i)
         if (istep > ivalue5) {
           offset = ivalue5 - ivalue1;
           istep = ivalue1 + (offset/ivalue3)*ivalue3 + ivalue3;
-          if (istep > ivalue2) istep = MAXBIGINT;
+          if (istep > ivalue2) istep = MAXBIGINT_DOUBLE;
         }
       }
-    } else istep = MAXBIGINT;
+    } else istep = MAXBIGINT_DOUBLE;
     arg = istep;
     return arg;
   }
@@ -3595,8 +3595,8 @@ int Variable::math_function(char *word, char *contents, Tree **tree, Tree **tree
       else if (update->ntimestep < ivalue2) {
         bigint offset = update->ntimestep - ivalue1;
         value = ivalue1 + (offset/ivalue3)*ivalue3 + ivalue3;
-        if (value > ivalue2) value = (double) MAXBIGINT;
-      } else value = (double) MAXBIGINT;
+        if (value > ivalue2) value = (double) MAXBIGINT_DOUBLE;
+      } else value = (double) MAXBIGINT_DOUBLE;
       argstack[nargstack++] = value;
     }
 
@@ -3630,10 +3630,10 @@ int Variable::math_function(char *word, char *contents, Tree **tree, Tree **tree
           if (istep > ivalue5) {
             offset = ivalue5 - ivalue1;
             istep = ivalue1 + (offset/ivalue3)*ivalue3 + ivalue3;
-            if (istep > ivalue2) istep = MAXBIGINT;
+            if (istep > ivalue2) istep = MAXBIGINT_DOUBLE;
           }
         }
-      } else istep = MAXBIGINT;
+      } else istep = MAXBIGINT_DOUBLE;
       double value = istep;
       argstack[nargstack++] = value;
     }

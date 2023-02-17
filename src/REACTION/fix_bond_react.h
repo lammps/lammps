9c73f3212190d2c42e19d06310be5812ac4a7dc0
Jacob Gissinger <jrgiss05@gmail.com>
2023-02-17 13:01:47 -0500

    increase MAXLINE to match other parts of lammps
    'custom' constraint could exceed 256 chars fairly easily

--- src/REACTION/fix_bond_react.h
+++ src/REACTION/fix_bond_react.h
@@ -33,7 +33,7 @@ namespace LAMMPS_NS {
 
 class FixBondReact : public Fix {
  public:
-  enum { MAXLINE = 256 };    // max length of line read from files
+  enum { MAXLINE = 1024 };    // max length of line read from files
   enum { MAXCONIDS = 4 };    // max # of IDs used by any constraint
   enum { MAXCONPAR = 5 };    // max # of constraint parameters
 

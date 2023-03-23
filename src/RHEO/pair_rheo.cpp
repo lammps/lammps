886c642e0155e97bf51d81475978ddcf74526a87
jtclemm <jtclemm@sandia.gov>
2023-03-22 21:15:54 -0600

    Cleaning up pairstyle

--- src/RHEO/pair_rheo.cpp
+++ src/RHEO/pair_rheo.cpp
@@ -22,6 +22,7 @@
 #include "error.h"
 #include "fix_rheo.h"
 #include "force.h"
+#include "math_extra.h"
 #include "memory.h"
 #include "modify.h"
 #include "neighbor.h"
@@ -32,6 +33,7 @@
 #include <cmath>
 
 using namespace LAMMPS_NS;
+using namespace MathExtra;
 
 #define EPSILON 1e-2
 
@@ -64,28 +66,15 @@ PairRHEO::~PairRHEO()
 void PairRHEO::compute(int eflag, int vflag)
 {
   int i, j, a, b, ii, jj, inum, jnum, itype, jtype;
-  int error_flag, pair_force_flag, pair_rho_flag, pair_avisc_flag;
-  double xtmp, ytmp, ztmp;
+  int pair_force_flag, pair_rho_flag, pair_avisc_flag;
+  double xtmp, ytmp, ztmp, w, wp, Ti, Tj, dT;
+  double rhoi, rhoj, voli, volj, Pi, Pj, etai, etaj, kappai, kappaj;
+  double mu, q, fp_prefactor, drho_damp, fmag, psi_ij, Fij;
+  double *dWij, *dWji, *dW1ij, *dW1ji;
+  double dx[3], du[3], fv[3], dfp[3], fsolid[3], ft[3], vi[3], vj[3];
 
   int *ilist, *jlist, *numneigh, **firstneigh;
-  double imass, jmass, rsq, r, ir;
-
-  double w, wp, rhoi, rhoj, voli, volj, Pi, Pj;
-  double *dWij, *dWji, *d2Wij, *d2Wji, *dW1ij, *dW1ji;
-  double vijeij, etai, etaj, kappai, kappaj;
-  double Ti, Tj, dT;
-  double drho_damp, fmag;
-  double mu, q, cs, fp_prefactor;
-  double dx[3] = {0};
-  double fv[3] = {0};
-  double dfp[3] = {0};
-  double fsolid[3] = {0};
-  double du[3] = {0};
-  double vi[3] = {0};
-  double vj[3] = {0};
-  double dv[3] = {0};
-  double psi_ij = 0.0;
-  double Fij = 0.0;
+  double imass, jmass, rsq, r, rinv;
 
   ev_init(eflag, vflag);
 
@@ -95,18 +84,17 @@ void PairRHEO::compute(int eflag, int vflag)
   double **v = atom->v;
   double **x = atom->x;
   double **f = atom->f;
-  double **fp = atom->fp;
+  double **fp = atom->fp; // rewrite later
+  double *pressure = atom->pressure; // rewrite later
   double *rho = atom->rho;
   double *mass = atom->mass;
   double *drho = atom->drho;
   double *temp = atom->temp;
   double *heat = atom->heat;
-  double *viscosity = atom->viscosity;
-  double *conductivity = atom->conductivity;
   double *special_lj = force->special_lj;
   tagint *tag = atom->tag;
   int *type = atom->type;
-  int *phase = atom->phase;
+  int *status = atom->status;
 
   int tmp1, tmp2;
   int index_visc = atom->find_custom("rheo_viscosity", tmp1, tmp2);
@@ -140,10 +128,11 @@ void PairRHEO::compute(int eflag, int vflag)
     jlist = firstneigh[i];
     jnum = numneigh[i];
     imass = mass[itype];
-
-    kappai = conductivity[i];
     etai = viscosity[i];
-    Ti = temp[i];
+    if (thermal_flag) {
+      kappai = conductivity[i];
+      Ti = temp[i];
+    }
 
     for (jj = 0; jj < jnum; jj++) {
       j = jlist[jj];
@@ -153,25 +142,27 @@ void PairRHEO::compute(int eflag, int vflag)
       dx[0] = xtmp - x[j][0];
       dx[1] = ytmp - x[j][1];
       dx[2] = ztmp - x[j][2];
-      rsq = dx[0]*dx[0] + dx[1]*dx[1] + dx[2]*dx[2];
+      rsq = lensq3(dx);
       jtype = type[j];
-      jmass = mass[jtype];
 
       if (rsq < cutsq[itype][jtype]) {
         r = sqrt(rsq);
-        ir = 1/r;
+        rinv = 1 / r;
 
-        kappaj = conductivity[j];
+        jmass = mass[jtype];
         etaj = viscosity[j];
-        Tj = temp[j];
+        if (thermal_flag) {
+          Tj = temp[j];
+          kappaj = conductivity[j];
+        }
 
         pair_rho_flag = 0;
         pair_force_flag = 0;
         pair_avisc_flag = 0;
-        if (phase[i] <= FixRHEO::FLUID_MAX || phase[j] <= FixRHEO::FLUID_MAX) {
+        if (status[i] <= FixRHEO::FLUID_MAX || status[j] <= FixRHEO::FLUID_MAX) {
           pair_force_flag = 1;
         }
-        if (phase[i] <= FixRHEO::FLUID_MAX && phase[j] <= FixRHEO::FLUID_MAX) {
+        if (status[i] <= FixRHEO::FLUID_MAX && status[j] <= FixRHEO::FLUID_MAX) {
           pair_avisc_flag = 1;
           pair_rho_flag = 1;
         }
@@ -189,31 +180,31 @@ void PairRHEO::compute(int eflag, int vflag)
         // Add corrections for walls
         rhoi = rho[i];
         rhoj = rho[j];
-        if (phase[i] <= FixRHEO::FLUID_MAX && phase[j] > FixRHEO::FLUID_MAX) {
-          compute_sinterpolation->correct_v(v[i], v[j], vi, i, j);
-          rhoj = compute_sinterpolation->correct_rho(j,i);
+        Pi = pressure[i];
+        Pj = pressure[j];
+        if ((status[i] & STATUS_FLUID) && !(status[j] & STATUS_FLUID)) {
+          compute_interface->correct_v(vi, vj, i, j);
+          rhoj = compute_interface->correct_rho(j, i);
+          Pj = calc_pressure(rhoj, jtype);
 
           // Repel if close to inner solid particle
-          if (compute_sinterpolation->chi[j] > 0.9 && r < (cut[itype][jtype] * 0.5)) {
-            fmag = (compute_sinterpolation->chi[j] - 0.9) * (cut[itype][jtype] * 0.5 - r);
-            fmag *= rho0[itype] * csq[itype] * cut[itype][jtype] * ir;
-            fsolid[0] = fmag * dx[0];
-            fsolid[1] = fmag * dx[1];
-            fsolid[2] = fmag * dx[2];
+          if (compute_interface->chi[j] > 0.9 && r < (h * 0.5)) {
+            fmag = (compute_interface->chi[j] - 0.9) * (h * 0.5 - r);
+            fmag *= rho0 * csq * h * ir;
+            scale3(fmag, dx, fsolid);
           }
-        } else if (phase[i] > FixRHEO::FLUID_MAX && phase[j] <= FixRHEO::FLUID_MAX) {
-          compute_sinterpolation->correct_v(v[j], v[i], vj, j, i);
-          rhoi = compute_sinterpolation->correct_rho(i,j);
+        } else if (!(status[i] & STATUS_FLUID) && (status[j] & STATUS_FLUID)) {
+          compute_interface->correct_v(vj, vi, j, i);
+          rhoi = compute_interface->correct_rho(i, j);
+          Pi = calc_pressure(rhoi, itype);
 
           // Repel if close to inner solid particle
-          if (compute_sinterpolation->chi[i] > 0.9 && r < (cut[itype][jtype] * 0.5)) {
-            fmag = (compute_sinterpolation->chi[i] - 0.9) * (cut[itype][jtype] * 0.5 - r);
-            fmag *= rho0[jtype] * csq[jtype] * cut[itype][jtype] * ir;
-            fsolid[0] = fmag * dx[0];
-            fsolid[1] = fmag * dx[1];
-            fsolid[2] = fmag * dx[2];
+          if (compute_interface->chi[i] > 0.9 && r < (h * 0.5)) {
+            fmag = (compute_interface->chi[i] - 0.9) * (h * 0.5 - r);
+            fmag *= rho0 * csq * h * ir;
+            scale3(fmag, dx, fsolid);
           }
-        } else if (phase[i] > FixRHEO::FLUID_MAX && phase[j] > FixRHEO::FLUID_MAX) {
+        } else if (!(status[i] & STATUS_FLUID) && !(status[j] & STATUS_FLUID)) {
           rhoi = 1.0;
           rhoj = 1.0;
         }
@@ -221,26 +212,19 @@ void PairRHEO::compute(int eflag, int vflag)
         // Compute volume and pressure after reconstructing
         voli = imass / rhoi;
         volj = jmass / rhoj;
-        Pj = calc_pressure(rhoj, jtype);
-        Pi = calc_pressure(rhoi, itype);
-
-        //Check if Second order kernels will be used for eta*Lap(v)
-        error_flag = 0;
-        if (laplacian_order == 2) {
-          error_flag = compute_kernel->calc_d2w(i, j, dx[0], dx[1], dx[2], r);
-          d2Wij = compute_kernel->d2Wij;
-          d2Wji = compute_kernel->d2Wji;
-        }
 
         //Thermal Evolution
         if (thermal_flag) {
-          dT = 0.0;
-          for (a = 0; a < dim; a++) {
-            dT += (kappai + kappaj) * (Ti-Tj) * dx[a] * dWij[a] * ir * ir;
-            //Assumes heat capacity and density = 1, needs to be generalized
-          }
-          dT *= voli * volj;
+          dT = dot3(dx, dWij);
+          dT *= (kappai + kappaj) * (Ti - Tj) * rinv * rinv * voli * volj;
+          //Assumes heat capacity and density = 1, needs to be generalized
           heat[i] += dT;
+
+          if (newton_pair || j < nlocal) {
+            dT = dot3(dx, dWji);
+            dT *= (kappai + kappaj) * (Tj - Ti) * rinv * rinv * voli * volj;
+            heat[j] -= dT;
+          }
         }
 
         // If either particle is fluid, compute hydrostatic and viscous forces
@@ -248,132 +232,93 @@ void PairRHEO::compute(int eflag, int vflag)
         if (pair_force_flag) {
           //Hydrostatic pressure forces
           fp_prefactor = voli * volj * (Pj + Pi);
+          sub3(v1, vj, du);
 
           //Add artificial viscous pressure if required
           if (artificial_visc_flag && pair_avisc_flag){
             //Interpolate velocities to midpoint and use this difference for artificial viscosity
-            for (a = 0; a < dim; a++) {
-              du[a] = vi[a] - vj[a];
-              for (b = 0; b < dim; b++) {
+            for (a = 0; a < dim; a++)
+              for (b = 0; b < dim; b++)
                 du[a] -= 0.5 * (gradv[i][a * dim + b] + gradv[j][a * dim + b]) * dx[b];
-              }
-            }
-            mu = (du[0] * dx[0] + du[1] * dx[1]+ du[2] * dx[2]) * hinv3;
+
+            mu = dot3(du, dx) * hinv3;
             mu = mu / (rsq * hinv3 * hinv3 + EPSILON);
-            mu= MIN(0.0, mu);
-            cs = 0.5 * (sqrt(csq[itype]) + sqrt(csq[jtype]));
-            // "kinematic viscous pressure"  q = Q/rho
-            q = av*(-2.0*cs*mu + 1.0*mu*mu);
-            fp_prefactor += voli*volj*q*(rhoj + rhoi);
+            mu = MIN(0.0, mu);
+            q = av * (-2.0 * cs * mu + mu * mu);
+            fp_prefactor += voli * volj * q * (rhoj + rhoi);
           }
 
           // -Grad[P + Q]
-          dfp[0] = - fp_prefactor*dWij[0];
-          dfp[1] = - fp_prefactor*dWij[1];
-          dfp[2] = - fp_prefactor*dWij[2];
+          scale3(-fp_prefactor, dWij, dfp);
 
           // Now compute viscous eta*Lap[v] terms
-          for (a = 0; a < dim; a ++) {
+          for (a = 0; a < dim; a++) {
             fv[a] = 0.0;
-            for (b = 0; b < dim; b++) {
-              fv[a] += (etai+etaj)*(vi[a]-vj[a])*dx[b]*dWij[b]*ir*ir;
-            }
-            fv[a] *= voli*volj;
+            for (b = 0; b < dim; b++)
+              fv[a] += du[a] * dx[b] * dWij[b];
+            fv[a] *= (etai + etaj) * voli * volj * rinv * rinv;
           }
 
-        } else {
-          for (a = 0; a < dim; a ++) {
-            fv[a] = 0;
-            dfp[a] = 0;
-          }
-        }
+          add3(fv, dfp, ft);
+          add3(fsolid, ft, ft);
 
-        if (pair_force_flag) {
-          f[i][0] += fv[0] + dfp[0] + fsolid[0];
-          f[i][1] += fv[1] + dfp[1] + fsolid[1];
-          f[i][2] += fv[2] + dfp[2] + fsolid[2];
+          f[i][0] += ft[0];
+          f[i][1] += ft[1];
+          f[i][2] += ft[2];
           fp[i][0] += dfp[0];
           fp[i][1] += dfp[1];
           fp[i][2] += dfp[2];
-        }
 
-        // Density damping
-        // conventional for low-order h
-        // interpolated for RK 1 & 2  (Antuono et al, Computers & Fluids 2021)
-        if (rho_damp_flag && pair_rho_flag) {
-          if (laplacian_order>=1 && error_flag == 0){
-            psi_ij = rhoj-rhoi;
-            Fij = 0.0;
-            for (a = 0; a < dim; a++){
-              psi_ij += 0.5*(gradr[i][a]+gradr[j][a])*dx[a];
-              Fij -= dx[a]*dWij[a];
+          if (newton_pair || j < nlocal) {
+            for (a = 0; a < dim; a ++) {
+              fv[a] = 0.0;
+              for (b = 0; b < dim; b++)
+                fv[a] += (vi[a] - vj[a]) * dx[b] * dWji[b];
+              fv[a] *= -(etai + etaj) * voli * volj * rinv * rinv;
+              // flip sign here b/c -= at accummulator
             }
-            Fij *= ir*ir;
-            drho[i] += 2*rho_damp*psi_ij*Fij*volj;
-          }
-          else {
-            drho_damp = 2*rho_damp*(rhoj-rhoi)*ir*wp;
-            drho[i] -= drho_damp*volj;
-          }
-        }
 
-        if (evflag) // Doesn't account for unbalanced forces
-          ev_tally_xyz(i, j, nlocal, newton_pair, 0.0, 0.0, fv[0]+dfp[0], fv[1]+dfp[1], fv[2]+dfp[2], dx[0], dx[1], dx[2]);
+            scale3(fp_prefact,r dWji, dfp);
 
-        // Newton neighbors
-        if (newton_pair || j < nlocal) {
+            add3(fv, dfp, ft);
+            add3(fsolid, ft, ft);
 
-          if (thermal_flag) {
-            dT = 0.0;
-            for(a = 0; a < dim; a++){
-              //dT += kappai*dWji[a]*gradt[i][a];
-              //dT -= kappaj*dWji[a]*gradt[j][a];
-              dT += 1/1*(kappai+kappaj)*(Ti-Tj)*dx[a]*dWji[a]*ir*ir; //Assumes heat capacity and density = 1, needs to be generalized
-            }
-            dT *= -voli*volj;
-            heat[j] -= dT;
-          }
-
-          for (a = 0; a < dim; a ++) {
-            fv[a] = 0.0;
-            for (b = 0; b < dim; b++) {
-              //fv[a] += etai*dWji[b]*(gradv[i][a*dim+b]+gradv[i][b*dim+a]);
-              //fv[a] -= etaj*dWji[b]*(gradv[j][a*dim+b]+gradv[j][b*dim+a]);
-              fv[a] += (etai+etaj)*(vi[a]-vj[a])*dx[b]*dWji[b]*ir*ir;
-            }
-            fv[a] *= -voli*volj; // flip sign here b/c -= at accummulator
-          }
-
-
-
-          if (pair_force_flag) {
-            for (a = 0; a < dim; a++)
-              dfp[a] = fp_prefactor*dWji[a];
-          }
-
-          if (rho_damp_flag && pair_rho_flag){
-            if (laplacian_order>=1 && error_flag == 0){
-              Fij = 0.0;
-              for (a = 0; a < dim; a++){
-                Fij += dx[a]*dWji[a];
-              }
-              Fij *= ir*ir;
-              psi_ij *= -1;
-              drho[j] += 2*rho_damp*psi_ij*Fij*voli;
-            }
-            else {
-              drho_damp = 2*rho_damp*(rhoj-rhoi)*ir*wp;
-              drho[j] += drho_damp*voli;
-            }
-          }
-          if (pair_force_flag) {
-            f[j][0] -= fv[0] + dfp[0] + fsolid[0];
-            f[j][1] -= fv[1] + dfp[1] + fsolid[1];
-            f[j][2] -= fv[2] + dfp[2] + fsolid[2];
+            f[j][0] -= ft[0];
+            f[j][1] -= ft[1];
+            f[j][2] -= ft[2];
 
             fp[j][0] -= dfp[0];
             fp[j][1] -= dfp[1];
             fp[j][2] -= dfp[2];
+          }
+
+          if (evflag) // Doesn't account for unbalanced forces
+            ev_tally_xyz(i, j, nlocal, newton_pair, 0.0, 0.0, ft[0], ft[1], ft[2], dx[0], dx[1], dx[2]);
+        }
+
+        // Density damping
+        // conventional for low-order h
+        // interpolated for RK 1 & 2  (Antuono et al., Computers & Fluids 2021)
+        if (rho_damp_flag && pair_rho_flag) {
+          if (laplacian_order >= 1) {
+            psi_ij = rhoj - rhoi;
+            Fij = -rinv * rinv * dot3(dx, dWij);
+            for (a = 0; a < dim; a++)
+              psi_ij += 0.5 * (gradr[i][a] + gradr[j][a]) * dx[a];
+            drho[i] += 2 * rho_damp * psi_ij * Fij * volj;
+          } else {
+            drho_damp = 2 * rho_damp * (rhoj - rhoi) * rinv * wp;
+            drho[i] -= drho_damp * volj;
+          }
+
+          if (newton_pair || j < nlocal) {
+            if (laplacian_order >= 1) {
+              Fij = rinv * rinv * dot3(dx, dWji);
+              psi_ij *= -1;
+              drho[j] += 2 * rho_damp * psi_ij * Fij * voli;
+            } else {
+              drho[j] += drho_damp * voli;
+            }
           }
         }
       }
@@ -411,15 +356,13 @@ void PairRHEO::settings(int narg, char **arg)
   while (iarg < narg) {
     if (strcmp(arg[iarg], "rho/damp") == 0) {
       if (iarg + 1 >= narg) error->all(FLERR,"Illegal pair_style command");
-
       rho_damp_flag = 1;
       rho_damp = utils::numeric(FLERR,arg[iarg + 1],false,lmp);
       iarg++;
     } else if (strcmp(arg[iarg], "artificial/visc") == 0) {
       if (iarg + 1 >= narg) error->all(FLERR,"Illegal pair_style command");
-
       artificial_visc_flag = 1;
-      av = utils::numeric(FLERR,arg[iarg+1],false,lmp);
+      av = utils::numeric(FLERR,arg[iarg + 1],false,lmp);
       iarg++;
     } else error->all(FLERR,"Illegal pair_style command, {}", arg[iarg]);
     iarg++;
@@ -438,8 +381,8 @@ void PairRHEO::coeff(int narg, char **arg)
     allocate();
 
   int ilo, ihi, jlo, jhi;
-  utils::bounds(FLERR,arg[0],1, atom->ntypes, ilo, ihi,error);
-  utils::bounds(FLERR,arg[1],1, atom->ntypes, jlo, jhi,error);
+  utils::bounds(FLERR, arg[0], 1, atom->ntypes, ilo, ihi,error);
+  utils::bounds(FLERR, arg[1], 1, atom->ntypes, jlo, jhi,error);
 
   int count = 0;
   for (int i = ilo; i <= ihi; i++) {
@@ -472,6 +415,8 @@ void PairRHEO::setup()
   rho0 = fix_rheo->rho0;
 
   hinv = 1.0 / h;
+  hinv3 = hinv * 3.0;
+  cs = sqrt(csq);
   laplacian_order = -1;
 
   if (comm->ghost_velocity == 0)

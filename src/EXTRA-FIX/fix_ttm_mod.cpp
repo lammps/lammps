5f34f53ab745388625a4eb4974ff29b5cd306a11
Axel Kohlmeyer <akohlmey@gmail.com>
2023-05-12 15:06:34 -0400

    also use 1-based indexing when writing out the electron temperature

--- src/EXTRA-FIX/fix_ttm_mod.cpp
+++ src/EXTRA-FIX/fix_ttm_mod.cpp
@@ -658,7 +658,7 @@ void FixTTMMod::write_electron_temperatures(const std::string &filename)
       for (iz = 0; iz < nzgrid; iz++) {
         if (movsur == 1 && T_electron[ix][iy][iz] == 0.0)
           T_electron[ix][iy][iz] = electron_temperature_min;
-        fprintf(fp,"%d %d %d %20.16g\n",ix,iy,iz,T_electron[ix][iy][iz]);
+        fprintf(fp,"%d %d %d %20.16g\n",ix+1,iy+1,iz+1,T_electron[ix][iy][iz]);
       }
 
   fclose(fp);

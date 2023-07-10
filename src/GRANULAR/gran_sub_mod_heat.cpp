91b16c59452c64e7df22864c6fd3ad74442fb554
Axel Kohlmeyer <akohlmey@gmail.com>
2023-07-09 21:10:44 -0400

    make certain conductivity class member is always initialized

--- src/GRANULAR/gran_sub_mod_heat.cpp
+++ src/GRANULAR/gran_sub_mod_heat.cpp
@@ -49,6 +49,7 @@ GranSubModHeatArea::GranSubModHeatArea(GranularModel *gm, LAMMPS *lmp) : GranSub
 {
   num_coeffs = 1;
   contact_radius_flag = 1;
+  conductivity = 0.0;
 }
 
 /* ---------------------------------------------------------------------- */

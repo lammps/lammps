89fb236144902530091f279d012b90f907b93b50
Axel Kohlmeyer <akohlmey@gmail.com>
2023-07-11 18:35:16 -0400

    port bugfix for colloid test failure

--- src/OPENMP/npair_half_size_multi_newton_tri_omp.cpp
+++ src/OPENMP/npair_half_size_multi_newton_tri_omp.cpp
@@ -135,6 +135,7 @@ void NPairHalfSizeMultiNewtonTriOmp::build(NeighList *list)
 
           if (cutcollectionsq[icollection][icollection] ==
               cutcollectionsq[jcollection][jcollection]) {
+            if (j <= i) continue;
             if (j >= nlocal) {
               jtag = tag[j];
               if (itag > jtag) {

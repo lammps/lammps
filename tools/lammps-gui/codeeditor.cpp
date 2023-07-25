5d34cc624d32b2cb6e73ee4ebb865df5ed5fff29
Axel Kohlmeyer <akohlmey@gmail.com>
2023-07-25 13:06:52 -0400

    temporarily disable highlighting a line

--- tools/lammps-gui/codeeditor.cpp
+++ tools/lammps-gui/codeeditor.cpp
@@ -23,10 +23,10 @@ CodeEditor::CodeEditor(QWidget *parent) : QPlainTextEdit(parent)
 
     connect(this, &CodeEditor::blockCountChanged, this, &CodeEditor::updateLineNumberAreaWidth);
     connect(this, &CodeEditor::updateRequest, this, &CodeEditor::updateLineNumberArea);
-    connect(this, &CodeEditor::cursorPositionChanged, this, &CodeEditor::highlightCurrentLine);
+    // connect(this, &CodeEditor::cursorPositionChanged, this, &CodeEditor::highlightCurrentLine);
 
     updateLineNumberAreaWidth(0);
-    highlightCurrentLine();
+    // highlightCurrentLine();
 }
 
 int CodeEditor::lineNumberAreaWidth()

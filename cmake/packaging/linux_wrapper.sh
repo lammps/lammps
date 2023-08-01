44cec03729c573d83dab38df235c230530828a20
Axel Kohlmeyer <akohlmey@gmail.com>
2023-08-01 04:11:57 -0400

    set environment variables to find potentials and frc files

--- cmake/packaging/linux_wrapper.sh
+++ cmake/packaging/linux_wrapper.sh
@@ -6,6 +6,10 @@ EXENAME=$(basename "$0")
 
 # append to LD_LIBRARY_PATH to prefer local (newer) libs
 LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${BASEDIR}/lib
-export LD_LIBRARY_PATH
+
+# set some environment variables for LAMMPS etc.
+LAMMPS_POTENTIALS=${BASEDIR}/share/lammps/potentials
+MSI2LMP_LIBRARY=${BASEDIR}/share/lammps/frc_files
+export LD_LIBRARY_PATH LAMMPS_POTENTIALS MSI2LMP_LIBRARY
 
 exec "${BASEDIR}/bin/${EXENAME}" "$@"

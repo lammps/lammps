6c6262a6378ae54705cecf36fe30bad55f995cf9
Axel Kohlmeyer <akohlmey@gmail.com>
2023-08-23 19:03:34 -0400

    the MESONT package depends on the MOLECULE package since the last upgrade

---
+++ src/MESONT/Install.sh
@@ -0,0 +1,40 @@
+# Install/unInstall package files in LAMMPS
+# mode = 0/1/2 for uninstall/install/update
+
+mode=$1
+
+# enforce using portable C locale
+LC_ALL=C
+export LC_ALL
+
+# arg1 = file, arg2 = file it depends on
+
+action () {
+  if (test $mode = 0) then
+    rm -f ../$1
+  elif (! cmp -s $1 ../$1) then
+    if (test -z "$2" || test -e ../$2) then
+      cp $1 ..
+      if (test $mode = 2) then
+        echo "  updating src/$1"
+      fi
+    fi
+  elif (test -n "$2") then
+    if (test ! -e ../$2) then
+      rm -f ../$1
+    fi
+  fi
+}
+
+# some styles in MESONT have base classes in MOLECULE
+
+if (test $1 = 1) then
+  if (test ! -e ../bond_harmonic.cpp) then
+    echo "Must install MOLECULE package with MESONT"
+    exit 1
+  fi
+fi
+
+for file in *.cpp *.h; do
+    action ${file}
+done

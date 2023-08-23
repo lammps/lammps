3465fb26bb2f1f8ea40d6e7a9911e274e8f177f7
Eddy Barraud <eddy.barraud@outlook.fr>
2023-08-23 11:07:43 +0200

    new harm copy paste

---
+++ src/fix_wall_harmonic_returned.cpp
@@ -0,0 +1,77 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   https://www.lammps.org/, Sandia National Laboratories
+   LAMMPS development team: developers@lammps.org
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#include "fix_wall_harmonic.h"
+#include "atom.h"
+#include "error.h"
+
+using namespace LAMMPS_NS;
+
+/* ---------------------------------------------------------------------- */
+
+FixWallHarmonic::FixWallHarmonic(LAMMPS *lmp, int narg, char **arg) : FixWall(lmp, narg, arg)
+{
+  dynamic_group_allow = 1;
+}
+
+/* ----------------------------------------------------------------------
+   interaction of all particles in group with a wall
+   m = index of wall coeffs
+   which = xlo,xhi,ylo,yhi,zlo,zhi
+   error if any particle is on or behind wall
+------------------------------------------------------------------------- */
+
+void FixWallHarmonic::wall_particle(int m, int which, double coord)
+{
+  double delta, dr, fwall;
+  double vn;
+
+  double **x = atom->x;
+  double **f = atom->f;
+  int *mask = atom->mask;
+  int nlocal = atom->nlocal;
+
+  int dim = which / 2;
+  int side = which % 2;
+  if (side == 0) side = -1;
+
+  int onflag = 0;
+
+  for (int i = 0; i < nlocal; i++)
+    if (mask[i] & groupbit) {
+      if (side < 0)
+        delta = x[i][dim] - coord;
+      else
+        delta = coord - x[i][dim];
+      if (delta >= cutoff[m]) continue;
+      if (delta <= 0.0) {
+        onflag = 1;
+        continue;
+      }
+      dr = cutoff[m] - delta;
+      fwall = side * 2.0 * epsilon[m] * dr;
+      f[i][dim] -= fwall;
+      ewall[0] += epsilon[m] * dr * dr;
+      ewall[m + 1] += fwall;
+
+      if (evflag) {
+        if (side < 0)
+          vn = -fwall * delta;
+        else
+          vn = fwall * delta;
+        v_tally(dim, i, vn);
+      }
+    }
+
+  if (onflag) error->one(FLERR, "Particle on or inside fix wall surface");
+}
---
+++ src/fix_wall_harmonic_returned.h
@@ -0,0 +1,37 @@
+/* -*- c++ -*- ----------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   https://www.lammps.org/, Sandia National Laboratories
+   LAMMPS development team: developers@lammps.org
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef FIX_CLASS
+// clang-format off
+FixStyle(wall/harmonic,FixWallHarmonic);
+// clang-format on
+#else
+
+#ifndef LMP_FIX_WALL_HARMONIC_H
+#define LMP_FIX_WALL_HARMONIC_H
+
+#include "fix_wall.h"
+
+namespace LAMMPS_NS {
+
+class FixWallHarmonic : public FixWall {
+ public:
+  FixWallHarmonic(class LAMMPS *, int, char **);
+  void precompute(int) override {}
+  void wall_particle(int, int, double) override;
+};
+
+}    // namespace LAMMPS_NS
+
+#endif
+#endif

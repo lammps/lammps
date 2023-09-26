d40fb4a337c67c2f0c3bc79bf29b87353cd80ceb
Evangelos Voyiatzis <evoyiatzis@gmail.com>
2023-09-26 16:45:46 +0300

    method implementation for dihedral contribution to compute_stress_mop_profile.cpp

--- src/EXTRA-COMPUTE/compute_stress_mop_profile.cpp
+++ src/EXTRA-COMPUTE/compute_stress_mop_profile.cpp
@@ -13,7 +13,7 @@
 
 /*------------------------------------------------------------------------
   Contributing Authors : Romain Vermorel (LFCR), Laurent Joly (ULyon)
-  Support for bonds and angles added by : Evangelos Voyiatzis (NovaMechanics)
+  Support for bonds, angles and dihedrals added by : Evangelos Voyiatzis (NovaMechanics)
   --------------------------------------------------------------------------*/
 
 #include "compute_stress_mop_profile.h"
@@ -23,6 +23,7 @@
 #include "atom_vec.h"
 #include "bond.h"
 #include "comm.h"
+#include "dihedral.h"
 #include "domain.h"
 #include "error.h"
 #include "force.h"
@@ -38,8 +39,10 @@
 
 using namespace LAMMPS_NS;
 
+#define SMALL     0.001
+
 enum { X, Y, Z };
-enum { TOTAL, CONF, KIN, PAIR, BOND, ANGLE };
+enum { TOTAL, CONF, KIN, PAIR, BOND, ANGLE, DIHEDRAL };
 
 /* ---------------------------------------------------------------------- */
 
@@ -50,6 +53,7 @@ ComputeStressMopProfile::ComputeStressMopProfile(LAMMPS *lmp, int narg, char **a
 
   bondflag = 0;
   angleflag = 0;
+  dihedralflag = 0;
 
   // set compute mode and direction of plane(s) for pressure calculation
 
@@ -114,6 +118,11 @@ ComputeStressMopProfile::ComputeStressMopProfile(LAMMPS *lmp, int narg, char **a
         which[nvalues] = ANGLE;
         nvalues++;
       }
+    } else if (strcmp(arg[iarg],"dihedral") == 0) {
+      for (i=0; i<3; i++) {
+        which[nvalues] = DIHEDRAL;
+        nvalues++;
+      }
     } else
       error->all(FLERR, "Illegal compute stress/mop/profile command");    //break;
 
@@ -141,6 +150,8 @@ ComputeStressMopProfile::ComputeStressMopProfile(LAMMPS *lmp, int narg, char **a
   bond_global = nullptr;
   angle_local = nullptr;
   angle_global = nullptr;
+  dihedral_local = nullptr;
+  dihedral_global = nullptr;
   local_contribution = nullptr;
 
   // bin setup
@@ -171,6 +182,8 @@ ComputeStressMopProfile::~ComputeStressMopProfile()
   memory->destroy(bond_global);
   memory->destroy(angle_local);
   memory->destroy(angle_global);
+  memory->destroy(dihedral_local);
+  memory->destroy(dihedral_global);
   memory->destroy(local_contribution);
   memory->destroy(array);
 }
@@ -227,10 +240,16 @@ void ComputeStressMopProfile::init()
       }
     }
 
-    if (force->dihedral)
-      if ((strcmp(force->dihedral_style, "zero") != 0) &&
-          (strcmp(force->dihedral_style, "none") != 0))
-        error->all(FLERR, "compute stress/mop/profile does not account for dihedral potentials");
+    if (force->dihedral) {
+      if (force->dihedral->born_matrix_enable == 0) {
+        if ((strcmp(force->dihedral_style, "zero") != 0) &&
+            (strcmp(force->dihedral_style, "none") != 0))
+          error->all(FLERR, "compute stress/mop/profile does not account for dihedral potentials");
+      } else {
+        dihedralflag = 1;
+      }
+    }
+
     if (force->improper)
       if ((strcmp(force->improper_style, "zero") != 0) &&
           (strcmp(force->improper_style, "none") != 0))
@@ -294,6 +313,17 @@ void ComputeStressMopProfile::compute_array()
 
   // sum angle contribution over all procs
   MPI_Allreduce(&angle_local[0][0],&angle_global[0][0],nbins*nvalues,MPI_DOUBLE,MPI_SUM,world);
+
+  if (dihedralflag) {
+    //Compute dihedral contribution on separate procs
+    compute_dihedrals();
+  } else {
+    for (int m = 0; m < nbins; m++) {
+      for (int i = 0; i < nvalues; i++) {
+        dihedral_local[m][i] = 0.0;
+      }
+    }
+  }
    
   for (int ibin = 0; ibin < nbins; ibin++) {
     array[ibin][0] = coord[ibin];
@@ -301,7 +331,7 @@ void ComputeStressMopProfile::compute_array()
     int mo = 1;
     int m = 0;
     while (m < nvalues) {
-      array[ibin][m + mo] = values_global[ibin][m] + bond_global[ibin][m] + angle_global[ibin][m];
+      array[ibin][m + mo] = values_global[ibin][m] + bond_global[ibin][m] + angle_global[ibin][m] + dihedral_global[ibin][m];
       m++;
     }
   }
@@ -835,6 +865,321 @@ void ComputeStressMopProfile::compute_angles()
   }
 }
 
+/*------------------------------------------------------------------------
+  compute dihedral contribution to pressure of local proc
+  -------------------------------------------------------------------------*/
+
+void ComputeStressMopProfile::compute_dihedrals()
+{
+  int i, nd, atom1, atom2, atom3, atom4, imol, iatom;
+  tagint tagprev;
+  double vb1x, vb1y, vb1z, vb2x, vb2y, vb2z, vb3x, vb3y, vb3z;
+  double vb2xm, vb2ym, vb2zm;
+  double sb1, sb2, sb3, rb1, rb3, c0, b1mag2, b1mag, b2mag2;
+  double b2mag, b3mag2, b3mag, c2mag, ctmp, r12c1, c1mag, r12c2;
+  double s1, s2, s12, sc1, sc2, a11, a22, a33, a12, a13, a23;
+  double df[3], f1[3], f2[3], f3[3], f4[3];
+  double c, sx2, sy2, sz2, sin2;
+
+  double **x = atom->x;
+  tagint *tag = atom->tag;
+  int *num_dihedral = atom->num_dihedral;
+  tagint **dihedral_atom1 = atom->dihedral_atom1;
+  tagint **dihedral_atom2 = atom->dihedral_atom2;
+  tagint **dihedral_atom3 = atom->dihedral_atom3;
+  tagint **dihedral_atom4 = atom->dihedral_atom4;
+  int *mask = atom->mask;
+
+  int *molindex = atom->molindex;
+  int *molatom = atom->molatom;
+  Molecule **onemols = atom->avec->onemols;
+
+  int nlocal = atom->nlocal;
+  int molecular = atom->molecular;
+
+  // loop over all atoms and their dihedrals
+
+  Dihedral *dihedral = force->dihedral;
+
+  double dudih, du2dih;
+
+  double diffx[3] = {0.0, 0.0, 0.0};
+  double x_atom_1[3] = {0.0, 0.0, 0.0};
+  double x_atom_2[3] = {0.0, 0.0, 0.0};
+  double x_atom_3[3] = {0.0, 0.0, 0.0};
+  double x_atom_4[3] = {0.0, 0.0, 0.0};
+
+  // initialization
+  for (int m = 0; m < nbins; m++) {
+    for (int i = 0; i < nvalues; i++) {
+      dihedral_local[m][i] = 0.0;
+    }
+    local_contribution[m][0] = 0.0;
+    local_contribution[m][1] = 0.0;
+    local_contribution[m][2] = 0.0;
+  }
+
+  for (atom2 = 0; atom2 < nlocal; atom2++) {
+    if (!(mask[atom2] & groupbit)) continue;
+
+    if (molecular == Atom::MOLECULAR)
+      nd = num_dihedral[atom2];
+    else {
+      if (molindex[atom2] < 0) continue;
+      imol = molindex[atom2];
+      iatom = molatom[atom2];
+      nd = onemols[imol]->num_dihedral[iatom];
+    }
+
+    for (i = 0; i < nd; i++) {
+      if (molecular == 1) {
+        if (tag[atom2] != dihedral_atom2[atom2][i]) continue;
+          atom1 = atom->map(dihedral_atom1[atom2][i]);
+          atom3 = atom->map(dihedral_atom3[atom2][i]);
+          atom4 = atom->map(dihedral_atom4[atom2][i]);
+      } else {
+        if (tag[atom2] != onemols[imol]->dihedral_atom2[atom2][i]) continue;
+        tagprev = tag[atom2] - iatom - 1;
+        atom1 = atom->map(onemols[imol]->dihedral_atom1[atom2][i] + tagprev);
+        atom3 = atom->map(onemols[imol]->dihedral_atom3[atom2][i] + tagprev);
+        atom4 = atom->map(onemols[imol]->dihedral_atom4[atom2][i] + tagprev);
+      }
+
+      if (atom1 < 0 || !(mask[atom1] & groupbit)) continue;
+      if (atom3 < 0 || !(mask[atom3] & groupbit)) continue;
+      if (atom4 < 0 || !(mask[atom4] & groupbit)) continue;
+
+      for (int ibin = 0; ibin<nbins; ibin++) {
+        double pos = coord[ibin];
+
+        // minimum image of atom1 with respect to the plane of interest
+        x_atom_1[0] = x[atom1][0];
+        x_atom_1[1] = x[atom1][1];
+        x_atom_1[2] = x[atom1][2];
+        x_atom_1[dir] -= pos;
+        domain->minimum_image(x_atom_1[0], x_atom_1[1], x_atom_1[2]);
+        x_atom_1[dir] += pos;
+
+        // minimum image of atom2 with respect to atom1
+        diffx[0] = x[atom2][0] - x_atom_1[0];
+        diffx[1] = x[atom2][1] - x_atom_1[1];
+        diffx[2] = x[atom2][2] - x_atom_1[2];
+        domain->minimum_image(diffx[0], diffx[1], diffx[2]);
+        x_atom_2[0] = x_atom_1[0] + diffx[0];
+        x_atom_2[1] = x_atom_1[1] + diffx[1];
+        x_atom_2[2] = x_atom_1[2] + diffx[2];
+
+        // minimum image of atom3 with respect to atom2
+        diffx[0] = x[atom3][0] - x_atom_2[0];
+        diffx[1] = x[atom3][1] - x_atom_2[1];
+        diffx[2] = x[atom3][2] - x_atom_2[2];
+        domain->minimum_image(diffx[0], diffx[1], diffx[2]);
+        x_atom_3[0] = x_atom_2[0] + diffx[0];
+        x_atom_3[1] = x_atom_2[1] + diffx[1];
+        x_atom_3[2] = x_atom_2[2] + diffx[2];
+
+        // minimum image of atom3 with respect to atom2
+        diffx[0] = x[atom4][0] - x_atom_3[0];
+        diffx[1] = x[atom4][1] - x_atom_3[1];
+        diffx[2] = x[atom4][2] - x_atom_3[2];
+        domain->minimum_image(diffx[0], diffx[1], diffx[2]);
+        x_atom_4[0] = x_atom_3[0] + diffx[0];
+        x_atom_4[1] = x_atom_3[1] + diffx[1];
+        x_atom_4[2] = x_atom_3[2] + diffx[2];
+
+        // check if any bond vector crosses the plane of interest
+        double tau_right = (x_atom_2[dir] - pos) / (x_atom_2[dir] - x_atom_1[dir]);
+        double tau_middle = (x_atom_3[dir] - pos) / (x_atom_3[dir] - x_atom_2[dir]);
+        double tau_left = (x_atom_4[dir] - pos) / (x_atom_4[dir] - x_atom_3[dir]);
+        bool right_cross = ((tau_right >=0) && (tau_right  <= 1));
+        bool middle_cross = ((tau_middle >= 0) && (tau_middle <= 1));
+        bool left_cross = ((tau_left >=0) && (tau_left <= 1));
+
+        // no bonds crossing the plane
+        if (!right_cross && !middle_cross && !left_cross) continue;
+
+        dihedral->born_matrix(i, atom1, atom2, atom3, atom4, dudih, du2dih);
+
+        // first bond
+        vb1x = x_atom_1[0] - x_atom_2[0];
+        vb1y = x_atom_1[1] - x_atom_2[1];
+        vb1z = x_atom_1[2] - x_atom_2[2];
+
+        // second bond
+        vb2x = x_atom_3[0] - x_atom_2[0];
+        vb2y = x_atom_3[1] - x_atom_2[1];
+        vb2z = x_atom_3[2] - x_atom_2[2];
+
+        vb2xm = -vb2x;
+        vb2ym = -vb2y;
+        vb2zm = -vb2z;
+
+        // third bond
+        vb3x = x_atom_4[0] - x_atom_3[0];
+        vb3y = x_atom_4[1] - x_atom_3[1];
+        vb3z = x_atom_4[2] - x_atom_3[2];
+
+        // c0 calculation
+        sb1 = 1.0 / (vb1x*vb1x + vb1y*vb1y + vb1z*vb1z);
+        sb2 = 1.0 / (vb2x*vb2x + vb2y*vb2y + vb2z*vb2z);
+        sb3 = 1.0 / (vb3x*vb3x + vb3y*vb3y + vb3z*vb3z);
+
+        rb1 = sqrt(sb1);
+        rb3 = sqrt(sb3);
+
+        c0 = (vb1x*vb3x + vb1y*vb3y + vb1z*vb3z) * rb1*rb3;
+        // 1st and 2nd angle
+        b1mag2 = vb1x*vb1x + vb1y*vb1y + vb1z*vb1z;
+        b1mag = sqrt(b1mag2);
+        b2mag2 = vb2x*vb2x + vb2y*vb2y + vb2z*vb2z;
+        b2mag = sqrt(b2mag2);
+        b3mag2 = vb3x*vb3x + vb3y*vb3y + vb3z*vb3z;
+        b3mag = sqrt(b3mag2);
+
+        ctmp = vb1x*vb2x + vb1y*vb2y + vb1z*vb2z;
+        r12c1 = 1.0 / (b1mag*b2mag);
+        c1mag = ctmp * r12c1;
+
+        ctmp = vb2xm*vb3x + vb2ym*vb3y + vb2zm*vb3z;
+        r12c2 = 1.0 / (b2mag*b3mag);
+        c2mag = ctmp * r12c2;
+
+        // cos and sin of 2 angles and final c
+        sin2 = MAX(1.0 - c1mag*c1mag,0.0);
+        sc1 = sqrt(sin2);
+        if (sc1 < SMALL) sc1 = SMALL;
+        sc1 = 1.0/sc1;
+
+        sin2 = MAX(1.0 - c2mag*c2mag,0.0);
+        sc2 = sqrt(sin2);
+        if (sc2 < SMALL) sc2 = SMALL;
+        sc2 = 1.0/sc2;
+
+        s1 = sc1 * sc1;
+        s2 = sc2 * sc2;
+        s12 = sc1 * sc2;
+        c = (c0 + c1mag*c2mag) * s12;
+
+        // error check
+        if (c > 1.0) c = 1.0;
+        if (c < -1.0) c = -1.0;
+
+        // forces on each particle
+        double a = dudih;
+        c = c * a;
+        s12 = s12 * a;
+        a11 = c*sb1*s1;
+        a22 = -sb2 * (2.0*c0*s12 - c*(s1+s2));
+        a33 = c*sb3*s2;
+        a12 = -r12c1 * (c1mag*c*s1 + c2mag*s12);
+        a13 = -rb1*rb3*s12;
+        a23 = r12c2 * (c2mag*c*s2 + c1mag*s12);
+
+        sx2  = a12*vb1x + a22*vb2x + a23*vb3x;
+        sy2  = a12*vb1y + a22*vb2y + a23*vb3y;
+        sz2  = a12*vb1z + a22*vb2z + a23*vb3z;
+
+        f1[0] = a11*vb1x + a12*vb2x + a13*vb3x;
+        f1[1] = a11*vb1y + a12*vb2y + a13*vb3y;
+        f1[2] = a11*vb1z + a12*vb2z + a13*vb3z;
+
+        f2[0] = -sx2 - f1[0];
+        f2[1] = -sy2 - f1[1];
+        f2[2] = -sz2 - f1[2];
+
+        f4[0] = a13*vb1x + a23*vb2x + a33*vb3x;
+        f4[1] = a13*vb1y + a23*vb2y + a33*vb3y;
+        f4[2] = a13*vb1z + a23*vb2z + a33*vb3z;
+
+        f3[0] = sx2 - f4[0];
+        f3[1] = sy2 - f4[1];
+        f3[2] = sz2 - f4[2];
+
+        // only right bond crossing the plane
+        if (right_cross && !middle_cross && !left_cross)
+        {
+          double sgn = copysign(1.0, x_atom_1[dir] - pos);
+          df[0] = sgn * f1[0];
+          df[1] = sgn * f1[1];
+          df[2] = sgn * f1[2];
+        }
+
+        // only middle bond crossing the plane
+        if (!right_cross && middle_cross && !left_cross)
+        {
+          double sgn = copysign(1.0, x_atom_2[dir] - pos);
+          df[0] = sgn * (f2[0] + f1[0]);
+          df[1] = sgn * (f2[1] + f1[1]);
+          df[2] = sgn * (f2[2] + f1[2]);
+        }
+
+        // only left bond crossing the plane
+        if (!right_cross && !middle_cross && left_cross)
+        {
+          double sgn = copysign(1.0, x_atom_4[dir] - pos);
+          df[0] = sgn * f4[0];
+          df[1] = sgn * f4[1];
+          df[2] = sgn * f4[2];
+        }
+
+        // only right & middle bonds crossing the plane
+        if (right_cross && middle_cross && !left_cross)
+        {
+          double sgn = copysign(1.0, x_atom_2[dir] - pos);
+          df[0] = sgn * f2[0];
+          df[1] = sgn * f2[1];
+          df[2] = sgn * f2[2];
+        }
+
+        // only right & left bonds crossing the plane
+        if (right_cross && !middle_cross && left_cross)
+        {
+          double sgn = copysign(1.0, x_atom_1[dir] - pos);
+          df[0] = sgn * (f1[0] + f4[0]);
+          df[1] = sgn * (f1[1] + f4[1]);
+          df[2] = sgn * (f1[2] + f4[2]);
+        }
+
+        // only middle & left bonds crossing the plane
+        if (!right_cross && middle_cross && left_cross)
+        {
+          double sgn = copysign(1.0, x_atom_3[dir] - pos);
+          df[0] = sgn * f3[0];
+          df[1] = sgn * f3[1];
+          df[2] = sgn * f3[2];
+        }
+
+        // all three bonds crossing the plane
+        if (right_cross && middle_cross && left_cross)
+        {
+          double sgn = copysign(1.0, x_atom_1[dir] - pos);
+          df[0] = sgn * (f1[0] + f3[0]);
+          df[1] = sgn * (f1[1] + f3[1]);
+          df[2] = sgn * (f1[2] + f3[2]);
+        }
+
+        local_contribution[ibin][0] += df[0]/area*nktv2p;
+        local_contribution[ibin][1] += df[1]/area*nktv2p;
+        local_contribution[ibin][2] += df[2]/area*nktv2p;
+      }
+    }
+  }
+
+  // loop over the keywords and if necessary add the dihedral contribution
+  int m = 0;
+  while (m < nvalues) {
+    if ((which[m] == CONF) || (which[m] == TOTAL) || (which[m] == DIHEDRAL)) {
+      for (int ibin = 0; ibin < nbins; ibin++) {
+        dihedral_local[ibin][m] = local_contribution[ibin][0];
+        dihedral_local[ibin][m+1] = local_contribution[ibin][1];
+        dihedral_local[ibin][m+2] = local_contribution[ibin][2];
+      }
+    }
+    m += 3;
+  }
+
+}
+
 /* ----------------------------------------------------------------------
    setup 1d bins and their extent and coordinates
    called at init()
@@ -870,6 +1215,8 @@ void ComputeStressMopProfile::setup_bins()
   memory->create(bond_global, nbins, nvalues, "stress/mop/profile:bond_global");
   memory->create(angle_local, nbins, nvalues, "stress/mop/profile:angle_local");
   memory->create(angle_global, nbins, nvalues, "stress/mop/profile:angle_global");
+  memory->create(dihedral_local,nbins,nvalues,"stress/mop/profile:dihedral_local");
+  memory->create(dihedral_global,nbins,nvalues,"stress/mop/profile:dihedral_global");
   memory->create(local_contribution, nbins, 3, "stress/mop/profile:local_contribution");
 
   // set bin coordinates

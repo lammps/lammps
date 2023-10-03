367d0ac90518d751af2d872e9cde355c7cef7f22
Stan Gerald Moore <stamoor@sandia.gov>
2023-10-03 10:07:52 -0600

    Add support for FLUX

--- src/KOKKOS/kokkos.cpp
+++ src/KOKKOS/kokkos.cpp
@@ -175,6 +175,14 @@ KokkosLMP::KokkosLMP(LAMMPS *lmp, int narg, char **arg) : Pointers(lmp)
           set_flag = 1;
         }
       }
+      if ((str = getenv("FLUX_TASK_LOCAL_ID"))) {
+        if (ngpus > 0) {
+          int local_rank = atoi(str);
+          device = local_rank % ngpus;
+          if (device >= skip_gpu) device++;
+          set_flag = 1;
+        }
+      }
 
       if (ngpus > 1 && !set_flag)
         error->all(FLERR,"Could not determine local MPI rank for multiple "

b3265729169f0d24c94b6c8a571b671627b872ae
Axel Kohlmeyer <akohlmey@gmail.com>
2023-11-15 09:49:58 -0500

    flag input errors

--- src/REPLICA/fix_pimd_langevin.cpp
+++ src/REPLICA/fix_pimd_langevin.cpp
@@ -455,7 +455,12 @@ void FixPIMDLangevin::init()
   langevin_init();
 
   c_pe = modify->get_compute_by_id(id_pe);
+  if (!c_pe)
+    error->universe_all(FLERR, fmt::format("Could not find fix {} potential energy compute ID {}", style, id_pe));
+
   c_press = modify->get_compute_by_id(id_press);
+  if (!c_press)
+    error->universe_all(FLERR, fmt::format("Could not find fix {} pressure compute ID {}", style, id_press));
 
   t_prim = t_vir = t_cv = p_prim = p_vir = p_cv = p_md = 0.0;
 }

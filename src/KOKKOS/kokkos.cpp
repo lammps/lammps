4926164050a3d58c3770802ee902e0ba7f4028b2
Axel Kohlmeyer <akohlmey@gmail.com>
2023-11-23 00:24:34 -0500

    report Kokkos library version and OpenMP standard version

--- src/KOKKOS/kokkos.cpp
+++ src/KOKKOS/kokkos.cpp
@@ -104,7 +104,8 @@ KokkosLMP::KokkosLMP(LAMMPS *lmp, int narg, char **arg) : Pointers(lmp)
 
   int me = 0;
   MPI_Comm_rank(world,&me);
-  if (me == 0) error->message(FLERR,"KOKKOS mode is enabled");
+  if (me == 0) error->message(FLERR,"KOKKOS mode with Kokkos version {}.{}.{} is enabled",
+                              KOKKOS_VERSION_MAJOR, KOKKOS_VERSION_MINOR, KOKKOS_VERSION_PATCH);
 
   // process any command-line args that invoke Kokkos settings
 
--- src/info.cpp
+++ src/info.cpp
@@ -1243,6 +1243,10 @@ std::string Info::get_accelerator_info(const std::string &package)
     if (has_accelerator_feature("KOKKOS","precision","single")) mesg += " single";
     if (has_accelerator_feature("KOKKOS","precision","mixed"))  mesg += " mixed";
     if (has_accelerator_feature("KOKKOS","precision","double")) mesg += " double";
+#if LMP_KOKKOS
+    mesg += "\nKokkos library version: " + std::to_string(KOKKOS_VERSION_MAJOR) + "."
+            + std::to_string(KOKKOS_VERSION_MINOR) + "." + std::to_string(KOKKOS_VERSION_PATCH);
+#endif
     mesg += "\n";
   }
   if ((package.empty() || (package == "OPENMP")) && has_package("OPENMP")) {
@@ -1253,6 +1257,9 @@ std::string Info::get_accelerator_info(const std::string &package)
     if (has_accelerator_feature("OPENMP","precision","single")) mesg += " single";
     if (has_accelerator_feature("OPENMP","precision","mixed"))  mesg += " mixed";
     if (has_accelerator_feature("OPENMP","precision","double")) mesg += " double";
+#if defined(_OPENMP)
+    mesg += "\nOpenMP standard: " + platform::openmp_standard();
+#endif
     mesg += "\n";
   }
   if ((package.empty() || (package == "INTEL")) && has_package("INTEL")) {

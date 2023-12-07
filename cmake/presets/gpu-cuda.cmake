e51a8ccfbf96a54b2eca8eba94d5d767f74d9312
Trung Nguyen <ndactrung@gmail.com>
2023-12-07 12:00:24 -0600

    Removed the unnecessary line since CUDA_BUILD_MULTIARCH is ON by default

--- cmake/presets/gpu-cuda.cmake
+++ cmake/presets/gpu-cuda.cmake
@@ -3,7 +3,6 @@
 set(PKG_GPU ON CACHE BOOL "Build GPU package" FORCE)
 set(GPU_API "cuda" CACHE STRING "APU used by GPU package" FORCE)
 set(GPU_PREC "mixed" CACHE STRING "" FORCE)
-set(GPU_ARCH "sm_60;sm_70;sm_80" CACHE STRING "LAMMPS GPU CUDA SM architectures" FORCE)
 
 set(CUDA_NVCC_FLAGS "-allow-unsupported-compiler" CACHE STRING "" FORCE)
 set(CUDA_NVCC_FLAGS_DEBUG "-allow-unsupported-compiler" CACHE STRING "" FORCE)

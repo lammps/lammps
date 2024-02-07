35fe562bdf919cb5c1994fc249e2690776bf2284
Stan Moore <stamoor@sandia.gov>
2024-02-07 11:47:44 -0700

    Fix issue from #4007

--- src/lmpfftsettings.h
+++ src/lmpfftsettings.h
@@ -25,18 +25,6 @@
 #endif
 #endif
 
-#ifdef LMP_KOKKOS
-# ifdef FFT_KOKKOS_FFTW
-#  undef FFT_KOKKOS_FFTW
-#  define FFT_KOKKOS_FFTW3
-# endif
-# ifdef FFT_KOKKOS_FFTW_THREADS
-#  if !defined(FFT_KOKKOS_FFTW3)
-#   error "Must use -DFFT_KOKKOS_FFTW3 with -DFFT_KOKKOS_FFTW_THREADS"
-#  endif
-# endif
-#endif
-
 // set strings for library info output
 
 #if defined(FFT_HEFFTE)
@@ -57,60 +45,6 @@
 #define LMP_FFT_LIB "hipFFT"
 #else
 #define LMP_FFT_LIB "KISS FFT"
-#endif
-
-#ifdef LMP_KOKKOS
-
-// with KOKKOS in CUDA or HIP mode we can only have
-//  CUFFT/HIPFFT or KISS, thus undefine all other
-//  FFTs here
-
-#ifdef KOKKOS_ENABLE_CUDA
-# if defined(FFT_KOKKOS_FFTW)
-#  undef FFT_KOKKOS_FFTW
-# endif
-# if defined(FFT_KOKKOS_FFTW3)
-#  undef FFT_KOKKOS_FFTW3
-# endif
-# if defined(FFT_KOKKOS_MKL)
-#  undef FFT_KOKKOS_MKL
-# endif
-# if !defined(FFT_KOKKOS_CUFFT) && !defined(FFT_KOKKOS_KISS)
-#  define FFT_KOKKOS_KISS
-# endif
-#elif defined(KOKKOS_ENABLE_HIP)
-# if defined(FFT_KOKKOS_FFTW)
-#  undef FFT_KOKKOS_FFTW
-# endif
-# if defined(FFT_KOKKOS_FFTW3)
-#  undef FFT_KOKKOS_FFTW3
-# endif
-# if defined(FFT_KOKKOS_MKL)
-#  undef FFT_KOKKOS_MKL
-# endif
-# if !defined(FFT_KOKKOS_HIPFFT) && !defined(FFT_KOKKOS_KISS)
-#  define FFT_KOKKOS_KISS
-# endif
-#else
-# if defined(FFT_KOKKOS_CUFFT)
-#  error "Must enable CUDA with KOKKOS to use -DFFT_KOKKOS_CUFFT"
-# endif
-# if defined(FFT_KOKKOS_HIPFFT)
-#  error "Must enable HIP with KOKKOS to use -DFFT_KOKKOS_HIPFFT"
-# endif
-#endif
-
-#if defined(FFT_KOKKOS_CUFFT)
-#define LMP_FFT_KOKKOS_LIB "cuFFT"
-#elif defined(FFT_KOKKOS_HIPFFT)
-#define LMP_FFT_KOKKOS_LIB "hipFFT"
-#elif defined(FFT_KOKKOS_FFTW3)
-#define LMP_FFT_KOKKOS_LIB "FFTW3"
-#elif defined(FFT_KOKKOS_MKL)
-#define LMP_FFT_KOKKOS_LIB "MKL FFT"
-#else
-#define LMP_FFT_KOKKOS_LIB "KISS FFT"
-#endif
 #endif
 
 #ifdef FFT_SINGLE

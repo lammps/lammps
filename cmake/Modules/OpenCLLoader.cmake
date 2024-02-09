801c62dbde1f72c504859c83a8545ff1ee9bd60e
Axel Kohlmeyer <akohlmey@gmail.com>
2024-02-09 04:14:14 -0500

    updated library for better Windows portability

--- cmake/Modules/OpenCLLoader.cmake
+++ cmake/Modules/OpenCLLoader.cmake
@@ -1,6 +1,6 @@
 message(STATUS "Downloading and building OpenCL loader library")
 set(OPENCL_LOADER_URL "${LAMMPS_THIRDPARTY_URL}/opencl-loader-2024.02.09.tar.gz" CACHE STRING "URL for OpenCL loader tarball")
-set(OPENCL_LOADER_MD5 "2f365597659a0c3d140190dec1de77ef" CACHE STRING "MD5 checksum of OpenCL loader tarball")
+set(OPENCL_LOADER_MD5 "f3573cf9daa3558ba46fd5866517f38f" CACHE STRING "MD5 checksum of OpenCL loader tarball")
 mark_as_advanced(OPENCL_LOADER_URL)
 mark_as_advanced(OPENCL_LOADER_MD5)
 

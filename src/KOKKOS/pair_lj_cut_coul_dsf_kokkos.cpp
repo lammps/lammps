a48da5b1e4f7c82f485e92912960b4813b159b86
Axel Kohlmeyer <akohlmey@gmail.com>
2024-02-13 08:02:32 -0500

    move ewald_const.h to src and apply it to a few more files

--- src/KOKKOS/pair_lj_cut_coul_dsf_kokkos.cpp
+++ src/KOKKOS/pair_lj_cut_coul_dsf_kokkos.cpp
@@ -21,6 +21,7 @@
 #include "atom_kokkos.h"
 #include "atom_masks.h"
 #include "error.h"
+#include "ewald_const.h"
 #include "force.h"
 #include "kokkos.h"
 #include "math_const.h"
@@ -35,14 +36,8 @@
 #include <cstring>
 
 using namespace LAMMPS_NS;
+using namespace EwaldConst;
 using MathConst::MY_PIS;
-
-static constexpr double EWALD_P = 0.3275911;
-static constexpr double A1 = 0.254829592;
-static constexpr double A2 = -0.284496736;
-static constexpr double A3 = 1.421413741;
-static constexpr double A4 = -1.453152027;
-static constexpr double A5 = 1.061405429;
 
 /* ---------------------------------------------------------------------- */
 

fc32826cd7cdf509d34e25747c7a0ff1811ba348
Jacob Gissinger <jrgiss05@gmail.com>
2024-05-12 16:44:24 -0400

    more uses of bounds_typelabel

--- src/set.cpp
+++ src/set.cpp
@@ -740,11 +740,7 @@ void Set::selection(int n)
       else select[i] = 0;
 
   } else if (style == TYPE_SELECT) {
-    if (char *typestr = utils::expand_type(FLERR, id, Atom::ATOM, lmp)) {
-      delete[] id;
-      id = typestr;
-    }
-    utils::bounds(FLERR,id,1,atom->ntypes,nlo,nhi,error);
+    utils::bounds_typelabel(FLERR,id,1,atom->ntypes,nlo,nhi,error,lmp,Atom::ATOM);
 
     int *type = atom->type;
     for (int i = 0; i < n; i++)

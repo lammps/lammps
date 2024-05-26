5685c5c74b737491393215d1eed0000fa3d77245
Gareth Aneurin Tribello <garethtribello@Gareths-MacBook-Pro.local>
2024-05-26 19:17:00 +0100

    Fixed list of supported version of plumed

--- src/PLUMED/fix_plumed.cpp
+++ src/PLUMED/fix_plumed.cpp
@@ -79,7 +79,7 @@ FixPlumed::FixPlumed(LAMMPS *lmp, int narg, char **arg) :
   p->cmd("getApiVersion",&api_version);
   if ((api_version < 5) || (api_version > 10))
     error->all(FLERR,"Incompatible API version for PLUMED in fix plumed. "
-               "Only Plumed 2.4.x, 2.5.x, 2.6.x, 2.7.x, 2.8.x are tested and supported.");
+               "Only Plumed 2.4.x, 2.5.x, 2.6.x, 2.7.x, 2.8.x 2.9.x are tested and supported.");
 
 #if !defined(MPI_STUBS)
   // If the -partition option is activated then enable

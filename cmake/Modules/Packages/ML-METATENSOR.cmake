dea5cba0687118565e6de2a6add3cff67286332d
Luthaf <luthaf@luthaf.fr>
2024-06-24 12:48:59 +0200

    Fix the issue with MKL_INCLUDE_DIR-NOTFOUND

--- cmake/Modules/Packages/ML-METATENSOR.cmake
+++ cmake/Modules/Packages/ML-METATENSOR.cmake
@@ -1,3 +1,40 @@
+if (BUILD_OMP AND APPLE)
+    message(FATAL_ERROR
+        "Can not enable both BUILD_OMP and PGK_ML-METATENSOR on Apple systems, "
+        "since this results in two different versions of libiomp5.dylib (one "
+        "from the system and one from Torch) being linked to the final "
+        "executable, which then segfaults"
+    )
+endif()
+
+# Bring the `torch` target in scope to allow evaluation
+# of cmake generator expression from `metatensor_torch`
+find_package(Torch REQUIRED)
+
+# The caffe2::mkl target contains MKL_INCLUDE_DIR in it's
+# INTERFACE_INCLUDE_DIRECTORIES even if MKL was not found, causing a build
+# failure with "Imported target "torch" includes non-existent path" down the
+# line. This code removes the missing path from INTERFACE_INCLUDE_DIRECTORIES,
+# allowing the build to continue further.
+if (TARGET caffe2::mkl)
+    get_target_property(CAFFE2_MKL_INCLUDE_DIRECTORIES caffe2::mkl INTERFACE_INCLUDE_DIRECTORIES)
+    set(MKL_INCLUDE_DIR_NOTFOUND "")
+    foreach(_include_dir_ ${CAFFE2_MKL_INCLUDE_DIRECTORIES})
+        if ("${_include_dir_}" MATCHES "MKL_INCLUDE_DIR-NOTFOUND")
+            set(MKL_INCLUDE_DIR_NOTFOUND "${_include_dir_}")
+        endif()
+    endforeach()
+
+    if (NOT "${MKL_INCLUDE_DIR_NOTFOUND}" STREQUAL "")
+        list(REMOVE_ITEM CAFFE2_MKL_INCLUDE_DIRECTORIES "${MKL_INCLUDE_DIR_NOTFOUND}")
+    endif()
+    set_target_properties(caffe2::mkl PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${CAFFE2_MKL_INCLUDE_DIRECTORIES}"
+    )
+endif()
+
+########### definition of metatensor and metatensor-torch targets ###########
+
 include(FetchContent)
 
 set(URL_BASE "https://github.com/lab-cosmo/metatensor/releases/download")
@@ -12,10 +49,10 @@ message(STATUS "Fetching metatensor v${METATENSOR_CORE_VERSION} from github")
 FetchContent_MakeAvailable(metatensor)
 
 
-set(METATENSOR_TORCH_VERSION "0.5.1")
+set(METATENSOR_TORCH_VERSION "0.5.2")
 FetchContent_Declare(metatensor-torch
     URL ${URL_BASE}/metatensor-torch-v${METATENSOR_TORCH_VERSION}/metatensor-torch-cxx-${METATENSOR_TORCH_VERSION}.tar.gz
-    URL_HASH SHA1=a95535876b27c998924ef438ca65c8533df7db50
+    URL_HASH SHA1=0144f8fb8a0f67124ce3ee69bc5cda183e431df3
 )
 
 message(STATUS "Fetching metatensor-torch v${METATENSOR_TORCH_VERSION} from github")
@@ -24,17 +61,4 @@ FetchContent_MakeAvailable(metatensor-torch)
 
 ################ lammps target modifications ################
 
-# Bring the `torch` target in scope to allow evaluation
-# of cmake generator expression from `metatensor_torch`
-find_package(Torch REQUIRED)
-
 target_link_libraries(lammps PRIVATE metatensor_torch)
-
-if (BUILD_OMP AND APPLE)
-    message(FATAL_ERROR
-        "Can not enable BUILD_OMP on Apple systems, since this results in "
-        "two different versions of libiomp5.dylib (one from the system and "
-        "one from Torch) being linked to the final executable, which then "
-        "segfaults"
-    )
-endif()

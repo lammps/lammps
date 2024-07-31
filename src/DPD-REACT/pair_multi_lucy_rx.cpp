d68bc9f628a8984f4925fe1d435d9d110e86d12c
Axel Kohlmeyer <akohlmey@gmail.com>
2024-07-30 21:02:55 -0400

    replace remaining calls to atof() with std::stod()

--- src/DPD-REACT/pair_multi_lucy_rx.cpp
+++ src/DPD-REACT/pair_multi_lucy_rx.cpp
@@ -617,15 +617,15 @@ void PairMultiLucyRX::param_extract(Table *tb, char *line)
       if (strcmp(word,"R") == 0) tb->rflag = RLINEAR;
       else if (strcmp(word,"RSQ") == 0) tb->rflag = RSQ;
       word = strtok(nullptr," \t\n\r\f");
-      tb->rlo = atof(word);
+      tb->rlo = std::stod(word);
       word = strtok(nullptr," \t\n\r\f");
-      tb->rhi = atof(word);
+      tb->rhi = std::stod(word);
     } else if (strcmp(word,"FP") == 0) {
       tb->fpflag = 1;
       word = strtok(nullptr," \t\n\r\f");
-      tb->fplo = atof(word);
+      tb->fplo = std::stod(word);
       word = strtok(nullptr," \t\n\r\f");
-      tb->fphi = atof(word);
+      tb->fphi = std::stod(word);
     } else {
       printf("WORD: %s\n",word);
       error->one(FLERR,"Invalid keyword in pair table parameters");

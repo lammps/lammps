d903026bd2706e7eb49a7871f6f80a8bf9eb77f7
Axel Kohlmeyer <akohlmey@gmail.com>
2024-08-02 04:16:05 -0400

    small updates and corrections

--- tools/lammps-gui/flatpak/build-lammps-gui-flatpak.sh
+++ tools/lammps-gui/flatpak/build-lammps-gui-flatpak.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-flatpak-builder --force-clean --user --install-deps-from=flathub --repo=repo --install builddir org.lammps.lammps-gui.yml
+flatpak-builder --gpg-sign=EEA103764C6C633EDC8AC428D9B44E93BF0C375A --force-clean --user --install-deps-from=flathub --repo=repo --install builddir org.lammps.lammps-gui.yml
 flatpak build-bundle repo lammps-gui.flatpak org.lammps.lammps-gui --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo

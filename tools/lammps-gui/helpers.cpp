658cadceeaa540be2a517902ce0233d21546fb25
Axel Kohlmeyer <akohlmey@gmail.com>
2024-08-09 18:29:38 -0400

    add helper to purge directories recursively

--- tools/lammps-gui/helpers.cpp
+++ tools/lammps-gui/helpers.cpp
@@ -13,6 +13,7 @@
 
 #include "helpers.h"
 
+#include <QDir>
 #include <QFile>
 #include <QFileInfo>
 #include <QProcess>
@@ -64,6 +65,23 @@ bool has_exe(const QString &exe)
         return true; // Found!
     else
         return false; // Not found!
+}
+
+// recursively remove all contents from a directory
+
+void purge_directory(const QString &dir)
+{
+    QDir directory(dir);
+
+    directory.setFilter(QDir::AllEntries | QDir::NoDotAndDotDot);
+    const auto &entries = directory.entryList();
+    for (auto &entry : entries) {
+        if (!directory.remove(entry)) {
+            directory.cd(entry);
+            directory.removeRecursively();
+            directory.cdUp();
+        }
+    }
 }
 
 // Local Variables:

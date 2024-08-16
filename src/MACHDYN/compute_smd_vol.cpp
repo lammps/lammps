b1654f11c1b80fc7e98a2590760ae354c1ff627e
Axel Kohlmeyer <akohlmey@gmail.com>
2024-08-16 14:58:47 -0400

    fix issues with compute smd/vol and related docs

--- src/MACHDYN/compute_smd_vol.cpp
+++ src/MACHDYN/compute_smd_vol.cpp
@@ -25,13 +25,14 @@
 
 #include "compute_smd_vol.h"
 
-#include <cstring>
 #include "atom.h"
-#include "update.h"
-#include "modify.h"
 #include "comm.h"
-#include "memory.h"
 #include "error.h"
+#include "memory.h"
+#include "modify.h"
+#include "update.h"
+
+#include <cstring>
 
 using namespace LAMMPS_NS;
 
@@ -39,12 +40,12 @@ using namespace LAMMPS_NS;
 
 ComputeSMDVol::ComputeSMDVol(LAMMPS *lmp, int narg, char **arg) :
                 Compute(lmp, narg, arg) {
-        if (narg != 3)
-                error->all(FLERR, "Illegal compute smd/volume command");
+        if (narg != 3) error->all(FLERR, "Illegal compute smd/volume command");
         if (atom->vfrac_flag != 1)
-                error->all(FLERR, "compute smd/volume command requires atom_style with density (e.g. smd)");
+          error->all(FLERR, "compute smd/volume command requires atom_style with density (e.g. smd)");
 
         scalar_flag = 1;
+        extscalar = 1;
         peratom_flag = 1;
         size_peratom_cols = 0;
 

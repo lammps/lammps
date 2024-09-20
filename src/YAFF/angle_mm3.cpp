83b2e88b734bedf651796c62c57242de8ff220f5
Evangelos Voyiatzis <evoyiatzis@gmail.com>
2024-09-20 19:31:20 +0200

    add extract() function to angle_mm3.cpp

--- src/YAFF/angle_mm3.cpp
+++ src/YAFF/angle_mm3.cpp
@@ -327,3 +327,15 @@ void AngleMM3::born_matrix(int type, int i1, int i2, int i3, double &du, double 
   du = -k2[type] * df / s;
   du2 = k2[type] * (d2f - df  * c / s) / (s * s) ;
 }
+
+/* ----------------------------------------------------------------------
+   return ptr to internal members upon request
+------------------------------------------------------------------------ */
+
+void *AngleMM3::extract(const char *str, int &dim)
+{
+  dim = 1;
+  if (strcmp(str, "k2") == 0) return (void *) k2;
+  if (strcmp(str, "theta0") == 0) return (void *) theta0;
+  return nullptr;
+}

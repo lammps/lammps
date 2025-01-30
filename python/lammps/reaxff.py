2b3ead94a3b1c5d84244c868908d9aa1c457c703
alphataubio <alphataubio@gmail.com>
2025-01-30 13:37:41 -0500

    add GEN to reaxff python interface

--- python/lammps/reaxff.py
+++ python/lammps/reaxff.py
@@ -29,12 +29,14 @@ class lammps_reaxff(lammps):
 
     #if has_package('REAXFF')
 
+    self.lib.lammps_set_reaxff_gen_parameter.argtypes = [c_void_p,c_int,c_double]
     self.lib.lammps_set_reaxff_atm_parameter.argtypes = [c_void_p,c_int,c_int,c_double]
     self.lib.lammps_set_reaxff_bnd_parameter.argtypes = [c_void_p,c_int,c_int,c_int,c_double]
     self.lib.lammps_set_reaxff_ofd_parameter.argtypes = [c_void_p,c_int,c_int,c_int,c_double]
     self.lib.lammps_set_reaxff_ang_parameter.argtypes = [c_void_p,c_int,c_int,c_int,c_int,c_double]
     self.lib.lammps_set_reaxff_tor_parameter.argtypes = [c_void_p,c_int,c_int,c_int,c_int,c_int,c_double]
     self.lib.lammps_set_reaxff_hbd_parameter.argtypes = [c_void_p,c_int,c_int,c_int,c_double]
+    self.lib.lammps_set_reaxff_gen_parameter.restype = None
     self.lib.lammps_set_reaxff_atm_parameter.restype = None
     self.lib.lammps_set_reaxff_bnd_parameter.restype = None
     self.lib.lammps_set_reaxff_ofd_parameter.restype = None
@@ -51,27 +53,31 @@ class lammps_reaxff(lammps):
 
         p_block_index = p[0]
 
-        # ATM
+        # GEN
         if p_block_index == 0:
+          self.lib.lammps_set_reaxff_gen_parameter(self.lmp,p[2],v)
+
+        # ATM
+        if p_block_index == 1:
           self.lib.lammps_set_reaxff_atm_parameter(self.lmp,p[1],p[2],v)
 
         # BND
-        if p_block_index == 1:
+        if p_block_index == 2:
           self.lib.lammps_set_reaxff_bnd_parameter(self.lmp,p[1],p[2],p[3],v)
 
         # OFD
-        if p_block_index == 2:
+        if p_block_index == 3:
           self.lib.lammps_set_reaxff_ofd_parameter(self.lmp,p[1],p[2],p[3],v)
 
         # ANG
-        if p_block_index == 3:
+        if p_block_index == 4:
           self.lib.lammps_set_reaxff_ang_parameter(self.lmp,p[1],p[2],p[3],p[4],v)
 
         # TOR
-        if p_block_index == 4:
+        if p_block_index == 5:
           self.lib.lammps_set_reaxff_tor_parameter(self.lmp,p[1],p[2],p[3],p[4],p[5],v)
 
         # HBD
-        if p_block_index == 5:
+        if p_block_index == 6:
           self.lib.lammps_set_reaxff_hbd_parameter(self.lmp,p[1],p[2],p[3],p[4],v)
 

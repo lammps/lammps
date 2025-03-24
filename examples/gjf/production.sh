c27efd36ba570f13f7b967b35c89d1cd6da565c7
talinke <talinke@ucdavis.edu>
2025-03-24 12:48:14 -0700

    Updated example files

--- examples/gjf/production.sh
+++ examples/gjf/production.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --time=00:05:00 
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
+#SBATCH --ntasks=4
 #SBATCH --job-name=langevin
 #SBATCH --partition=low
 #SBATCH --output=vtest1-new.out
 #SBATCH --error=vtest1-new.err
 
-srun lmp.exe -in in.gjf.vfull
+srun lmp.exe -in in.gjf.vhalf

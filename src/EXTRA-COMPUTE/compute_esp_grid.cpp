2b4884a71beb7c54835d74d3a34c99cbfb4ea454
alphataubio <alphataubio@gmail.com>
2025-05-05 15:46:00 -0400

    Update compute_esp_grid.cpp

--- src/EXTRA-COMPUTE/compute_esp_grid.cpp
+++ src/EXTRA-COMPUTE/compute_esp_grid.cpp
@@ -2,9 +2,11 @@
 #include "compute_esp_grid.h"
 
 #include "atom.h"
+#include "comm.h"
 #include "domain.h"
 #include "error.h"
 #include "force.h"
+#include "grid3d.h"
 #include "memory.h"
 #include "pair.h"
 #include "update.h"
@@ -18,8 +20,8 @@ using namespace LAMMPS_NS;
 
 ComputeESPGrid::ComputeESPGrid(LAMMPS *lmp,int narg,char **arg) :
   Compute(lmp,narg,arg),
-  spacing(1.0),nx(0),ny(0),nz(0),
-  bcut_acks2(nullptr),esp(nullptr),reference(nullptr),reaxflag(0)
+  spacing(1.0),nx(0),ny(0),nz(0),bcut_acks2(nullptr),reaxflag(0),
+  esp_grid(nullptr),esp(nullptr),reference(nullptr),weight(nullptr)
 {
   if(narg<4) error->all(FLERR,"Illegal compute esp/grid command");
 
@@ -65,17 +67,22 @@ void ComputeESPGrid::allocate_grid()
   ny=std::max(1,int(std::ceil(domain->prd[1]/spacing)));
   nz=std::max(1,int(std::ceil(domain->prd[2]/spacing)));
 
-  utils::logmesg(lmp, "*** xlo ylo zlo {} {} {} prd {} {} {} spacing {} nx ny nz {} {} {}\n",
-    xlo, ylo, zlo, domain->prd[0], domain->prd[1], domain->prd[2], spacing, nx, ny, nz);
+  // Allocate grid
+  esp_grid = new Grid3d(lmp, world, nx, ny, nz);
+  esp_grid->setup_grid(
+    ixlo, ixhi, iylo, iyhi, izlo, izhi,
+    oxlo, oxhi, oylo, oyhi, ozlo, ozhi
+  );
 
   // Allocate memory
-  memory->create3d_offset(esp,0,nz-1,0,ny-1,0,nx-1,"esp/grid:esp");
-  memory->create3d_offset(reference,0,nz-1,0,ny-1,0,nx-1,"esp/grid:reference");
-  memory->create3d_offset(weight,0,nz-1,0,ny-1,0,nx-1,"esp/grid:weight");
+  memory->create3d_offset(esp, izlo, izhi, iylo, iyhi, ixlo, ixhi, "esp/grid:esp");
+  memory->create3d_offset(reference, izlo, izhi, iylo, iyhi, ixlo, ixhi, "esp/grid:reference");
+  memory->create3d_offset(weight, izlo, izhi, iylo, iyhi, ixlo, ixhi, "esp/grid:weight");
 
   // Set up vector interface
-  size_vector = nx * ny * nz;
-  vector = &(reference[0][0][0]);
+  int local_size = (izhi-izlo+1) * (iyhi-iylo+1) * (ixhi-ixlo+1);
+  size_vector = local_size;
+  vector = &(reference[izlo][iylo][ixlo]);
 
 }
 
@@ -113,48 +120,75 @@ void ComputeESPGrid::init()
   }
 }
 
-/* ---------------------------------------------------------------------- */
-
 void ComputeESPGrid::compute_pergrid()
 {
   invoked_pergrid = update->ntimestep;
 
-  double **x=atom->x;
-  double *q =atom->q;
-  int   *type=atom->type;
-  int    nlocal=atom->nlocal;
+  double **x = atom->x;
+  double *q = atom->q;
+  int *type = atom->type;
+  int nlocal = atom->nlocal;
+  int nghost = atom->nghost;
+  int ntotal = nlocal + nghost;
+  
+  // FIXED: Explicitly calculate grid spacing - do not compute inside the loop
+  double dx = (oxhi - oxlo) / std::max(1, ixhi - ixlo);
+  double dy = (oyhi - oylo) / std::max(1, iyhi - iylo);
+  double dz = (ozhi - ozlo) / std::max(1, izhi - izlo);
+  
+  // Debug grid spacing
+  utils::logmesg(lmp, "*** Grid spacing: dx={:.6f}, dy={:.6f}, dz={:.6f}\n", dx, dy, dz);
 
-  for(int iz=0;iz<nz;++iz){
-    double gz=zlo+(iz+0.5)*spacing;
-    for(int iy=0;iy<ny;++iy){
-      double gy=ylo+(iy+0.5)*spacing;
-      for(int ix=0;ix<nx;++ix){
-        double gx=xlo+(ix+0.5)*spacing;
+  for (int iz = izlo; iz <= izhi; ++iz) {
+    double gz = ozlo + (iz - izlo + 0.5) * dz;
+    for (int iy = iylo; iy <= iyhi; ++iy) {
+      double gy = oylo + (iy - iylo + 0.5) * dy;
+      for (int ix = ixlo; ix <= ixhi; ++ix) {
+        double gx = oxlo + (ix - ixlo + 0.5) * dx;
 
-        double rmin2=1e60;
-        int tnear=-1;
-        for(int i=0;i<nlocal;++i){
-          double dx=gx-x[i][0],dy=gy-x[i][1],dz=gz-x[i][2];
-          double r2=dx*dx+dy*dy+dz*dz;
-          if(r2<rmin2){rmin2=r2;tnear=type[i];}
+        // Initialize ESP and weight values for this cell
+        esp[iz][iy][ix] = 0.0;
+        weight[iz][iy][ix] = 0.0;
+        
+
+        // Find nearest atom type including ghost atoms
+        double rmin2 = 1e60;
+        int tnear = -1;
+        for (int i = 0; i < ntotal; ++i) {
+          double dx = gx - x[i][0];
+          double dy = gy - x[i][1];
+          double dz = gz - x[i][2];
+          
+          // Apply minimum image convention for periodic boundaries
+          domain->minimum_image(dx, dy, dz);
+          
+          double r2 = dx*dx + dy*dy + dz*dz;
+          if (r2 < rmin2) {rmin2 = r2; tnear = type[i];}
         }
-        if(tnear<0) continue;
+        
+        if (tnear < 0 || tnear > atom->ntypes) continue;
 
-        double rcut=reaxflag?bcut_acks2[tnear]:bcut_acks2[0];
-        double r=std::sqrt(rmin2);
-        esp[iz][iy][ix]=0.0;
-        if(r<1.4||r>rcut) {
-          weight[iz][iy][ix]=0.0;
+        double rcut = reaxflag ? bcut_acks2[tnear] : bcut_acks2[0];
+        double r = std::sqrt(rmin2);
+
+        if (r < 1.4 || r > rcut) {
+          weight[iz][iy][ix] = 0.0;
           continue;
         }
-        weight[iz][iy][ix]=compute_weight(r,rcut);
+        weight[iz][iy][ix] = compute_weight(r, rcut);
 
-        for(int i=0;i<nlocal;++i){
-          double dx=gx-x[i][0],dy=gy-x[i][1],dz=gz-x[i][2];
-          double r2=dx*dx+dy*dy+dz*dz+1e-12;
-          esp[iz][iy][ix]+=q[i]/std::sqrt(r2);
+        // Calculate ESP including contributions from all atoms
+        for (int i = 0; i < ntotal; ++i) {
+          double dx = gx - x[i][0];
+          double dy = gy - x[i][1];
+          double dz = gz - x[i][2];
+          
+          // Apply minimum image convention for periodic boundaries
+          domain->minimum_image(dx, dy, dz);
+          
+          double r2 = dx*dx + dy*dy + dz*dz + 1e-12;
+          esp[iz][iy][ix] += q[i] / std::sqrt(r2);
         }
-
       }
     }
   }
@@ -173,27 +207,34 @@ void ComputeESPGrid::compute_vector()
 double ComputeESPGrid::compute_scalar()
 {
   if (invoked_pergrid != update->ntimestep) compute_pergrid();
-  invoked_scalar=update->ntimestep;
+  invoked_scalar = update->ntimestep;
 
-  double loss_sum=0.0,weight_sum=0.0;
+  double loss_sum = 0.0, weight_sum = 0.0;
 
-  for(int iz=0;iz<nz;++iz){
-    for(int iy=0;iy<ny;++iy){
-      for(int ix=0;ix<nx;++ix){
-        double w=weight[iz][iy][ix];
-        if(w==0.0) continue;
-        double diff=esp[iz][iy][ix]-reference[iz][iy][ix];
-        loss_sum+=w*diff*diff;
-        weight_sum+=w;
+  for (int iz = izlo; iz <= izhi; ++iz) {
+    for (int iy = iylo; iy <= iyhi; ++iy) {
+      for (int ix = ixlo; ix <= ixhi; ++ix) {
+        double w = weight[iz][iy][ix];
+        if (w == 0.0) continue;
+        double diff = esp[iz][iy][ix] - reference[iz][iy][ix];
+        loss_sum += w * diff * diff;
+        weight_sum += w;
+        utils::logmesg(lmp, "*** iz iy ix {} {} {} weight {} esp {} reference {}\n", iz, iy, ix, w, esp[iz][iy][ix], reference[iz][iy][ix] );
       }
     }
   }
 
-  scalar = (weight_sum>0.0)?loss_sum/weight_sum:0.0;
-  utils::logmesg(lmp, "*** scalar {}\n", scalar);
+  double local_result[2], global_result[2];
+  local_result[0] = loss_sum;
+  local_result[1] = weight_sum;
+  
+  MPI_Allreduce(local_result, global_result, 2, MPI_DOUBLE, MPI_SUM, world);
+  
+  scalar = (global_result[1] > 0.0) ? global_result[0] / global_result[1] : 0.0;
+  if (comm->me == 0) utils::logmesg(lmp, "*** scalar {}\n", scalar);
   return scalar;
-
 }
+
 
 inline double ComputeESPGrid::compute_weight(double r,double rcut) const
 {
@@ -205,7 +246,7 @@ inline double ComputeESPGrid::compute_weight(double r,double rcut) const
 
 int ComputeESPGrid::get_grid_by_name(const std::string &name, int &dim)
 {
-  if (name == "grid")
+  if (name == "esp")
     return 0;
   else if (name == "reference")
     return 1;
@@ -215,10 +256,8 @@ int ComputeESPGrid::get_grid_by_name(const std::string &name, int &dim)
 
 void *ComputeESPGrid::get_grid_by_index(int index)
 {
-  if (index == 0)
+  if( (index == 0) || (index == 1) )
     return esp_grid;
-  else if (index == 1)
-    return reference_grid;
 
   return nullptr;
 }

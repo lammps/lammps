769cd4a8349020998bab19c6cd8e0ccb574daf43
alphataubio <alphataubio@gmail.com>
2025-11-10 16:56:42 -0500

    remove no-op (mu is never -1)

--- src/ML-PACE/compute_pace.cpp
+++ src/ML-PACE/compute_pace.cpp
@@ -211,11 +211,9 @@ void ComputePACE::compute_array()
       acecimpl->ace->element_type_mapping.init(ntypes+1);
       for (int ik = 1; ik <= ntypes; ik++) {
         for(int mu = 0; mu < acecimpl->basis_set->nelements; mu++){
-          if (mu != -1) {
-            if (mu == ik - 1) {
-              map[ik] = mu;
-              acecimpl->ace->element_type_mapping(ik) = mu;
-            }
+          if (mu == ik - 1) {
+            map[ik] = mu;
+            acecimpl->ace->element_type_mapping(ik) = mu;
           }
         }
       }

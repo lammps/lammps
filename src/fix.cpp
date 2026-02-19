a259220d1d7a0362155f4fe51f1cff71b2b8f429
Axel Kohlmeyer <akohlmey@gmail.com>
2026-02-18 21:20:16 -0500

    make sure vflag_either is initialized

--- src/fix.cpp
+++ src/fix.cpp
@@ -291,6 +291,7 @@ void Fix::v_setup(int vflag)
   int i,n;
 
   evflag = 1;
+  vflag_either = vflag;
   vflag_global = vflag & (VIRIAL_PAIR | VIRIAL_FDOTR);
   if (centroidstressflag != CENTROID_AVAIL) {
     vflag_atom = vflag & (VIRIAL_ATOM | VIRIAL_CENTROID);

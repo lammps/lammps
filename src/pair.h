5f9f0df66267866fbe41db641ac09400d0ce3556
Markos Poulos <markos_poulos@yahoo.gr>
2026-03-05 12:57:54 +0100

    Add the new centroid virial tally functions in pair.cpp

--- src/pair.h
+++ src/pair.h
@@ -153,6 +153,10 @@ class Pair : protected Pointers {
   void v_tally3(int, int, int, double *, double *, double *, double *);
   void v_tally4(int, int, int, int, double *, double *, double *, double *, double *, double *);
 
+  // Centroid tally functions (they perform only the virial tallying, not the energy)
+  void cv_tally3(int, int, int, double *, double *, double *, double, double, double, double);
+  void cv_tally4(int, int, int, int, double *, double *, double *, double *, double, double, double, double, double);
+ 
   // general child-class methods
 
   virtual void compute(int, int) = 0;
@@ -230,7 +234,8 @@ class Pair : protected Pointers {
   virtual void min_x_set(int) {}
   virtual void transfer_history(double *, double *, int, int) {}
 
-  // management of callbacks to be run from ev_tally()
+  // management of callbacks to be run from ev_tally() 
+  // and cv_tally3/4()
 
  protected:
   int num_tally_compute, did_tally_flag;

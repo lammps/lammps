fe4daa56008075e654d79993b298cf22ddf4fc32
alphataubio <alphataubio@gmail.com>
2026-03-16 15:25:40 -0400

    Update region_grid_kokkos.cpp

--- src/KOKKOS/region_grid_kokkos.cpp
+++ src/KOKKOS/region_grid_kokkos.cpp
@@ -48,9 +48,30 @@ RegGridKokkos<DeviceType>::~RegGridKokkos()
 /* ---------------------------------------------------------------------- */
 
 template<class DeviceType>
+void RegGridKokkos<DeviceType>::init()
+{
+  RegGrid::init();
+  allocate_grid_view();
+}
+
+/* ---------------------------------------------------------------------- */
+
+template<class DeviceType>
+void RegGridKokkos<DeviceType>::allocate_grid_view()
+{
+  if (!grid3d || !griddata) return;
+
+  RegGrid::resolve_grid_reference();
+  memoryKK->create_kokkos(k_griddata,griddata,nx,ny,nz,"RegGridKokkos:k_griddata");
+
+}
+
+/* ---------------------------------------------------------------------- */
+
+template<class DeviceType>
 void RegGridKokkos<DeviceType>::match_all_kokkos(int groupbit_in, DAT::tdual_int_1d k_match_in)
 {
-  sync_grid_to_device();
+  k_griddata.sync_device();
 
   groupbit = groupbit_in;
   d_match = k_match_in.template view<DeviceType>();
@@ -73,40 +94,11 @@ void RegGridKokkos<DeviceType>::sync_grid_to_device()
 {
   if (!grid3d || !griddata) return;
 
-  int nz_size = nzhi_out - nzlo_out + 1;
-  int ny_size = nyhi_out - nylo_out + 1;
-  int nx_size = nxhi_out - nxlo_out + 1;
+  allocate_grid_view();
 
-  if (nz_size <= 0 || ny_size <= 0 || nx_size <= 0) return;
-
-  if ((int)d_gridvals.extent(0) != nz_size ||
-      (int)d_gridvals.extent(1) != ny_size ||
-      (int)d_gridvals.extent(2) != nx_size) {
-    d_gridvals = Kokkos::View<double***, DeviceType>(
-        "RegGridKokkos:gridvals", nz_size, ny_size, nx_size);
-  }
-
-  auto h_gridvals = Kokkos::create_mirror_view(d_gridvals);
+  if (d_gridvals.extent(0) == 0) return;
 
   int col = (gridindex > 0) ? gridindex - 1 : 0;
-
-  if (ncol == 0) {
-    auto vec3d = (double ***) griddata;
-    for (int iz = nzlo_out; iz <= nzhi_out; iz++)
-      for (int iy = nylo_out; iy <= nyhi_out; iy++)
-        for (int ix = nxlo_out; ix <= nxhi_out; ix++)
-          h_gridvals(iz - nzlo_out, iy - nylo_out, ix - nxlo_out) =
-              vec3d[iz][iy][ix];
-  } else {
-    auto array3d = (double ****) griddata;
-    for (int iz = nzlo_out; iz <= nzhi_out; iz++)
-      for (int iy = nylo_out; iy <= nyhi_out; iy++)
-        for (int ix = nxlo_out; ix <= nxhi_out; ix++)
-          h_gridvals(iz - nzlo_out, iy - nylo_out, ix - nxlo_out) =
-              array3d[iz][iy][ix][col];
-  }
-
-  Kokkos::deep_copy(d_gridvals, h_gridvals);
 
   k_boxlo0 = domain->boxlo[0];
   k_boxlo1 = domain->boxlo[1];
@@ -117,6 +109,7 @@ void RegGridKokkos<DeviceType>::sync_grid_to_device()
   k_dx = domain->xprd / nx;
   k_dy = domain->yprd / ny;
   k_dz = domain->zprd / nz;
+  grid_synced = 1;
 }
 
 /* ---------------------------------------------------------------------- */

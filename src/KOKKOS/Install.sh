0f40592e0721a147a801e45fc6c45d58d56ee7a2
RichardZJM <richardzjm@gmail.com>
2026-04-08 15:33:28 -0400

    Added Kokkos Inference; Support for Mixed and Single Precision

--- src/KOKKOS/Install.sh
+++ src/KOKKOS/Install.sh
@@ -487,6 +487,8 @@ action pair_morse_kokkos.cpp
 action pair_morse_kokkos.h
 action pair_morse_smooth_linear_kokkos.cpp pair_morse_smooth_linear.cpp
 action pair_morse_smooth_linear_kokkos.h pair_morse_smooth_linear.h
+action pair_mtp_kokkos.h pair_mtp.h
+action pair_mtp_kokkos.cpp pair_mtp.cpp
 action pair_multi_lucy_rx_kokkos.cpp pair_multi_lucy_rx.cpp
 action pair_multi_lucy_rx_kokkos.h pair_multi_lucy_rx.h
 action pair_nm_cut_kokkos.cpp pair_nm_cut.cpp

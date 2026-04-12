77b17400d7fb6b725b79b1e140cdf22a89d18f58
alphataubio <alphataubio@gmail.com>
2026-04-12 11:19:16 -0400

    templated unmap/kk for double/KK_FLOAT convenience

--- src/KOKKOS/domain_kokkos.h
+++ src/KOKKOS/domain_kokkos.h
@@ -74,9 +74,9 @@ class DomainKokkos : public Domain {
   void operator()(TagDomain_x2lamda_group, const int&) const;
 
 // NOLINTNEXTLINE
+  template <typename T>
   static KOKKOS_INLINE_FUNCTION
-  Few<double,3> unmap(Few<double,3> prd, Few<double,6> h, int triclinic,
-      Few<double,3> x, imageint image);
+  Few<T,3> unmap(Few<T,3> prd, Few<T,6> h, int triclinic, Few<T,3> x, imageint image);
 
  private:
   int groupbit;
@@ -88,27 +88,31 @@ class DomainKokkos : public Domain {
 };
 
 // NOLINTNEXTLINE
+template <typename T>
 KOKKOS_INLINE_FUNCTION
-Few<double,3> DomainKokkos::unmap(Few<double,3> prd, Few<double,6> h,
-    int triclinic, Few<double,3> x, imageint image)
+Few<T,3> DomainKokkos::unmap(Few<T,3> prd, Few<T,6> h, int triclinic,
+                             Few<T,3> x, imageint image)
 {
   int xbox = (image & IMGMASK) - IMGMAX;
   int ybox = (image >> IMGBITS & IMGMASK) - IMGMAX;
   int zbox = (image >> IMG2BITS) - IMGMAX;
-  Few<double,3> y;
+  Few<T,3> y;
   if (triclinic == 0) {
-    y[0] = x[0] + xbox*prd[0];
-    y[1] = x[1] + ybox*prd[1];
-    y[2] = x[2] + zbox*prd[2];
+    y[0] = Kokkos::fma(xbox, prd[0], x[0]);
+    y[1] = Kokkos::fma(ybox, prd[1], x[1]);
+    y[2] = Kokkos::fma(zbox, prd[2], x[2]);
   } else {
-    y[0] = x[0] + h[0]*xbox + h[5]*ybox + h[4]*zbox;
-    y[1] = x[1] + h[1]*ybox + h[3]*zbox;
-    y[2] = x[2] + h[2]*zbox;
+    // x[0] + h[0]*xbox + h[5]*ybox + h[4]*zbox
+    y[0] = Kokkos::fma(h[5], ybox, Kokkos::fma(h[4], zbox, Kokkos::fma(h[0], xbox, x[0])));
+    // x[1] + h[1]*ybox + h[3]*zbox
+    y[1] = Kokkos::fma(h[3], zbox, Kokkos::fma(h[1], ybox, x[1]));
+    // h[2]*zbox
+    y[2] = Kokkos::fma(h[2], zbox, x[2]);
   }
   return y;
 }
 
 }
 
-#endif
+#endif // !LMP_DOMAIN_KOKKOS_H
 

d1fb14f0ccf37bd5274d47aaa453d1291d89f83f
Axel Kohlmeyer <akohlmey@gmail.com>
2026-07-06 14:15:37 -0400

    make one more attribute suffix uppercase

--- src/MOLFILE/reader_molfile.cpp
+++ src/MOLFILE/reader_molfile.cpp
@@ -194,7 +194,7 @@ bigint ReaderMolfile::read_header(double box[3][3], int &boxinfo, int &triclinic
   // heuristics to determine if we have boxinfo (first if)
   // and whether we have an orthogonal box (second if)
 
-  if (!is_smalldiff(cell[0]*cell[1]*cell[2], 0.0f)) {
+  if (!is_smalldiff(cell[0]*cell[1]*cell[2], 0.0F)) {
     boxinfo = 1;
     if (is_smalldiff(cell[3],90.0F) && is_smalldiff(cell[4],90.0F) &&
         is_smalldiff(cell[5],90.0F)) {

d8abad39768300139c4d8f92dfb065f41e639ee3
Axel Kohlmeyer <akohlmey@gmail.com>
2026-07-09 15:20:59 -0400

    fix inconsistent transparency of the two bond halves in dump image
    When bonds are colored by atom, each bond is drawn as two half
    cylinders so that each half can follow the color of its atom.  The
    second half was drawn with the color of its atom but the transparency
    (dump_modify atrans) of the *other* atom, so the two halves of a bond
    between an opaque and a transparent atom were rendered inconsistently.
    Apply color and transparency from the same atom, as the autobond
    drawing code already does.

--- src/GRAPHICS/dump_image.cpp
+++ src/GRAPHICS/dump_image.cpp
@@ -1716,7 +1716,7 @@ void DumpImage::create_image()
           xmid[1] = x[atom2][1] - 0.5*dely;
           xmid[2] = x[atom2][2] - 0.5*delz;
           if (bcolor == ATOM)
-            image->draw_cylinder(xmid,x[atom2],color2,diameter,3,aopacity[type[atom1]]);
+            image->draw_cylinder(xmid,x[atom2],color2,diameter,3,aopacity[type[atom2]]);
           else image->draw_cylinder(xmid,x[atom2],color,diameter,3,bopacity[btype]);
 
         } else image->draw_cylinder(x[atom1],x[atom2],color,diameter,3,bopacity[btype]);

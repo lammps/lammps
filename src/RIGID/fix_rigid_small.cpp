fab0ebbcb763b2bac0cf0aec1f2f83ee6e1fb8d5
Axel Kohlmeyer <akohlmey@gmail.com>
2026-07-23 00:22:04 -0400

    fix broken rigid body update for tri and line particles

--- src/RIGID/fix_rigid_small.cpp
+++ src/RIGID/fix_rigid_small.cpp
@@ -1823,13 +1823,11 @@ void FixRigidSmall::setup_bodies_static()
       eflags[i] = 0;
       if (bodytag[i] == 0) continue;
 
-      // set to POINT or SPHERE or ELLIPSOID or LINE
+      // set to POINT or SPHERE or ELLIPSOID or LINE or TRIANGLE
+      // check for bonus data before radius: line and tri particles
+      // also store a bounding-sphere radius for neighboring purposes
 
-      if (radius && radius[i] > 0.0) {
-        eflags[i] |= SPHERE;
-        eflags[i] |= OMEGA;
-        eflags[i] |= TORQUE;
-      } else if (ellipsoid && ellipsoid[i] >= 0) {
+      if (ellipsoid && ellipsoid[i] >= 0) {
         eflags[i] |= ELLIPSOID;
         eflags[i] |= ANGMOM;
         eflags[i] |= TORQUE;
@@ -1840,6 +1838,10 @@ void FixRigidSmall::setup_bodies_static()
       } else if (tri && tri[i] >= 0) {
         eflags[i] |= TRIANGLE;
         eflags[i] |= ANGMOM;
+        eflags[i] |= TORQUE;
+      } else if (radius && radius[i] > 0.0) {
+        eflags[i] |= SPHERE;
+        eflags[i] |= OMEGA;
         eflags[i] |= TORQUE;
       } else eflags[i] |= POINT;
 

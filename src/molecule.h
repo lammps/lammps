463a2cd53bc5b217efc67b247fe9d539f6f602bd
Axel Kohlmeyer <akohlmey@gmail.com>
2026-07-24 12:07:42 -0400

    apply clang-format

--- src/molecule.h
+++ src/molecule.h
@@ -40,7 +40,7 @@ class Molecule : protected Pointers {
   int ntypes, nmolecules, nfragments;
   int nbondtypes, nangletypes, ndihedraltypes, nimpropertypes;
   int nibody, ndbody;
-  int nlines,ntris;
+  int nlines, ntris;
 
   // max bond,angle,etc per atom
 
@@ -79,12 +79,12 @@ class Molecule : protected Pointers {
   double *rmass;       // mass of each atom
   double **mu;         // dipole vector of each atom
 
-  int *molline;        // molecule-ID of each line
-  int *typeline;       // type of each line
-  double **lines;      // line end points
-  int *moltri;         // molecule-ID of each triangles
-  int *typetri;        // type of each triangle
-  double **tris;       // triangle corner points
+  int *molline;      // molecule-ID of each line
+  int *typeline;     // type of each line
+  double **lines;    // line end points
+  int *moltri;       // molecule-ID of each triangles
+  int *typetri;      // type of each triangle
+  double **tris;     // triangle corner points
 
   int *num_bond;    // bonds, angles, dihedrals, impropers for each atom
   int **bond_type;
@@ -155,7 +155,7 @@ class Molecule : protected Pointers {
   void check_attributes();
 
   double memory_usage();
-  void print(FILE *fp=stdout);
+  void print(FILE *fp = stdout);
 
  private:
   SafeFilePtr fp;

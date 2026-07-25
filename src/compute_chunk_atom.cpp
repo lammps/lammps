c40f4fa2e73f79a5c72439e5596a4e54f2a8919d
Axel Kohlmeyer <akohlmey@gmail.com>
2026-07-25 11:19:28 -0400

    don't leak memory when using the same argument multiple times
    (cherry picked from commit 9c487a62db53123a9dc3b609aefebb12b4a9d5e5)

--- src/compute_chunk_atom.cpp
+++ src/compute_chunk_atom.cpp
@@ -182,6 +182,7 @@ ComputeChunkAtom::ComputeChunkAtom(LAMMPS *lmp, int narg, char **arg) :
       if (!domain->get_region_by_id(arg[iarg + 1]))
         error->all(FLERR, iarg + 1, "Region {} for compute chunk/atom does not exist",
                    arg[iarg + 1]);
+      delete[] idregion;
       idregion = utils::strdup(arg[iarg + 1]);
       regionflag = 1;
       iarg += 2;

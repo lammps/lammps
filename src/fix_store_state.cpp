a55ffc996713289e99d60f05093a0179f8d9681a
Axel Kohlmeyer <akohlmey@gmail.com>
2026-08-13 22:30:43 -0400

    avoid rare memory leak

--- src/fix_store_state.cpp
+++ src/fix_store_state.cpp
@@ -250,15 +250,14 @@ FixStoreState::FixStoreState(LAMMPS *lmp, int narg, char **arg) :
 
         values.push_back(val);
       }
-      if (breakflag) break;
-
-      // free earg memory from expand_args()
+      // free earg memory from expand_args() also when leaving the outer loop
 
       if (expand) {
         for (int i = 0; i < nargnew; i++) delete [] earg[i];
         memory->sfree(earg);
         memory->sfree(amap);
       }
+      if (breakflag) break;
     }
     if (!value_added) values.push_back(std::move(val));
     iarg++;

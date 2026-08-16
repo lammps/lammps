08f49835093ffc5330e09f018ea3b81a0aef3f98
Axel Kohlmeyer <akohlmey@gmail.com>
2026-08-15 21:37:51 -0400

    make it safe to call Modify::delete_fix() and Modify::delete_compute()
    This silently returns if fixes or computes have already been purged
    (during the Modify class destructor), so it is no longer needed to
    prefix the delete_XXX() call with if (modify->nXXX).

--- src/modify.cpp
+++ src/modify.cpp
@@ -1076,6 +1076,9 @@ void Modify::modify_fix(int narg, char **arg)
 
 void Modify::delete_fix(const std::string &id)
 {
+  // no more fixes, nothing to do
+  if (!nfix) return;
+
   int ifix = find_fix(id);
   if (ifix < 0) error->all(FLERR, Error::NOLASTLINE, "Could not find fix ID {} to delete", id);
   delete_fix(ifix);
@@ -1083,7 +1086,8 @@ void Modify::delete_fix(const std::string &id)
 
 void Modify::delete_fix(int ifix)
 {
-  if ((ifix < 0) || (ifix >= nfix)) return;
+  // don't do anything if out of range or no fixes left
+  if (!nfix || (ifix < 0) || (ifix >= nfix)) return;
 
   // delete instance and move other Fixes and fmask down in list one slot
 
@@ -1359,6 +1363,9 @@ void Modify::modify_compute(int narg, char **arg)
 
 void Modify::delete_compute(const std::string &id)
 {
+  // no more computes, nothing to do
+  if (!ncompute) return;
+
   int icompute = find_compute(id);
   if (icompute < 0)
     error->all(FLERR, Error::NOLASTLINE, "Could not find compute ID {} to delete", id);
@@ -1367,7 +1374,8 @@ void Modify::delete_compute(const std::string &id)
 
 void Modify::delete_compute(int icompute)
 {
-  if ((icompute < 0) || (icompute >= ncompute)) return;
+  // don't do anything if out of range or no computes left
+  if (!ncompute || (icompute < 0) || (icompute >= ncompute)) return;
 
   // delete and move other Computes down in list one slot
 

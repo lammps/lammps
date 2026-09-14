df8a7d0dc304eff4d3ab66c9960f9b5f873b7624
Stan Moore <stanmoore1@gmail.com>
2026-09-14 14:48:30 +0000

    KOKKOS: drop the unused image flags from GroupKokkos::vcm_kk
    vcm_kk() binds d_image and asks for IMAGE_MASK in both of its syncs, but
    neither of its reduction kernels uses it: the centre-of-mass velocity is
    a sum of m*v and nothing is unwrapped.  It is a leftover from xcm_kk(),
    angmom_kk() and inertia_kk(), which do unwrap and do need the flags.
    
    Besides the pointless copy of the image array between host and device on
    every temperature evaluation, the stale-data checker reports the bind as
    a read of a stale array on every call.

--- src/KOKKOS/group_kokkos.h
+++ src/KOKKOS/group_kokkos.h
@@ -296,13 +296,16 @@ void vcm_kk(int igroup, double masstotal, double *vcm)
   int groupbit = bitmask[igroup];
   auto d_v = atomKK->k_v.template view<DeviceType>();
   auto d_mask = atomKK->k_mask.template view<DeviceType>();
-  auto d_image = atomKK->k_image.template view<DeviceType>();
   double p[3] = {0.0, 0.0, 0.0};
+
+  // the image flags are not needed here: the centre-of-mass velocity is a sum
+  // of m*v and nothing is unwrapped.  xcm_kk(), angmom_kk() and inertia_kk()
+  // do unwrap and do need them; this routine was copied from one of those.
 
   if (atomKK->rmass) {
 
     auto d_rmass = atomKK->k_rmass.template view<DeviceType>();
-    atomKK->sync(execution_space,V_MASK|MASK_MASK|IMAGE_MASK|RMASS_MASK);
+    atomKK->sync(execution_space,V_MASK|MASK_MASK|RMASS_MASK);
 
     Kokkos::parallel_reduce(Kokkos::RangePolicy<DeviceType>(0,atom->nlocal), KOKKOS_LAMBDA(const int i, double &l_px, double &l_py, double &l_pz) {
       if (d_mask(i) & groupbit) {
@@ -317,7 +320,7 @@ void vcm_kk(int igroup, double masstotal, double *vcm)
 
     auto d_mass = atomKK->k_mass.template view<DeviceType>();
     auto d_type = atomKK->k_type.template view<DeviceType>();
-    atomKK->sync(execution_space,V_MASK|MASK_MASK|IMAGE_MASK|TYPE_MASK);
+    atomKK->sync(execution_space,V_MASK|MASK_MASK|TYPE_MASK);
     atomKK->k_mass.template sync<DeviceType>();
 
     Kokkos::parallel_reduce(Kokkos::RangePolicy<DeviceType>(0,atom->nlocal), KOKKOS_LAMBDA(const int i, double &l_px, double &l_py, double &l_pz) {
